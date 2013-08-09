0afc419b621aacd7c791168cac2eb027c88c2c0f
Bryan Edds <bryanedds@gmail.com>
2013-08-09 16:39:13 -0400

    Added missing files...
    Former-commit-id: cc75ce86bcc9bc2bc14524b92d6ecd796563ed8d [formerly a65a088b0eec43d78fe7f167b3fd276e33457aca]
    Former-commit-id: 51b85bfdc5d387208237faef317750f7481c9cbd

---
+++ Nu/Nu/Nu/Rendering.fs
@@ -0,0 +1,13 @@
+﻿module Nu.Rendering
+
+type [<StructuralEquality; StructuralComparison>] RenderDescriptor =
+    | SpriteDescriptor // of ...
+
+type [<StructuralEquality; StructuralComparison>] RenderMessage =
+    | ScreenFlashMessage // of ...
+
+type [<ReferenceEquality>] Renderer =
+    { RenderContext : unit }
+
+let render renderMessages renderDescriptors renderer : unit =
+    ()  // TODO: render
