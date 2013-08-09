cc75ce86bcc9bc2bc14524b92d6ecd796563ed8d
Bryan Edds <bryanedds@gmail.com>
2013-08-09 16:39:13 -0400

    Added missing files...
    Former-commit-id: a65a088b0eec43d78fe7f167b3fd276e33457aca

---
+++ Nu/Nu/Nu/RQueue.fs
@@ -0,0 +1,5 @@
+﻿[<AutoOpen>]
+module Nu.RQueue
+
+/// Type alias to emphasize that messages arrive in a reversed queue since I am too lazy to write an immutable queue directly.
+type 'a rQueue = 'a list
