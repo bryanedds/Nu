da5b8d07a72f6677109cebbaf9990c06ea1936f6
Bryan Edds <bryanedds@gmail.com>
2013-08-13 21:27:29 -0400

    Some code reorganization.
    Former-commit-id: 7e78dcbef49f78322fda0dadcac650c63bacf8d9

---
+++ Nu/Nu/Nu/Input.fs
@@ -0,0 +1,23 @@
+﻿module Nu.Input
+open Nu.Core
+
+/// Describes human input.
+/// A serializable value type.
+type [<StructuralEquality; StructuralComparison>] HumanInput =
+    | PadInput // of ...
+    | KeyboardInput // of ...
+    | MouseInput // of ...
+
+/// Describes a mouse button.
+/// A serializable value type.
+type [<StructuralEquality; StructuralComparison>] MouseButton =
+    | MouseLeft
+    | MouseRight
+    | MouseCenter
+
+/// Describes a mouse event.
+/// A serializable value type.
+type [<StructuralEquality; StructuralComparison>] MouseEvent =
+    { Button : MouseButton
+      Position : Vector2 }
+
