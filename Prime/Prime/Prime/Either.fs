668eee726402ebd1d2f6c9b0db686005c7055445
Bryan Edds <bryanedds@gmail.com>
2013-10-15 04:28:06 -0400

    Figured out a 'scripting' convention.
    Former-commit-id: df7bbd9f3bd5b124b5915cef385e94242b355a66

--- Prime/Prime/Prime/Either.fs
+++ Prime/Prime/Prime/Either.fs
@@ -1,12 +1,40 @@
 ﻿[<AutoOpen>]
 module Either
 
-/// Hsakell-style either type.
-/// Probably needs some nice operators such as map, <|>, et al.
+/// Hsakell-style Either type.
+/// TODO: more nice operators definitions.
 type Either<'a, 'b> =
     | Left of 'a
     | Right of 'b
 
-/// Lower-case alias like option.
-type either<'a, 'b> =
-    Either<'a, 'b>
+/// Monadic return.
+let return' a =
+    Right a
+
+/// Monadic returnFrom.
+/// TODO: ensure this is defined correctly!
+let returnFrom a =
+    a
+
+/// Monadic bind.
+let (>>=) c f =
+    match c with
+    | Left _ -> c
+    | Right a -> Right (f a)
+
+/// Bind that allows indication of failure.
+let (>>=?) c f =
+    match c with
+    | Left _ -> c
+    | Right a -> f a
+
+/// Bind that allows handling of failure.
+let (>>=??) (c : Either<_, _>) f : Either<_, _> =
+    f c
+
+type EitherBuilder () =
+    member this.Bind (c, f) = c >>= f
+    member this.Return a = return' a
+    member this.ReturnFrom a = returnFrom a
+
+let either = EitherBuilder ()
