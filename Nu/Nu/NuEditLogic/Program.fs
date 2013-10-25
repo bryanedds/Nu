b3bd0c96f4c0bfa5f38538b0cdb6258f831d0d30
Bryan Edds <bryanedds@gmail.com>
2013-10-25 01:51:10 -0400

    Started working on NuEdit.
    Former-commit-id: 26a9ab207c0be53b5ad80a405080bd72ad5d776d

---
+++ Nu/Nu/NuEditLogic/Program.fs
@@ -0,0 +1,12 @@
+﻿module NuEditLogic.Program
+open NuEditDesign
+open System
+open System.Windows.Forms
+open System.Threading
+
+[<EntryPoint>]
+let main _ = 
+    use form = new NuEditForm ()
+    Application.EnableVisualStyles ()
+    Application.Run form
+    0
