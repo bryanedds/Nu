2a30ede7b59fc0a7d5a8c7e7852e11095a5783a1
Bryan Edds <bryanedds@gmail.com>
2013-10-29 15:33:27 -0400

    Made editor program slightly more comprehensible.
    Former-commit-id: 8f6d8aef138f13373aea42b528c899b2c1a6d683

--- Nu/Nu/NuEditLogic/Program.fs
+++ Nu/Nu/NuEditLogic/Program.fs
@@ -7,84 +7,20 @@ open System.Collections.Generic
 open System.Reflection
 open System.Windows.Forms
 open System.ComponentModel
-open Nu.Sdl
 open Nu.Core
 open Nu.AssetMetadata
+open Nu.Sdl
 open Nu.Entity
 open Nu.Group
 open Nu.Screen
 open Nu.Game
 open Nu.Simulation
 open Nu.TestGame
-
-type EntityChange =
-    { Address : Address
-      FieldInfo : FieldInfo
-      Value : obj }
-
-let applyEntityChange world (change : EntityChange) =
-    let entityLens = World.entity change.Address
-    let entity = get world entityLens
-    let fieldInfo = change.FieldInfo
-    let value = change.Value
-    let entity_ =
-        let entity_ = { entity with Id = entity.Id } // NOTE: this is just a hacky way to copy an entity in lieu of reflection
-        if entity_.GetType(). GetField (fieldInfo.Name, BindingFlags.Instance ||| BindingFlags.NonPublic) = fieldInfo
-        then let _ = fieldInfo.SetValue (entity_, value) in entity_
-        else
-            match entity.EntitySemantic with
-            | Gui gui ->
-                let gui_ = { gui with Position = gui.Position } // NOTE: hacky copy
-                if gui_.GetType().GetField (fieldInfo.Name, BindingFlags.Instance ||| BindingFlags.NonPublic) = fieldInfo
-                then let _ = fieldInfo.SetValue (gui_, value) in { entity with EntitySemantic = Gui gui_ }
-                else
-                    match gui.GuiSemantic with
-                    | Button button ->
-                        let button_ = { button with IsDown = button.IsDown } // NOTE: hacky copy
-                        fieldInfo.SetValue (button_, value)
-                        { entity with EntitySemantic = Gui { gui with GuiSemantic = Button button_ }}
-                    | Label label ->
-                        let label_ = { label with LabelSprite = label.LabelSprite } // NOTE: hacky copy
-                        fieldInfo.SetValue (label_, value)
-                        { entity with EntitySemantic = Gui { gui with GuiSemantic = Label label_ }}
-                    | TextBox textBox ->
-                        let textBox_ = { textBox with BoxSprite = textBox.BoxSprite } // NOTE: hacky copy
-                        fieldInfo.SetValue (textBox_, value)
-                        { entity with EntitySemantic = Gui { gui with GuiSemantic = TextBox textBox_ }}
-                    | Toggle toggle ->
-                        let toggle_ = { toggle with IsPressed = toggle.IsPressed } // NOTE: hacky copy
-                        fieldInfo.SetValue (toggle_, value)
-                        { entity with EntitySemantic = Gui { gui with GuiSemantic = Toggle toggle_ }}
-                    | Feeler feeler ->
-                        let feeler_ = { feeler with IsTouched = feeler.IsTouched } // NOTE: hacky copy
-                        fieldInfo.SetValue (feeler_, value)
-                        { entity with EntitySemantic = Gui { gui with GuiSemantic = Feeler feeler_ }}
-            | Actor actor ->
-                let actor_ = { actor with Position = actor.Position } // NOTE: hacky copy
-                if actor_.GetType().GetField (fieldInfo.Name, BindingFlags.Instance ||| BindingFlags.NonPublic) = fieldInfo
-                then let _ = fieldInfo.SetValue (actor_, value) in { entity with EntitySemantic = Actor actor_ }
-                else
-                    match actor.ActorSemantic with
-                    | Block block ->
-                        let block_ = { block with PhysicsId = block.PhysicsId } // NOTE: hacky copy
-                        fieldInfo.SetValue (block_, value)
-                        { entity with EntitySemantic = Actor { actor with ActorSemantic = Block block_ }}
-                    | Avatar avatar ->
-                        let avatar_ = { avatar with PhysicsId = avatar.PhysicsId } // NOTE: hacky copy
-                        fieldInfo.SetValue (avatar_, value)
-                        { entity with EntitySemantic = Actor { actor with ActorSemantic = Avatar avatar_ }}
-                    | TileMap tileMap ->
-                        let tileMap_ = { tileMap with PhysicsIds = tileMap.PhysicsIds } // NOTE: hacky copy
-                        fieldInfo.SetValue (tileMap_, value)
-                        { entity with EntitySemantic = Actor { actor with ActorSemantic = TileMap tileMap_ }}
-    set entity_ world entityLens
-
-let applyEntityChanges changes world =
-    Seq.fold applyEntityChange world changes
+open NuEditLogic.Entity
 
 // NOTE: I believe .NET's lack of parameterization in one aspect is forcing me to use this global-
 // style variable (but perhaps I've merely overlooked how to parameterize this?)
-let private gEntityChanges = List<EntityChange> ()
+let private gEntityFieldChanges = List<EntityFieldChange> ()
 
 type [<TypeDescriptionProvider (typeof<EntityTypeDescriptorProvider>)>] EntityTypeDescriptorSource =
     { Address : Address
@@ -92,48 +28,32 @@ type [<TypeDescriptionProvider (typeof<EntityTypeDescriptorProvider>)>] EntityTy
 
 and EntityPropertyDescriptor (fieldInfo : FieldInfo) =
     inherit PropertyDescriptor (fieldInfo.Name, Array.empty)
-    override this.ComponentType with get () = typeof<obj>
+    override this.ComponentType with get () = null // TODO: figure out what this definition should be!
     override this.PropertyType with get () = fieldInfo.FieldType
     override this.CanResetValue source = false
     override this.ResetValue source = ()
     override this.ShouldSerializeValue source = true
+
     override this.IsReadOnly
         // NOTE: we make entity id read-only
         // TODO: use attribute from the corresponding property here instead
         with get () = fieldInfo.Name = "Id@"
+
     override this.GetValue source =
         let entityTds = source :?> EntityTypeDescriptorSource
         let entityLens = World.entity entityTds.Address
         let entity = get entityTds.RefWorld.Value entityLens
-        if entity.GetType().GetField (fieldInfo.Name, BindingFlags.Instance ||| BindingFlags.NonPublic) = fieldInfo
-        then fieldInfo.GetValue entity
-        else
-            match entity.EntitySemantic with
-            | Gui gui ->
-                if gui.GetType().GetField (fieldInfo.Name, BindingFlags.Instance ||| BindingFlags.NonPublic) = fieldInfo
-                then fieldInfo.GetValue gui
-                else
-                    match gui.GuiSemantic with
-                    | Button button -> fieldInfo.GetValue button
-                    | Label label -> fieldInfo.GetValue label
-                    | TextBox textBox -> fieldInfo.GetValue textBox
-                    | Toggle toggle -> fieldInfo.GetValue toggle
-                    | Feeler feeler -> fieldInfo.GetValue feeler
-            | Actor actor ->
-                if actor.GetType().GetField (fieldInfo.Name, BindingFlags.Instance ||| BindingFlags.NonPublic) = fieldInfo
-                then fieldInfo.GetValue actor
-                else
-                    match actor.ActorSemantic with
-                    | Block block -> fieldInfo.GetValue block
-                    | Avatar avatar -> fieldInfo.GetValue avatar
-                    | TileMap tileMap -> fieldInfo.GetValue tileMap
+        getEntityFieldValue fieldInfo entity
+
     override this.SetValue (source, value) =
         let entityTds = source :?> EntityTypeDescriptorSource
-        let entityChange = { Address = entityTds.Address; FieldInfo = fieldInfo; Value = value }
+        let entityFieldChange = { Address = entityTds.Address; FieldInfo = fieldInfo; Value = value }
         // NOTE: even though this ref world will eventually get blown away, it must still be
         // updated here so that the change is reflected immediately by the property grid.
-        entityTds.RefWorld := applyEntityChange entityTds.RefWorld.Value entityChange
-        gEntityChanges.Add entityChange
+        entityTds.RefWorld := setEntityFieldValue entityTds.RefWorld.Value entityFieldChange
+        gEntityFieldChanges.Add entityFieldChange
+
+    // NOTE: This has to be a static member in order to see the relevant types in the recursive definitions.
     static member GetPropertyDescriptors (aType : Type) =
         let fields = aType.GetFields (BindingFlags.Instance ||| BindingFlags.NonPublic)
         let propertyDescriptors = Seq.map (fun field -> new EntityPropertyDescriptor (field) :> PropertyDescriptor) fields
@@ -149,32 +69,16 @@ and EntityTypeDescriptor (optSource : obj) =
             | :? EntityTypeDescriptorSource as source ->
                 let entityLens = World.entity source.Address
                 let entity = get source.RefWorld.Value entityLens
-                let types =
-                    match entity.EntitySemantic with
-                    | Gui gui ->
-                        let guiSemType =
-                            match gui.GuiSemantic with
-                            | Button _ -> typeof<Button>
-                            | Label _ -> typeof<Label>
-                            | TextBox _ -> typeof<TextBox>
-                            | Toggle _ -> typeof<Toggle>
-                            | Feeler _ -> typeof<Feeler>
-                        [typeof<Entity>; typeof<Gui>; guiSemType]
-                     | Actor actor ->
-                        let actorSemType =
-                            match actor.ActorSemantic with
-                            | Block _ -> typeof<Block>
-                            | Avatar _ -> typeof<Avatar>
-                            | TileMap _ -> typeof<TileMap>
-                        [typeof<Entity>; typeof<Actor>; actorSemType]
+                let entityTypes = getEntityTypes entity
                 // NOTE: this line could be simplified by a List.concatBy function.
-                List.fold (fun propertyDescriptors aType -> EntityPropertyDescriptor.GetPropertyDescriptors aType @ propertyDescriptors) [] types
+                List.fold (fun propertyDescriptors aType -> EntityPropertyDescriptor.GetPropertyDescriptors aType @ propertyDescriptors) [] entityTypes
             | _ -> EntityPropertyDescriptor.GetPropertyDescriptors typeof<Entity>
         PropertyDescriptorCollection (Array.ofList propertyDescriptors)
 
 and EntityTypeDescriptorProvider () =
     inherit TypeDescriptionProvider ()
-    override this.GetTypeDescriptor (_, optSource) = EntityTypeDescriptor optSource :> ICustomTypeDescriptor
+    override this.GetTypeDescriptor (_, optSource) =
+        EntityTypeDescriptor optSource :> ICustomTypeDescriptor
 
 let [<EntryPoint; STAThread>] main _ =
     use form = new NuEditForm ()
@@ -195,8 +99,8 @@ let [<EntryPoint; STAThread>] main _ =
                 form.Show ()
                 Right refWorld.Value)
         (fun world ->
-            let world_ = applyEntityChanges gEntityChanges world
-            gEntityChanges.Clear ()
+            let world_ = setEntityFieldValues gEntityFieldChanges world
+            gEntityFieldChanges.Clear ()
             let (keepRunning, world_) = testHandleUpdate world_
             refWorld := world_ // the old refWorld is blown away here
             (keepRunning && not form.IsDisposed, world_))
