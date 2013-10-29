ed04d2ce111b5bce8dcf1efc048bcf113108e4fc
Bryan Edds <bryanedds@gmail.com>
2013-10-29 19:14:46 -0400

    Started working on NuEdit save / open.

---
+++ Nu/Nu/Nu/Serialization.fs
@@ -0,0 +1,21 @@
+﻿module Nu.Serialization
+open System
+open System.Reflection
+open System.Xml
+open System.Xml.Serialization
+
+let writePublicProperties (writer : XmlWriter) obj =
+    let aType = obj.GetType ()
+    let publicProperties = aType.GetProperties (BindingFlags.Instance ||| BindingFlags.Public)
+    for property in publicProperties do
+        if not <| property.Name.EndsWith "Semantic" then // TODO: use attribute to filter, not name pattern!
+            writer.WriteElementString (property.Name, str <| property.GetValue obj)
+
+let readPublicProperties (reader : XmlReader) obj =
+    let aType = obj.GetType ()
+    let publicProperties = aType.GetProperties (BindingFlags.Instance ||| BindingFlags.Public)
+    for property in publicProperties do
+        if not <| property.Name.EndsWith "Semantic" then // TODO: use attribute to filter, not name pattern!
+            let valueStr = reader.ReadElementString property.Name
+            let value = Convert.ChangeType (valueStr, property.PropertyType)
+            property.SetValue (obj, value)
