e2f9382ccb4d432bd27661d06d36778686f4ddd0
Bryan Edds <bryanedds@gmail.com>
2013-10-30 00:26:51 -0400

    Slight code clean-up and organization.
    Former-commit-id: 3b5e5a0dc2df21a7aa3df4788624f5e15ec14cf6

--- Nu/Nu/Nu/Camera.fs
+++ Nu/Nu/Nu/Camera.fs
@@ -1,6 +1,7 @@
 ﻿module Nu.Camera
 open OpenTK
 
+/// The camera used to dictate what is rendered on the screen.
 type [<StructuralEquality; NoComparison>] Camera =
     { EyePosition : Vector2
       EyeSize : Vector2 }
