2bda05e5a9118c05278ace4a65ff4a036f423592
Bryan Edds <bryanedds@gmail.com>
2013-10-29 20:38:53 -0400

    Got rid of LunTrie as Map<Lun, _> is faster. Made Prime into a pure library.
    Former-commit-id: d5c5c9d7109a9e2ad5bc08e56545e0a773827dcd

--- Prime/Prime/Prime/LunTriePlusModule.fs
+++
@@ -1,9 +0,0 @@
-﻿// Prime - A PRIMitivEs code library.
-// Copyright (C) Bryan Edds, 2012-2013.
-
-[<AutoOpenAttribute>]
-module LunTriePlusModule
-open System
-
-// A LunTrie with additional key input (such as a version number).
-type LunTriePlus<'p, 'v when 'p : comparison> = Map<'p, 'v LunTrie>
