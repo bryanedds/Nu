3547cdadf12e5aa2900fee7558e86da4e62d41a3
Bryan Edds <bryanedds@gmail.com>
2013-11-27 02:39:52 -0500

    Broken commit; working on OmniBlade states...
    Former-commit-id: 1dc10ad5112d9383836f443b7eacfa5ac676bb61

--- Nu/Nu/Nu/OmniData.fs
+++ Nu/Nu/Nu/OmniData.fs
@@ -1,67 +1,66 @@
 ﻿namespace Nu
 open OpenTK
-open Nu.OmniTypes
-module OmniData =
 
-    type [<StructuralEquality; NoComparison>] WeaponData =
-        { Type : WeaponType
-          PhysicalPower : int
-          MagicalPower : int
-          ElementTypes : ElementType Set }
+type [<StructuralEquality; NoComparison>] OmniWeaponData =
+    { Type : OmniWeaponType
+      PhysicalPower : int
+      MagicalPower : int
+      ElementTypes : OmniElementType Set }
 
-    type [<StructuralEquality; NoComparison>] ArmorData =
-        { Type : ArmorType
-          PhysicalDefense : int
-          MagicalDefense : int
-          ElementTypes : ElementType Set }
+type [<StructuralEquality; NoComparison>] OmniArmorData =
+    { Type : OmniArmorType
+      PhysicalDefense : int
+      MagicalDefense : int
+      ElementTypes : OmniElementType Set }
 
-    type [<StructuralEquality; NoComparison>] MoveData =
-        { Name : string
-          Type : MoveType
-          SpecialAbilityType : SpecialAbilityType
-          EffectType : EffectType
-          SpecialPoints : int
-          PowerMultiplier : single // multiplier to the relevant powers queried from the move's participants
-          SuccessRate : single
-          Curative : bool
-          Percentive : bool
-          Defending : bool
-          Countering : bool
-          ElementType : ElementType
-          AddStatusType : StatusType Set
-          RemoveStatusType : StatusType Set
-          TargetType : TargetType
-          Revive : bool }
+type [<StructuralEquality; NoComparison>] OmniMoveData =
+    { Type : OmniMoveType
+      Name : string
+      SpecialAbilityType : OmniSpecialAbilityType
+      OmniEffectType : OmniEffectType
+      SpecialPointCost : int
+      PowerMultiplier : single // multiplier to the relevant powers queried from the move's participants
+      SuccessRate : single
+      Curative : bool
+      Percentive : bool
+      Defending : bool
+      PreCountering : bool
+      Countering : bool // can also be active with pre-countering!
+      OmniElementType : OmniElementType
+      AddStatusType : OmniStatusType Set
+      RemoveStatusType : OmniStatusType Set
+      TargetType : OmniTargetType
+      MortalityType : OmniMortalityType }
 
-    type [<StructuralEquality; NoComparison>] ItemData =
-        { Name : string
-          Type : ItemType
-          OptMoveData : MoveData option }
+type [<StructuralEquality; NoComparison>] OmniItemData =
+    { Type : OmniItemType
+      Name : string
+      OptMoveData : OmniMoveData option }
 
-    type [<StructuralEquality; NoComparison>] CharacterData =
-        { Name : string
-          Type : CharacterType
-          MoveData : MoveData list // usable moves are queried
-          EquipWeaponType : WeaponType
-          EquipArmorType : ArmorType
-          PhysicalRating : int // physical power is queried
-          MagicRating : int // magic power is queried
-          StaminaRating : int // hp max is queried
-          AgilityRating : int // tt gain is queried
-          WillRating : int // sp max is queried
-          StartingExperience : int // this is the only experience for enemies
-          ExperienceEarned : int // only used with enemies
-          GoldEarned : int } // only used with enemies
+type [<StructuralEquality; NoComparison>] OmniCharacterData =
+    { Type : OmniCharacterType
+      Name : string
+      MoveData : OmniMoveData list // usable moves are queried
+      EquipWeaponType : OmniWeaponType
+      EquipArmorType : OmniArmorType
+      PhysicalRating : int // physical power is queried
+      MagicRating : int // magic power is queried
+      StaminaRating : int // hp max is queried
+      AgilityRating : int // tt gain is queried
+      WillRating : int // sp max is queried
+      StartingExperience : int // this is the only experience for enemies
+      ExperienceEarned : int // only used with enemies
+      GoldEarned : int } // only used with enemies
 
-    type [<StructuralEquality; NoComparison>] FieldData =
-        { Id : Id
-          GroupModelFileName : string }
+type [<StructuralEquality; NoComparison>] OmniFieldData =
+    { Id : Id
+      GroupModelFileName : string }
 
-    type [<StructuralEquality; NoComparison>] BattleData =
-        { Id : Id
-          Name : string
-          BossEnemy : CharacterData
-          BossEnemyDefeatEndsBattle : bool
-          MinorEnemies : CharacterData list
-          MinorEnemySpawnsMax : int
-          AwardType : ItemType }
+type [<StructuralEquality; NoComparison>] OmniBattleData =
+    { Id : Id
+      Name : string
+      BossEnemy : OmniCharacterData
+      BossEnemyDefeatEndsBattle : bool
+      MinorEnemies : OmniCharacterData list
+      MinorEnemySpawnsMax : int
+      AwardType : OmniItemType }
