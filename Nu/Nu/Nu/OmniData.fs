b5b863695885c7588e9cd979a4bf21049fc2f047
Bryan Edds <bryanedds@gmail.com>
2013-11-26 20:57:47 -0500

    Split up OmniData.

--- Nu/Nu/Nu/OmniData.fs
+++ Nu/Nu/Nu/OmniData.fs
@@ -1,163 +1,21 @@
 ﻿namespace Nu
 open OpenTK
+open Nu.OmniTypes
 module OmniData =
 
-    type [<StructuralEquality; StructuralComparisonAttribute>] Element =
-        | Fire
-        | Ice
-        | Lightning
-        | Dark
-        | Holy
-
-    type [<StructuralEquality; StructuralComparisonAttribute>] Status =
-        | Strong
-        | Gifted
-        | Shell
-        | Haste
-        | Slow
-        | Poison
-        | Numb
-        | Mute
-        | Sleep
-
-    type [<StructuralEquality; NoComparison>] WeaponType =
-        | LightSword
-        | HeavySword
-        | Arrow
-        | Staff
-
-    type [<StructuralEquality; NoComparison>] ArmorType =
-        | Heavy // better protection against physical damage
-        | Incanted // better protection against magical damage
-
-    type [<StructuralEquality; NoComparison>] RelicType =
-        | GoldBrace // +50 HPMax
-        | RubyRing // 2x SP gain
-        | SilverLeaf // +2 Agility
-        | GemStone // Start Battle w/ Full SP
-        | RedCrystal // Double Damage of Offensive Items
-        | EmeraldEarring // Can Participate in Combo with Half AT
-        | HawkFeather // Anti-Counter
-        | SpiritBox // Counter-Counter
-        | SapphireOrb // +5 MDef
-        | Gauntlet // +5 PDef
-        | IronCross // 1.5x HPMax
-        | SilverCross // Anti-Ailment
-
-    type [<StructuralEquality; NoComparison>] EquipmentType =
-        | Weapon of WeaponType
-        | Armor of ArmorType
-        | Relic of RelicType
-
-    type [<StructuralEquality; NoComparison>] ConsumableType = // consumables only work in battle in OmniBlade
-        | GreenHerb // Cure 30 HP
-        | RedHerb // Cure 50 HP
-        | BlueHerb // Cure 100 HP
-        | Remedy // Remove Status Ailment (except perhaps Numb)
-        | FireBall // Cast Fire
-        | IceBall // Cast Ice
-        | LitBall // Cast Lightning
-        | FireCrystal // Cast Flare
-        | IceCrystal // Cast Blizzard
-        | LitCrystal // Cast T-Storm
-        | SmellingSalts // Revive Ally
-        | PaleAle // +10 SP, but inflicts Numb
-        | NillaAle // +30 SP, but inflicts Numb
-        | ChocoAle // +50 SP, but inflicts Numb
-
-    type [<StructuralEquality; NoComparison>] SpecialType =
-        | GuardCounter // defend + counter
-        | PreCounter
-        | HolySlash // directed line of holy dmg
-        | HolyShell // Physical def up
-        | DoubleGreenHerb // uses a green herb at 2x healing
-        | DoubleRedHerb
-        | DoubleBlueHerb
-        | GrayBlades // both holy and dark physical damage
-        | DualCounter // if enemy attacks either ally, both allies counter
-        | FireCounter
-        | DeathCounter
-        | GreenTonic // Double Green Herb All
-        | RedTonic // Double Red Herb All
-        | BlueTonic // Double Blue Herb All
-        | SuperSalts // super salts restores life and fill HP to max
-        | TriCounter
-        | HolyWind // shell all
-        | LightningBlades
-        | CrossBlades // holy dmg in both horizontal and vertical line (a cross)
-
-    type [<StructuralEquality; NoComparison>] EffectType =
-        | Physical
-        | Magical
-
-    type [<StructuralEquality; NoComparison>] DualSpecialType =
-        | JakkAndLana
-        | JakkAndMeryl
-        | JakkAndKane
-        | LanaAndMaryl
-        | LanaAndKane
-        | MaerylAndKane
-        //| GoblinAndTurkey
-
-    type [<StructuralEquality; NoComparison>] TripleSpecialType =
-        | JakkAndLanaAndMaryl
-        | JakkAndMarylAndKane
-        | JakkAndLanaAndKane
-        | LanaAndMarylAndKane
-        //| GoblinAndTurkeyAndFrog
-
-    type [<StructuralEquality; NoComparison>] SpecialAbilityType =
-        | SingleSpecial
-        | DualSpecial of DualSpecialType
-        | TripleSpecial of TripleSpecialType
-
-    type [<StructuralEquality; NoComparison>] Targetype =
-        | Single
-        | DirectedLine
-        | HorizontalLine
-        | VerticalLine
-        | Cross
-        | SmallRadius
-        | MediumRadius
-        | LargeRadius
-        | All
-
-    type [<StructuralEquality; NoComparison>] MoveType =
-        | Attack
-        | Defend
-        | Consumable of ConsumableType
-        | Special of SpecialType
-
-    type [<StructuralEquality; NoComparison>] TargetingInputType =
-        | AttackInput of Id
-        | ItemInput of Id // for now, just uses GreenHerb
-        | SpecialInput of Id // for now, just uses DefendCounter?
-
-     type [<StructuralEquality; NoComparison>] BattleInputState =
-        | GoInput of Id Set
-        | CrossInput of Id * Id Set
-        | TargetableInput of TargetingInputType
-        | TargetInput of TargetingInputType
-
-    type [<StructuralEquality; NoComparison>] BattleState =
-        | BattleBegin
-        | BattleAdvance of BattleInputState
-        | BattleInput of BattleInputState
-        | BattleEnding of bool
-
-    type [<StructuralEquality; NoComparison>] Weapon =
+    type [<StructuralEquality; NoComparison>] WeaponData =
         { Type : WeaponType
           PhysicalPower : int
           MagicalPower : int
-          Elements : Element Set }
+          ElementTypes : ElementType Set }
 
-    type [<StructuralEquality; NoComparison>] Armor =
+    type [<StructuralEquality; NoComparison>] ArmorData =
         { Type : ArmorType
           PhysicalDefense : int
           MagicalDefense : int
-          Elements : Element Set }
+          ElementTypes : ElementType Set }
 
-    type [<StructuralEquality; NoComparison>] Move =
+    type [<StructuralEquality; NoComparison>] MoveData =
         { Name : string
           Type : MoveType
           SpecialAbilityType : SpecialAbilityType
@@ -169,20 +27,21 @@ module OmniData =
           Percentive : bool
           Defending : bool
           Countering : bool
-          Element : Element
-          AddStatus : Status Set
-          RemoveStatus : Status Set
-          Targetype : Targetype
+          ElementType : ElementType
+          AddStatusType : StatusType Set
+          RemoveStatusType : StatusType Set
+          TargetType : TargetType
           Revive : bool }
 
-    type [<StructuralEquality; NoComparison>] CharacterType =
-        | Player
-        | Enemy
+    type [<StructuralEquality; NoComparison>] ItemData =
+        { Name : string
+          Type : ItemType
+          OptMoveData : MoveData option }
 
     type [<StructuralEquality; NoComparison>] CharacterData =
         { Name : string
           Type : CharacterType
-          Moves : Move list // usable moves are queried
+          MoveData : MoveData list // usable moves are queried
           EquipWeaponType : WeaponType
           EquipArmorType : ArmorType
           PhysicalRating : int // physical power is queried
@@ -193,25 +52,6 @@ module OmniData =
           StartingExperience : int // this is the only experience for enemies
           ExperienceEarned : int // only used with enemies
           GoldEarned : int } // only used with enemies
-
-    type [<StructuralEquality; NoComparison>] Character =
-        { Data : CharacterData
-          Hp : int // hp max is queried
-          Sp : int // sp max is queried
-          Tt : int // turn ticks
-          EquippedWeapon : WeaponType option
-          EquippedArmor : ArmorType option
-          EquippedRelics : RelicType list
-          Experience : int } // level is queried
-
-    type [<StructuralEquality; NoComparison>] ItemType =
-        | Equipment of EquipmentType
-        | Consumable of ConsumableType
-
-    type [<StructuralEquality; NoComparison>] Item =
-        { Name : string
-          Type : ItemType
-          OptMove : Move option }
 
     type [<StructuralEquality; NoComparison>] FieldData =
         { Id : Id
@@ -225,19 +65,3 @@ module OmniData =
           MinorEnemies : CharacterData list
           MinorEnemySpawnsMax : int
           AwardType : ItemType }
-
-    type [<StructuralEquality; NoComparison>] Battle =
-        { Data : BattleData
-          Enemies : Character option list
-          MinorEnemySpawns : int }
-
-    type [<StructuralEquality; NoComparison>] Player =
-        { FieldId : Id
-          FieldPosition : Vector2
-          OptBattleDataId : Id option
-          Allies : Character option list
-          Gold : int }
-
-    type [<StructuralEquality; NoComparison>] MoveUsage =
-        { Move : Move
-          Characters : Character list } // characters involved in the move
---
+++ Nu/Nu/Nu/OmniState.fs
@@ -0,0 +1,82 @@
+﻿namespace Nu
+open OpenTK
+open Nu.OmniTypes
+open Nu.OmniData
+module OmniState =
+
+    type [<StructuralEquality; NoComparison>] Character =
+        { Id : Id
+          Data : CharacterData
+          Hp : int // hp max is queried
+          Sp : int // sp max is queried
+          Tt : int // turn ticks
+          Defending : bool
+          Statuses : StatusType Set
+          EquippedWeapon : WeaponType option
+          EquippedArmor : ArmorType option
+          EquippedRelics : RelicType list
+          Experience : int } // level is queried
+
+    // NOTE: movement states probably ought to be keyed off the animation engine, but since it's
+    // not implemented, movement states are defined manually
+    type [<StructuralEquality; NoComparison>] CharacterUseAttackState =
+        | AttackLag1 of int
+        | AttackLaunch1 of int
+        | AttackLag2 of int
+        | AttackLaunch2 of int
+        | AttackDeltaSpawn
+        | AttackLag3 of int
+
+    type [<StructuralEquality; NoComparison>] CharacterUseItemState =
+        | ItemLag1 of int
+        | ItemLaunch1 of int
+        | ItemLag2 of int
+        | ItemEffect2 of int
+        | ItemDeltaSpawn
+        | ItemLag3 of int
+
+    type [<StructuralEquality; NoComparison>] CharacterUseSpecialState =
+        | SpecialLag1 of int
+        | SpecialLaunch1 of int
+        | SpecialLag2 of int
+        | SpecialEffect2 of int
+        | SpecialDeltaSpawn
+        | SpecialLag3 of int
+
+    type [<StructuralEquality; NoComparison>] CharacterActionState =
+        | CharacterUseAttack of CharacterUseAttackState
+        | CharacterUseItem of CharacterUseItemState
+        | CharacterUseSpecial of CharacterUseSpecialState
+
+    type [<StructuralEquality; NoComparison>] BattleActionState =
+        | IdleAction
+        | CharacterAction of CharacterActionState
+
+    type [<StructuralEquality; NoComparison>] BattleInputState =
+        | GoInput of Id Set
+        | CrossInput of Id * Id Set
+        | TargetableInput of TargetingInputType
+        | TargetInput of TargetingInputType
+
+    type [<StructuralEquality; NoComparison>] BattleState =
+        | BattleBegin
+        | BattleAdvance of BattleInputState * CharacterActionState
+        | BattleEnding of bool
+
+    type [<StructuralEquality; NoComparison>] Move =
+        { Data : MoveData
+          CharacterIds : Id list } // characters involved in the move
+
+    type [<StructuralEquality; NoComparison>] Battle =
+        { Data : BattleData
+          State : BattleState
+          MoveQueue : Move rQueue
+          Enemies : Character list
+          MinorEnemySpawns : int }
+
+    type [<StructuralEquality; NoComparison>] Player =
+        { FieldId : Id
+          FieldPosition : Vector2
+          OptBattleDataId : Id option
+          Allies : Character list
+          Gold : int }
