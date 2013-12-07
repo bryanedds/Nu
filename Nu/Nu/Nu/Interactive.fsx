48d0c41b25d9f4ba35124ec30d329b116b5e5820
Bryan Edds <bryanedds@gmail.com>
2013-12-07 12:45:35 -0500

    Reverted songs to Reason 3.0. Fixed a couple fsx file issues.
    Former-commit-id: 1342837c5cc234133a6b3e68bda86f8e27fdf693

--- Nu/Nu/Nu/Interactive.fsx
+++ Nu/Nu/Nu/Interactive.fsx
@@ -1,19 +1,21 @@
-﻿// IMPORTANT!
+﻿// TODO: fix this script
+
+// IMPORTANT!
 // NOTE: this MUST be run in the 64-bit FSI (In VS, Tools -> Options... -> F# Tools -> 64-bit F# Interactive [set to true]).
 // IMPORTANT!
+#r "System.Configuration"
 
 // IMPORTANT!
 // NOTE: change these paths to make this script run with your Nu installation.
 // IMPORTANT!
 #r "C:/OmniBlade/Prime/xUnit/xunit.dll"
 #r "C:/OmniBlade/Prime/FSharpx.Core/FSharpx.Core.dll"
-#r "C:/OmniBlade/Prime/Prime/Prime/bin/Release/Prime.exe"
-#r "C:/OmniBlade/Aml/Aml/Aml/bin/Optimized/Aml.exe"
+#r "C:/OmniBlade/Prime/Prime/Prime/bin/Debug/Prime.dll"
 #r "C:/OmniBlade/Nu/xUnit/xunit.dll"
 #r "C:/OmniBlade/Nu/FSharpx.Core/FSharpx.Core.dll"
 #r "C:/OmniBlade/Nu/Farseer/FarseerPhysics.dll"
-#r "C:/OmniBlade/Nu/SDL2#/Release/SDL2#.dll"
-#r "C:/OmniBlade/Nu/TiledSharp/Release/TiledSharp.dll"
+#r "C:/OmniBlade/Nu/SDL2#/Debug/SDL2#.dll"
+#r "C:/OmniBlade/Nu/TiledSharp/Debug/TiledSharp.dll"
 System.IO.Directory.SetCurrentDirectory "C:/OmniBlade/Nu/Nu/Nu/bin/Release"
 
 #load "Core.fs"
@@ -23,18 +25,22 @@ System.IO.Directory.SetCurrentDirectory "C:/OmniBlade/Nu/Nu/Nu/bin/Release"
 #load "Voords.fs"
 #load "Assets.fs"
 #load "Physics.fs"
-#load "Audio.fs"
 #load "Rendering.fs"
+#load "Audio.fs"
 #load "Metadata.fs"
 #load "Input.fs"
 #load "Camera.fs"
 #load "DomainModel.fs"
+#load "Sdl.fs"
+#load "OmniTypes.fs"
+#load "OmniData.fs"
+#load "OmniState.fs"
+#load "Simulation.fs"
 #load "Entities.fs"
 #load "Groups.fs"
 #load "Screens.fs"
 #load "Games.fs"
-#load "Sdl.fs"
-#load "Sim.fs"
+#load "World.fs"
 #load "OmniBlade.fs"
 
 open System
@@ -42,21 +48,3 @@ open SDL2
 open OpenTK
 open TiledSharp
 open Nu
-open Nu.Core
-open Nu.Constants
-open Nu.Math
-open Nu.Voords
-open Nu.Assets
-open Nu.Physics
-open Nu.Audio
-open Nu.Rendering
-open Nu.Metadata
-open Nu.CameraModule
-open Nu.DomainModel
-open Nu.Entities
-open Nu.Groups
-open Nu.Screens
-open Nu.Games
-open Nu.Sdl
-open Nu.WorldModule
-open Nu.OmniBlade
