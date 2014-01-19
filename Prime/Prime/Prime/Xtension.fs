1797ad8a8c6c553c22e641f67c58cc41326ece39
Bryan Edds <bryanedds@gmail.com>
2014-01-19 12:40:48 -0500

    Removed GameModel concept.

--- Prime/Prime/Prime/Xtension.fs
+++ Prime/Prime/Prime/Xtension.fs
@@ -2,9 +2,3 @@
 
 /// The empty extension.
 let empty = { OptName = None; Fields = Map.empty }
-
-// Some syntactic tests.
-let test = { OptName = None; Fields = Map.empty }
-let test' = test?SomeField <- 0
-let test_ = test'?SomeField () : int
-let test'' = test'?SomeMethod <| { Dispatchers = Map.empty } : unit
