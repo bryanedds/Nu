0dbf3025a55eca4463fef939f920261e9aab0948
Bryan Edds <bryanedds@gmail.com>
2014-02-25 23:41:48 -0500

    More work on nu documentation.
    Former-commit-id: 7b671e9dd17618064eae2025ab83a4e0a0e1ec31

--- OmniBlade/OmniBlade/OmniBlade/Program.fs
+++ OmniBlade/OmniBlade/OmniBlade/Program.fs
@@ -5,15 +5,33 @@ open OpenTK
 open TiledSharp
 open Nu
 open OmniBlade
-open OmniBlade.OmniFlow
 module Program =
 
     let [<EntryPoint>] main _ =
+
         World.initTypeConverters ()
-        let sdlViewConfig = NewWindow { WindowTitle = "OmniBlade"; WindowX = 32; WindowY = 32; WindowFlags = SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN }
-        let sdlRenderFlags = enum<SDL.SDL_RendererFlags> (int SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED ||| int SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC)
-        let sdlConfig = Sdl.makeSdlConfig sdlViewConfig Voords.VirtualResolutionX Voords.VirtualResolutionY sdlRenderFlags 1024
+
+        let sdlViewConfig =
+            NewWindow
+                { WindowTitle = "OmniBlade"
+                  WindowX = 32
+                  WindowY = 32
+                  WindowFlags = SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN }
+
+        let sdlRenderFlags =
+            enum<SDL.SDL_RendererFlags>
+                (int SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED |||
+                 int SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC)
+
+        let sdlConfig =
+            Sdl.makeSdlConfig
+                sdlViewConfig
+                Voords.VirtualResolutionX
+                Voords.VirtualResolutionY
+                sdlRenderFlags
+                1024
+
         World.run
-            (fun sdlDeps -> tryCreateOmniBladeWorld sdlDeps ())
+            (fun sdlDeps -> OmniFlow.tryCreateOmniBladeWorld sdlDeps ())
             (fun world -> (true, world))
             sdlConfig
