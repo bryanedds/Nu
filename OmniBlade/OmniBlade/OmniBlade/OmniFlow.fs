abe514600d153b6fad7681afa2c416eaba4ffb17
Bryan Edds <bryanedds@gmail.com>
2014-02-25 23:41:48 -0500

    More work on nu documentation.
    Former-commit-id: 7b671e9dd17618064eae2025ab83a4e0a0e1ec31

--- OmniBlade/OmniBlade/OmniBlade/OmniFlow.fs
+++ OmniBlade/OmniBlade/OmniBlade/OmniFlow.fs
@@ -6,82 +6,206 @@ open TiledSharp
 open Nu
 module OmniFlow =
 
-    // transition literals
+    // transition constants
     let IncomingTimeSplash = 60
     let IncomingTime = 20
     let IdlingTime = 60
     let OutgoingTimeSplash = 40
     let OutgoingTime = 20
 
-    // splash literals
+    // splash constants
     let SplashAddress = NuCore.addr "Splash"
 
-    // title literals
+    // title constants
     let TitleAddress = NuCore.addr "Title"
     let TitleGroupName = Lun.make "Group"
     let TitleGroupAddress = TitleAddress @ [TitleGroupName]
     let TitleGroupFileName = "Assets/OmniBlade/Groups/Title.nugroup"
-    let ClickTitleGroupNewAddress = NuCore.straddrstr "Click" TitleGroupAddress "New"
-    let ClickTitleGroupLoadAddress = NuCore.straddrstr "Click" TitleGroupAddress "Load"
-    let ClickTitleGroupCreditsAddress = NuCore.straddrstr "Click" TitleGroupAddress "Credits"
-    let ClickTitleGroupExitAddress = NuCore.straddrstr "Click" TitleGroupAddress "Exit"
+    let ClickTitleNewGameAddress = NuCore.straddrstr "Click" TitleGroupAddress "NewGame"
+    let ClickTitleLoadGameAddress = NuCore.straddrstr "Click" TitleGroupAddress "LoadGame"
+    let ClickTitleCreditsAddress = NuCore.straddrstr "Click" TitleGroupAddress "Credits"
+    let ClickTitleExitAddress = NuCore.straddrstr "Click" TitleGroupAddress "Exit"
 
-    // load literals
-    let LoadAddress = NuCore.addr "Load"
-    let LoadGroupName = Lun.make "Group"
-    let LoadGroupAddress = LoadAddress @ [LoadGroupName]
-    let LoadGroupFileName = "Assets/OmniBlade/Groups/Load.nugroup"
-    let ClickLoadGroupBackAddress = NuCore.straddrstr "Click" LoadGroupAddress "Back"
+    // load game constants
+    let LoadGameAddress = NuCore.addr "LoadGame"
+    let LoadGameGroupName = Lun.make "Group"
+    let LoadGameGroupAddress = LoadGameAddress @ [LoadGameGroupName]
+    let LoadGameGroupFileName = "Assets/OmniBlade/Groups/LoadGame.nugroup"
+    let ClickLoadGameBackAddress = NuCore.straddrstr "Click" LoadGameGroupAddress "Back"
 
-    // credits literals
+    // credits constants
     let CreditsAddress = NuCore.addr "Credits"
     let CreditsGroupName = Lun.make "Group"
     let CreditsGroupAddress = CreditsAddress @ [CreditsGroupName]
     let CreditsGroupFileName = "Assets/OmniBlade/Groups/Credits.nugroup"
-    let ClickCreditsGroupBackAddress = NuCore.straddrstr "Click" CreditsGroupAddress "Back"
+    let ClickCreditsBackAddress = NuCore.straddrstr "Click" CreditsGroupAddress "Back"
 
-    // field literals
+    // field constants
     let FieldAddress = NuCore.addr "Field"
     let FieldGroupName = Lun.make "Group"
     let FieldGroupAddress = FieldAddress @ [FieldGroupName]
     let FieldGroupFileName = "Assets/OmniBlade/Groups/Field.nugroup"
-    let ClickFieldGroupBackAddress = NuCore.straddrstr "Click" FieldGroupAddress "Back"
+    let ClickFieldBackAddress = NuCore.straddrstr "Click" FieldGroupAddress "Back"
 
-    // time literals
+    // time constants
     let TimeAddress = NuCore.addr "Time"
 
+    // describes how to add the OmniBlade title screen to the world.
     let addTitleScreen world =
-        let world_ = World.addDissolveScreenFromFile TitleGroupFileName TitleGroupName IncomingTime OutgoingTime TitleAddress world
-        let world_ = World.subscribe ClickTitleGroupNewAddress [] (World.handleEventAsScreenTransition TitleAddress FieldAddress) world_
-        let world_ = World.subscribe ClickTitleGroupLoadAddress [] (World.handleEventAsScreenTransition TitleAddress LoadAddress) world_
-        let world_ = World.subscribe ClickTitleGroupCreditsAddress [] (World.handleEventAsScreenTransition TitleAddress CreditsAddress) world_
-        World.subscribe ClickTitleGroupExitAddress [] World.handleEventAsExit world_
+        
+        // add a dissolve screen from the specified file with the given
+        // parameter
+        let world_ =
+            World.addDissolveScreenFromFile
+                TitleGroupFileName
+                TitleGroupName
+                IncomingTime
+                OutgoingTime
+                TitleAddress
+                world
+        
+        // subscribe to the event that is raised when the Title screen's New
+        // Game button is clicked, and handle the event by transitioning to the
+        // Field screen
+        let world_ =
+            World.subscribe
+                ClickTitleNewGameAddress
+                []
+                (World.handleEventAsScreenTransition TitleAddress FieldAddress)
+                world_
+        
+        // subscribe to the event that is raised when the Title screen's Load
+        // Game button is clicked, and handle the event by transitioning to the
+        // Field screen
+        let world_ =
+            World.subscribe
+                ClickTitleLoadGameAddress
+                []
+                (World.handleEventAsScreenTransition TitleAddress LoadGameAddress)
+                world_
+        
+        // subscribe to the event that is raised when the Title screen's Credits
+        // button is clicked, and handle the event by transitioning to the
+        // Field screen
+        let world_ =
+            World.subscribe
+                ClickTitleCreditsAddress
+                []
+                (World.handleEventAsScreenTransition TitleAddress CreditsAddress)
+                world_
+        
+        // subscribe to the event that is raised when the Title screen's Exit
+        // button is clicked, and handle the event by exiting the game
+        World.subscribe ClickTitleExitAddress [] World.handleEventAsExit world_
 
-    let addLoadScreen world =
-        let world' = World.addDissolveScreenFromFile LoadGroupFileName LoadGroupName IncomingTime OutgoingTime LoadAddress world
-        World.subscribe ClickLoadGroupBackAddress [] (World.handleEventAsScreenTransition LoadAddress TitleAddress) world'
+    // pretty much the same as above, but for the Load Game screen
+    let addLoadGameScreen world =
+        
+        let world' =
+            World.addDissolveScreenFromFile
+                LoadGameGroupFileName
+                LoadGameGroupName
+                IncomingTime
+                OutgoingTime
+                LoadGameAddress
+                world
+        
+        World.subscribe
+            ClickLoadGameBackAddress
+            []
+            (World.handleEventAsScreenTransition LoadGameAddress TitleAddress)
+            world'
 
+    // and so on...
     let addCreditsScreen world =
-        let world' = World.addDissolveScreenFromFile CreditsGroupFileName CreditsGroupName IncomingTime OutgoingTime CreditsAddress world
-        World.subscribe ClickCreditsGroupBackAddress [] (World.handleEventAsScreenTransition CreditsAddress TitleAddress) world'
+        
+        let world' =
+            World.addDissolveScreenFromFile
+                CreditsGroupFileName
+                CreditsGroupName
+                IncomingTime
+                OutgoingTime
+                CreditsAddress
+                world
+        
+        World.subscribe
+            ClickCreditsBackAddress
+            []
+            (World.handleEventAsScreenTransition CreditsAddress TitleAddress) world'
 
+    // and so on.
     let addFieldScreen world =
-        let world' = World.addDissolveScreenFromFile FieldGroupFileName FieldGroupName IncomingTime OutgoingTime FieldAddress world
-        World.subscribe ClickFieldGroupBackAddress [] (World.handleEventAsScreenTransition FieldAddress TitleAddress) world'
+        
+        let world' =
+            World.addDissolveScreenFromFile
+                FieldGroupFileName
+                FieldGroupName
+                IncomingTime
+                OutgoingTime
+                FieldAddress
+                world
+        
+        World.subscribe
+            ClickFieldBackAddress
+            []
+            (World.handleEventAsScreenTransition FieldAddress TitleAddress)
+            world'
 
+    // here we create the OmniBlade world in a callback from the World.run function.
     let tryCreateOmniBladeWorld sdlDeps extData =
+
+        // our custom game dispatcher here is OmniGameDispatcher
         let gameDispatcher = OmniGameDispatcher () :> obj
+
+        // we use the World.tryCreateEmptyWorld as a convenience function to
+        // create an empty world that we will transform to create the OmniBlade
+        // world.
         let optWorld = World.tryCreateEmptyWorld sdlDeps gameDispatcher extData
         match optWorld with
         | Left _ as left -> left
         | Right world ->
-            let playSong = PlaySong { Song = { SongAssetName = Lun.make "Song"; PackageName = Lun.make "Default"; PackageFileName = "AssetGraph.xml" }; FadeOutCurrentSong = true }
-            let splashScreenSprite = { SpriteAssetName = Lun.make "Image5"; PackageName = Lun.make "Default"; PackageFileName = "AssetGraph.xml" }
-            let world_ = { world with AudioMessages = playSong :: world.AudioMessages }
-            let world_ = World.addSplashScreenFromData (World.transitionScreenHandler TitleAddress) SplashAddress IncomingTimeSplash IdlingTime OutgoingTimeSplash splashScreenSprite world_
+            
+            // specify how to create the sprite used for the game's splash screen
+            let splashScreenSprite =
+                { SpriteAssetName = Lun.make "Image5"
+                  PackageName = Lun.make "Default"
+                  PackageFileName = "AssetGraph.xml" }
+
+            // add to the world a splash screen that automatically transitions to the
+            // Title screen when finished
+            let world_ =
+                World.addSplashScreenFromData
+                    (World.transitionScreenHandler TitleAddress)
+                    SplashAddress
+                    IncomingTimeSplash
+                    IdlingTime
+                    OutgoingTimeSplash
+                    splashScreenSprite
+                    world
+            
+            // specify the song to play for the duration of the game
+            let gameSong =
+                { SongAssetName = Lun.make "Song"
+                  PackageName = Lun.make "Default"
+                  PackageFileName = "AssetGraph.xml" }
+            
+            // create a message to the audio system to play said song
+            let playGameSong =
+                PlaySong
+                    { Song = gameSong
+                      FadeOutCurrentSong = true }
+            
+            // add the message to the audio message queue
+            let world_ = { world_ with AudioMessages = playGameSong :: world.AudioMessages }
+
+            // add our UI screens to the world
             let world_ = addTitleScreen world_
-            let world_ = addLoadScreen world_
+            let world_ = addLoadGameScreen world_
             let world_ = addCreditsScreen world_
             let world_ = addFieldScreen world_
+            
+            // transition the world to splash screen
             let world_ = World.transitionScreen SplashAddress world_
+
+            // return our world within the expected Either type, and we're off!
             Right world_
