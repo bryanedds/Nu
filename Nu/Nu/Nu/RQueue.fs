67c8d8815b48bedc4f9673e9011d5aebec135358
Bryan Edds <bryanedds@gmail.com>
2014-02-27 22:37:45 -0500

    Ticked Aml version number.
    Former-commit-id: cd33398782e605e9e5d4b33e8947d0634afe55cd

--- Nu/Nu/Nu/RQueue.fs
+++ Nu/Nu/Nu/RQueue.fs
@@ -1,6 +1,8 @@
-﻿[<AutoOpen>]
-module RQueue
+﻿namespace Nu
 
-/// Type alias to emphasize that messages arrive in a reversed queue since I am
-/// too lazy to write an immutable queue directly.
-type 'a rQueue = 'a list
+[<AutoOpen>]
+module RQueue =
+
+    /// Type alias to emphasize that messages arrive in a reversed queue since I am
+    /// too lazy to write an immutable queue directly.
+    type 'a rQueue = 'a list
