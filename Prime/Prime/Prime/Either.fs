164fbcd6de5b8b8ef4365d46a380a0c6b0fcf903
Bryan Edds <bryanedds@gmail.com>
2014-04-18 13:43:03 -0400

    Added detail to various variable names.
    Former-commit-id: 1fab9b46c0d0537d92572f53cf5e7995bc99afaf

--- Prime/Prime/Prime/Either.fs
+++ Prime/Prime/Prime/Either.fs
@@ -10,35 +10,35 @@ module EitherModule =
         | Right of 'b
 
     /// Monadic bind.
-    let inline (>>=) c f =
-        match c with
-        | Left _ -> c
-        | Right a -> Right <| f a
+    let inline (>>=) either fn =
+        match either with
+        | Left _ -> either
+        | Right value -> Right <| fn value
 
     /// Bind that allows indication of failure.
-    let inline (>>=?) c f =
-        match c with
-        | Left _ -> c
-        | Right a -> f a
+    let inline (>>=?) either fn =
+        match either with
+        | Left _ -> either
+        | Right value -> fn value
 
     /// Bind that allows handling of failure.
-    let inline (>>=??) (c : Either<_, _>) f : Either<_, _> =
-        f c
+    let inline (>>=??) (either : Either<_, _>) fn : Either<_, _> =
+        fn either
 
 module Either =
 
     /// Monadic return.
-    let inline return' a =
-        Right a
+    let inline return' value =
+        Right value
 
     /// Monadic returnFrom.
     /// TODO: ensure this is defined correctly!
-    let inline returnFrom a =
-        a
+    let inline returnFrom value =
+        value
 
     type EitherBuilder () =
-        member this.Bind (c, f) = c >>= f
-        member this.Return a = return' a
-        member this.ReturnFrom a = returnFrom a
+        member this.Bind (either, fn) = either >>= fn
+        member this.Return value = return' value
+        member this.ReturnFrom value = returnFrom value
 
     let either = EitherBuilder ()
