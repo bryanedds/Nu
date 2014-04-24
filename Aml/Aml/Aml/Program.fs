ad981a3050ded88d6087b694afc132de839e33e5
Bryan Edds <bryanedds@gmail.com>
2014-04-24 08:50:26 -0400

    Improved code formatting of Aml. Removed dead code. Fixed a few outdated unit tests.
    Former-commit-id: 146d3c9c54c61898046d97457ebfc6208eae951e

--- Aml/Aml/Aml/Program.fs
+++ Aml/Aml/Aml/Program.fs
@@ -1,7 +1,7 @@
 // Aml - A Modular Language.
 // Copyright (C) Bryan Edds, 2012-2013.
 
-module Aml.Program
+namespace Aml
 open System
 open System.Runtime
 open Prime
@@ -12,50 +12,51 @@ open Aml.Primitives
 open Aml.Evaluator
 open Aml.Repl
 open Aml.Environment
+module Program =
 
-// (let (c (ref 8000000)) (while! (i/= (get c) 0) (set! c (iDec (get c)))))
-// (let (c (ref 3800000)) (while! (/= (get c) 0) (set! c (dec (get c)))))
+    // (let (c (ref 8000000)) (while! (i/= (get c) 0) (set! c (iDec (get c)))))
+    // (let (c (ref 3800000)) (while! (/= (get c) 0) (set! c (dec (get c)))))
 
-// GCSettings.LatencyMode <- GCLatencyMode.SustainedLowLatency
+    // GCSettings.LatencyMode <- GCLatencyMode.SustainedLowLatency
 
-let [<Literal>] StdlibPath = "../../Stdlib/Stdlib.aml"
+    let [<Literal>] StdlibPath = "../../Stdlib/Stdlib.aml"
 
-let echoIntroduction () =
-    Console.ForegroundColor <- NormalConsoleColor
-    Console.WriteLine "Welcome to Aml (A Modular Language)."
-    Console.WriteLine "Designed and implemented by Bryan Edds, 2012-2013."
-    Console.WriteLine ()
-    Console.WriteLine "NOTE: The best way to learn Aml currently is to study the *.aml files in"
-    Console.WriteLine "the Stdlib folder, and to read AmlSpec.rtf in the Documentation folder."
-    Console.WriteLine ()
-    Console.WriteLine ("Current version is " + strf AmlVersion "F3" + ".")
-    Console.WriteLine ()
-    Console.WriteLine "Loading Stdlib..."
+    let echoIntroduction () =
+        Console.ForegroundColor <- NormalConsoleColor
+        Console.WriteLine "Welcome to Aml (A Modular Language)."
+        Console.WriteLine "Designed and implemented by Bryan Edds, 2012-2013."
+        Console.WriteLine ()
+        Console.WriteLine "NOTE: The best way to learn Aml currently is to study the *.aml files in"
+        Console.WriteLine "the Stdlib folder, and to read AmlSpec.rtf in the Documentation folder."
+        Console.WriteLine ()
+        Console.WriteLine ("Current version is " + strf AmlVersion "F3" + ".")
+        Console.WriteLine ()
+        Console.WriteLine "Loading Stdlib..."
 
-let echoStdlibSuccess () =
-    Console.WriteLine "Stdlib loaded!"
-    Console.WriteLine ()
+    let echoStdlibSuccess () =
+        Console.WriteLine "Stdlib loaded!"
+        Console.WriteLine ()
 
-let echoStdlibFailure (message : string) =
-    Console.ForegroundColor <- ErrorConsoleColor
-    Console.WriteLine "Stdlib failed to load due to:"
-    Console.WriteLine message
-    Console.WriteLine ()
-    Console.ForegroundColor <- NormalConsoleColor
+    let echoStdlibFailure (message : string) =
+        Console.ForegroundColor <- ErrorConsoleColor
+        Console.WriteLine "Stdlib failed to load due to:"
+        Console.WriteLine message
+        Console.WriteLine ()
+        Console.ForegroundColor <- NormalConsoleColor
 
-let echoPrompt () =
-    Console.WriteLine "Enter Aml expression(s) at the prompt or '?quit' to quit."
+    let echoPrompt () =
+        Console.WriteLine "Enter Aml expression(s) at the prompt or '?quit' to quit."
 
-let loadStdlib () =
-    let stdlibEvalResult = evalUsingFile (makeInitialEnv ()) (makeUsingFileRecord StdlibPath false None)
-    match stdlibEvalResult.Value with
-    | Violation violation -> echoStdlibFailure violation.VioMessage.SVValue
-    | _ -> echoStdlibSuccess ()
-    echoPrompt ()
-    stdlibEvalResult
+    let loadStdlib () =
+        let stdlibEvalResult = evalUsingFile (makeInitialEnv ()) (makeUsingFileRecord StdlibPath false None)
+        match stdlibEvalResult.Value with
+        | Violation violation -> echoStdlibFailure violation.VioMessage.SVValue
+        | _ -> echoStdlibSuccess ()
+        echoPrompt ()
+        stdlibEvalResult
 
-let [<EntryPoint>] main _ =
-    echoIntroduction ()
-    let stdlibEvalResult = loadStdlib ()
-    repl stdlibEvalResult.Env
-    0
+    let [<EntryPoint>] main _ =
+        echoIntroduction ()
+        let stdlibEvalResult = loadStdlib ()
+        repl stdlibEvalResult.Env
+        0
