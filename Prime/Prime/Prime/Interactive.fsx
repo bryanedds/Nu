420b151a19a9ce566f6df45f4d3c14410c2ca2a0
Bryan Edds <bryanedds@gmail.com>
2014-04-25 23:37:19 -0400

    Moved OmniBlade folder locally.

--- Prime/Prime/Prime/Interactive.fsx
+++ Prime/Prime/Prime/Interactive.fsx
@@ -2,8 +2,8 @@
 // Copyright (C) Bryan Edds, 2012-2014.
 
 (* NOTE: change these paths to make this script run with your Prime installation. *)
-#r "C:/OmniBlade/Prime/xUnit/xunit.dll"
-#r "C:/OmniBlade/Prime/FSharpx.Core/FSharpx.Core.dll"
+#r "C:/Development/OmniBlade/Prime/xUnit/xunit.dll"
+#r "C:/Development/OmniBlade/Prime/FSharpx.Core/FSharpx.Core.dll"
 
 #load "ReferenceEquality.fs"
 #load "Miscellanea.fs"
