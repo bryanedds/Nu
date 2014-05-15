dcaef6fc5b370801d0974ece68a3322cf5109d72
Bryan Edds <bryanedds@gmail.com>
2014-05-15 14:33:59 -0400

    Added BlazeGameDispatcher placeholder.

---
+++ BlazeVector/BlazeVector/BlazeVector/BlazeDispatchers.fs
@@ -0,0 +1,12 @@
+﻿namespace BlazeVector
+open OpenTK
+open Prime
+open Nu
+
+[<AutoOpen>]
+module BlazeDispatchersModule =
+
+    /// The custom type for BlazeVector's game dispatcher.
+    /// Currently just a placeholder as it doesn't yet have any special implementation.
+    type BlazeGameDispatcher () =
+        inherit GameDispatcher ()
