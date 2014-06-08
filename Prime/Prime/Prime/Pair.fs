c54faba5afbeda93eab4af22adc32bf217b1e897
Bryan Edds <bryanedds@gmail.com>
2014-06-07 22:01:50 -0400

    Added some composable pair / triple functions.
    Former-commit-id: 3c85b72879bb00184013b503abfa2385d8aa8604

---
+++ Prime/Prime/Prime/Pair.fs
@@ -0,0 +1,21 @@
+﻿// Prime - A PRIMitivEs code library.
+// Copyright (C) Bryan Edds, 2012-2014.
+
+namespace Prime
+
+[<AutoOpen>]
+module Pair =
+
+    /// The first item in a pair.
+    let inline a_ pair =
+        match pair with
+        | (a, _) -> a
+    
+    /// The second item in a pair.
+    let inline _b pair =
+        match pair with
+        | (_, b) -> b
+    
+    /// Make a pair.
+    let make a b =
+        (a, b)
