8d77518892f4034e3c6b9dd8a6cc732f93c54911
Bryan Edds <bryanedds@gmail.com>
2014-06-10 18:34:39 -0400

    Added multiple sections to BlazeVector. Added Xtension derivation query.
    Former-commit-id: 9de6262b2786dee613e9060b87e8fd9aec68aa15

--- BlazeVector/BlazeVector/BlazeVector/BlazeDispatchers.fs
+++ BlazeVector/BlazeVector/BlazeVector/BlazeDispatchers.fs
@@ -15,45 +15,47 @@ module BlazeDispatchersModule =
     type BlazeStageGroupDispatcher () =
         inherit GroupDispatcher ()
 
-        let getAvatarAddress groupAddress =
-            groupAddress @ [BlazeConstants.StageAvatarName]
+        let getCharacterAddress groupAddress =
+            groupAddress @ [BlazeConstants.StageCharacterName]
 
-        let getAvatar groupAddress world =
-            let avatarAddress = getAvatarAddress groupAddress
-            get world <| World.worldEntity avatarAddress
+        let getCharacter groupAddress world =
+            let characterAddress = getCharacterAddress groupAddress
+            get world <| World.worldEntity characterAddress
 
-        let withAvatar fn groupAddress world =
-            let avatarAddress = getAvatarAddress groupAddress
-            World.withEntity fn avatarAddress world
+        let withCharacter fn groupAddress world =
+            let characterAddress = getCharacterAddress groupAddress
+            World.withEntity fn characterAddress world
 
         let adjustCamera groupAddress world =
-            let avatar = getAvatar groupAddress world
-            let eyeCenter = Vector2 (avatar.Position.X + avatar.Size.X * 0.5f, world.Camera.EyeCenter.Y)
+            let character = getCharacter groupAddress world
+            let eyeCenter = Vector2 (character.Position.X + character.Size.X * 0.5f, world.Camera.EyeCenter.Y)
             { world with Camera = { world.Camera with EyeCenter = eyeCenter }}
 
         let adjustCameraHandler _ _ groupAddress message world =
             (message, true, adjustCamera groupAddress world)
 
-        let moveAvatarHandler _ _ groupAddress message world =
-            let avatar = getAvatar groupAddress world
-            let applyImpulseMessage = { PhysicsId = avatar.PhysicsId; Impulse = Vector2 (100.0f, 0.0f) }
-            let world' = { world with PhysicsMessages = ApplyImpulseMessage applyImpulseMessage :: world.PhysicsMessages }
+        let moveCharacterHandler _ _ groupAddress message world =
+            let character = getCharacter groupAddress world
+            let linearVelocity = Physics.getLinearVelocity character.PhysicsId world.Integrator
+            let linearVelocityDesired = Vector2 (100.0f, linearVelocity.Y)
+            let setLinearVelocityMessage = { PhysicsId = character.PhysicsId; LinearVelocity = linearVelocityDesired }
+            let world' = { world with PhysicsMessages = SetLinearVelocityMessage setLinearVelocityMessage :: world.PhysicsMessages }
             (message, true, world')
 
-        let jumpAvatarHandler _ _ groupAddress message world =
-            let avatar = getAvatar groupAddress world
-            if not <| Physics.isBodyOnGround avatar.PhysicsId world.Integrator then (message, true, world)
+        let jumpCharacterHandler _ _ groupAddress message world =
+            let character = getCharacter groupAddress world
+            if not <| Physics.isBodyOnGround character.PhysicsId world.Integrator then (message, true, world)
             else
-                let applyImpulseMessage = { PhysicsId = avatar.PhysicsId; Impulse = Vector2 (0.0f, 10000.0f) }
-                let world' = { world with PhysicsMessages = ApplyImpulseMessage applyImpulseMessage :: world.PhysicsMessages }
+                let applyLinearImpulseMessage = { PhysicsId = character.PhysicsId; LinearImpulse = Vector2 (0.0f, 3000.0f) }
+                let world' = { world with PhysicsMessages = ApplyLinearImpulseMessage applyLinearImpulseMessage :: world.PhysicsMessages }
                 (message, true, world')
 
         override dispatcher.Register (group, address, entities, world) =
             let world' =
                 world |>
-                World.subscribe NuConstants.TickEvent address -<| CustomSub moveAvatarHandler |>
+                World.subscribe NuConstants.TickEvent address -<| CustomSub moveCharacterHandler |>
                 World.subscribe NuConstants.TickEvent address -<| CustomSub adjustCameraHandler |>
-                World.subscribe NuConstants.DownMouseRightEvent address -<| CustomSub jumpAvatarHandler
+                World.subscribe NuConstants.DownMouseRightEvent address -<| CustomSub jumpCharacterHandler
             let world'' = base.Register (group, address, entities, world')
             adjustCamera address world''
 
@@ -68,12 +70,27 @@ module BlazeDispatchersModule =
     type BlazeStageScreenDispatcher () =
         inherit ScreenDispatcher ()
 
+        let shiftEntities xShift entities world =
+            List.map
+                (fun (entity : Entity) ->
+                    if Xtension.derivesFrom typeof<Entity2dDispatcher> entity.Xtension world then entity
+                    else entity.SetPosition <| entity.Position + Vector2 (xShift, 0.0f))
+                entities
+
+        let makeSectionFromFile fileName sectionName xShift world =
+            let (sectionGroup, sectionEntities) = World.loadGroupFromFile fileName true world
+            let sectionEntities' = shiftEntities xShift sectionEntities world
+            (sectionName, sectionGroup, sectionEntities')
+
         override dispatcher.Register (screen, address, groupDescriptors, world) =
-            let stagePlayGroup = World.loadGroupFile BlazeConstants.StagePlayFileName true world
-            let section0Group = World.loadGroupFile BlazeConstants.Section0FileName true world
-            let stagePlayGroupDescriptor = Triple.prepend BlazeConstants.StagePlayName stagePlayGroup
-            let section0GroupDescriptor = Triple.prepend BlazeConstants.Section0Name section0Group
-            let groupDescriptors' = stagePlayGroupDescriptor :: section0GroupDescriptor :: groupDescriptors
+            let stagePlay = World.loadGroupFromFile BlazeConstants.StagePlayFileName true world
+            let stagePlayDescriptor = Triple.prepend BlazeConstants.StagePlayName stagePlay
+            let sectionDescriptor0 = makeSectionFromFile BlazeConstants.Section0FileName BlazeConstants.Section0Name 0.0f world
+            let sectionDescriptor1 = makeSectionFromFile BlazeConstants.Section1FileName BlazeConstants.Section1Name 2048.0f world
+            let sectionDescriptor2 = makeSectionFromFile BlazeConstants.Section2FileName BlazeConstants.Section2Name 4096.0f world
+            let sectionDescriptor3 = makeSectionFromFile BlazeConstants.Section3FileName BlazeConstants.Section3Name 6144.0f world
+            let sectionDescriptors = [sectionDescriptor0; sectionDescriptor1; sectionDescriptor2; sectionDescriptor3]
+            let groupDescriptors' = stagePlayDescriptor :: sectionDescriptors @ groupDescriptors
             base.Register (screen, address, groupDescriptors', world)
 
         override dispatcher.Unregister (screen, address, world) =
