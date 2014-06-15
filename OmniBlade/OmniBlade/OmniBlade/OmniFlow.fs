35484be0cf61f0fad8e7b615c1d77eb573a09f77
Bryan Edds <bryanedds@gmail.com>
2014-06-14 20:34:56 -0400

    Removed use of ' and _ variable name suffixes in editor and game play code. Made PhysicsId into a value type.
    Former-commit-id: 35767b6e48fa0d7e6b8a06d2687be30aa9dcbfb8

--- OmniBlade/OmniBlade/OmniBlade/OmniFlow.fs
+++ OmniBlade/OmniBlade/OmniBlade/OmniFlow.fs
@@ -7,23 +7,23 @@ open OmniBlade.OmniConstants
 module OmniFlow =
 
     let addTitleScreen world =
-        let world_ = World.addDissolveScreenFromFile typeof<ScreenDispatcher>.Name TitleGroupFileName (List.last TitleGroupAddress) IncomingTime OutgoingTime TitleAddress true world
-        let world_ = World.subscribe ClickTitleNewGameEvent [] (ScreenTransitionSub FieldAddress) world_
-        let world_ = World.subscribe ClickTitleLoadGameEvent [] (ScreenTransitionSub LoadGameAddress) world_
-        let world_ = World.subscribe ClickTitleCreditsEvent [] (ScreenTransitionSub CreditsAddress) world_
-        World.subscribe ClickTitleExitEvent [] ExitSub world_
+        let world = World.addDissolveScreenFromFile typeof<ScreenDispatcher>.Name TitleGroupFileName (List.last TitleGroupAddress) IncomingTime OutgoingTime TitleAddress true world
+        let world = World.subscribe ClickTitleNewGameEvent [] (ScreenTransitionSub FieldAddress) world
+        let world = World.subscribe ClickTitleLoadGameEvent [] (ScreenTransitionSub LoadGameAddress) world
+        let world = World.subscribe ClickTitleCreditsEvent [] (ScreenTransitionSub CreditsAddress) world
+        World.subscribe ClickTitleExitEvent [] ExitSub world
 
     let addLoadGameScreen world =
-        let world' = World.addDissolveScreenFromFile typeof<ScreenDispatcher>.Name LoadGameGroupFileName (List.last LoadGameGroupAddress) IncomingTime OutgoingTime LoadGameAddress true world
-        World.subscribe ClickLoadGameBackEvent [] (ScreenTransitionSub TitleAddress) world'
+        let world = World.addDissolveScreenFromFile typeof<ScreenDispatcher>.Name LoadGameGroupFileName (List.last LoadGameGroupAddress) IncomingTime OutgoingTime LoadGameAddress true world
+        World.subscribe ClickLoadGameBackEvent [] (ScreenTransitionSub TitleAddress) world
 
     let addCreditsScreen world =
-        let world' = World.addDissolveScreenFromFile typeof<ScreenDispatcher>.Name CreditsGroupFileName (List.last CreditsGroupAddress) IncomingTime OutgoingTime CreditsAddress true world
-        World.subscribe ClickCreditsBackEvent [] (ScreenTransitionSub TitleAddress) world'
+        let world = World.addDissolveScreenFromFile typeof<ScreenDispatcher>.Name CreditsGroupFileName (List.last CreditsGroupAddress) IncomingTime OutgoingTime CreditsAddress true world
+        World.subscribe ClickCreditsBackEvent [] (ScreenTransitionSub TitleAddress) world
 
     let addFieldScreen world =
-        let world' = World.addDissolveScreenFromFile typeof<ScreenDispatcher>.Name FieldGroupFileName (List.last FieldGroupAddress) IncomingTime OutgoingTime FieldAddress true world
-        World.subscribe ClickFieldBackEvent [] (ScreenTransitionSub TitleAddress) world'
+        let world = World.addDissolveScreenFromFile typeof<ScreenDispatcher>.Name FieldGroupFileName (List.last FieldGroupAddress) IncomingTime OutgoingTime FieldAddress true world
+        World.subscribe ClickFieldBackEvent [] (ScreenTransitionSub TitleAddress) world
 
     let tryMakeOmniBladeWorld sdlDeps extData =
         let gameDispatcher = OmniGameDispatcher () :> obj
@@ -32,15 +32,15 @@ module OmniFlow =
         | Left _ as left -> left
         | Right world ->
             let hintRenderPackageUse = HintRenderingPackageUse { FileName = NuConstants.AssetGraphFileName; PackageName = OmniGuiPackageName } 
-            let world_ = { world with RenderMessages = hintRenderPackageUse :: world.RenderMessages }
+            let world = { world with RenderMessages = hintRenderPackageUse :: world.RenderMessages }
             let gameSong = { SongAssetName = "Song"; PackageName = NuConstants.DefaultPackageName; PackageFileName = NuConstants.AssetGraphFileName }
             let playSongMessage = PlaySong { Song = gameSong; FadeOutCurrentSong = true }
-            let world_ = { world_ with AudioMessages = playSongMessage :: world_.AudioMessages }
+            let world = { world with AudioMessages = playSongMessage :: world.AudioMessages }
             let splashScreenSprite = { SpriteAssetName = "Image5"; PackageName = NuConstants.DefaultPackageName; PackageFileName = NuConstants.AssetGraphFileName }
-            let world_ = World.addSplashScreenFromData (ScreenTransitionSub TitleAddress) SplashAddress typeof<ScreenDispatcher>.Name IncomingTimeSplash IdlingTime OutgoingTimeSplash splashScreenSprite world_
-            let world_ = addTitleScreen world_
-            let world_ = addLoadGameScreen world_
-            let world_ = addCreditsScreen world_
-            let world_ = addFieldScreen world_
-            let world_ = World.transitionScreen SplashAddress world_
-            Right world_
+            let world = World.addSplashScreenFromData (ScreenTransitionSub TitleAddress) SplashAddress typeof<ScreenDispatcher>.Name IncomingTimeSplash IdlingTime OutgoingTimeSplash splashScreenSprite world
+            let world = addTitleScreen world
+            let world = addLoadGameScreen world
+            let world = addCreditsScreen world
+            let world = addFieldScreen world
+            let world = World.transitionScreen SplashAddress world
+            Right world
