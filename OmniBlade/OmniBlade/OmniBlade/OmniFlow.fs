cc7d51981fab0c787d371dfe19535d4ab0028b48
bryanedds <bryanedds@gmail.com>
2014-06-30 02:23:38 -0400

    Added granularity to 'interactivity' concept. Slight editor usability improvements.
    Former-commit-id: e8f9ce3b43d6bb3b1e8df5cd0ce9be01a1ab9ded

--- OmniBlade/OmniBlade/OmniBlade/OmniFlow.fs
+++ OmniBlade/OmniBlade/OmniBlade/OmniFlow.fs
@@ -28,7 +28,7 @@ module OmniFlow =
 
     let tryMakeOmniBladeWorld sdlDeps extData =
         let gameDispatcher = OmniBladeDispatcher () :> obj
-        let optWorld = World.tryMakeEmpty sdlDeps gameDispatcher true extData
+        let optWorld = World.tryMakeEmpty sdlDeps gameDispatcher GuiAndPhysics extData
         match optWorld with
         | Left _ as left -> left
         | Right world ->
