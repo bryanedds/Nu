87b2e3be73186df8d876e1b99b75b056041dbb7d
bryanedds <bryanedds@gmail.com>
2014-09-03 13:18:11 -0400

    Better organized input code.

--- Nu/Nu/Nu/Input.fs
+++ Nu/Nu/Nu/Input.fs
@@ -17,18 +17,47 @@ module MouseButtonModule =
             | MouseCenter -> "Center"
             | MouseRight -> "Right"
 
-module MouseButton =
+[<RequireQualifiedAccess>]
+module MouseState =
 
-    let toSdl mouseButton =
+    /// Convert a MouseButton to SDL's representation.
+    let toSdlButton mouseButton =
         match mouseButton with
         | MouseLeft -> SDL.SDL_BUTTON_LEFT
         | MouseCenter -> SDL.SDL_BUTTON_MIDDLE
         | MouseRight -> SDL.SDL_BUTTON_RIGHT
 
-    let toNu mouseButton =
+    /// Convert SDL's representation of a mouse button to a MouseButton.
+    let toNuButton mouseButton =
         match mouseButton with
         | SDL2.SDL.SDL_BUTTON_LEFT -> MouseLeft
         | SDL2.SDL.SDL_BUTTON_MIDDLE -> MouseCenter
         | SDL2.SDL.SDL_BUTTON_RIGHT -> MouseRight
         | _ -> failwith "Invalid SDL mouse button."
 
+    /// Query that the given mouse button is down.
+    let isButtonDown mouseButton =
+        let sdlMouseButton = toSdlButton mouseButton
+        SDL.SDL_BUTTON sdlMouseButton = 1u
+
+    /// Get the position of the mouse.
+    let getPosition () =
+        let x = ref 0
+        let y = ref 0
+        ignore <| SDL.SDL_GetMouseState (x, y)
+        Vector2I (!x, !y)
+
+    /// Get the position of the mouse in floating-point coordinates.
+    let getPositionF world =
+        let mousePosition = getPosition world
+        Vector2 (single mousePosition.X, single mousePosition.Y)
+
+[<RequireQualifiedAccess>]
+module KeyboardState =
+
+    /// Query that the given keyboard key is down.
+    let isKeyDown scanCode =
+        let keyboardStatePtr = SDL.SDL_GetKeyboardState (ref 0)
+        let keyboardStatePtr = NativeInterop.NativePtr.ofNativeInt keyboardStatePtr
+        let state = NativeInterop.NativePtr.get<byte> keyboardStatePtr scanCode
+        state = byte 1
