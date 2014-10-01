4d50a254fe9ad0d36819e2aa4be9ad9163548b5f
bryanedds <bryanedds@gmail.com>
2014-09-30 23:47:53 -0400

    Renamed React.using to unto.
    Former-commit-id: 345bfc852127e3215ce6516f22f6743914ce18aa

---
+++ Nu/Nu/Nu/ReactTests.fs
@@ -0,0 +1,74 @@
+﻿namespace Nu
+open System
+open System.IO
+open System.Xml
+open SDL2
+open Xunit
+open Prime
+open Nu
+open Nu.Constants
+module ReactTests =
+
+    let TestEventName = !* "Test"
+    let incUserStateAndPropagate _ world = (Propagate, World.transformUserState incI world)
+    let incUserStateAndResolve _ world = (Resolved, World.transformUserState incI world)
+
+    let [<Fact>] subscribeWorks () =
+        World.init ()
+        let world = World.makeEmpty 0
+        let reactor = React.subscribe TestEventName ^^ React.unto Address.empty incUserStateAndPropagate world
+        let world = World.publish4 TestEventName Address.empty (NoData ()) reactor.World
+        Assert.Equal (1, World.getUserState world)
+
+    let [<Fact>] unsubscribeWorks () =
+        World.init ()
+        let world = World.makeEmpty 0
+        let reactor = React.subscribe TestEventName ^^ React.unto Address.empty incUserStateAndPropagate world
+        let world = reactor.Unsubscriber reactor.World
+        let world = World.publish4 TestEventName Address.empty (NoData ()) world
+        Assert.True <| Map.isEmpty world.Callbacks.Subscriptions
+        Assert.Equal (0, World.getUserState world)
+
+    let [<Fact>] filterWorks () =
+        World.init ()
+        let world = World.makeEmpty 0
+        let reactor =
+            React.filter (fun _ world -> World.getUserState world = 0) ^^
+            React.unto Address.empty incUserStateAndPropagate world
+        let reactor = React.subscribe TestEventName reactor
+        let reactor = React.subscribe TestEventName reactor
+        let world = World.publish4 TestEventName Address.empty (NoData ()) reactor.World
+        Assert.Equal (1, World.getUserState world)
+
+    let [<Fact>] mapWorks () =
+        World.init ()
+        let world = World.makeEmpty 0
+        let reactor =
+            React.subscribe TestEventName ^^
+            React.map React.unwrapV ^^
+            React.unto Address.empty (fun a world -> (Propagate, World.setUserState a world)) world
+        let world = World.publish4 TestEventName Address.empty (UserData { UserValue = 0 }) reactor.World
+        Assert.Equal (0, World.getUserState world)
+
+    let [<Fact>] scanWorks () =
+        World.init ()
+        let world = World.makeEmpty 0
+        let reactor =
+            React.subscribe TestEventName ^^
+            React.map React.unwrapV ^^
+            React.scan (fun b a _ -> b + a) 0 ^^
+            React.unto Address.empty (fun a world -> (Propagate, World.setUserState a world)) world
+        let world = World.publish4 TestEventName Address.empty (UserData { UserValue = 1 }) reactor.World
+        let world = World.publish4 TestEventName Address.empty (UserData { UserValue = 2 }) world
+        Assert.Equal (3, World.getUserState world)
+
+    let [<Fact>] scanDoesntLeaveGarbage () =
+        World.init ()
+        let world = World.makeEmpty 0
+        let reactor =
+            React.subscribe TestEventName ^^
+            React.scan2 (fun a _ _ -> a) ^^
+            React.unto Address.empty (fun _ world -> (Propagate, world)) world
+        let world = World.publish4 TestEventName Address.empty (NoData ()) reactor.World
+        let world = reactor.Unsubscriber world
+        Assert.True <| Map.isEmpty world.Callbacks.CallbackStates
