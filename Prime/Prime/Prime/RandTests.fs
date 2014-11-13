e5d11333d34c02b4b8a086d28b45f63455ee97b2
bryanedds <bryanedds@gmail.com>
2014-11-13 15:55:41 -0500

    Added Rand to Prime.

---
+++ Prime/Prime/Prime/RandTests.fs
@@ -0,0 +1,30 @@
+﻿namespace Prime
+open System
+open Xunit
+open Prime
+module RandTests =
+
+    let Samples = 32768
+
+    let makeSamples (next : Rand -> ('n * Rand)) =
+        let rand = ref <| Rand.makeDefault ()
+        [for _ in 0 .. Samples - 1 do
+            let (n, r) = next !rand
+            rand := r
+            yield n]
+
+    let [<Fact>] nextDoubleIsInRange () =
+        let samples = makeSamples (fun rand -> Rand.nextDouble rand)
+        let avg = List.average samples
+        Assert.InRange (avg, 0.49, 0.51)
+
+    let [<Fact>] nextSingleIsInRange () =
+        let samples = makeSamples (fun rand -> Rand.nextSingle rand)
+        let avg = List.average samples
+        Assert.InRange (avg, 0.49f, 0.51f)
+
+    let [<Fact>] nextIntIsInRange () =
+        let samples = makeSamples (fun rand -> Rand.nextInt rand)
+        let sampleDoubles = List.map double samples
+        let avg = List.average sampleDoubles
+        Assert.InRange (avg, 1003741823.0, 1143741823.0)
