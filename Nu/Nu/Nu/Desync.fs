b7cc714d97792b38a23c4914b7f0d6757f2e8fc2
bryanedds <bryanedds@gmail.com>
2014-12-06 01:52:53 -0500

    First semi-successful pass at Desync.
    Former-commit-id: fd43894b66263e11a1023e83a7a64af18dac282b

--- Nu/Nu/Nu/Desync.fs
+++ Nu/Nu/Nu/Desync.fs
@@ -4,51 +4,39 @@ open Nu
 [<AutoOpen>]
 module DesyncModule =
 
-    // desync zero
-    let zero () = []
+    let internal desyncBind (m : 'a list) (f : unit -> 'a list) : 'a list = m @ f ()
+    let internal desyncReturn op = [op]
+    let internal desyncReturnFrom op = op
+    let internal desyncZero () = []
+    let rec internal desyncCombine m n = m @ n
 
-    // essentially the desync constructor
-    let returnM op = [op]
-
-    // seems like it would be hard to get this wrong...
-    let returnFrom op = op
-
-    // this works, but is not really bind due to no 'b type...
-    let bind (m : 'a list) (f : unit -> 'a list) : 'a list =
-        m @ f ()
-
-    let forM (ts : int seq) (f : int -> 'a list) : 'a list =
-        Seq.fold (fun m t -> m @ f t) [] ts
-
-    let rec combine m n =
-        m @ n
-
-    let call op =
-        returnM op
-
-    // NOTE: this is NOT general to Desync!
-    let pass () =
-        call (fun (_, world) -> (Cascade, world))
-
+    /// The desynchronous computation expression builder.
     type DesyncBuilder () =
+        member this.Bind (m, f) = desyncBind m f
+        member this.Return op = desyncReturn op
+        member this.ReturnFrom op = desyncReturnFrom op
+        member this.Zero () = desyncZero ()
+        member this.Combine (m, n) = desyncCombine m n
 
-        member this.Bind (m, f) = bind m f
-        member this.Return op = returnM op
-        member this.ReturnFrom op = returnFrom op
-        member this.For (ops, f) = forM ops f
-        member this.Combine (m, n) = combine m n
-        member this.Yield op = returnM op
-        member this.Zero () = zero ()
-        member this.Delay f = (fun x -> f x)
+    /// The global desync builder instance.
+    let desync = DesyncBuilder ()
 
-    let desync =
-        DesyncBuilder ()
-
-[<RequireQualifiedAccess>]
 module Desync =
 
-    /// Integrates desync with Nu's purely functional event system.
-    let run shouldAdvance (observable : Observable<'a, 'o>) (desync : ('a Event * World -> EventHandling * World) list) world =
+    /// Invoke an operation in the context of desynchronization.
+    let call op = desync.Return op
+
+    /// Skip in the context of desynchronization.
+    let skip () = desyncZero ()
+
+    /// Pass in the context of desynchronization.
+    let pass () = call World.handleAsPass
+
+    /// Loop in the context of desynchronization.
+    let loop (ts : int seq) (f : int -> 'a list) : 'a list =
+        Seq.fold (fun m t -> m @ f t) [] ts
+
+    let private run4 makeSubscription (observable : Observable<'a, 'o>) desync world =
         if not <| List.isEmpty desync then
             let callbackKey = World.makeCallbackKey ()
             let world = World.addCallbackState callbackKey desync world
@@ -58,19 +46,66 @@ module Desync =
                 let world = World.removeCallbackState callbackKey world
                 let world = unsubscribe world
                 World.unsubscribe subscriptionKey world
-            let subscription =
-                fun event world ->
-                    let desync = World.getCallbackState callbackKey world
-                    match desync with
-                    | [] -> failwith "Invalid desync value in-flight."
-                    | head :: tail ->
-                        let (eventHandling, world) = head (event, world)
-                        let world =
-                            if shouldAdvance event then // perhaps also pass world?
-                                if not <| List.isEmpty tail then World.addCallbackState callbackKey tail world
-                                else unsubscribe world
-                            else world
-                        (eventHandling, world)
+            let subscription = makeSubscription unsubscribe callbackKey
             let world = World.subscribe<'a, 'o> subscriptionKey subscription eventAddress observable.ObserverAddress world
             (unsubscribe, world)
         else (id, world)
+
+    let run6 shouldAdvance advance eventHandling (observable : Observable<'a, 'o>) desync world =
+        let makeSubscription unsubscribe callbackKey =
+            fun event world ->
+                let desync = World.getCallbackState callbackKey world
+                match desync with
+                | [] -> failwith "Invalid desync value in-flight."
+                | head :: tail ->
+                    let world = advance event head world
+                    let world =
+                        if shouldAdvance event then // perhaps also pass world?
+                            if not <| List.isEmpty tail then World.addCallbackState callbackKey tail world
+                            else unsubscribe world
+                        else world
+                    (eventHandling, world)
+        run4 makeSubscription observable desync world
+
+    /// Run the given desynchronized process on top of Nu's event system.
+    /// Allows each desynchronized operation to reference its source event and specify its event
+    /// handling approach.
+    let runWithEventSpecifyingHandling shouldAdvance (observable : Observable<'a, 'o>) (desync : ('a Event -> World -> EventHandling * World) list) world =
+        let makeSubscription unsubscribe callbackKey =
+            fun event world ->
+                let desync = World.getCallbackState callbackKey world
+                match desync with
+                | [] -> failwith "Invalid desync value in-flight."
+                | head :: tail ->
+                    let (eventHandling, world) = head (event, world)
+                    let world =
+                        if shouldAdvance event then // perhaps also pass world?
+                            if not <| List.isEmpty tail then World.addCallbackState callbackKey tail world
+                            else unsubscribe world
+                        else world
+                    (eventHandling, world)
+        run4 makeSubscription observable desync world
+
+    /// Run the given desynchronized process on top of Nu's event system.
+    /// Allows each desynchronized operation to reference its source event without specifying its
+    /// event handling approach by assuming Cascade.
+    let runWithEventCascading shouldAdvance (observable : Observable<'a, 'o>) (desync : ('a Event -> World -> World) list) world =
+        run6 shouldAdvance (fun event desync world -> desync event world) Cascade observable desync world
+
+    /// Run the given desynchronized process on top of Nu's event system.
+    /// Allows each desynchronized operation to reference its source event without specifying its
+    /// event handling approach by assuming Resolve.
+    let runWithEventResolving shouldAdvance (observable : Observable<'a, 'o>) (desync : ('a Event -> World -> World) list) world =
+        run6 shouldAdvance (fun event desync world -> desync event world) Resolve observable desync world
+
+    /// Run the given desynchronized process on top of Nu's event system.
+    /// Allows each desynchronized operation to run without referencing its source event, and
+    /// without specifying its event handling approach by assuming Cascade.
+    let runCascading shouldAdvance (observable : Observable<'a, 'o>) (desync : (World -> World) list) world =
+        run6 shouldAdvance (fun _ desync world -> desync world) Cascade observable desync world
+
+    /// Run the given desynchronized process on top of Nu's event system.
+    /// Allows each desynchronized operation to run without referencing its source event, and
+    /// without specifying its event handling approach by assuming Resolve.
+    let runResolving shouldAdvance (observable : Observable<'a, 'o>) (desync : (World -> World) list) world =
+        run6 shouldAdvance (fun _ desync world -> desync world) Resolve observable desync world
--- Nu/Nu/Nu/DesyncTests.fs
+++ Nu/Nu/Nu/DesyncTests.fs
@@ -6,35 +6,31 @@ open Nu
 open Nu.Constants
 open Nu.WorldConstants
 open Nu.Observer
+open Nu.Desync
 module DesyncTests =
 
     let UnitEventAddress = stoa<unit> "Test"
-    let incUserState (_, world) = (Cascade, World.transformUserState inc world)
-    let incUserStateTwice (_, world) = (Cascade, World.transformUserState (inc >> inc) world)
+    let incUserState _ world = (Cascade, World.transformUserState inc world)
+    let incUserStateTwice _ world = (Cascade, World.transformUserState (inc >> inc) world)
 
     let [<Fact>] desyncWorks () =
         World.init ()
         let world = World.makeEmpty 0
         let obs = observe UnitEventAddress (atooa UnitEventAddress)
         
-        let proc2 =
-            desync.Bind (call incUserState, fun () ->
-                desync.For ([0 .. 1], fun _ ->
-                    desync.Bind (pass (), fun () -> desync.Return incUserState)))
-        ignore proc2
-        
         let proc =
             desync {
                 do! call incUserState
-                //for i in [0 .. 1] do (fun i -> pass ())
-                do! forM [0 .. 1] (fun i -> if i = 0 then pass () else call incUserState)
+                do! loop [0 .. 1] (fun i ->
+                    if i = 0 then call incUserState
+                    else pass ())
                 return incUserStateTwice }
-        
-        let world = snd <| Desync.run tautology obs (proc ()) world
+
+        let world = snd <| Desync.runWithEventSpecifyingHandling tautology obs proc world
         let world = World.publish4 () UnitEventAddress GameAddress world
         Assert.Equal (1, World.getUserState world)
         let world = World.publish4 () UnitEventAddress GameAddress world
-        Assert.Equal (1, World.getUserState world)
+        Assert.Equal (2, World.getUserState world)
         let world = World.publish4 () UnitEventAddress GameAddress world
         Assert.Equal (2, World.getUserState world)
         let world = World.publish4 () UnitEventAddress GameAddress world
