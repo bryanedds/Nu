64dd4c38ba1bf032dadb8c1f643e8a1aee52db7d
bryanedds <bryanedds@gmail.com>
2014-12-09 04:39:51 -0500

    I *think* I have Desync working...

--- Nu/Nu/Nu/Desync.fs
+++ Nu/Nu/Nu/Desync.fs
@@ -4,33 +4,36 @@ open Nu
 
 module Unsync =
 
-    type [<NoComparison; NoEquality>] Unsync<'s, 'a> =
-        Unsync of ('s -> 's * Either<Unsync<'s, 'a>, 'a>)
+    type [<NoComparison; NoEquality>] Unsync<'e, 's, 'a> =
+        Unsync of ('s -> 's * Either<'e -> Unsync<'e, 's, 'a>, 'a>)
 
-    let step (m : Unsync<'s, 'a>) (s : 's) : 's * Either<Unsync<'s, 'a>, 'a> =
+    let step (m : Unsync<'e, 's, 'a>) (s : 's) : 's * Either<'e -> Unsync<'e, 's, 'a>, 'a> =
         match m with Unsync f -> f s
 
-    let rec run (m : Unsync<'s, 'a>) (s : 's) : ('s * 'a) =
+    let advance (m : 'e -> Unsync<'e, 's, 'a>) (e : 'e) (s : 's) =
+        step (m e) s
+
+    let rec run (m : Unsync<'e, 's, 'a>) (e : 'e) (s : 's) : ('s * 'a) =
         match step m s with
-        | (s', Left m') -> run m' s'
+        | (s', Left m') -> run (m' e) e s'
         | (s', Right v) -> (s', v)
 
-    let get : Unsync<'s, 's> =
+    let get : Unsync<'e, 's, 's> =
         Unsync (fun s -> (s, Right s))
 
-    let put (s : 's) : Unsync<'s, unit> =
+    let put (s : 's) : Unsync<'e, 's, unit> =
         Unsync (fun _ -> (s, Right ()))
 
-    let wait m : Unsync<'s, 'a> =
-        Unsync (fun s -> (s, Left m))
+    let wait (k : 'e -> Unsync<'e, 's, 'a>) : Unsync<'e, 's, 'a> =
+        Unsync (fun s -> (s, Left k))
 
-    let returnM (a : 'a) : Unsync<'s, 'a> =
+    let returnM (a : 'a) : Unsync<'e, 's, 'a> =
         Unsync (fun s -> (s, Right a))
         
-    let rec bind (m : Unsync<'s, 'a>) (cont : 'a -> Unsync<'s, 'b>) : Unsync<'s, 'b> =
+    let rec bind (m : Unsync<'e, 's, 'a>) (cont : 'a -> Unsync<'e, 's, 'b>) : Unsync<'e, 's, 'b> =
         Unsync (fun s ->
             match step m s with
-            | (s', Left m') -> (s', Left (bind m' cont))
+            | (s', Left m') -> (s', Left (fun e -> bind (m' e) cont))
             | (s', Right v) -> step (cont v) s')
 
     type UnsyncBuilder () =
@@ -42,32 +45,30 @@ module Unsync =
         UnsyncBuilder ()
 
     let pass () =
-        wait <| returnM ()
+        wait <| (fun _ -> returnM ())
 
     let lift expr =
-        unsync {
-            let! s = get
-            let s = expr s
-            do! put s }
+        fun e ->
+            unsync {
+                let! s = get
+                let s = expr e s
+                do! put s }
 
     let respond expr =
-        wait <| lift expr
+        wait <| (fun e -> lift expr e)
 
     let rec loop state advance pred expr =
-        if pred state then
-            let state = advance state
-            unsync {
-                do! expr state
-                do! loop state advance pred expr }
-        else returnM ()
+        fun e ->
+            if pred state then
+                let state = advance state
+                unsync {
+                    do! expr e state
+                    do! loop state advance pred expr e }
+            else returnM ()
 
-    /// Loop in the context of desynchronization.
-    //let loop (ts : 't seq) (cont : 't -> 'a list) : 'a list =
-    //    Seq.fold (fun ops t -> ops @ cont t) [] ts
-
-    let private runDesync4 makeSubscription (observable : Observable<'a, 'o>) (desync : Unsync<World, unit>) world =
+    let private runDesync4 makeSubscription (observable : Observable<'a, 'o>) (desync : Unsync<'a Event, World, unit>) world =
         let callbackKey = World.makeCallbackKey ()
-        let world = World.addCallbackState callbackKey desync world
+        let world = World.addCallbackState callbackKey (fun (_ : 'a Event) -> desync) world
         let subscriptionKey = World.makeSubscriptionKey ()
         let (eventAddress, unsubscribe, world) = observable.Subscribe world
         let unsubscribe = fun world ->
@@ -78,12 +79,12 @@ module Unsync =
         let world = World.subscribe<'a, 'o> subscriptionKey subscription eventAddress observable.ObserverAddress world
         (unsubscribe, world)
 
-    let runDesync4' eventHandling (observable : Observable<'a, 'o>) desync world =
+    let runDesync4' eventHandling (observable : Observable<'a, 'o>) (desync : Unsync<'a Event, World, unit>) world =
         let makeSubscription unsubscribe callbackKey =
-            fun _ world ->
-                let desync = World.getCallbackState callbackKey world
-                let (world, desyncResult) = step desync world
-                match desyncResult with
+            fun event world ->
+                let desync = World.getCallbackState callbackKey world : 'a Event -> Unsync<'a Event, World, unit>
+                let (world, advanceResult) = advance desync event world
+                match advanceResult with
                 | Left desyncNext ->
                     let world = World.addCallbackState callbackKey desyncNext world
                     (eventHandling, world)
