6f5f2d4f74bdfa407f3e5c1cfe4aab67ae2ed4b0
bryanedds <bryanedds@gmail.com>
2014-12-10 04:08:59 -0500

    More work on Desync integration.

--- InfinityRpg/InfinityRpg/InfinityRpg/OccupationMap.fs
+++ InfinityRpg/InfinityRpg/InfinityRpg/OccupationMap.fs
@@ -1,19 +1,11 @@
 ﻿namespace InfinityRpg
 open System
 open OpenTK
+open Prime
 open Nu
 open InfinityRpg.Constants
 
 module OccupationMap =
-
-    let makeFromFieldTiles fieldTiles =
-        Map.fold
-            (fun occupationMap fieldTilePositionM fieldTile ->
-                match fieldTile.TileType with
-                | Impassable -> Map.add fieldTilePositionM true occupationMap
-                | Passable -> Map.add fieldTilePositionM false occupationMap)
-            Map.empty
-            fieldTiles
 
     let occupyByTurn characterTurn occupationMap =
         match characterTurn with
@@ -27,7 +19,10 @@ module OccupationMap =
     let occupyByCharacter (character : Entity) occupationMap =
         let characterPositionM = Vector2i (Vector2.Divide (character.Position, TileSize))
         Map.add characterPositionM true occupationMap
-            
+
+    let occupyByCharacters characters occupationMap =
+        List.fold (flip occupyByCharacter) occupationMap characters
+
     let occupyByAdjacentCharacter positionM (character : Entity) occupationMap =
         let characterPositionM = Vector2i (Vector2.Divide (character.Position, TileSize))
         if  characterPositionM = positionM + Vector2i.Up ||
@@ -36,13 +31,36 @@ module OccupationMap =
             characterPositionM = positionM + Vector2i.Left then
             Map.add characterPositionM true occupationMap
         else occupationMap
-            
+
+    let occupyByAdjacentCharacters positionM characters occupationMap =
+        List.fold (flip <| occupyByAdjacentCharacter positionM) occupationMap characters
+
     let unoccupyByCharacter (character : Entity) occupationMap =
         let characterPositionM = Vector2i (Vector2.Divide (character.Position, TileSize))
         Map.add characterPositionM false occupationMap
+
+    let unoccupyByCharacters characters occupationMap =
+        List.fold (flip unoccupyByCharacter) occupationMap characters
 
     let transferByTurn characterTurn character occupationMap =
         match characterTurn with
         | NavigationTurn _ -> unoccupyByCharacter character occupationMap |> occupyByTurn characterTurn
         | CancelTurn -> occupationMap
         | NoTurn -> occupationMap
+
+    let makeFromFieldTiles fieldTiles =
+        Map.fold
+            (fun occupationMap fieldTilePositionM fieldTile ->
+                match fieldTile.TileType with
+                | Impassable -> Map.add fieldTilePositionM true occupationMap
+                | Passable -> Map.add fieldTilePositionM false occupationMap)
+            Map.empty
+            fieldTiles
+
+    let makeFromFieldTilesAndCharacters fieldTiles characters =
+        let occupationMap = makeFromFieldTiles fieldTiles
+        occupyByCharacters characters occupationMap
+
+    let makeFromFieldTilesAndAdjacentCharacters positionM fieldTiles characters =
+        let occupationMap = makeFromFieldTiles fieldTiles
+        occupyByAdjacentCharacters positionM characters occupationMap
