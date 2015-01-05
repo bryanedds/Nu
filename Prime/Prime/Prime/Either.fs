4db135e59e0f8625607b4f525cd7e7c39bbf78b0
bryanedds <bryanedds@gmail.com>
2015-01-05 18:17:47 -0500

    Fixed Either implementation.
    Former-commit-id: 37eac9bf6f6fe5459681a5575d8d6ab0a2c20a0a

--- Prime/Prime/Prime/Either.fs
+++ Prime/Prime/Prime/Either.fs
@@ -15,88 +15,80 @@ module EitherModule =
         | Right of 'r
         | Left of 'l
 
-    /// Monadic bind.
-    let inline (>>=) either fn =
-        match either with
-        | Right r -> Right <| fn r
-        | Left _ -> either
+    /// Monadic return for Either.
+    let internal returnM r = Right r
 
-    /// Bind that allows indication of failure.
-    let inline (>>=?) either fn =
+    /// Monadic bind for Either.
+    let internal bind either fn =
         match either with
         | Right r -> fn r
-        | Left _ -> either
-
-    /// Bind that allows handling of failure.
-    let inline (>>=??) (either : Either<_, _>) fn : Either<_, _> = fn either
+        | Left l -> Left l
 
 module Either =
 
-    /// Monadic return.
-    let inline returnM r = Right r
-
-    /// Monadic returnFrom.
-    /// TODO: ensure this is defined correctly!
-    let inline returnFrom r = r
+    /// Monadic return for Either.
+    let returnM = returnM
+    
+    /// Monadic bind for Either.
+    let bind = bind
 
     /// Builds an either monad.
     type EitherBuilder () =
-        member this.Bind (either, fn) = either >>= fn
+        member this.Bind (either, fn) = bind either fn
         member this.Return r = returnM r
-        member this.ReturnFrom r = returnFrom r
 
-    /// The either monad.
+    /// The computation expression builder for Either.
     let either = EitherBuilder ()
 
-    /// Query whether the either is a Left value.
+    /// Query whether an Either value is a Left value.
     let isLeft either =
         match either with
         | Right _ -> false
         | Left _ -> true
     
-    /// Query whether the either is a Right value.
+    /// Query whether an Either value is a Right value.
     let isRight either =
         match either with
         | Right _ -> true
         | Left _ -> false
 
-    /// Get the Left value of an either, failing if not available.
+    /// Get the Left value of an Either value, failing if not available.
     let getLeftValue either =
         match either with
         | Right _ -> failwith "Could not get Left value from a Right value."
         | Left l -> l
 
-    /// Get the Right value of an either, failing if not available.
+    /// Get the Right value of an Either value, failing if not available.
     let getRightValue either =
         match either with
         | Right r -> r
         | Left _ -> failwith "Could not get Left value from a Right value."
 
-    /// Get only the Left values of a sequence of either.
+    /// Get only the Left values of a sequence of an Either value.
     let getLeftValues eithers =
         Seq.fold
             (fun lefts either -> match either with Right _ -> lefts | Left left -> left :: lefts)
             eithers
 
-    /// Get only the Right values of a sequence of either.
+    /// Get only the Right values of a sequence of an Either value.
     let getRightValues eithers =
         Seq.fold
             (fun rights either -> match either with Right _ -> rights | Left right -> right :: rights)
             eithers
 
-    /// Map over the left side of an either value.
+    /// Map over the left side of an Either value.
     let mapLeft mapper either =
         match either with
         | Right r -> Right r
         | Left l -> Left <| mapper l
 
-    /// Map over the right side of an either value.
+    /// Map over the right side of an Either value.
     let mapRight mapper either =
         match either with
         | Right r -> Right <| mapper r
         | Left l -> Left l
 
-    /// Split a sequences of eithers into a pair of left and right value lists.
+    /// Split a sequences of Either values into a pair of left and right value lists.
     let split eithers =
         Seq.fold
             (fun (ls, rs) either ->
