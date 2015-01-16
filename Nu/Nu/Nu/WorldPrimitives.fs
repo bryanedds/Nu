3c8951b4d0509d7d875eda51b7e1b8c42280aad7
bryanedds <bryanedds@gmail.com>
2015-01-16 16:34:12 -0500

    More work.

--- Nu/Nu/Nu/WorldPrimitives.fs
+++ Nu/Nu/Nu/WorldPrimitives.fs
@@ -1,11 +1,70 @@
 ﻿namespace Nu
 open System
-open System.Collections.Generic
-open FSharpx
 open Prime
 open Nu
 open Nu.Constants
 open Nu.WorldConstants
 
-[<RequireQualifiedAccess>]
-module World =
+[<AutoOpen>]
+module WorldAddressModule =
+
+    /// Convert any type of address to a simulant's address.
+    let atoua address = World.atoua address
+
+    /// Convert any type of address to a screen's address.
+    let atosa address = World.atosa address
+
+    /// Convert any type of address to a group's address.
+    let atoga address = World.atoga address
+
+    /// Convert any type of address to an entity's address.
+    let atoea address = World.atoea address
+
+    /// Convert a group's address to an entity's by appending the entity's name at the end.
+    let gatoea groupAddress entityName = World.gatoea groupAddress entityName
+
+    /// Convert a screen's address to a group's by appending the group's name at the end.
+    let satoga screenAddress groupName = World.satoga screenAddress groupName
+
+    /// Convert a screen's address to an entity's by appending the group and entity's names at the end.
+    let satoea screenAddress groupName entityName = World.satoea screenAddress groupName entityName
+
+    /// Convert an entity's address to a group's by removing the entity's name from the end.
+    let eatoga entityAddress = World.eatoga entityAddress
+
+    /// Convert a group's address to a screen's by removing the group's name from the end.
+    let gatosa groupAddress = World.gatosa groupAddress
+
+    /// Convert a entity's address to a screen's by removing the group and entity's names from the end.
+    let eatosa entityAddress = World.eatosa entityAddress
+
+[<AutoOpen>]
+module WorldInputModule =
+
+    type World with
+
+        /// Convert a MouseButton to SDL's representation.
+        static member internal toSdlMouseButton mouseButton =
+            MouseState.toSdlButton mouseButton
+
+        /// Convert SDL's representation of a mouse button to a MouseButton.
+        static member internal toNuMouseButton mouseButton =
+            MouseState.toNuButton mouseButton
+
+        /// Query that the given mouse button is down.
+        static member isMouseButtonDown mouseButton (_ : World) =
+            MouseState.isButtonDown mouseButton
+
+        /// Get the position of the mouse.
+        static member getMousePosition (_ : World) =
+            MouseState.getPosition ()
+
+        /// Get the position of the mouse in floating-point coordinates.
+        static member getMousePositionF (_ : World) =
+            MouseState.getPositionF ()
+
+        /// Query that the given keyboard key is down.
+        static member isKeyboardKeyDown scanCode (_ : World) =
+            KeyboardState.isKeyDown scanCode
+
+        // TODO: implement isKeyboardModifierActive.
