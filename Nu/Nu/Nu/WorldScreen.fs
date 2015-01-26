281a983cdbe28595d1cef67b5445a1e997273b7e
bryanedds <bryanedds@gmail.com>
2015-01-25 22:49:08 -0500

    Exposed a couple of methods that I previously meant to.
    Former-commit-id: d573f790b0ea2fb1ca88261966eac207f024cb3e

--- Nu/Nu/Nu/WorldScreen.fs
+++ Nu/Nu/Nu/WorldScreen.fs
@@ -46,14 +46,14 @@ module WorldScreenModule =
             
         /// Provides a view of all the properties of a screen. Useful for debugging such as with
         /// the Watch feature in Visual Studio.
-        member private this.ViewProperties world =
+        member this.ViewProperties world =
             let state = World.getScreenState this world
             let properties = Array.map (fun (property : PropertyInfo) -> (property.Name, (property.Name, property.GetValue state))) ((state.GetType ()).GetProperties ())
             Map.ofSeq properties
             
         /// Provides a view of all the xtension fields of a screen. Useful for debugging such as
         /// with the Watch feature in Visual Studio.
-        member private this.ViewXFields world =
+        member this.ViewXFields world =
             let state = World.getScreenState this world
             Map.map (fun name field -> (name, field.FieldValue)) state.Xtension.XFields
 
