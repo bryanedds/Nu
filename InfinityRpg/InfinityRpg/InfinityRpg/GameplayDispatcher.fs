d5ce0b5b651ab814ef6291fd5585ea0299c25efc
bryanedds <bryanedds@gmail.com>
2015-01-28 05:15:11 -0500

    Silly code change.

--- InfinityRpg/InfinityRpg/InfinityRpg/GameplayDispatcher.fs
+++ InfinityRpg/InfinityRpg/InfinityRpg/GameplayDispatcher.fs
@@ -403,7 +403,7 @@ module GameplayDispatcherModule =
             // NOTE: currently just implements attack
             let chain = chain {
                 do! update ^ character.SetActivityState ^ Action newActionDescriptor
-                do! during (fun world -> ActivityState.isActing <| character.GetActivityState world) ^ chain {
+                do! during (fun world -> ActivityState.isActing ^ character.GetActivityState world) ^ chain {
                     do! update ^ fun world ->
                         let actionDescriptor =
                             match character.GetActivityState world  with
