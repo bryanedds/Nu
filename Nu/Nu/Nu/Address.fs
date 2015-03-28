a293c582f2a9bfa50d10521886922e53f4ce2d4f
bryanedds <bryanedds@gmail.com>
2015-03-28 12:34:40 -0400

    More principle key equality for entity state caching.
    Former-commit-id: dd630a922ec3c872320b7b0b72e746dee36886e1

--- Nu/Nu/Nu/Address.fs
+++ Nu/Nu/Nu/Address.fs
@@ -42,66 +42,75 @@ module NameKey =
 module AddressModule =
 
     /// Specifies the address of an element in a game, or name of an event.
-    /// OPTIMIZATION: Comparison is done using a reversed list since the backs of addresses tend to
-    /// be much more unique than the fronts.
+    /// OPTIMIZATION: Comparison is done using the full string of names for speed.
     /// OPTIMIZATION: In the face of using a PersistentHashMap for simulant storage, I've made the
     /// NameKeys field available for faster look-ups.
     /// OPTIMIZATION: At little cost, I've also added the Hash field for fast keying directly
     /// on addresses.
-    /// TODO: There seem to be several low-hanging fruit for simple optimizations on this type's
-    /// operations. Consider pursuing them as soon as time permits.
     type [<CustomEquality; CustomComparison; TypeConverter (typeof<AddressConverter>)>] 'a Address =
-        { Names : string list
-          NamesRev : string list
+        { NamesStr : string
           NameKeys : NameKey list
           Hash : int
           TypeCarrier : 'a -> unit }
 
-        static member private join (list : string list) =
-            String.Join ("/", list)
+        static member internal join (seq : string seq) =
+            String.Join ("/", seq)
 
-        static member private split (str : string) =
+        static member internal split (str : string) =
             List.ofArray <| str.Split '/'
 
         /// Make an address from a list of names.
-        static member make list =
+        static member makeFromList list =
+            let str = Address<'a>.join list
             let keys = List.map NameKey.make list
-            let hash = List.fold (fun hash (key : NameKey) -> hash ^^^ key.Hash) 0 keys
-            { Names = list; NamesRev = List.rev list; NameKeys = keys; Hash = hash; TypeCarrier = fun (_ : 'a) -> () }
+            { NamesStr = str; NameKeys = keys; Hash = hash str; TypeCarrier = fun (_ : 'a) -> () }
+
+        /// Make an address from a list of names.
+        static member makeFromStrAndKeys str keys =
+            { NamesStr = str; NameKeys = keys; Hash = hash str; TypeCarrier = fun (_ : 'a) -> () }
+
+        /// Make an address from a list of names.
+        static member makeFromKeys keys =
+            let str = keys |> Seq.map (fun key -> key.Name) |> Address<'a>.join
+            Address<'a>.makeFromStrAndKeys str keys
+
+        static member makeFromString str =
+            let list = Address<'a>.split str
+            let keys = List.map NameKey.make list
+            { NamesStr = str; NameKeys = keys; Hash = hash str; TypeCarrier = fun (_ : 'a) -> () }
 
         /// Convert a string into a list.
         static member stoa (str : string) =
-            let list = Address<'a>.split str
-            Address<'a>.make list
+            Address<'a>.makeFromString str
 
         /// The empty address.
         static member empty =
-            Address<'a>.make []
+            { NamesStr = String.Empty; NameKeys = []; Hash = 0; TypeCarrier = fun (_ : 'a) -> () }
 
         interface 'a Address IComparable with
             member this.CompareTo that =
-                List.compareStrings this.NamesRev that.NamesRev
+                String.Compare (this.NamesStr, that.NamesStr)
 
         interface IComparable with
             member this.CompareTo that =
                 match that with
-                | :? ('a Address) as that -> List.compareStrings this.NamesRev that.NamesRev
+                | :? ('a Address) as that -> String.Compare (this.NamesStr, that.NamesStr)
                 | _ -> failwith "Invalid Address comparison (comparee not of type Address)."
 
         interface 'a Address IEquatable with
             member this.Equals that =
-                this.NamesRev = that.NamesRev
+                this.NamesStr = that.NamesStr
 
         override this.Equals that =
             match that with
-            | :? ('a Address) as that -> this.Names = that.Names
+            | :? ('a Address) as that -> this.NamesStr = that.NamesStr
             | _ -> false
 
         override this.GetHashCode () =
             this.Hash
         
         override this.ToString () =
-            Address<'a>.join this.Names
+            this.NamesStr
 
     /// Converts Address types.
     and AddressConverter (targetType : Type) =
@@ -131,13 +140,17 @@ module AddressModule =
                 if targetType.IsInstanceOfType source then source
                 else failwith "Invalid AddressConverter conversion from source."
 
-    /// Convert a string into a list.
+    /// Convert a string into an address.
     let stoa<'a> str =
         Address<'a>.stoa str
 
+    /// Convert a name key list into an address.
+    let ktoa<'a> keys =
+        Address<'a>.makeFromKeys keys
+
     /// Convert a list into an address.
     let ltoa<'a> list =
-        Address<'a>.make list
+        Address<'a>.makeFromList list
 
     /// Convert a single name into an address.
     let ntoa<'a> name =
@@ -145,17 +158,19 @@ module AddressModule =
 
     /// Convert any address to an obj Address.
     let atooa<'a> (address : 'a Address) =
-        { Names = address.Names; NamesRev = address.NamesRev; NameKeys = address.NameKeys; Hash = address.Hash; TypeCarrier = fun (_ : obj) -> () }
+        { NamesStr = address.NamesStr; NameKeys = address.NameKeys; Hash = address.Hash; TypeCarrier = fun (_ : obj) -> () }
 
     /// Concatenate two addresses of the same type.
     let acat<'a> (address : 'a Address) (address2 : 'a Address) =
-        let list = address.Names @ address2.Names
-        Address<'a>.make list
+        Address<'a>.makeFromStrAndKeys
+            (Address<'a>.join [address.NamesStr; address2.NamesStr])
+            (address.NameKeys @ address2.NameKeys)
 
     /// Concatenate two addresses, taking the type of first address.
     let acatf<'a> (address : 'a Address) (address2 : obj Address) =
-        let list = address.Names @ address2.Names
-        Address<'a>.make list
+        Address<'a>.makeFromStrAndKeys
+            (Address<'a>.join [address.NamesStr; address2.NamesStr])
+            (address.NameKeys @ address2.NameKeys)
     
     /// Concatenate two addresses, forcing the type of first address.
     let acatff<'a, 'b> (address : 'a Address) (address2 : 'b Address) =
@@ -163,8 +178,9 @@ module AddressModule =
 
     /// Concatenate two addresses, taking the type of the second address.
     let acats<'a> (address : obj Address) (address2 : 'a Address) =
-        let list = address.Names @ address2.Names
-        Address<'a>.make list
+        Address<'a>.makeFromStrAndKeys
+            (Address<'a>.join [address.NamesStr; address2.NamesStr])
+            (address.NameKeys @ address2.NameKeys)
     
     /// Concatenate two addresses, forcing the type of second address.
     let acatsf<'a, 'b> (address : 'a Address) (address2 : 'b Address) =
@@ -190,55 +206,40 @@ module Address =
 
     /// Change the type of an address.
     let changeType<'a, 'b> (address : 'a Address) =
-        { Names = address.Names; NamesRev = address.NamesRev; NameKeys = address.NameKeys; Hash = address.Hash; TypeCarrier = fun (_ : 'b) -> () }
+        { NamesStr = address.NamesStr; NameKeys = address.NameKeys; Hash = address.Hash; TypeCarrier = fun (_ : 'b) -> () }
 
     /// Take the head of an address.
     let head address =
-        List.head address.Names
+        List.head address.NameKeys
         
     /// Take the tail of an address.
     let tail<'a> address =
-        Address<'a>.make <| List.tail address.Names
+        Address<'a>.makeFromKeys <| List.tail address.NameKeys
 
-    /// Take a name of an address.
+    /// Take a name key of an address.
     let at index address =
-        List.at index address.Names
-
-    /// Map over an address.
-    let map<'a> mapper (address : 'a Address) =
-        let list = List.map mapper address.Names
-        Address<'a>.make list
-
-    /// Filter the names of an address.
-    /// NOTE: This doesn't seem to be a sensible operation for an address?
-    let filter predicate address =
-        let list = List.filter predicate address.Names
-        Address.make list
-
-    /// Fold over an address.
-    let fold folder state address =
-        List.fold folder state address.Names
+        List.at index address.NameKeys
 
     /// Take an address composed of the names of an address minus a skipped amount of names.
     let skip<'a, 'b> n (address : 'a Address) =
-        Address<'b>.make <| List.skip n address.Names
+        Address<'b>.makeFromKeys <| List.skip n address.NameKeys
 
     /// Take an address composed of the given number of names of an address.
     let take<'a, 'b> n (address : 'a Address) =
-        Address<'b>.make <| List.take n address.Names
+        Address<'b>.makeFromKeys <| List.take n address.NameKeys
 
     /// Take the last name of an address.
     let last address =
-        List.last address.Names
+        List.last address.NameKeys
 
     /// Take an address composed of all but the last name of an address.
     let allButLast<'a, 'b> (address : 'a Address) =
-        Address<'b>.make <| List.allButLast address.Names
+        Address<'b>.makeFromKeys <| List.allButLast address.NameKeys
 
     /// Get the length of an address by its names.
     let length address =
-        List.length address.Names
+        List.length address.NameKeys
 
     /// Query that an address is devoid of names.
     let isEmpty address =
-        List.isEmpty address.Names
+        List.isEmpty address.NameKeys
