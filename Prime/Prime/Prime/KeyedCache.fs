43188a3c80bd4b7bed420626cc2debb1c8960c17
bryanedds <bryanedds@gmail.com>
2015-03-28 20:20:58 -0400

    Added MutantCache.fs.
    Former-commit-id: 405adb6965648cf981852e857b9de95533d0ffc5

---
+++ Prime/Prime/Prime/KeyedCache.fs
@@ -0,0 +1,44 @@
+﻿namespace Prime
+
+[<RequireQualifiedAccess>]
+module internal KeyedCacheMetrics =
+    let mutable GlobalCacheHits = 0L
+    let mutable GlobalCacheMisses = 0L
+
+type [<ReferenceEquality>] KeyedCache<'k, 'v when 'k : equality> =
+    private
+        { mutable CacheKey : 'k
+          mutable CacheValue : 'v }
+
+    static member make (cacheKey : 'k) (cacheValue : 'v) =
+        { CacheKey = cacheKey
+          CacheValue = cacheValue }
+
+    static member getValue (keyEquality : 'k -> 'k -> bool) getFreshKeyAndValue cacheKey keyedCache : 'v =
+        if not <| keyEquality keyedCache.CacheKey cacheKey then
+#if DEBUG
+            KeyedCacheMetrics.GlobalCacheMisses <- KeyedCacheMetrics.GlobalCacheMisses + 1L
+#endif
+            let (freshKey, freshValue) = getFreshKeyAndValue ()
+            keyedCache.CacheKey <- freshKey
+            keyedCache.CacheValue <- freshValue
+            freshValue
+        else
+#if DEBUG
+            KeyedCacheMetrics.GlobalCacheHits <- KeyedCacheMetrics.GlobalCacheHits + 1L
+#endif
+            keyedCache.CacheValue
+
+    static member tryGetGlobalCacheHits () : int64 option =
+#if DEBUG
+        Some KeyedCacheMetrics.GlobalCacheHits
+#else
+        None
+#endif
+
+    static member tryGetGlobalCacheMisses () : int64 option =
+#if DEBUG
+        Some KeyedCacheMetrics.GlobalCacheMisses
+#else
+        None
+#endif
---
+++ Prime/Prime/Prime/MutantCache.fs
@@ -0,0 +1,37 @@
+﻿namespace Prime
+
+[<RequireQualifiedAccess>]
+module internal MutantCacheMetrics =
+    let mutable GlobalMutantRebuilds = 0L
+
+type [<ReferenceEquality>] MutantCache<'k, 'm when 'k : equality> =
+    private
+        { ConstantKey : 'k
+          RefKey : 'k ref
+          RefMutant : 'm ref }
+
+    static member getMutant keyEquality generateKey rebuildMutant (mutantCache : MutantCache<'k, 'm>) =
+        if not <| keyEquality !mutantCache.RefKey mutantCache.ConstantKey then
+#if DEBUG
+            MutantCacheMetrics.GlobalMutantRebuilds <- MutantCacheMetrics.GlobalMutantRebuilds + 1L
+#endif
+            let newKey = generateKey ()
+            let newMutant = rebuildMutant ()
+            mutantCache.RefKey := newKey
+            mutantCache.RefMutant := newMutant
+            (newMutant, { mutantCache with ConstantKey = newKey })
+        else (!mutantCache.RefMutant, mutantCache)
+
+    static member mutateMutant keyEquality rebuildMutant generateKey mutateMutant mutantCache =
+        let (mutant, mutantCache) = MutantCache<'k, 'm>.getMutant keyEquality generateKey rebuildMutant mutantCache
+        let newKey = generateKey ()
+        mutantCache.RefKey := newKey
+        mutantCache.RefMutant := mutateMutant mutant
+        { mutantCache with ConstantKey = newKey }
+
+    static member tryGetGlobalMutantRebuilds () : int64 option =
+#if DEBUG
+        Some MutantCacheMetrics.GlobalMutantRebuilds
+#else
+        None
+#endif
