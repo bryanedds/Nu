0a832015a7f63d2656acf7b15ae1391a878f6c9f
bryanedds <bryanedds@gmail.com>
2015-04-05 17:04:23 -0400

    Put overlay stuff into a single file.

--- Nu/Nu/Nu/Overlay.fs
+++ Nu/Nu/Nu/Overlay.fs
@@ -199,3 +199,23 @@ module Overlayer =
 
         // make overlay
         { Overlays = overlays }
+
+[<AutoOpen>]
+module OverlayRouterModule =
+
+    /// Maps from dispatcher names to opt overlay names.
+    type OverlayRouter = Map<string, string option>
+
+[<RequireQualifiedAccess>]
+module OverlayRouter =
+
+    /// Make an OverlayRouter.
+    let make dispatchers userRoutes =
+        let router = 
+            Map.fold
+                (fun overlayRouter _ dispatcher ->
+                    let dispatcherName = (dispatcher.GetType ()).Name
+                    Map.add dispatcherName (Some dispatcherName) overlayRouter)
+                Map.empty
+                dispatchers
+        Map.addMany userRoutes router
