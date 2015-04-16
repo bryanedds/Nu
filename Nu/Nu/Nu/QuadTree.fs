3474c6c2ab910ab552e04848dadf6b9175b94da1
bryanedds <bryanedds@gmail.com>
2015-04-16 12:19:34 -0400

    More work on QuadTree implementation.

--- Nu/Nu/Nu/QuadTree.fs
+++ Nu/Nu/Nu/QuadTree.fs
@@ -9,26 +9,30 @@ type [<NoEquality; NoComparison>] QuadNode =
         { Depth : int
           Position : Vector2
           Size : Vector2
-          Nodes : QuadNode array
+          Children : QuadNode array
           Elements : obj Address HashSet }
 
+    static member private makeChildPosition i position (childSize : Vector2) =
+        match i with
+        | 0 -> position
+        | 1 -> position + Vector2 (childSize.X, 0.0f)
+        | 2 -> position + Vector2 (0.0f, childSize.Y)
+        | 3 -> position + childSize
+        | _ -> failwithumf ()
+
+    static member private makeChildren depth position (size : Vector2) =
+        [|for i in 0 .. 3 do
+            let childDepth = depth - 1
+            let childSize = size * 0.25f
+            let childPosition = QuadNode.makeChildPosition i position childSize
+            yield QuadNode.make childDepth childPosition childSize|]
+
     static member make depth position (size : Vector2) =
-        let nodes =
-            [|for i in 0 .. 3 do
-                let childDepth = depth - 1
-                let childSize = size * 0.25f
-                let childPosition =
-                    match i with
-                    | 0 -> position
-                    | 1 -> position + Vector2 (childSize.X, 0.0f)
-                    | 2 -> position + Vector2 (0.0f, childSize.Y)
-                    | 3 -> position + size
-                    | _ -> failwithumf ()
-                yield QuadNode.make childDepth childPosition childSize|]
+        let children = QuadNode.makeChildren depth position size
         { Depth = depth
           Position = position
           Size = size
-          Nodes = nodes
+          Children = children
           Elements = HashSet () }
         
 type [<NoEquality; NoComparison>] QuadTree =
