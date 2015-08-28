58c23c671206b5df0888adb74f83787464d6a7bf
bryanedds <bryanedds@gmail.com>
2015-08-27 20:41:07 -0400

    Removed Aml from ClocForFPWorks.bat.
    Former-commit-id: 5d7b07a977430148b1e0bc978195080ad35fea3f

--- Cloc/ClocForFPWorks.bat
+++ Cloc/ClocForFPWorks.bat
@@ -1,5 +1,4 @@
 cloc-1.60.exe --by-file --force-lang="Fortran 90",fs --exclude-ext=xml .. > Loc.txt
 cloc-1.60.exe --by-file --force-lang="Fortran 90",fs --exclude-ext=xml ..\Prime > PrimeLoc.txt
-cloc-1.60.exe --by-file --force-lang="Fortran 90",fs --exclude-ext=xml ..\Aml > AmlLoc.txt
 cloc-1.60.exe --by-file --force-lang="Fortran 90",fs --exclude-ext=xml ..\Nu > NuLoc.txt
 pause
