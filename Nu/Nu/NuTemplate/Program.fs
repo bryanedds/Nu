36a25e4f72114c6ac4c095ae4f9bea2900c786b4
bryanedds <bryanedds@gmail.com>
2015-08-28 01:08:47 -0400

    Some documentation clean-up.

--- Nu/Nu/NuTemplate/Program.fs
+++ Nu/Nu/NuTemplate/Program.fs
@@ -51,7 +51,7 @@ module Program =
               AudioChunkSize = Constants.Audio.AudioBufferSizeDefault }
 
         // this is a callback that attempts to make 'the world' in a functional programming
-        // sense. In a Nu game, the world is represented as a complex record type named World.
+        // sense. In a Nu game, the world is represented as an abstract data type named World.
         let tryMakeWorld sdlDeps =
 
             // an instance of the above plugin
@@ -63,10 +63,10 @@ module Program =
 
         // this is a callback that specifies your game's unique behavior when updating the world
         // every frame. The World value is the state of the world after the callback transforms
-        // the one it receives. It is here where we first clearly see Nu's purely-functional(ish)
-        // design. The World type is almost entirely immutable, and thus the only way to update it
-        // is by making a new copy of an existing instance. Since we need no special update
-        // behavior in this program, we simply return the world as it was received.
+        // the one it receives. It is here where we first clearly see Nu's purely-functional
+        // design. The World type is immutable, and thus the only way to update it is by making 
+        // a new copy of an existing instance. Since we need no special update behavior in this
+        // program, we simply return the world as it was received.
         let updateWorld world = world
 
         // similar to the above, but for rendering. Most games probably won't do anything here.
