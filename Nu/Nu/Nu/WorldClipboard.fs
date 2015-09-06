dd829a5b1840d7c81b8f0a5e8dfe2e63e5e828db
bryanedds <bryanedds@gmail.com>
2015-09-05 23:11:07 -0400

    Reimplemented clipboard, albeit with remaining issues.

---
+++ Nu/Nu/Nu/WorldClipboard.fs
@@ -0,0 +1,48 @@
+﻿namespace Nu
+open System
+open Prime
+open Nu
+
+[<AutoOpen>]
+module WorldClipboard =
+
+    type World with
+        
+        /// Copy an entity to the clipboard.
+        static member copyToClipboard entity world =
+            let entityState = World.getEntityState entity world
+            world.State.Clipboard := Some entityState
+
+        /// Cut an entity to the clipboard.
+        static member cutToClipboard entity world =
+            World.copyToClipboard entity world
+            World.destroyEntityImmediate entity world
+
+        /// Paste an entity from the clipboard.
+        static member pasteFromClipboard atMouse rightClickPosition positionSnap rotationSnap group world =
+            match !world.State.Clipboard with
+            | Some entityState ->
+                let id = Core.makeId ()
+                let name = acstring id
+                let entityState = { entityState with Id = id; Name = name }
+                let camera = World.getCamera world
+                let entityPosition =
+                    if atMouse
+                    then Camera.mouseToWorld entityState.ViewType rightClickPosition camera
+                    else Camera.mouseToWorld entityState.ViewType (camera.EyeSize * 0.5f) camera
+                let entityTransform =
+                    { Transform.Position = entityPosition
+                      Depth = entityState.Depth
+                      Size = entityState.Size
+                      Rotation = entityState.Rotation }
+                let entityTransform = Math.snapTransform positionSnap rotationSnap entityTransform
+                let entityState =
+                    { entityState with
+                        Position = entityTransform.Position
+                        Depth = entityTransform.Depth
+                        Size = entityTransform.Size
+                        Rotation = entityTransform.Rotation }
+                let entity = gtoe group name
+                let world = World.addEntity false entityState entity world
+                (Some entity, world)
+            | None -> (None, world)
