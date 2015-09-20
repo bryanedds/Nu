be87757aa81bc0b9c7fadcb20365115f7aef751c
bryanedds <bryanedds@gmail.com>
2015-09-20 00:07:34 -0400

    Moved NameKey type into its own file.

--- Nu/Nu/Nu/Address.fs
+++ Nu/Nu/Nu/Address.fs
@@ -10,56 +10,6 @@ open System.Text
 open Prime
 open Nu
 
-/// A name key for optimized look-up in hashing containers.
-/// OPTIMIZATION: OptHashCode is lazy for speed.
-/// TODO: implement custom comparison.
-type [<CustomEquality; NoComparison>] NameKey =
-    private
-        { Name : string
-          mutable OptHashCode : int option }
-
-    /// Hash a NameKey.
-    static member hash nameKey =
-        match nameKey.OptHashCode with
-        | Some hashCode -> hashCode
-        | None ->
-            let hashCode = nameKey.Name.GetHashCode ()
-            nameKey.OptHashCode <- Some hashCode
-            hashCode
-
-    /// Equate NameKeys.
-    static member equals nameKey nameKey2 =
-        nameKey.Name.Equals nameKey2.Name // OPTIMIZATION: faster than (=) here
-
-    interface NameKey IEquatable with
-        member this.Equals that =
-            NameKey.equals this that
-
-    override this.Equals that =
-        match that with
-        | :? NameKey as that -> NameKey.equals this that
-        | _ -> false
-
-    override this.GetHashCode () =
-        NameKey.hash this
-
-    override this.ToString () =
-        this.Name
-
-[<RequireQualifiedAccess; CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
-module NameKey =
-
-    /// Make a name key from a non-empty name string.
-    let make name =
-        match name with
-        | "" -> failwith "Invalid name; must be a non-empty string."
-        | _ when name.Contains "/" -> failwith "Invalid name; must have no '/' characters."
-        | _ -> { Name = name; OptHashCode = None }
-
-    /// Get the name of a name key.
-    let getName nameKey =
-        nameKey.Name
-
 /// Converts Address types.
 type AddressConverter (targetType : Type) =
     inherit TypeConverter ()
