22a3bf7ae41a027c2fe64c3a98674847d678eff8
bryanedds <bryanedds@gmail.com>
2015-09-24 20:03:40 -0400

    Got effect serializing.

--- Nu/Nu/Nu/Effect.fs
+++ Nu/Nu/Nu/Effect.fs
@@ -1,56 +1,102 @@
 ﻿namespace Nu
 open OpenTK
 
-type PlaybackValue =
-    | Once
-    | Loop of int
-    | Bounce of int
+type Algorithm =
+    | Constant
+    | Linear
+    | EaseIn
+    | EaseOut
+    | Ease
 
-type Playback =
-    | PlaybackValue of PlaybackValue
-    | PlaybackVar of string
-
-type App =
+type FlipApplicator =
     | Or
     | Nor
     | Xor
     | Xnor
     | And
     | Nand
+    | Over
+
+type TweenApplicator =
     | Sum
     | Diff
     | Mult
     | Ratio
     | Over
 
+type FlipNode =
+    { FlipValue : bool
+      FlipLength : int }
+
+type UpdateNodeNode =
+    { FlipValue : bool
+      FlipLength : string }
+
+type TweenNode =
+    { TweenValue : single
+      TweenLength : int }
+
+type [<NoComparison>] Tween2Node =
+    { TweenValue : Vector2
+      TweenLength : int }
+
+type [<NoComparison>] Tween3Node =
+    { TweenValue : Vector3
+      TweenLength : int }
+
+type [<NoComparison>] Tween4Node =
+    { TweenValue : Vector4
+      TweenLength : int }
+
+type TweenINode =
+    { TweenValue : int
+      TweenLength : int }
+
+type Tween2INode =
+    { TweenValue : Vector2i
+      TweenLength : int }
+
+type Playback =
+    | Once
+    | Loop of int
+    | Bounce of int
+
 type Resource =
-    | Resource of AssetTag
-    | ResourceVar of string
+    | Resource of string * string
+    | ExpandResource of string * unit list
 
-type Gesture =
-    | Tween of unit
-    | Mount of unit
-    | Emit of unit
-    | GestureVar of string
+type [<NoComparison>] Gesture =
+    | ExpandGesture of string
+    | Update of string * UpdateNodeNode list
+    | Flip of string * FlipApplicator * FlipNode list
+    | Tween of string * TweenApplicator * Algorithm * TweenNode list
+    | Tween2 of string * TweenApplicator * Algorithm * Tween2Node list
+    | Tween3 of string * TweenApplicator * Algorithm * Tween3Node list
+    | Tween4 of string * TweenApplicator * Algorithm * Tween4Node list
+    | Mount of Animation
+    | Emit // TODO
 
-type StaticSpriteEffect =
-    { SpriteResource : AssetTag }
+and [<NoComparison>] Animation =
+    | ExpandAnimation of string * Argument list
+    | Container of Gesture list
+    | StaticSprite of Resource * Gesture list
+    | AnimatedSprite of Resource * Vector2i * Vector2i * int * Gesture list
 
-type Animation =
-    | Container of unit
-    | StaticSprite of StaticSpriteEffect
-    | AnimatedSprite of unit
-    | AnimationVar of unit
+and [<NoComparison>] Argument =
+    | PassPlayback of Playback
+    | PassResource of Resource
+    | PassGesture of Gesture
+    | PassAnimation of Animation
 
-type AsDefinition =
-    | AsPlayback of PlaybackValue
-    | AsTween of unit
-    | AsMount of unit
-    | AsEmit of unit
-    | AsResource of unit
+type [<NoComparison>] Definition =
+    | AsPlayback of Playback
+    | AsResource of Resource
+    | AsGesture of Gesture
+    | AsAnimation of string list * Animation
 
 type [<NoComparison>] Effect =
     { Name : string
       Playback : Playback
-      Definitions : Map<string, AsDefinition>
-      Animations : Map<string, Animation> }
+      Lifetime : int
+      Definitions : Map<string, Definition>
+      Animation : Animation }
