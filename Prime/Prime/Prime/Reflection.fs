5440633113d0a048908e85f3a1fa0b6b9bfb696a
bryanedds <bryanedds@gmail.com>
2016-02-10 12:14:54 -0500

    Forgotten commits...
    Former-commit-id: 549ae2b6211ce3f171d00d84684f8a15622bdde6

--- Prime/Prime/Prime/Reflection.fs
+++ Prime/Prime/Prime/Reflection.fs
@@ -1,33 +1,11 @@
 ﻿// Prime - A PRIMitivEs code library.
-// Copyright (C) Bryan Edds, 2012-2015.
+// Copyright (C) Bryan Edds, 2012-2016.
 
 namespace Prime
 open System
-open System.Diagnostics
 open System.ComponentModel
 open System.Reflection
 open System.IO
-
-/// A generalized identification code.
-type Id = int64
-
-[<AutoOpen; CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
-module Id =
-
-    /// The invalid Id.
-    let [<Literal>] InvalidId = 0L
-
-    /// Make a function that gets a unique number.
-    /// TODO: place a mutex lock in this
-    /// TODO: see if returned function can be optimized by minimizing dereferences
-    let makeIdMaker () =
-        let id = ref InvalidId
-        let makeId =
-            (fun () ->
-                id := !id + 1L
-                if !id = 0L then Debug.Fail "Id counter overflowed (flipped back to zero). Big trouble likely ahead!"
-                !id)
-        makeId
 
 /// Along with the Symbol binding, is used to elaborate the name of a symbol without using a
 /// string literal.
