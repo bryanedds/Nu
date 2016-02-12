518e85d72e4f383a1c1fa8abe0ebcd4c7e5025fb
bryanedds <bryanedds@gmail.com>
2016-02-11 23:00:18 -0500

    Separated SymbolIndex from Symbol.
    Former-commit-id: 498c1cbbc9df7689e9e4be9b78f98db2297615c7

--- Prime/Prime/Prime/Symbol.fs
+++ Prime/Prime/Prime/Symbol.fs
@@ -34,10 +34,6 @@ module Symbol =
     let [<Literal>] CloseQuoteStr = "\'"
     let [<Literal>] StructureChars = "[]\"`\'"
 
-    type SymbolIndex =
-        | AtomIndex of int64 * SymbolIndex list
-        | MoleculeIndex of int64 * SymbolIndex list
-
     let skipWhitespace = skipAnyOf WhitespaceChars
     let skipWhitespaces = skipMany skipWhitespace
 
@@ -58,67 +54,47 @@ module Symbol =
              (attempt (openStringForm >>. readStringChars .>> closeStringForm)) <|>
              (readAtomChars .>> skipWhitespaces))
 
-    module private Reader =
+    let (readSymbol : Parser<Symbol, unit>, private refReadSymbol : Parser<Symbol, unit> ref) =
+        createParserForwardedToRef ()
 
-        let (readSymbol : Parser<Symbol, unit>, refReadSymbol : Parser<Symbol, unit> ref) =
-            createParserForwardedToRef ()
+    let readAtom =
+        parse {
+            let! chars = readAtomChars
+            do! skipWhitespaces
+            return chars |> String.implode |> (fun str -> str.TrimEnd ()) |> Atom }
 
-        let readAtom =
-            parse {
-                let! chars = readAtomChars
-                do! skipWhitespaces
-                return chars |> String.implode |> (fun str -> str.TrimEnd ()) |> Atom }
+    let readAtomAsString =
+        parse {
+            do! openStringForm
+            let! stringChars = readStringChars
+            do! closeStringForm
+            return stringChars |> String.implode |> Atom }
 
-        let readAtomAsString =
-            parse {
-                do! openStringForm
-                let! stringChars = readStringChars
-                do! closeStringForm
-                return stringChars |> String.implode |> Atom }
+    let readQuote =
+        parse {
+            do! openQuoteForm
+            let! quoteChars = readQuoteChars
+            do! closeQuoteForm
+            return quoteChars |> String.implode |> Quote }
 
-        let readQuote =
-            parse {
-                do! openQuoteForm
-                let! quoteChars = readQuoteChars
-                do! closeQuoteForm
-                return quoteChars |> String.implode |> Quote }
+    let readMolecule =
+        parse {
+            do! openMoleculeForm
+            let! values = many readSymbol
+            do! closeMoleculeForm
+            return values |> Molecule }
 
-        let readMolecule =
-            parse {
-                do! openMoleculeForm
-                let! values = many readSymbol
-                do! closeMoleculeForm
-                return values |> Molecule }
+    do refReadSymbol :=
+        attempt readAtomAsString <|>
+        attempt readQuote <|>
+        attempt readAtom <|>
+        readMolecule
 
-        do refReadSymbol :=
-            attempt readAtomAsString <|>
-            attempt readQuote <|>
-            attempt readAtom <|>
-            readMolecule
-
-    module private Indexer =
-
-        let (readSymbolIndex : Parser<SymbolIndex, unit>, refReadSymbolIndex : Parser<SymbolIndex, unit> ref) =
-            createParserForwardedToRef ()
-
-        let readAtomIndex =
-            parse {
-                let! openPosition = getPosition
-                let! _ = readContentChars
-                let! symbolIndices = many readSymbolIndex
-                return AtomIndex (openPosition.Index, symbolIndices) }
-
-        let readMoleculeIndex =
-            parse {
-                let! openPosition = getPosition
-                do! openMoleculeForm
-                let! symbolIndices = many readSymbolIndex
-                do! closeMoleculeForm
-                return MoleculeIndex (openPosition.Index, symbolIndices) }
-
-        do refReadSymbolIndex :=
-            attempt readMoleculeIndex <|>
-            readAtomIndex
+    let rec writeSymbol symbol =
+        match symbol with
+        | Atom str -> str
+        | Quote str -> OpenQuoteStr + str + CloseQuoteStr
+        | Molecule symbols -> OpenMoleculeStr + String.Join (" ", List.map writeSymbol symbols) + CloseMoleculeStr
 
     /// Convert a string to a symbol, with the following parses:
     /// 
@@ -142,40 +118,30 @@ module Symbol =
     /// [Gem `[Some 1]']
     ///
     let fromString str =
-        match run (skipWhitespaces >>. Reader.readSymbol) str with
+        match run (skipWhitespaces >>. readSymbol) str with
         | Success (value, _, _) -> value
         | Failure (error, _, _) -> failwith error
 
-    /// Convert a symbol to a string.
+    /// Convert a symbol to a string, with the following unparses:
+    /// 
+    /// (* Atom values *)
+    /// 
+    /// 0
+    /// None
+    /// [2 2]
+    /// Hello_World
+    /// CharacterAnimationFacing
+    /// "String with quoted spaces."
+    /// String_with_underscores_for_spaces.
+    /// 
+    /// (* Molecule values *)
+    ///
+    /// []
+    /// [Some 0]
+    /// [Left 0]
+    /// [[0 1] [2 4]]
+    /// [AnimationData 4 8]
+    /// [Gem `[Some 1]']
+    ///
     let rec toString symbol =
-        match symbol with
-        | Atom str -> str
-        | Quote str -> OpenQuoteStr + str + CloseQuoteStr
-        | Molecule symbols -> OpenMoleculeStr + String.Join (" ", List.map toString symbols) + CloseMoleculeStr
-
-    /// Attempt to convert a string to a its symbol indices.
-    let stringToOptSymbolIndex str =
-        if String.IsNullOrWhiteSpace str then None
-        else
-            match run (skipWhitespaces >>. Indexer.readSymbolIndex) str with
-            | Success (value, _, _) -> Some value
-            | Failure (error, _, _) -> failwith error
-
-    /// Pretty-print a string in the form an symbolic-expression.
-    let prettyPrint (str : string) =
-        let builder = Text.StringBuilder str
-        let mutable builderIndex = 0
-        let rec advance hasContentParent tabDepth childIndex symbolIndex =
-            match symbolIndex with
-            | AtomIndex (_, symbolIndices) ->
-                List.iteri (advance true tabDepth) symbolIndices
-            | MoleculeIndex (index, symbolIndices) ->
-                if index <> 0L && (hasContentParent || childIndex <> 0) then
-                    let whitespace = "\n" + String.replicate tabDepth " "
-                    ignore ^ builder.Insert (int index + builderIndex, whitespace)
-                    builderIndex <- builderIndex + whitespace.Length
-                List.iteri (advance false (tabDepth + 1)) symbolIndices
-        match stringToOptSymbolIndex str with
-        | Some indexLocation -> advance false 0 0 indexLocation
-        | None -> ()
-        builder.ToString ()
+        writeSymbol symbol
---
+++ Prime/Prime/Prime/SymbolIndex.fs
@@ -0,0 +1,64 @@
+﻿// Prime - A PRIMitivEs code library.
+// Copyright (C) Bryan Edds, 2012-2016.
+
+namespace Prime
+open System
+open FParsec
+open Prime
+
+// TODO: see if this can be got rid of by having an alternatve way to write symbols directly.
+type SymbolIndex =
+    | AtomIndex of int64 * SymbolIndex list
+    | MoleculeIndex of int64 * SymbolIndex list
+
+[<RequireQualifiedAccess; CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
+module SymbolIndex =
+
+    let (readSymbolIndex : Parser<SymbolIndex, unit>, private refReadSymbolIndex : Parser<SymbolIndex, unit> ref) =
+        createParserForwardedToRef ()
+
+    let readAtomIndex =
+        parse {
+            let! openPosition = getPosition
+            let! _ = Symbol.readContentChars
+            let! symbolIndices = many readSymbolIndex
+            return AtomIndex (openPosition.Index, symbolIndices) }
+
+    let readMoleculeIndex =
+        parse {
+            let! openPosition = getPosition
+            do! Symbol.openMoleculeForm
+            let! symbolIndices = many readSymbolIndex
+            do! Symbol.closeMoleculeForm
+            return MoleculeIndex (openPosition.Index, symbolIndices) }
+
+    do refReadSymbolIndex :=
+        attempt readMoleculeIndex <|>
+        readAtomIndex
+
+    /// Attempt to a symbol index from a string.
+    let fromString str =
+        if String.IsNullOrWhiteSpace str then None
+        else
+            match run (Symbol.skipWhitespaces >>. readSymbolIndex) str with
+            | Success (value, _, _) -> Some value
+            | Failure (error, _, _) -> failwith error
+
+    /// Pretty-print a string in the form an symbolic-expression.
+    let prettyPrint (str : string) =
+        let builder = Text.StringBuilder str
+        let mutable builderIndex = 0
+        let rec advance hasContentParent tabDepth childIndex symbolIndex =
+            match symbolIndex with
+            | AtomIndex (_, symbolIndices) ->
+                List.iteri (advance true tabDepth) symbolIndices
+            | MoleculeIndex (index, symbolIndices) ->
+                if index <> 0L && (hasContentParent || childIndex <> 0) then
+                    let whitespace = "\n" + String.replicate tabDepth " "
+                    ignore ^ builder.Insert (int index + builderIndex, whitespace)
+                    builderIndex <- builderIndex + whitespace.Length
+                List.iteri (advance false (tabDepth + 1)) symbolIndices
+        match fromString str with
+        | Some indexLocation -> advance false 0 0 indexLocation
+        | None -> ()
+        builder.ToString ()
