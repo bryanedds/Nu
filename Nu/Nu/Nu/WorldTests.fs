bc70147451ea3d07473e80d3ccb5e0a695182df5
bryanedds <bryanedds@gmail.com>
2016-02-13 16:44:16 -0500

    Moved more event tests to Prime.
    Former-commit-id: 25552af3aa06f1df197e716f99c2bc84eab201e1

--- Nu/Nu/Nu/WorldTests.fs
+++ Nu/Nu/Nu/WorldTests.fs
@@ -11,13 +11,12 @@ open Prime
 open Nu
 module WorldTests =
 
-    let UnitEventAddress = ntoa<unit> !!"Unit"
-    let StringEventAddress = ntoa<string> !!"String"
     let TestFilePath = "TestFile.xml"
-    let incUserStateAndCascade (_ : Event<unit, Game>) world = (Cascade, World.updateUserState inc world)
-    let incUserStateAndResolve (_ : Event<unit, Game>) world = (Resolve, World.updateUserState inc world)
+    let StringEvent = ntoa<string> !!"String"
+    let Jim = Simulants.DefaultGroup => "Jim"
+    let Bob = Simulants.DefaultGroup => "Bob"
 
-    let [<Fact>] runNuForOneFrameThenCleanUp () =
+    let [<Fact>] runOneFrameThenCleanUp () =
         let world = World.makeEmpty ()
         World.run4 (fun world -> World.getTickTime world < 1L) SdlDeps.empty Running world
 
@@ -27,8 +26,8 @@ module WorldTests =
         let world = World.createGroup typeof<GroupDispatcher>.Name None (Some Simulants.DefaultGroup.GroupName) Simulants.DefaultScreen world |> snd
         let world = World.createEntity typeof<EntityDispatcher>.Name None (Some Simulants.DefaultEntity.EntityName) Simulants.DefaultGroup world |> snd
         let handleEvent = fun evt world -> (Cascade, World.updateUserState (fun _ -> evt.Subscriber) world)
-        let world = World.subscribe handleEvent StringEventAddress Simulants.DefaultEntity world
-        let world = World.publish String.Empty StringEventAddress ["Test"] Simulants.Game world
+        let world = World.subscribe handleEvent StringEvent Simulants.DefaultEntity world
+        let world = World.publish String.Empty StringEvent ["Test"] Simulants.Game world
         Assert.Equal<Simulant> (Simulants.DefaultEntity :> Simulant, World.getUserState world)
 
     let [<Fact>] gameSerializationWorks () =
@@ -43,3 +42,30 @@ module WorldTests =
         Assert.Equal<Name> (Simulants.DefaultScreen.GetName oldWorld, Simulants.DefaultScreen.GetName world)
         Assert.Equal<Name> (Simulants.DefaultGroup.GetName oldWorld, Simulants.DefaultGroup.GetName world)
         Assert.Equal<Name> (Simulants.DefaultEntity.GetName oldWorld, Simulants.DefaultEntity.GetName world)
+
+    let [<Fact>] iterativeFrpWorks () =
+        let world = World.makeEmpty ()
+        let world = World.createScreen typeof<ScreenDispatcher>.Name None (Some Simulants.DefaultScreen.ScreenName) world |> snd
+        let world = World.createGroup typeof<GroupDispatcher>.Name None (Some Simulants.DefaultGroup.GroupName) Simulants.DefaultScreen world |> snd
+        let world = World.createEntity typeof<EntityDispatcher>.Name None (Some Jim.EntityName) Simulants.DefaultGroup world |> snd
+        let world = World.createEntity typeof<EntityDispatcher>.Name None (Some Bob.EntityName) Simulants.DefaultGroup world |> snd
+        let world = world |> Jim.SetPublishChanges true |> Bob.SetPublishChanges true
+        let world = world |> (Bob, Bob.GetVisible) *-> (Jim, Jim.SetVisible)
+        let world = Bob.SetVisible false world
+        Assert.False ^ Bob.GetVisible world
+        Assert.False ^ Jim.GetVisible world
+
+    let [<Fact>] iterativeFrpCyclicWorks () =
+        let world = World.makeEmpty ()
+        let world = World.createScreen typeof<ScreenDispatcher>.Name None (Some Simulants.DefaultScreen.ScreenName) world |> snd
+        let world = World.createGroup typeof<GroupDispatcher>.Name None (Some Simulants.DefaultGroup.GroupName) Simulants.DefaultScreen world |> snd
+        let world = World.createEntity typeof<EntityDispatcher>.Name None (Some Jim.EntityName) Simulants.DefaultGroup world |> snd
+        let world = World.createEntity typeof<EntityDispatcher>.Name None (Some Bob.EntityName) Simulants.DefaultGroup world |> snd
+        let world = world |> Jim.SetPublishChanges true |> Bob.SetPublishChanges true
+        let world =
+            world |>
+                (Bob, Bob.GetVisible) *-> (Jim, Jim.SetVisible) |>
+                (Jim, Jim.GetVisible) /-> (Bob, not >> Bob.SetVisible)
+        let world = Bob.SetVisible false world
+        Assert.True ^ Bob.GetVisible world
+        Assert.True ^ Jim.GetVisible world
--- Prime/Prime/Prime/EventTests.fs
+++ Prime/Prime/Prime/EventTests.fs
@@ -32,55 +32,116 @@ module EventTests =
         static member incTestState this = { this with TestState = inc this.TestState }
         static member make () = { TestState = 0; TestEventSystem = EventSystem.make () }
 
-    [<RequireQualifiedAccess>]
-    module Participants =
-        let TestParticipant = { TestAddress = Address.empty<TestParticipant> }
-
-    let IntEventAddress = ntoa<int> !!"Int"
-    let UnitEventAddress = ntoa<unit> !!"Unit"
-    let StringEventAddress = ntoa<string> !!"String"
+    let TestEvent = ntoa<int> !!"Int"
+    let TestParticipant = { TestAddress = Address.empty<TestParticipant> }
     let incTestState _ world = TestWorld.incTestState world
     let incTestStateNoEvent world = TestWorld.incTestState world
     let incTestStateTwice _ world = TestWorld.incTestState ^ TestWorld.incTestState world
     let incTestStateTwiceNoEvent world = TestWorld.incTestState ^ TestWorld.incTestState world
-    let incTestStateAndCascade (_ : Event<unit, TestParticipant>) world = (Cascade, TestWorld.incTestState world)
-    let incTestStateAndResolve (_ : Event<unit, TestParticipant>) world = (Resolve, TestWorld.incTestState world)
+    let incTestStateAndCascade (_ : Event<int, TestParticipant>) world = (Cascade, TestWorld.incTestState world)
+    let incTestStateAndResolve (_ : Event<int, TestParticipant>) world = (Resolve, TestWorld.incTestState world)
 
     let [<Fact>] subscribeWorks () =
         let world = TestWorld.make ()
-        let world = Eventable.subscribe incTestStateAndCascade UnitEventAddress Participants.TestParticipant world
-        let world = Eventable.publish () UnitEventAddress ["Test"] Participants.TestParticipant world
+        let world = Eventable.subscribe incTestStateAndCascade TestEvent TestParticipant world
+        let world = Eventable.publish 0 TestEvent ["Test"] TestParticipant world
         Assert.Equal (1, world.TestState)
 
     let [<Fact>] subscribeAndPublishTwiceWorks () =
         let world = TestWorld.make ()
-        let world = Eventable.subscribe incTestStateAndCascade UnitEventAddress Participants.TestParticipant world
-        let world = Eventable.publish () UnitEventAddress ["Test"] Participants.TestParticipant world
-        let world = Eventable.publish () UnitEventAddress ["Test"] Participants.TestParticipant world
+        let world = Eventable.subscribe incTestStateAndCascade TestEvent TestParticipant world
+        let world = Eventable.publish 0 TestEvent ["Test"] TestParticipant world
+        let world = Eventable.publish 0 TestEvent ["Test"] TestParticipant world
         Assert.Equal (2, world.TestState)
 
     let [<Fact>] subscribeTwiceAndPublishWorks () =
         let world = TestWorld.make ()
-        let world = Eventable.subscribe incTestStateAndCascade UnitEventAddress Participants.TestParticipant world
-        let world = Eventable.subscribe incTestStateAndCascade UnitEventAddress Participants.TestParticipant world
-        let world = Eventable.publish () UnitEventAddress ["Test"] Participants.TestParticipant world
+        let world = Eventable.subscribe incTestStateAndCascade TestEvent TestParticipant world
+        let world = Eventable.subscribe incTestStateAndCascade TestEvent TestParticipant world
+        let world = Eventable.publish 0 TestEvent ["Test"] TestParticipant world
         Assert.Equal (2, world.TestState)
 
     let [<Fact>] subscribeWithResolutionWorks () =
         let world = TestWorld.make ()
-        let world = Eventable.subscribe incTestStateAndCascade UnitEventAddress Participants.TestParticipant world
-        let world = Eventable.subscribe incTestStateAndResolve UnitEventAddress Participants.TestParticipant world
-        let world = Eventable.publish () UnitEventAddress ["Test"] Participants.TestParticipant world
+        let world = Eventable.subscribe incTestStateAndCascade TestEvent TestParticipant world
+        let world = Eventable.subscribe incTestStateAndResolve TestEvent TestParticipant world
+        let world = Eventable.publish 0 TestEvent ["Test"] TestParticipant world
         Assert.Equal (1, world.TestState)
 
     let [<Fact>] unsubscribeWorks () =
         let key = makeGuid ()
         let world = TestWorld.make ()
-        let world = Eventable.subscribe5 key incTestStateAndResolve UnitEventAddress Participants.TestParticipant world
+        let world = Eventable.subscribe5 key incTestStateAndResolve TestEvent TestParticipant world
         let world = Eventable.unsubscribe key world
-        let world = Eventable.publish () UnitEventAddress ["Test"] Participants.TestParticipant world
+        let world = Eventable.publish 0 TestEvent ["Test"] TestParticipant world
         Assert.Equal (0, world.TestState)
-        
+
+    let [<Fact>] observeWorks () =
+        let world = TestWorld.make ()
+        let world = observe TestEvent TestParticipant |> subscribe incTestStateAndCascade <| world
+        let world = Eventable.publish 0 TestEvent ["Test"] TestParticipant world
+        Assert.Equal (1, world.TestState)
+
+    let [<Fact>] observeSubscribeTwiceUnsubscribeOnceWorks () =
+        let world = TestWorld.make ()
+        let observation = observe TestEvent TestParticipant
+        let world = subscribe incTestStateAndCascade observation world
+        let (unsubscribe, world) = subscribePlus incTestStateAndCascade observation world
+        let world = unsubscribe world
+        let world = Eventable.publish 0 TestEvent ["Test"] TestParticipant world
+        Assert.Equal (1, world.TestState)
+
+    let [<Fact>] observeUnsubscribeWorks () =
+        let world = TestWorld.make ()
+        let (unsubscribe, world) = observe TestEvent TestParticipant |> subscribePlus incTestStateAndCascade <| world
+        let world = unsubscribe world
+        let world = Eventable.publish 0 TestEvent ["Test"] TestParticipant world
+        Assert.True ^ Vmap.isEmpty ^ Eventable.getSubscriptions world
+        Assert.Equal (0, world.TestState)
+
+    let [<Fact>] filterWorks () =
+        let world = TestWorld.make ()
+        let world =
+            observe TestEvent TestParticipant |>
+            filter (fun _ world -> world.TestState = 0) |>
+            subscribe incTestStateAndCascade <|
+            world
+        let world = Eventable.publish 0 TestEvent ["Test"] TestParticipant world
+        let world = Eventable.publish 0 TestEvent ["Test"] TestParticipant world
+        Assert.Equal (1, world.TestState)
+
+    let [<Fact>] mapWorks () =
+        let world = TestWorld.make ()
+        let world =
+            observe TestEvent TestParticipant |>
+            map (fun evt _ -> evt.Data * 2) |>
+            subscribe (fun evt world -> (Cascade, { world with TestState = evt.Data })) <|
+            world
+        let world = Eventable.publish 1 TestEvent ["Test"] TestParticipant world
+        Assert.Equal (2, world.TestState)
+
+    let [<Fact>] scanWorks () =
+        let world = TestWorld.make ()
+        let world =
+            observe TestEvent TestParticipant |>
+            scan (fun acc evt _ -> acc + evt.Data) 0 |>
+            subscribe (fun evt world -> (Cascade, { world with TestState = evt.Data })) <|
+            world
+        let world = Eventable.publish 1 TestEvent ["Test"] TestParticipant world
+        let world = Eventable.publish 2 TestEvent ["Test"] TestParticipant world
+        Assert.Equal (3, world.TestState)
+
+    let [<Fact>] scanDoesntLeaveGarbage () =
+        let world = TestWorld.make ()
+        let (unsubscribe, world) =
+            observe TestEvent TestParticipant |>
+            scan2 (fun a _ _ -> a) |>
+            subscribePlus incTestStateAndCascade <|
+            world
+        let world = Eventable.publish 0 TestEvent ["Test"] TestParticipant world
+        let world = unsubscribe world
+        Assert.True ^ Vmap.isEmpty ^ Eventable.getSubscriptions world
+
     let [<Fact>] chainWorks () =
         
         // build everything
@@ -93,24 +154,24 @@ module EventTests =
                 do! reactE incTestState
                 do! pass
                 do! loop 0 inc (fun i _ -> i < 2) (fun _ -> update incTestStateTwiceNoEvent) }
-        let observation = observe IntEventAddress Participants.TestParticipant
+        let observation = observe TestEvent TestParticipant
         let world = Chain.runAssumingCascade chain observation world |> snd
         Assert.Equal (0, world.TestState)
 
         // assert the first publish executes the first chained operation
-        let world = Eventable.publish 1 IntEventAddress ["Test"] Participants.TestParticipant world
+        let world = Eventable.publish 1 TestEvent ["Test"] TestParticipant world
         Assert.Equal (1, world.TestState)
 
         // assert the second publish executes the second chained operation
-        let world = Eventable.publish 2 IntEventAddress ["Test"] Participants.TestParticipant world
+        let world = Eventable.publish 2 TestEvent ["Test"] TestParticipant world
         Assert.Equal (2, world.TestState)
         
         // and so on...
-        let world = Eventable.publish 3 IntEventAddress ["Test"] Participants.TestParticipant world
+        let world = Eventable.publish 3 TestEvent ["Test"] TestParticipant world
         Assert.Equal (3, world.TestState)
         
         // and so on...
-        let world = Eventable.publish 4 IntEventAddress ["Test"] Participants.TestParticipant world
+        let world = Eventable.publish 4 TestEvent ["Test"] TestParticipant world
         Assert.Equal (7, world.TestState)
         
         // assert no garbage is left over after chained computation is concluded
