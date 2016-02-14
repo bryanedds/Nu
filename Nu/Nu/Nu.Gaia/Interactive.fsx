b9c934c04aaef4f7daa3405629bb31f88bee56b9
bryanedds <bryanedds@gmail.com>
2016-02-13 20:57:15 -0500

    Fixed references in Interactive.fsx files.
    Former-commit-id: e78b3e424b01d9a4f75c2b75b31a60d674b0c290

--- Nu/Nu/Nu.Gaia/Interactive.fsx
+++ Nu/Nu/Nu.Gaia/Interactive.fsx
@@ -3,16 +3,16 @@
 
 #I __SOURCE_DIRECTORY__
 #r "System.Configuration"
-#r "../../../Prime/FSharpx.Core/FSharpx.Core.dll"
-#r "../../../Prime/FSharpx.Collections/FSharpx.Collections.dll"
-#r "../../../Prime/FParsec/FParsecCS.dll" // MUST be referenced BEFORE FParsec.dll!
-#r "../../../Prime/FParsec/FParsec.dll"
-#r "../../../Prime/xUnit/xunit.dll"
-#r "../../../Prime/Prime/Prime/bin/Debug/Prime.exe"
+#r "../../../Nu/FSharpx.Core/FSharpx.Core.dll"
+#r "../../../Nu/FSharpx.Collections/FSharpx.Collections.dll"
+#r "../../../Nu/FParsec/FParsecCS.dll" // MUST be referenced BEFORE FParsec.dll!
+#r "../../../Nu/FParsec/FParsec.dll"
+#r "../../../Nu/xUnit/xunit.dll"
+#r "../../../Nu/Prime/Release/Prime.exe"
 #r "../../../Nu/Farseer/FarseerPhysics.dll"
 #r "../../../Nu/Magick.NET/Magick.NET-AnyCPU.dll"
 #r "../../../Nu/SDL2#/Debug/SDL2#.dll"
-#r "../../../Nu/TiledSharp/Debug/TiledSharp.dll"
+#r "../../../Nu/TiledSharp/Release/TiledSharp.dll"
 #r "../../../SDL2Addendum/SDL2Addendum/SDL2Addendum/bin/Debug/SDL2Addendum.dll"
 #r "../../../Nu/Nu/Nu/bin/Debug/Nu.exe"
 #r "../../../Nu/Nu/Nu.Gaia.Design/bin/Debug/Nu.Gaia.Design.exe"
