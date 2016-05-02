b9f74dd9f6552bb6fd984d6b5e4b6a2bba760771
Bryan Edds <bryanedds@gmail.com>
2016-05-01 23:44:23 -0400

    Update WorldDebug.fs
    Former-commit-id: 9c2c6eba8e6da525190374dfcbf384d587351c2f

--- Nu/Nu/Nu/WorldDebug.fs
+++ Nu/Nu/Nu/WorldDebug.fs
@@ -5,7 +5,7 @@ namespace Debug
 open System
 module internal World =
 
-    /// The latest value of the world for debugging in an IDE. Not to be used for anything else.
+    /// The value of the world currently chosen for debugging in an IDE. Not to be used for anything else.
     let mutable internal Chosen = obj ()
     let mutable internal viewGame = fun (_ : obj) -> Array.create 0 (String.Empty, obj ())
     let mutable internal viewScreen = fun (_ : obj) (_ : obj) -> Array.create 0 (String.Empty, obj ())
