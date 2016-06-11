0a080f63fef7bc430dda4c6f09a2cad6d107520f
bryanedds <bryanedds@gmail.com>
2016-06-11 14:36:12 -0400

    Improved syntax highlighting.

--- Nu/Nu/Nu.Gaia.Design/SymbolicTextBox.cs
+++ Nu/Nu/Nu.Gaia.Design/SymbolicTextBox.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Linq;
 using ScintillaNET;
 
 namespace Nu.Gaia.Design
@@ -8,18 +9,32 @@ namespace Nu.Gaia.Design
         public SymbolicTextBox()
         {
             // Make default styles monospaced!
-            Styles[Style.Default].Font = "Lucida Console"; // super compact
+            Styles[Style.Default].Font = "Lucida Console";
+
+            // Add a little more line spacing for new font
+            ExtraDescent = 1;
 
             // Lisp lexer
             Lexer = Lexer.Lisp;
 
+            // Add keyword styles
+            Styles[Style.Lisp.Keyword].ForeColor = Styles[Style.Lisp.KeywordKw].ForeColor = Color.DarkBlue;
+
+            // Add number style
+            Styles[Style.Lisp.Number].ForeColor = Color.DarkBlue;
+
+            // Add special character styles
+            Styles[Style.Lisp.Operator].ForeColor = Color.RoyalBlue; // brackets, actually
+
+            // Add string style
+            Styles[Style.Lisp.String].ForeColor = Color.Teal;
+
+            // Add brace matching styles
+            Styles[Style.BraceLight].BackColor = Color.LightGray;
+            Styles[Style.BraceBad].BackColor = Color.Red;
+
             // No tabs
             UseTabs = false;
-
-            // Configure brace visuals
-            Styles[Style.BraceLight].BackColor = Color.LightGray;
-            Styles[Style.BraceLight].ForeColor = Color.BlueViolet;
-            Styles[Style.BraceBad].ForeColor = Color.Red;
 
             // Implement brace matching
             UpdateUI += this_UpdateUI;
@@ -35,9 +50,23 @@ namespace Nu.Gaia.Design
                 var bracePos1 = -1;
                 var bracePos2 = -1;
 
-                // Is there a brace to the left or right?
-                if (IsBrace(GetCharAt(caretPos))) bracePos1 = caretPos;
-                else if (caretPos > 0 && IsBrace(GetCharAt(caretPos - 1))) bracePos1 = (caretPos - 1);
+                if (IsBrace(GetCharAt(caretPos)))
+                {
+                    // Select the brace to the immediate right
+                    bracePos1 = caretPos;
+                }
+                else
+                {
+                    // Select the brace anywhere to the left
+                    for (var i = caretPos - 1; i >= 0; ++i)
+                    {
+                        if (IsBrace(GetCharAt(i)))
+                        {
+                            bracePos1 = i;
+                            break;
+                        }
+                    }
+                }
 
                 if (bracePos1 >= 0)
                 {
