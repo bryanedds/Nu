b51d212327a71ab9a56e1bd9e79a5139ae4ae4f8
bryanedds <bryanedds@gmail.com>
2016-06-14 23:53:18 -0400

    Forgotten portions of previous commit...

--- Prime/Prime/Prime/Program.fs
+++ Prime/Prime/Prime/Program.fs
@@ -9,6 +9,8 @@ open Prime
 open Prime.Tests
 module Program =
 
+    SymbolTests.canPrettyPrintGuid ()
+
     /// Performs some ad-hoc tests to compare performance of maps.
     let runTimings make lookup name =
         printfn "%s timings..." name
