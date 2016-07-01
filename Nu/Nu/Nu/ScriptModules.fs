af38e9c3d47b8c2195e9ef61b10ef831e36282b2
bryanedds <bryanedds@gmail.com>
2016-06-30 23:38:42 -0400

    Fixed compilation error.

--- Nu/Nu/Nu/ScriptModules.fs
+++ Nu/Nu/Nu/ScriptModules.fs
@@ -544,33 +544,10 @@ module Scripting =
         | _ -> (expr, env)
 
 [<AutoOpen>]
-module ScriptEnvModule =
-
-    /// An environemt in which scripting declarations can be found.
-    /// Internally a Dictionary for fast look-ups, but without add functions for immutability. Append, however, stays
-    /// functional without greater big-O compexity!
-    type [<NoEquality; NoComparison>] ScriptEnv =
-        private
-            ScriptEnv of Dictionary<string, Declaration>
-            
-    [<RequireQualifiedAccess; CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
-    module ScriptEnv =
-
-        let tryFind key (ScriptEnv env) =
-            Dictionary.tryFind key env
-
-        let make declarations =
-            ScriptEnv ^ dictC declarations
-
-        let append (ScriptEnv env) (ScriptEnv env2) =
-            make (Seq.append env env2)
-
-[<AutoOpen>]
 module ScriptSystemModule =
 
     /// An abstract data type for executing scripts.
     type [<NoEquality; NoComparison>] ScriptSystem =
         private
-            { ScriptEnvs : Vmap<obj Address, ScriptEnv>
-              Scripts : Vmap<Guid, Script>
+            { Scripts : Vmap<Guid, Script>
               Debugging : bool }
