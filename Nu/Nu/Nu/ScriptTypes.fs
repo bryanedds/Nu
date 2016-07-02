b3e424d34ed0b92b69ee31561f454b90c7d7cda5
bryanedds <bryanedds@gmail.com>
2016-07-02 02:25:32 -0400

    Made only constructors and special operator names capitalized in scripting language.
    Former-commit-id: 0b9f2964cbae66ba0071a62daf5c0c7258fc965c

--- Nu/Nu/Nu/ScriptTypes.fs
+++ Nu/Nu/Nu/ScriptTypes.fs
@@ -21,66 +21,63 @@ module Scripting =
         | EyeCenter
         | Simulant of obj Address
 
-    and [<Syntax(   "Not " +
-                    "And Or " +
+    and [<Syntax(   "List Some None " +
+                    "Not And Or " +
                     "Eq Not_Eq Lt Gt Lt_Eq Gt_Eq " +
-                    "Add Sub Mul Div " +
-                    "Mod Pow Root Sqr Sqrt " +
-                    "Floor Ceiling Truncate Round Exp Log " +
-                    "Sin Cos Tan Asin Acos Atan " +
-                    "Length Normal " +
-                    "Cross Dot " +
-                    "ToInteger ToInteger64 ToSingle ToDouble ToVector2 " +
-                    "ToString " +
-                    "Head Tail Empty Cons " +
-                    "Map Filter Fold All Any NotAny",
+                    "Add Sub Mul Div Mod " +
+                    "pow root sqr sqrt " +
+                    "floor ceiling truncate round exp log " +
+                    "sin cos tan asin acos atan " +
+                    "length normal " +
+                    "cross dot " +
+                    "bool int int64 single double string " +
+                    "head tail empty cons " +
+                    "map filter fold all any notAny",
                     "");
           NoComparison>]
         Expr =
         | Unit of Origin option
-        | Boolean of bool * Origin option
-        | Integer of int * Origin option
-        | Integer64 of int64 * Origin option
+        | Bool of bool * Origin option
+        | Int of int * Origin option
+        | Int64 of int64 * Origin option
         | Single of single * Origin option
         | Double of double * Origin option
         | Vector2 of Vector2 * Origin option
         | String of string * Origin option
         | List of Expr list * Origin option
-        | Mapping of Origin option
         | Violation of string * Origin option
         | Reference of Referent * Origin option
         | Break of Expr * Origin option
+        | Do of Name * Expr list * Origin option // executes an engine command, some can be found in the NuPlugin
         | Call of Expr list * Origin option
         | Fun of string list * Expr * int * Origin option
         | Get of Referent * string * Origin option
         | Let of Name * Expr * Origin option
         | If of Expr * Expr * Expr * Origin option
         | Try of Expr * Expr list * Origin option
-        | Do of Name * Expr list * Origin option // executes an engine command, some can be found in the NuPlugin
         | Binding of Name * Origin option
         | Quote of string * Origin option
         static member getOptOrigin term =
             match term with
             | Unit optOrigin
-            | Boolean (_, optOrigin)
-            | Integer (_, optOrigin)
-            | Integer64 (_, optOrigin)
+            | Bool (_, optOrigin)
+            | Int (_, optOrigin)
+            | Int64 (_, optOrigin)
             | Single (_, optOrigin)
             | Double (_, optOrigin)
             | Vector2 (_, optOrigin)
             | String (_, optOrigin)
             | List (_, optOrigin)
-            | Mapping optOrigin
             | Violation (_, optOrigin)
             | Reference (_, optOrigin)
             | Call (_, optOrigin)
             | Break (_, optOrigin)
+            | Do (_, _, optOrigin)
             | Fun (_, _, _, optOrigin)
             | Get (_, _, optOrigin)
             | Let (_, _, optOrigin)
             | If (_, _, _, optOrigin)
             | Try (_, _, optOrigin)
-            | Do (_, _, optOrigin)
             | Binding (_, optOrigin)
             | Quote (_, optOrigin) -> optOrigin
 
@@ -96,7 +93,7 @@ module Scripting =
             destType = typeof<Expr>
 
         override this.ConvertTo (_, _, _, _) =
-            failwith "Not yet implemented"
+            failwith "Not yet implemented."
 
         override this.CanConvertFrom (_, sourceType) =
             sourceType = typeof<Symbol> ||
@@ -113,10 +110,10 @@ module Scripting =
                     | None -> Binding (!!str, optOrigin) :> obj
                 | Symbol.Number (str, optOrigin) ->
                     match Int32.TryParse str with
-                    | (true, int) -> Integer (int, optOrigin) :> obj
+                    | (true, int) -> Int (int, optOrigin) :> obj
                     | (false, _) ->
                         match Int64.TryParse str with
-                        | (true, int64) -> Integer64 (int64, optOrigin) :> obj
+                        | (true, int64) -> Int64 (int64, optOrigin) :> obj
                         | (false, _) ->
                             if str.EndsWith "f" || str.EndsWith "F" then
                                 match Single.TryParse str with
@@ -130,24 +127,27 @@ module Scripting =
                 | Symbol.Quote (str, optOrigin) -> Quote (str, optOrigin) :> obj
                 | Symbol.Symbols (symbols, optOrigin) ->
                     match symbols with
-                    | Atom (name, nameOptOrigin) :: tail when name = "Let" || name = "Try" || name = "If" || name = "Break" ->
+                    | Atom (name, nameOptOrigin) :: tail when name = "let" || name = "try" || name = "if" || name = "do" || name = "break" ->
                         match name with
-                        | "Let" ->
+                        | "let" ->
                             match tail with
                             | [Atom (name, _); body] -> Let (!!name, symbolToExpr body, optOrigin) :> obj
-                            | _ -> Violation ("Invalid Let form. Requires 1 name and 1 body.", optOrigin) :> obj
-                        | "Try" ->
+                            | _ -> Violation ("Invalid let form. Requires 1 name and 1 body.", optOrigin) :> obj
+                        | "try" ->
                             match tail with
                             | [body; Symbols (handlers, _)] -> Try (symbolToExpr body, List.map symbolToExpr handlers, optOrigin) :> obj
-                            | _ -> Violation ("Invalid Try form. Requires 1 body and a handler list.", optOrigin) :> obj
-                        | "If" ->
+                            | _ -> Violation ("Invalid try form. Requires 1 body and a handler list.", optOrigin) :> obj
+                        | "if" ->
                             match tail with
                             | [condition; consequent; alternative] -> If (symbolToExpr condition, symbolToExpr consequent, symbolToExpr alternative, optOrigin) :> obj
-                            | _ -> Violation ("Invalid If form. Requires 3 arguments.", optOrigin) :> obj
-                        | "Break" ->
+                            | _ -> Violation ("Invalid if form. Requires 3 arguments.", optOrigin) :> obj
+                        | "do" ->
                             match tail with
-                            | [body] -> Break (symbolToExpr body, optOrigin) :> obj
-                            | _ -> Violation ("Invalid Break form. Requires 1 body.", optOrigin) :> obj
+                            | Atom (name, _) :: args -> Do (!!name, List.map symbolToExpr args, optOrigin) :> obj
+                            | _ -> Violation ("Invalid do form. Requires 1 name and an argument list.", optOrigin) :> obj
+                        | "break" ->
+                            let content = symbolToExpr (Symbols (tail, optOrigin))
+                            Break (content, optOrigin) :> obj
                         | _ -> Call (Binding (!!name, nameOptOrigin) :: List.map symbolToExpr tail, optOrigin) :> obj
                     | _ -> Call (List.map symbolToExpr symbols, optOrigin) :> obj
             | :? Expr -> source
