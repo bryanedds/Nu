c03fcbacb11c6a831a03bff64455d5e2e4eb1443
bryanedds <bryanedds@gmail.com>
2016-08-20 11:53:47 -0400

    Removed HashSet.ForceAdd as it just doesn't make much sense.

--- Prime/Prime/Prime/HashSet.fs
+++ Prime/Prime/Prime/HashSet.fs
@@ -17,12 +17,6 @@ module HashSetExtension =
     /// HashSet extension methods.
     type HashSet<'a> with
 
-        /// Force the addition of an item, replacing the existing one if necessary.
-        member this.ForceAdd item =
-            let forced = this.Remove item
-            this.Add item |> ignore
-            forced
-
         /// Try to add an item, returning false upon failure.
         member this.TryAdd item =
             if not ^ this.Contains item
