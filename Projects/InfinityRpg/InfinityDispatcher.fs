60ac65c5e8f18d49400cdc331ab17204b365396e
bryanedds <bryanedds@gmail.com>
2016-10-20 00:09:25 -0400

    Simplified splash / dissolve screen creation. *BREAKING CHANGE*
    Former-commit-id: 413fc2f794218d1b0cf7bc49f3750f782bcd18d2

--- Projects/InfinityRpg/InfinityDispatcher.fs
+++ Projects/InfinityRpg/InfinityDispatcher.fs
@@ -12,18 +12,18 @@ type InfinityDispatcher () =
         World.handleAsScreenTransition Simulants.Gameplay event world
 
     static let createTitle world =
-        let world = World.createDissolveScreenFromGroupFile false Constants.InfinityRpg.DissolveData Constants.FilePaths.TitleGroup None (Some Simulants.Title.ScreenName) world |> snd
+        let world = World.createDissolveScreenFromGroupFile None (Some Simulants.Title.ScreenName) Constants.InfinityRpg.DissolveData Constants.FilePaths.TitleGroup world |> snd
         let world = World.subscribe (World.handleAsScreenTransition Simulants.Credits) (Events.Click ->- Simulants.TitleCredits) Simulants.Game world
         let world = World.subscribe (handleAsScreenTransitionToGameplay false) (Events.Click ->- Simulants.TitleNewGame) Simulants.Game world
         let world = World.subscribe (handleAsScreenTransitionToGameplay true) (Events.Click ->- Simulants.TitleLoadGame) Simulants.Game world
         World.subscribe World.handleAsExit (Events.Click ->- Simulants.TitleExit) Simulants.Game world
 
     static let createCredits world =
-        let world = World.createDissolveScreenFromGroupFile false Constants.InfinityRpg.DissolveData Constants.FilePaths.CreditsGroup None (Some Simulants.Credits.ScreenName) world |> snd
+        let world = World.createDissolveScreenFromGroupFile None (Some Simulants.Credits.ScreenName) Constants.InfinityRpg.DissolveData Constants.FilePaths.CreditsGroup world |> snd
         World.subscribe (World.handleAsScreenTransition Simulants.Title) (Events.Click ->- Simulants.CreditsBack) Simulants.Game world
 
     static let createGameplay world =
-        let world = World.createDissolveScreenFromGroupFile<GameplayDispatcher> true Constants.InfinityRpg.DissolveData Constants.FilePaths.HudGroup None (Some Simulants.Gameplay.ScreenName) world |> snd
+        let world = World.createDissolveScreenFromGroupFile<GameplayDispatcher> None (Some Simulants.Gameplay.ScreenName) Constants.InfinityRpg.DissolveData Constants.FilePaths.HudGroup world |> snd
         let world = Simulants.Hud.SetPersistent false world // do not persist the Hud
         World.subscribe (World.handleAsScreenTransition Simulants.Title) (Events.Click ->- Simulants.HudBack) Simulants.Game world
 
@@ -33,5 +33,5 @@ type InfinityDispatcher () =
         let world = createTitle world
         let world = createCredits world
         let world = createGameplay world
-        let (splash, world) = World.createSplashScreen false Constants.InfinityRpg.NuSplashData Simulants.Title None (Some Simulants.Splash.ScreenName) world
+        let (splash, world) = World.createSplashScreen None (Some Simulants.Splash.ScreenName) Constants.InfinityRpg.NuSplashData Simulants.Title world
         World.selectScreen splash world
