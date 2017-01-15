ef80b7dd3cf8168a9158041103a46504d724ee23
Bryan <bryan@jet.com>
2017-01-15 16:37:11 -0500

    Testing nested expressions.

--- Nu/Nu/ScriptingTests.fs
+++ Nu/Nu/ScriptingTests.fs
@@ -19,6 +19,14 @@ module ScriptingTests =
         | (Scripting.Int (result, _), _) -> Assert.Equal (2, result)
         | (_, _) -> Assert.False true
 
+    let [<Fact>] oneAndOneAndOneAndOneIsFour () =
+        let world = World.makeEmpty ()
+        let env = Env.make World.choose false (dictPlus []) (Game :> Simulant) world
+        let expr = scvalue<Scripting.Expr> "[+ [+ 1 1] [+ 1 1]]"
+        match ScriptSystem.eval expr env with
+        | (Scripting.Int (result, _), _) -> Assert.Equal (4, result)
+        | (_, _) -> Assert.False true
+
     let [<Fact>] setEyeCenterFromGameScriptWorks () =
         let world = World.makeEmpty ()
         let onInit = scvalue<Scripting.Expr> "[set EyeCenter [v2 10f 10f]]"
