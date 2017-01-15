0a0e6aefe92a89e6fe339788e8f789c1532f294a
Bryan <bryan@jet.com>
2017-01-15 16:34:13 -0500

    Tested binop.
    Former-commit-id: 64ed237336af44f14afdc1467f58acf34be9c847

--- Nu/Nu/ScriptingTests.fs
+++ Nu/Nu/ScriptingTests.fs
@@ -11,9 +11,17 @@ open Nu.Simulants
 open OpenTK
 module ScriptingTests =
 
+    let [<Fact>] oneAndOneIsTwo () =
+        let world = World.makeEmpty ()
+        let env = Env.make World.choose false (dictPlus []) (Game :> Simulant) world
+        let expr = scvalue<Scripting.Expr> "[+ 1 1]"
+        match ScriptSystem.eval expr env with
+        | (Scripting.Int (result, _), _) -> Assert.Equal (2, result)
+        | (_, _) -> Assert.False true
+
     let [<Fact>] setEyeCenterFromGameScriptWorks () =
         let world = World.makeEmpty ()
-        let onInit = scvalue<Scripting.Expr> "[set EyeCenter [v2 10.0f 10.0f]]"
+        let onInit = scvalue<Scripting.Expr> "[set EyeCenter [v2 10f 10f]]"
         let script = { Script.empty with OnInit = onInit }
         let world = Game.SetScript script world
         Assert.Equal (Vector2 (10.0f, 10.0f), Game.GetEyeCenter world)
