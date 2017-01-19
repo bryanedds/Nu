f2c0c45e2a6ddca4895a3c7f5817243c2493f9b5
Bryan <bryan@jet.com>
2017-01-19 10:49:39 -0500

    Typo fixes.
    Former-commit-id: d07c18e98f374231d937447791300e1eaf041860

--- Nu/Nu/ScriptingTests.fs
+++ Nu/Nu/ScriptingTests.fs
@@ -114,7 +114,7 @@ module ScriptingTests =
 
     let [<Fact>] setEyeCenterFromGameScriptWorks () =
         let world = World.makeEmpty ()
-        let onInit = scvalue<Scripting.Expr> "[set EyeCenter [v2 10f 10f]]"
-        let script = { Script.empty with OnInit = onInit }
+        let onRegister = scvalue<Scripting.Expr> "[set EyeCenter [v2 10f 10f]]"
+        let script = { Script.empty with OnRegister = onRegister }
         let world = Game.SetScript script world
         Assert.Equal (Vector2 (10.0f, 10.0f), Game.GetEyeCenter world)
