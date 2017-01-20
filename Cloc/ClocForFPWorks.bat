0883f53b70eb9b980ac8bfafb9585389b3992bf3
Bryan <bryan@jet.com>
2017-01-19 21:42:01 -0500

    Added back cloc. New primitives for scripting lang.
    Former-commit-id: 30f06b2173b6e7a104bf640c72abd4c271c6f5f8

---
+++ Cloc/ClocForFPWorks.bat
@@ -0,0 +1,4 @@
+cloc-1.60.exe --by-file --force-lang="Fortran 90",fs --exclude-ext=xml .. > Loc.txt
+cloc-1.60.exe --by-file --force-lang="Fortran 90",fs --exclude-ext=xml ..\Prime > PrimeLoc.txt
+cloc-1.60.exe --by-file --force-lang="Fortran 90",fs --exclude-ext=xml ..\Nu > NuLoc.txt
+pause
