61fb38cc434d816211c26ad0f1220baa3086e92a
Bryan <bryan@jet.com>
2017-02-11 18:08:07 -0500

    Used of monitor / monitorPlus rather than subscribe / subscribePlus is now canonical.

--- Projects/InfinityRpg/InfinityDispatcher.fs
+++ Projects/InfinityRpg/InfinityDispatcher.fs
@@ -13,19 +13,19 @@ type InfinityDispatcher () =
 
     static let createTitle world =
         let world = World.createDissolveScreenFromLayerFile None (Some Simulants.Title.ScreenName) Constants.InfinityRpg.DissolveData Assets.TitleLayerFilePath world |> snd
-        let world = World.subscribe (World.handleAsScreenTransition Simulants.Credits) (Events.Click ->- Simulants.TitleCredits) Simulants.Game world
-        let world = World.subscribe (handleAsScreenTransitionToGameplay false) (Events.Click ->- Simulants.TitleNewGame) Simulants.Game world
-        let world = World.subscribe (handleAsScreenTransitionToGameplay true) (Events.Click ->- Simulants.TitleLoadGame) Simulants.Game world
-        World.subscribePlus (makeGuid ()) World.handleAsExit (Events.Click ->- Simulants.TitleExit) Simulants.Game world |> snd
+        let world = World.monitor (World.handleAsScreenTransition Simulants.Credits) (Events.Click ->- Simulants.TitleCredits) Simulants.Game world
+        let world = World.monitor (handleAsScreenTransitionToGameplay false) (Events.Click ->- Simulants.TitleNewGame) Simulants.Game world
+        let world = World.monitor (handleAsScreenTransitionToGameplay true) (Events.Click ->- Simulants.TitleLoadGame) Simulants.Game world
+        World.monitorPlus World.handleAsExit (Events.Click ->- Simulants.TitleExit) Simulants.Game world |> snd
 
     static let createCredits world =
         let world = World.createDissolveScreenFromLayerFile None (Some Simulants.Credits.ScreenName) Constants.InfinityRpg.DissolveData Assets.CreditsLayerFilePath world |> snd
-        World.subscribe (World.handleAsScreenTransition Simulants.Title) (Events.Click ->- Simulants.CreditsBack) Simulants.Game world
+        World.monitor (World.handleAsScreenTransition Simulants.Title) (Events.Click ->- Simulants.CreditsBack) Simulants.Game world
 
     static let createGameplay world =
         let world = World.createDissolveScreenFromLayerFile<GameplayDispatcher> None (Some Simulants.Gameplay.ScreenName) Constants.InfinityRpg.DissolveData Assets.HudLayerFilePath world |> snd
         let world = Simulants.Hud.SetPersistent false world // do not persist the Hud
-        World.subscribe (World.handleAsScreenTransition Simulants.Title) (Events.Click ->- Simulants.HudBack) Simulants.Game world
+        World.monitor (World.handleAsScreenTransition Simulants.Title) (Events.Click ->- Simulants.HudBack) Simulants.Game world
 
     override dispatcher.Register (_, world) =
         let world = World.hintRenderPackageUse Assets.GuiPackageName world
