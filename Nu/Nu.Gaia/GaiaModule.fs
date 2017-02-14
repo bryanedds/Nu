b86cdf9b41e103f8d32c45ede016bd8e0919685a
Bryan <bryan@jet.com>
2017-02-14 13:37:28 -0500

    Fixes to entity mounting.

--- Nu/Nu.Gaia/GaiaModule.fs
+++ Nu/Nu.Gaia/GaiaModule.fs
@@ -210,8 +210,8 @@ module Gaia =
                 let world =
                     World.updateUserState (fun editorState ->
                         let mousePositionWorld = World.mouseToWorld (entity.GetViewType world) mousePosition world
-                        let entityPosition = 
-                            if entity.HasFacet typeof<MountFacet> world && Option.isSome (entity.GetParentOpt world)
+                        let entityPosition =
+                            if entity.HasFacet typeof<MountFacet> world && entity.ParentExists world
                             then entity.GetPositionLocal world
                             else entity.GetPosition world
                         { editorState with DragEntityState = DragEntityPosition (entityPosition + mousePositionWorld, mousePositionWorld, entity) })
@@ -822,7 +822,7 @@ module Gaia =
                 let entityPosition = (pickOffset - mousePositionWorldOrig) + (mousePositionWorld - mousePositionWorldOrig)
                 let entityPositionSnapped = Math.snap2F positionSnap entityPosition
                 let world =
-                    if entity.HasFacet typeof<MountFacet> world && Option.isSome (entity.GetParentOpt world)
+                    if entity.HasFacet typeof<MountFacet> world && entity.ParentExists world
                     then entity.SetPositionLocal entityPositionSnapped world
                     else entity.SetPosition entityPositionSnapped world
                 let world = World.propagateEntityPhysics entity world
