48d1589b78026b14f7b6f654a311a77af5337d5c
symbolsoft <symbolsoft7@gmail.com>
2018-04-03 18:34:35 -0400

    Initial design work on pickers.

---
+++ Nu/Nu.Gaia.Design/AssetPicker.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Nu.Gaia.Design
+{
+    partial class AssetPicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.assetTreeView = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(175, 248);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(97, 29);
+            this.cancelButton.TabIndex = 9;
+            this.cancelButton.Text = "&Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(72, 248);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(97, 29);
+            this.okButton.TabIndex = 8;
+            this.okButton.Text = "&OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Search";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(12, 25);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(260, 20);
+            this.searchTextBox.TabIndex = 5;
+            // 
+            // assetTreeView
+            // 
+            this.assetTreeView.Location = new System.Drawing.Point(12, 51);
+            this.assetTreeView.Name = "assetTreeView";
+            this.assetTreeView.Size = new System.Drawing.Size(260, 191);
+            this.assetTreeView.TabIndex = 10;
+            // 
+            // AssetPicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 286);
+            this.Controls.Add(this.assetTreeView);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "AssetPicker";
+            this.Text = "Pick Asset";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Button cancelButton;
+        public System.Windows.Forms.Button okButton;
+        public System.Windows.Forms.TextBox searchTextBox;
+        public System.Windows.Forms.TreeView assetTreeView;
+    }
+}
---
+++ Nu/Nu.Gaia.Design/EntityPicker.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Nu.Gaia.Design
+{
+    partial class EntityPicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.entitiesListView = new System.Windows.Forms.ListView();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(12, 25);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(260, 20);
+            this.searchTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search";
+            // 
+            // entitiesListView
+            // 
+            this.entitiesListView.Location = new System.Drawing.Point(12, 51);
+            this.entitiesListView.Name = "entitiesListView";
+            this.entitiesListView.Size = new System.Drawing.Size(260, 191);
+            this.entitiesListView.TabIndex = 2;
+            this.entitiesListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(72, 248);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(97, 29);
+            this.okButton.TabIndex = 3;
+            this.okButton.Text = "&OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(175, 248);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(97, 29);
+            this.cancelButton.TabIndex = 4;
+            this.cancelButton.Text = "&Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // EntityPicker
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(284, 286);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.entitiesListView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "EntityPicker";
+            this.Text = "Pick Entity";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView entitiesListView;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
