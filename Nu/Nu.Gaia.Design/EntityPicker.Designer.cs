9c4b27a2f67165df7e644a58a254d006d9efafca
symbolsoft <symbolsoft7@gmail.com>
2018-04-04 09:38:49 -0400

    More work on entity picker.

--- Nu/Nu.Gaia.Design/EntityPicker.Designer.cs
+++ Nu/Nu.Gaia.Design/EntityPicker.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.searchTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.entitiesListView = new System.Windows.Forms.ListView();
             this.okButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
+            this.entityListBox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // searchTextBox
@@ -45,19 +45,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(12, 8);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Search";
-            // 
-            // entitiesListView
-            // 
-            this.entitiesListView.Location = new System.Drawing.Point(12, 51);
-            this.entitiesListView.Name = "entitiesListView";
-            this.entitiesListView.Size = new System.Drawing.Size(260, 191);
-            this.entitiesListView.TabIndex = 2;
-            this.entitiesListView.UseCompatibleStateImageBehavior = false;
             // 
             // okButton
             // 
@@ -78,6 +70,14 @@
             this.cancelButton.Text = "&Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
             // 
+            // entityListBox
+            // 
+            this.entityListBox.FormattingEnabled = true;
+            this.entityListBox.Location = new System.Drawing.Point(12, 51);
+            this.entityListBox.Name = "entityListBox";
+            this.entityListBox.Size = new System.Drawing.Size(260, 186);
+            this.entityListBox.TabIndex = 5;
+            // 
             // EntityPicker
             // 
             this.AcceptButton = this.okButton;
@@ -85,9 +85,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
             this.ClientSize = new System.Drawing.Size(284, 286);
+            this.Controls.Add(this.entityListBox);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.okButton);
-            this.Controls.Add(this.entitiesListView);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.searchTextBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
@@ -101,8 +101,8 @@
         #endregion
         private System.Windows.Forms.Label label1;
         public System.Windows.Forms.TextBox searchTextBox;
-        public System.Windows.Forms.ListView entitiesListView;
         public System.Windows.Forms.Button okButton;
         public System.Windows.Forms.Button cancelButton;
+        public System.Windows.Forms.ListBox entityListBox;
     }
 }
