8eb11f7bcdf8ced76f64b7f87210434ba6995efc
bryanedds <bryanedds@gmail.com>
2018-10-19 03:34:12 -0400

    Added experimental Simulation generalization.
    Former-commit-id: c5db41c6c725ea795b135e2ccdbfb50d2e3a261b

---
+++ Nu/Nu/Simulation.fs
@@ -0,0 +1,18 @@
+﻿namespace Nu
+
+/// A generalization of simulations in terms of an observable property bag.
+type Simulation<'key, 'uuid> =
+    abstract member GetProperty : 'key -> obj option
+    abstract member SetProperty : 'key -> obj option -> Simulation<'key, 'uuid>
+    abstract member HandlePropertyChange : 'key -> PropertyChangeHandler<'key, 'uuid> -> ('uuid * Simulation<'key, 'uuid>)
+    abstract member ForgetPropertyChange : 'uuid -> Simulation<'key, 'uuid>
+
+/// Handles property changes.
+and PropertyChangeHandler<'key, 'uuid> = Simulation<'key, 'uuid> -> Simulation<'key, 'uuid> -> Simulation<'key, 'uuid>
+
+[<RequireQualifiedAccess>]
+module Simulation =
+    let getProperty (key : 'key) (simulation : Simulation<'key, 'uuid>) = simulation.GetProperty key
+    let setProperty (key : 'key) (valueOpt : obj option) (simulation : Simulation<'key, 'uuid>) = simulation.SetProperty key valueOpt
+    let handlePropertyChange (key : 'key) handler (simulation : Simulation<'key, 'uuid>) = simulation.HandlePropertyChange key handler
+    let forgetPropertyChange (uuid : 'uuid) (simulation : Simulation<'key, 'uuid>) = simulation.ForgetPropertyChange uuid
