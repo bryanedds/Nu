298311c0a532f4535231bd6122abe3235b998f2b
bryanedds <bryanedds@gmail.com>
2018-11-07 15:02:56 -0500

    Updated Sedela documentation.
    Former-commit-id: 6e2d1ccff4914e62003dd429866827682cd1f1cb

--- Nu/Nu/Simulation.fs
+++ Nu/Nu/Simulation.fs
@@ -1,14 +1,14 @@
 ﻿namespace Nu
 open Prime
 
-/// A generalization of simulations in terms of an observable property bag.
-type Simulation<'key> =
+/// Handles simulation property changes.
+type PropertyChangeHandler<'key> = Simulation<'key> -> Simulation<'key> -> Simulation<'key>
+
+/// A simulation in terms of an observable property bag.
+and Simulation<'key> =
     abstract member GetProperty<'a> : 'key -> 'a option
     abstract member SetProperty<'a> : 'key -> 'a option -> Simulation<'key>
     abstract member HandlePropertyChange : 'key -> PropertyChangeHandler<'key> -> (Simulation<'key> -> Simulation<'key>) * Simulation<'key>
-
-/// Handles property changes.
-and PropertyChangeHandler<'key> = Simulation<'key> -> Simulation<'key> -> Simulation<'key>
 
 [<RequireQualifiedAccess>]
 module Simulation =
