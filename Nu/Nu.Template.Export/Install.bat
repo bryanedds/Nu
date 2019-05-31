4daec49d7898dd58bf716347794110b6a5803abd
symbolsoft <symbolsoft7@gmail.com>
2019-05-31 02:36:52 -0400

    Reverted to VS2017 - VS2019 is not yet ready for F# development.
    Former-commit-id: 9722c14198864ff66c3637f50f7e41136466f796

--- Nu/Nu.Template.Export/Install.bat
+++ Nu/Nu.Template.Export/Install.bat
@@ -1,4 +1,4 @@
 @echo off
-@echo Installing NuGame project template for Visual Studio 2019...
-copy NuGame.zip "%userprofile%\Documents\Visual Studio 2019\Templates\ProjectTemplates"
+@echo Installing NuGame project template for Visual Studio 2017...
+copy NuGame.zip "%userprofile%\Documents\Visual Studio 2017\Templates\ProjectTemplates"
 pause
