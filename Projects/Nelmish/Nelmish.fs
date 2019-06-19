23f1a87e3bd2e9347c041d6c6cd59886da232f2a
symbolsoft <symbolsoft7@gmail.com>
2019-06-19 00:47:31 -0400

    Added some comments to Nelmish.
    Former-commit-id: 092a8d01145f896fc24431e17f316ef2dc15381d

--- Projects/Nelmish/Nelmish.fs
+++ Projects/Nelmish/Nelmish.fs
@@ -1,40 +1,48 @@
 ﻿namespace Nelmish
-open System
 open Prime
 open Nu
 open Nu.Declarative
 module Nelmish =
 
+    // here we create addresses for the simulants that we are going to define for our game
     let Screen = new Screen "Screen"
     let Layer = Screen => "Layer"
     let IncButton = Layer => "Inc"
     let DecButton = Layer => "Dec"
     let CountText = Layer => "Count"
 
+    // this is our Elm-style model type
     type Model =
         int
 
+    // this is our Elm-style message type
     type Message =
         | Increment
         | Decrement
 
+    // this is our model property declaration
     type Game with
         member this.GetModel world : Model = this.Get Property? Model world
         member this.SetModel (value : Model) world = this.Set Property? Model value world
         member this.Model = PropertyTag.make this Property? Model this.GetModel this.SetModel
 
+    // this is our Elm-style game simulant dispatcher
     type NelmishDispatcher () =
         inherit GameDispatcher<Model, Message, unit> (fun game -> game.Model)
 
+        // here we define the Bindings used to connect events to their desired messages
         override this.Bindings (_, _, _) =
             [IncButton.ClickEvent ==> Increment
              DecButton.ClickEvent ==> Decrement]
 
+        // here we handle the above messages
         override this.Update (message, model, _, _) =
             match message with
             | Increment -> just (inc model)
             | Decrement -> just (dec model)
 
+        // here we describe the layout of the game including its one screen, one layer, and three
+        // entities, two of which are button controls and one of which is a text control
         override this.Layout (_, game, _) =
             [Layout.screen Screen Vanilla []
                 [Layout.layer Layer []
