f1ff3edd9adb355b70d34332b956ea0db2f7c46c
symbolsoft <symbolsoft7@gmail.com>
2019-06-22 10:46:52 -0400

    Updated Prime.
    Former-commit-id: 7790dc97aa3ed6765d0060f4986ca8f63aab3224

--- Projects/Nelmish/Nelmish.fs
+++ Projects/Nelmish/Nelmish.fs
@@ -8,11 +8,12 @@ module Nelmish =
     // here we create references for the entities that we are going to define for our game
     let DecrementButton = Default.Layer => "Decrement"
     let IncrementButton = Default.Layer => "Increment"
-    let CounterButton = Default.Layer => "Counter"
+    let ResetButton = Default.Layer => "Reset"
+    let CounterText = Default.Layer => "Counter"
 
     // this is our Elm-style model type
     type Model =
-        int * unit list
+        int
 
     // this is our Elm-style message type
     type Message =
@@ -32,33 +33,37 @@ module Nelmish =
 
         // this is our model property definition
         static member Properties =
-            [define Game.Model (0, [])]
+            [define Game.Model 0]
 
         // here we define the Bindings used to connect events to their desired messages
         override this.Bindings (_, _, _) =
             [DecrementButton.ClickEvent ==> Decrement
              IncrementButton.ClickEvent ==> Increment
-             CounterButton.ClickEvent ==> Reset]
+             ResetButton.ClickEvent ==> Reset]
 
         // here we handle the above messages
         override this.Update (message, model, _, _) =
             match message with
-            | Decrement -> just (dec (fst model), snd model)
-            | Increment -> just (inc (fst model), () :: snd model)
-            | Reset -> just (0, [])
+            | Decrement -> just (dec model)
+            | Increment -> just (inc model)
+            | Reset -> just 0
 
-        // here we describe the layout of the game including its one screen, one layer, and three button entities
+        // here we describe the layout of the game including its one screen, one layer, three
+        // button entities, and one text control.
         override this.Layout (_, game, _) =
             [Layout.screen Default.Screen Vanilla []
                 [Layout.layer Default.Layer []
-                    [Layout.entities
-                        (game.Model --> snd) $ fun () -> Layout.button Default.Entity [Entity.Text == "Alpha"]
-                     Layout.button DecrementButton
+                    [Layout.button DecrementButton
                         [Entity.Text == "-"
                          Entity.Position == Vector2 (-256.0f, 64.0f)]
                      Layout.button IncrementButton
                         [Entity.Text == "+"
                          Entity.Position == Vector2 (0.0f, 64.0f)]
-                     Layout.button CounterButton
-                        [Entity.Text =|= game.Model --> (fst >> string)
-                         Entity.Position == Vector2 (-128.0f, -32.0f)]]]]
+                     Layout.text CounterText
+                        [Entity.Text =|= game.Model --> scstring
+                         Entity.Position == Vector2 (-128.0f, -32.0f)]
+                     Layout.entityIf
+                        (game.Model --> isNonZero) $ fun () ->
+                            Layout.button ResetButton
+                                [Entity.Text == "Reset"
+                                 Entity.Position == Vector2 (-128.0f, -128.0f)]]]]
