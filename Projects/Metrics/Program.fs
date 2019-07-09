1d774c4203c67bd9a3c9724ba5d35a615c137920
bryanedds <bryanedds@gmail.com>
2019-07-09 10:32:17 -0400

    Using isGuid everywhere for consistency. Added more entities to Metrics.
    Former-commit-id: ba82790df170f54de772fdf2731c3c5d8a701ff3

--- Projects/Metrics/Program.fs
+++ Projects/Metrics/Program.fs
@@ -23,15 +23,16 @@ type MyGameDispatcher () =
         let world = World.createScreen (Some Default.Screen.ScreenName) world |> snd
         let world = World.createLayer (Some Default.Layer.LayerName) Default.Screen world |> snd
         let indices =
+            // create approximately 3000 entities
             seq {
-                for i in 0 .. 50 do
-                    for j in 0 .. 30 do
-                    yield v2 (single i * 16.0f) (single j * 16.0f) }
+                for i in 0 .. 70 do
+                    for j in 0 .. 43 do
+                    yield v2 (single i * 12.0f) (single j * 12.0f) }
         let world =
             Seq.fold (fun world position ->
                 let (entity, world) = World.createEntity<MyEntityDispatcher> None DefaultOverlay Default.Layer world
-                let world = entity.SetPosition (position + v2 -400.0f -240.0f) world
-                let world = entity.SetSize (v2One * 10.0f) world
+                let world = entity.SetPosition (position + v2 -420.0f -265.0f) world
+                let world = entity.SetSize (v2One * 8.0f) world
                 let world = entity.SetImperative true world // makes updates faster by using mutation
                 let world = entity.SetIgnoreLayer true world // makes actualization faster by not touching the containing layer
                 let world = entity.SetOmnipresent true world // makes updates faster by not touching the entity tree
