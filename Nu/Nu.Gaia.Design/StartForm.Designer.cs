19cd5f43ea324670f106dbc2dc88670327bec6f1
bryanedds <bryanedds@gmail.com>
2019-07-14 10:35:19 -0400

    More documentation updates.

--- Nu/Nu.Gaia.Design/StartForm.Designer.cs
+++ Nu/Nu.Gaia.Design/StartForm.Designer.cs
@@ -37,30 +37,27 @@
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
-            // binaryText
+            // binaryFilePathText
             // 
-            this.binaryFilePathText.Location = new System.Drawing.Point(14, 45);
-            this.binaryFilePathText.Margin = new System.Windows.Forms.Padding(2);
-            this.binaryFilePathText.Name = "binaryText";
-            this.binaryFilePathText.Size = new System.Drawing.Size(411, 20);
+            this.binaryFilePathText.Location = new System.Drawing.Point(21, 69);
+            this.binaryFilePathText.Name = "binaryFilePathText";
+            this.binaryFilePathText.Size = new System.Drawing.Size(614, 26);
             this.binaryFilePathText.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 18);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(16, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(180, 13);
+            this.label1.Size = new System.Drawing.Size(268, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Select game\'s executable for editing.";
             // 
             // selectExecutable
             // 
-            this.selectExecutable.Location = new System.Drawing.Point(429, 45);
-            this.selectExecutable.Margin = new System.Windows.Forms.Padding(2);
+            this.selectExecutable.Location = new System.Drawing.Point(644, 69);
             this.selectExecutable.Name = "selectExecutable";
-            this.selectExecutable.Size = new System.Drawing.Size(24, 20);
+            this.selectExecutable.Size = new System.Drawing.Size(36, 31);
             this.selectExecutable.TabIndex = 2;
             this.selectExecutable.Text = "...";
             this.selectExecutable.UseVisualStyleBackColor = true;
@@ -68,9 +65,10 @@
             // 
             // customButton
             // 
-            this.customButton.Location = new System.Drawing.Point(262, 141);
+            this.customButton.Location = new System.Drawing.Point(393, 217);
+            this.customButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.customButton.Name = "customButton";
-            this.customButton.Size = new System.Drawing.Size(96, 23);
+            this.customButton.Size = new System.Drawing.Size(144, 35);
             this.customButton.TabIndex = 3;
             this.customButton.Text = "Use Settings";
             this.customButton.UseVisualStyleBackColor = true;
@@ -79,9 +77,10 @@
             // defaultButton
             // 
             this.defaultButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.defaultButton.Location = new System.Drawing.Point(364, 141);
+            this.defaultButton.Location = new System.Drawing.Point(546, 217);
+            this.defaultButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.defaultButton.Name = "defaultButton";
-            this.defaultButton.Size = new System.Drawing.Size(91, 23);
+            this.defaultButton.Size = new System.Drawing.Size(136, 35);
             this.defaultButton.TabIndex = 4;
             this.defaultButton.Text = "Use Defaults";
             this.defaultButton.UseVisualStyleBackColor = true;
@@ -89,9 +88,10 @@
             // openGameplayScreenCheckBox
             // 
             this.openGameplayScreenCheckBox.AutoSize = true;
-            this.openGameplayScreenCheckBox.Location = new System.Drawing.Point(14, 91);
+            this.openGameplayScreenCheckBox.Location = new System.Drawing.Point(21, 140);
+            this.openGameplayScreenCheckBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.openGameplayScreenCheckBox.Name = "openGameplayScreenCheckBox";
-            this.openGameplayScreenCheckBox.Size = new System.Drawing.Size(139, 17);
+            this.openGameplayScreenCheckBox.Size = new System.Drawing.Size(205, 24);
             this.openGameplayScreenCheckBox.TabIndex = 5;
             this.openGameplayScreenCheckBox.Text = "Open Gameplay Screen";
             this.openGameplayScreenCheckBox.UseVisualStyleBackColor = true;
@@ -103,10 +103,10 @@
             // StartForm
             // 
             this.AcceptButton = this.customButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.defaultButton;
-            this.ClientSize = new System.Drawing.Size(467, 179);
+            this.ClientSize = new System.Drawing.Size(700, 275);
             this.ControlBox = false;
             this.Controls.Add(this.openGameplayScreenCheckBox);
             this.Controls.Add(this.defaultButton);
@@ -114,11 +114,11 @@
             this.Controls.Add(this.selectExecutable);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.binaryFilePathText);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "StartForm";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Editor Start Configuration";
             this.ResumeLayout(false);
             this.PerformLayout();
