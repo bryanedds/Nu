d0f71e5c4c9fcc08041e711219f216071064e184
bryanedds <bryanedds@gmail.com>
2020-01-04 03:11:46 -0500

    More fixes for simulant streamers.
    Former-commit-id: 425caae663c2ecbb900f88443a81a90f40c8dd1c

--- Nu/Nu/WorldScreen.fs
+++ Nu/Nu/WorldScreen.fs
@@ -293,12 +293,12 @@ module WorldScreenModule =
                         WorldModule.equate5 name simulant property breaking world)
                         world equations
                 let world =
-                    List.fold (fun world (screen, lens, mapper) ->
-                        World.expandLayerStream lens mapper screen world)
+                    List.fold (fun world (screen, lens, indexer, mapper) ->
+                        World.expandLayerStream lens indexer mapper screen world)
                         world layerStreams
                 let world =
-                    List.fold (fun world (layer, lens, mapper) ->
-                        World.expandEntityStream lens mapper None layer world)
+                    List.fold (fun world (layer, lens, indexer, mapper) ->
+                        World.expandEntityStream lens indexer mapper None layer world)
                         world entityStreams
                 let world =
                     List.fold (fun world (owner, entityContents) ->
