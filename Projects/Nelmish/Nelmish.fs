7d1d55c3d79464bef6a0bda083b2813bd41ed378
bryanedds <bryanedds@gmail.com>
2020-01-27 07:57:45 -0500

    More work on elmish operator naming.
    Former-commit-id: 68036da7293dddeaed80c8bad9d1fd05040f1aca

--- Projects/Nelmish/Nelmish.fs
+++ Projects/Nelmish/Nelmish.fs
@@ -33,16 +33,16 @@ module Nelmish =
                     [Content.button "Decrement"
                         [Entity.Text == "-"
                          Entity.Position == v2 -256.0f 64.0f
-                         Entity.ClickEvent >=> msg Decrement]
+                         Entity.ClickEvent ==> msg Decrement]
                      Content.button "Increment"
                         [Entity.Text == "+"
                          Entity.Position == v2 0.0f 64.0f
-                         Entity.ClickEvent >=> msg Increment]
+                         Entity.ClickEvent ==> msg Increment]
                      Content.text "Counter"
-                        [Entity.Text ==> model --> scstring
+                        [Entity.Text <== model --> scstring
                          Entity.Position == v2 -128.0f -32.0f]
                      Content.entityIf (model --> isNonZero) $ fun _ _ _ ->
                         Content.button "Reset"
-                            [Entity.Text == "Reset"
+                            [Entity.Text == "Reset!"
                              Entity.Position == v2 -128.0f -128.0f
-                             Entity.ClickEvent >=> msg Reset]]]]
+                             Entity.ClickEvent ==|> fun _ -> msg Reset]]]]
