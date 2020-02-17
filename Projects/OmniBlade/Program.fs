d8929668e6b6071c8f6facdb0bfda3e0ffe2dea3
bryanedds <bryanedds@gmail.com>
2020-02-17 07:17:52 -0500

    Minor code clean-up.

--- Projects/OmniBlade/Program.fs
+++ Projects/OmniBlade/Program.fs
@@ -5,16 +5,9 @@ open OmniBlade
 
 type OmniPlugin () =
     inherit NuPlugin ()
-
-    override this.GetStandAloneGameDispatcher () =
-        typeof<StandAloneDispatcher>
-
-    override this.GetEditorGameDispatcher () =
-        typeof<EditorDispatcher>
-
-    override this.GetEditorScreenDispatcherOpt () =
-        Some typeof<BattleDispatcher>
-
+    override this.GetStandAloneGameDispatcher () = typeof<StandAloneDispatcher>
+    override this.GetEditorGameDispatcher () = typeof<EditorDispatcher>
+    override this.GetEditorScreenDispatcherOpt () = Some typeof<BattleDispatcher>
     override this.MakeOverlayRoutes () =
         [typeof<ButtonDispatcher>.Name, Some "ButtonDispatcherRouted"]
 
