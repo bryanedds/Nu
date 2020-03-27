0ca489e7774887ede8f025deba62139d59be2fb5
bryanedds <bryanedds@gmail.com>
2020-03-27 15:35:14 -0400

    Removed unecessary tabs in Elmario and Nelmish.
    Former-commit-id: ed07f61ce0c2e529578545d94d03ff4b5e8ca532

--- Projects/Elmario/Elmario.fs
+++ Projects/Elmario/Elmario.fs
@@ -31,41 +31,41 @@ type ElmarioDispatcher () =
             elif KeyboardState.isKeyDown (int SDL.SDL_Scancode.SDL_SCANCODE_RIGHT) then cmd MoveRight
             else cmd Nop]
 
-        // here we handle the Elm-style commands
-        override this.Command (_, command, _, world) =
-            let world =
-                match command with
-                | MoveLeft ->
-                    let physicsId = Simulants.Elmario.GetPhysicsId world
-                    if World.isBodyOnGround physicsId world
-                    then World.applyBodyForce (v2 -30000.0f 0.0f) physicsId world
-                    else World.applyBodyForce (v2 -7500.0f 0.0f) physicsId world
-                | MoveRight ->
-                    let physicsId = Simulants.Elmario.GetPhysicsId world
-                    if World.isBodyOnGround physicsId world
-                    then World.applyBodyForce (v2 30000.0f 0.0f) physicsId world
-                    else World.applyBodyForce (v2 7500.0f 0.0f) physicsId world
-                | Jump ->
-                    let physicsId = Simulants.Elmario.GetPhysicsId world
-                    if World.isBodyOnGround physicsId world then
-                        let world = World.applyBodyForce (v2 0.0f 2000000.0f) physicsId world
-                        World.playSound 0.5f (asset "Gameplay" "Jump") world
-                    else world
-                | Nop -> world
-            just world
+    // here we handle the Elm-style commands
+    override this.Command (_, command, _, world) =
+        let world =
+            match command with
+            | MoveLeft ->
+                let physicsId = Simulants.Elmario.GetPhysicsId world
+                if World.isBodyOnGround physicsId world
+                then World.applyBodyForce (v2 -30000.0f 0.0f) physicsId world
+                else World.applyBodyForce (v2 -7500.0f 0.0f) physicsId world
+            | MoveRight ->
+                let physicsId = Simulants.Elmario.GetPhysicsId world
+                if World.isBodyOnGround physicsId world
+                then World.applyBodyForce (v2 30000.0f 0.0f) physicsId world
+                else World.applyBodyForce (v2 7500.0f 0.0f) physicsId world
+            | Jump ->
+                let physicsId = Simulants.Elmario.GetPhysicsId world
+                if World.isBodyOnGround physicsId world then
+                    let world = World.applyBodyForce (v2 0.0f 2000000.0f) physicsId world
+                    World.playSound 0.5f (asset "Gameplay" "Jump") world
+                else world
+            | Nop -> world
+        just world
 
-        // here we describe the content of the game including elmario, the ground he walks on, and a rock.
-        override this.Content (_, _, _) =
-            [Content.screen Default.Screen.Name Vanilla []
-                [Content.layer Default.Layer.Name []
-                    [Content.character Simulants.Elmario.Name
-                        [Entity.Position == v2 0.0f 0.0f
-                         Entity.Size == v2 144.0f 144.0f]
-                     Content.block "Ground"
-                        [Entity.Position == v2 -384.0f -256.0f
-                         Entity.Size == v2 768.0f 64.0f
-                         Entity.StaticImage == asset "Gameplay" "TreeTop"]
-                     Content.block "Rock"
-                        [Entity.Position == v2 320.0f -192.0f
-                         Entity.Size == v2 64.0f 64.0f
-                         Entity.StaticImage == asset "Gameplay" "Rock"]]]]
+    // here we describe the content of the game including elmario, the ground he walks on, and a rock.
+    override this.Content (_, _, _) =
+        [Content.screen Default.Screen.Name Vanilla []
+            [Content.layer Default.Layer.Name []
+                [Content.character Simulants.Elmario.Name
+                    [Entity.Position == v2 0.0f 0.0f
+                     Entity.Size == v2 144.0f 144.0f]
+                 Content.block "Ground"
+                    [Entity.Position == v2 -384.0f -256.0f
+                     Entity.Size == v2 768.0f 64.0f
+                     Entity.StaticImage == asset "Gameplay" "TreeTop"]
+                 Content.block "Rock"
+                    [Entity.Position == v2 320.0f -192.0f
+                     Entity.Size == v2 64.0f 64.0f
+                     Entity.StaticImage == asset "Gameplay" "Rock"]]]]
