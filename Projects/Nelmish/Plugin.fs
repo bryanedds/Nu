c6b66f403f7c378d22ab3da9bbc8aa6aa39b8a77
bryanedds <bryanedds@gmail.com>
2020-04-04 19:37:42 -0400

    Added interactive script to Nelmish.

---
+++ Projects/Nelmish/Plugin.fs
@@ -0,0 +1,19 @@
+﻿namespace Nelmish
+open Nu
+open Nelmish
+
+// this is a plugin for the Nu game engine that directs the execution of your application and editor
+type NelmishPlugin () =
+    inherit NuPlugin ()
+
+    // this specifies the game dispatcher to use in your application
+    override this.GetStandAloneGameDispatcher () =
+        typeof<NelmishDispatcher>
+
+    // this specifies the game dispatcher to use in the editor
+    override this.GetEditorGameDispatcher () =
+        typeof<NelmishDispatcher>
+
+    // this specifies no screen dispatcher is to be created by the editor
+    override this.GetEditorScreenDispatcherOpt () =
+        None
