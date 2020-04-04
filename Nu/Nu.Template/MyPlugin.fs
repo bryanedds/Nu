10914b90462a06774ad2e441d1e34a32532d3ae0
bryanedds <bryanedds@gmail.com>
2020-04-04 17:52:33 -0400

    Decoupled Interactive.fsx from Program.fs in Nu.Template.

---
+++ Nu/Nu.Template/MyPlugin.fs
@@ -0,0 +1,18 @@
+﻿namespace MyGame
+open Nu
+
+// this is a plugin for the Nu game engine that directs the execution of your application and editor
+type MyPlugin () =
+    inherit NuPlugin ()
+
+    // this specifies the game dispatcher to use in your application
+    override this.GetStandAloneGameDispatcher () =
+        typeof<MyGameDispatcher>
+
+    // this specifies the game dispatcher to use in the editor
+    override this.GetEditorGameDispatcher () =
+        typeof<GameDispatcher>
+
+    // this specifies the screen dispatcher to optionally use in the editor
+    override this.GetEditorScreenDispatcherOpt () =
+        Some typeof<MyGameplayDispatcher>
