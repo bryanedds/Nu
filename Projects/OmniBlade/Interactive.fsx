e693d7c8dd6350cb6c46fdccdb57a52aca0d96f8
bryanedds <bryanedds@gmail.com>
2020-05-06 17:21:41 -0400

    Updated interactive files with image magick.

--- Projects/OmniBlade/Interactive.fsx
+++ Projects/OmniBlade/Interactive.fsx
@@ -2,6 +2,7 @@
 // Copyright (C) Bryan Edds, 2013-2020.
 
 #I __SOURCE_DIRECTORY__
+#r "../../packages/Magick.NET-Q8-x64.7.16.1/lib/net40/Magick.NET-Q8-x64.dll"
 #r "../../packages/FParsec.1.0.3/lib/net40-client/FParsecCS.dll" // MUST be referenced BEFORE FParsec.dll!
 #r "../../packages/FParsec.1.0.3/lib/net40-client/FParsec.dll"
 #r "../../packages/FsCheck.2.11.0/lib/net452/FsCheck.dll"
@@ -11,7 +12,6 @@
 #r "../../Nu/Nu.Dependencies/FSharpx.Core/FSharpx.Core.dll"
 #r "../../Nu/Nu.Dependencies/FSharpx.Collections/FSharpx.Collections.dll"
 #r "../../Nu/Nu.Dependencies/Farseer/FarseerPhysics.dll"
-#r "../../Nu/Nu.Dependencies/Magick.NET/Magick.NET-AnyCPU.dll"
 #r "../../Nu/Nu.Dependencies/Nito.Collections.Deque/Nito.Collections.Deque.dll"
 #r "../../Nu/Nu.Dependencies/SDL2-CS/Debug/SDL2-CS.dll"
 #r "../../Nu/Nu.Dependencies/TiledSharp/Debug/TiledSharp.dll"
