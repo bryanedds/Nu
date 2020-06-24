26fe247739d49db41072c067081bbec77b6ef862
bryanedds <bryanedds@gmail.com>
2020-06-24 18:38:55 -0400

    Attempted ECS progress. Doesn't look why we can have long-lived references in .NET, tho.

--- Nu/Nu/Ecs.fs
+++ Nu/Nu/Ecs.fs
@@ -7,12 +7,10 @@ open System.Collections.Generic
 open Prime
 
 type Component =
-    abstract Occupied : bool with get, set
+    abstract RefCount : int with get, set
 
 type [<AbstractClass>] 'w System () =
     abstract Update : 'w Ecs -> obj
-    abstract PostUpdate : 'w Ecs -> obj
-    abstract Actualize : 'w Ecs -> obj
 
 and [<AbstractClass>] SystemSingleton<'t, 'w when 't : struct and 't :> Component> (comp : 't) =
     inherit System<'w> ()
@@ -51,7 +49,7 @@ and [<AbstractClass>] SystemUncorrelated<'t, 'w when 't : struct and 't :> Compo
 
     member this.RemoveComponent index =
         if index <> freeIndex then
-            components.[index].Occupied <- false
+            components.[index].RefCount <- dec components.[index].RefCount
             freeList.Enqueue index
         else freeIndex <- dec freeIndex
 
@@ -74,52 +72,99 @@ and [<AbstractClass>] SystemCorrelated<'t, 'w when 't : struct and 't :> Compone
         if not found then raise (InvalidOperationException "entityId")
         &components.[index]
 
-    member this.AddComponent entityId comp =
-        if not (correlations.ContainsKey entityId) then
-            if freeList.Count = 0 then
-                if freeIndex < components.Length - 1 then
-                    components.[freeIndex] <- comp
-                    freeIndex <- inc freeIndex
-                else
-                    let arr = Array.zeroCreate (components.Length * 2)
-                    components.CopyTo (arr, 0)
-                    components <- arr
-                    components.[freeIndex] <- comp
-                    freeIndex <- inc freeIndex
-                let index = dec freeIndex
-                correlations.Add (entityId, index)
-            else components.[freeList.Dequeue ()] <- comp
-            true
-        else false
+    member this.Register entityId : 't byref =
+        if correlations.ContainsKey entityId then
+            failwith ("Entity '" + scstring entityId + "' already registered.")
+        let comp = Unchecked.defaultof<'t>
+        if freeList.Count = 0 then
+            if freeIndex < components.Length - 1 then
+                components.[freeIndex] <- comp
+                freeIndex <- inc freeIndex
+            else
+                let arr = Array.zeroCreate (components.Length * 2)
+                components.CopyTo (arr, 0)
+                components <- arr
+                components.[freeIndex] <- comp
+                freeIndex <- inc freeIndex
+            let index = dec freeIndex
+            correlations.Add (entityId, index)
+            &components.[index]
+        else
+            let index = freeList.Dequeue ()
+            components.[index] <- comp
+            &components.[index]
 
-    member this.RemoveComponent entityId =
+    member this.Unregister entityId =
         match correlations.TryGetValue entityId with
         | (true, index) ->
             if index <> freeIndex then
-                components.[index].Occupied <- false
+                components.[index].RefCount <- dec components.[index].RefCount
                 freeList.Enqueue index
             else freeIndex <- dec freeIndex
             true
         | (false, _) -> false
 
-and [<NoEquality; NoComparison>] ComponentSource =
-    | ComponentUncorrelated of string * int
-    | ComponentCorrelated of string * Guid
-    | ComponentSimulant of Simulant
+and [<AbstractClass>] SystemIntersection<'t, 'w when 't : struct and 't :> Component>
+    (correlatedSystemNames : string array, mapper : Dictionary<string, 'w System> -> Guid -> 'w Ecs -> 't) =
+    inherit System<'w> ()
 
-and SystemMapper =
-    interface end
+    let mutable components = [||] : 't array
+    let mutable freeIndex = 0
+    let mutable intersectionsOpt = None : Dictionary<string, 'w System> option
+    let freeList = Queue<int> ()
+    let correlations = dictPlus [] : Dictionary<Guid, int>
+    
+    member this.Components
+        with get () = components
+    
+    member this.FreeIndex
+        with get () = freeIndex
 
-and [<NoEquality; NoComparison>] SystemMapper<'i, 'd, 'w when 'i :> Component and 'd :> Component> =
-    { SourcesToIntersection : ComponentSource array -> 'i
-      IntersectionToDestination : 'i -> 'w -> 'd
-      SystemSourceNames : string array
-      SystemIntersectionName : string
-      SystemDestinationName : string }
-    interface SystemMapper
+    member this.GetIntersections getSystem (ecs : 'w Ecs) =
+        match intersectionsOpt with
+        | Some intersections -> intersections
+        | None ->
+            let intersections = correlatedSystemNames |> Array.map (fun sourceName -> (sourceName, getSystem sourceName ecs)) |> dictPlus
+            intersectionsOpt <- Some intersections
+            intersections
 
-and [<NoEquality; NoComparison>] SystemMappers =
-  { Mappers : SystemMapper array }
+    member this.GetComponent entityId =
+        let (found, index) = correlations.TryGetValue entityId
+        if not found then raise (InvalidOperationException "entityId")
+        &components.[index]
+
+    member this.Register entityId getSystem ecs =
+        if correlations.ContainsKey entityId then
+            failwith ("Entity '" + scstring entityId + "' already registered.")
+        let intersections = this.GetIntersections getSystem ecs
+        let comp = mapper intersections entityId ecs
+        if freeList.Count = 0 then
+            if freeIndex < components.Length - 1 then
+                components.[freeIndex] <- comp
+                freeIndex <- inc freeIndex
+            else
+                let arr = Array.zeroCreate (components.Length * 2)
+                components.CopyTo (arr, 0)
+                components <- arr
+                components.[freeIndex] <- comp
+                freeIndex <- inc freeIndex
+            let index = dec freeIndex
+            correlations.Add (entityId, index)
+            &components.[index]
+        else
+            let index = freeList.Dequeue ()
+            components.[index] <- comp
+            &components.[index]
+
+    member this.Unregister entityId =
+        match correlations.TryGetValue entityId with
+        | (true, index) ->
+            if index <> freeIndex then
+                components.[index].RefCount <- dec components.[index].RefCount
+                freeList.Enqueue index
+            else freeIndex <- dec freeIndex
+            true
+        | (false, _) -> false
 
 /// NOTE: Uncorrelated systems can update in parallel.
 and [<NoEquality; NoComparison>] 'w Ecs =
@@ -132,16 +177,6 @@ module Ecs =
     let update ecs =
         ecs.Systems |>
         Seq.map (fun (system : KeyValuePair<string, 'w System>) -> (system.Key, system.Value.Update ecs)) |>
-        dictPlus
-
-    let postUpdate ecs =
-        ecs.Systems |>
-        Seq.map (fun (system : KeyValuePair<string, 'w System>) -> (system.Key, system.Value.PostUpdate ecs)) |>
-        dictPlus
-
-    let actualize ecs =
-        ecs.Systems |>
-        Seq.map (fun (system : KeyValuePair<string, 'w System>) -> (system.Key, system.Value.Actualize ecs)) |>
         dictPlus
 
     let addSystem systemName system ecs =
@@ -159,12 +194,12 @@ module Ecs =
         tryGetSystem systemName ecs |> Option.get
 
     let getComponent<'t, 'w when 't : struct and 't :> Component> systemName index ecs =
-        match tryGetSystem systemName ecs with
-        | Some system ->
-            match system with
-            | :? SystemUncorrelated<'t, 'w> as systemUnc -> systemUnc.GetComponent index
-            | _ -> failwith ("Could not find expected system '" + systemName + "' of required type.")
-        | _ -> failwith ("Could not find expected system '" + systemName + "'.")
+        let systemOpt = tryGetSystem systemName ecs
+        if Option.isNone systemOpt then failwith ("Could not find expected system '" + systemName + "'.")
+        let system = Option.get systemOpt
+        if not (system :? SystemUncorrelated<'t, 'w>) then failwith ("Could not find expected system '" + systemName + "' of required type.")
+        let systemUnc = system :?> SystemUncorrelated<'t, 'w>
+        &systemUnc.GetComponent index
 
     let addComponent<'t, 'w when 't : struct and 't :> Component> systemName (comp : 't) ecs =
         match tryGetSystem systemName ecs with
@@ -182,41 +217,45 @@ module Ecs =
             | _ -> failwith ("Could not find expected system '" + systemName + "' of required type.")
         | _ -> failwith ("Could not find expected system '" + systemName + "'.")
 
-    let getComponentCorrelated<'t, 'w when 't : struct and 't :> Component> systemName entityId ecs =
-        match tryGetSystem systemName ecs with
-        | Some system ->
-            match system with
-            | :? SystemCorrelated<'t, 'w> as systemCorr -> systemCorr.GetComponent entityId
-            | _ -> failwith ("Could not find expected system '" + systemName + "' of required type.")
-        | _ -> failwith ("Could not find expected system '" + systemName + "'.")
+    let inline getComponentCorrelated<'t, 'w when 't : struct and 't :> Component> systemName entityId ecs =
+        let systemOpt = tryGetSystem systemName ecs
+        if Option.isNone systemOpt then failwith ("Could not find expected system '" + systemName + "'.")
+        let system = Option.get systemOpt
+        if not (system :? SystemCorrelated<'t, 'w>) then failwith ("Could not find expected system '" + systemName + "' of required type.")
+        let systemUnc = system :?> SystemCorrelated<'t, 'w>
+        &systemUnc.GetComponent entityId
 
-    let addComponentCorrelated<'t, 'w when 't : struct and 't :> Component> systemName entityId (comp : 't) ecs =
-        match tryGetSystem systemName ecs with
-        | Some system ->
-            match system with
-            | :? SystemCorrelated<'t, 'w> as systemCorr ->
-                if systemCorr.AddComponent entityId comp then
-                    match ecs.Correlations.TryGetValue entityId with
-                    | (true, correlation) -> correlation.Add systemName
-                    | (false, _) -> ecs.Correlations.Add (entityId, List [systemName])
-                else ()
-            | _ -> failwith ("Could not find expected system '" + systemName + "' of required type.")
-        | _ -> failwith ("Could not find expected system '" + systemName + "'.")
-
-    let removeComponentCorrelated<'t, 'w when 't : struct and 't :> Component> systemName entityId ecs =
+    let registerEntity<'t, 'w when 't : struct and 't :> Component> systemName entityId ecs =
         match tryGetSystem systemName ecs with
         | Some system ->
             match system with
             | :? SystemCorrelated<'t, 'w> as systemCorr ->
-                if systemCorr.RemoveComponent entityId then
-                    match ecs.Correlations.TryGetValue entityId with
-                    | (true, correlation) -> correlation.Add systemName
-                    | (false, _) -> ecs.Correlations.Add (entityId, List [systemName])
-                else ()
+                let _ = systemCorr.Register entityId
+                match ecs.Correlations.TryGetValue entityId with
+                | (true, correlation) -> correlation.Add systemName
+                | (false, _) -> ecs.Correlations.Add (entityId, List [systemName])
+            | :? SystemIntersection<'t, 'w> as systemInter ->
+                let _ = systemInter.Register entityId getSystem ecs
+                match ecs.Correlations.TryGetValue entityId with
+                | (true, correlation) -> correlation.Add systemName
+                | (false, _) -> ecs.Correlations.Add (entityId, List [systemName])
             | _ -> failwith ("Could not find expected system '" + systemName + "' of required type.")
-        | _ -> failwith ("Could not find expected system '" + systemName + "'.")
+        | None -> failwith ("Could not find expected system '" + systemName + "'.")
 
-    let correlateComponent entityId ecs =
+    //let unregisterEntity systemName entityId ecs =
+    //    match tryGetSystem systemName ecs with
+    //    | Some system ->
+    //        match system with
+    //        | :? SystemCorrelated<'t, 'w> as systemCorr ->
+    //            if systemCorr.Unregister entityId then
+    //                match ecs.Correlations.TryGetValue entityId with
+    //                | (true, correlation) -> correlation.Add systemName
+    //                | (false, _) -> ecs.Correlations.Add (entityId, List [systemName])
+    //            else ()
+    //        | _ -> failwith ("Could not find expected system '" + systemName + "' of required type.")
+    //    | _ -> failwith ("Could not find expected system '" + systemName + "'.")
+
+    let correlateEntity entityId ecs =
         ecs.Correlations.[entityId] |>
         Seq.map (fun systemName -> (systemName, getSystem systemName ecs)) |>
         dictPlus
