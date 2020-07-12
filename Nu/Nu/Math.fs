9b1946eb0e16809362b88072b31d6ea3f84e53f7
bryanedds <bryanedds@gmail.com>
2020-07-12 17:43:34 -0400

    Some more ECS API improvements.

--- Nu/Nu/Math.fs
+++ Nu/Nu/Math.fs
@@ -701,5 +701,7 @@ type private ExampleJunctioned () =
     override this.Update _ =
         for i = 0 to this.Components.Length - 1 do
             let comp = &this.Components.[i]
-            comp.Transform <! comp.Transform.Value
+            let transform = &comp.Transform.Index
+            transform.Enabled <- false
+            transform.Absolute <- false
         () :> obj
