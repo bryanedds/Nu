770b592480aef16fc3af5182cbf33389c7aa3409
bryanedds <bryanedds@gmail.com>
2020-10-15 05:26:43 -0400

    Fixed missing image properties for new OB tile sets.
    Former-commit-id: 1f8988481bd85e0b58d9aaec1844b4265ee32378

--- Projects/OmniBlade/Assets/Field/Cave.tsx
+++ Projects/OmniBlade/Assets/Field/Cave.tsx
@@ -1,5 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.4" name="Cave" tilewidth="64" tileheight="64" tilecount="256" columns="16">
+ <properties>
+  <property name="Image" value="[Field Cave]"/>
+ </properties>
  <image source="Cave.png" width="1024" height="1024"/>
  <tile id="49">
   <properties>
--- Projects/OmniBlade/Assets/Field/Tent.tsx
+++ Projects/OmniBlade/Assets/Field/Tent.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="Tent" tilewidth="64" tileheight="64" tilecount="16" columns="4">
- <image source="Tent.png" width="256" height="256"/>
+<tileset version="1.2" tiledversion="1.3.4" name="Tent" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <properties>
+  <property name="Image" value="[Field Tent]"/>
+ </properties>
+ <image source="Tent.png" width="512" height="512"/>
 </tileset>
