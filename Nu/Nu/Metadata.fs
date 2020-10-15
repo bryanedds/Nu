26bf239dbad054a17cad7965bf37f3257d3705be
bryanedds <bryanedds@gmail.com>
2020-10-15 06:10:23 -0400

    Minor error wording improvement.
    Former-commit-id: 65b068036e60f4e4114f0af7c4523e7bcc171f7b

--- Nu/Nu/Metadata.fs
+++ Nu/Nu/Metadata.fs
@@ -39,7 +39,7 @@ module Metadata =
             let errorMessage =
                 "TileSet '" + tileSet.Name + "' missing Image property.\n" +
                 "You must add a Custom Property to the tile set called 'Image' and give it an asset value like '[PackageName AssetName]'.\n" +
-                "This will specify where the engine can find the tile set's associated tile map image."
+                "This will specify where the engine can find the tile set's associated image asset."
             raise (TileSetPropertyNotFoundException errorMessage)
 
     let private generateTextureMetadata asset =
