de4bf2f0ff74c82b3fcbdf226231691455978809
bryanedds <bryanedds@gmail.com>
2020-10-23 00:58:32 -0400

    Got rid of Build.bat - doesn't work.
    Former-commit-id: a6d2dbfb39073a19cc2ad7724bda53a7cf7f9bce

--- Build.bat
+++
@@ -1,7 +0,0 @@
-@echo Attempting to restore dependencies (this doesn't seem to work)...
-"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/MSBuild.exe" Nu.sln /t:restore
-
-@echo Attempting to build (will work if dependencies are restored)...
-"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/MSBuild.exe" Nu.sln /t:build
-
-@pause
