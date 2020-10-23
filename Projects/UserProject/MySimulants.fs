b2962883dd94586deeaa4ed5b81c1add64076af5
bryanedds <bryanedds@gmail.com>
2020-10-23 01:10:53 -0400

    Added UserProject for those having trouble creating projects on non-Windows machines.
    Former-commit-id: b35f7f560f169c24867b6658e82a225d16e893f3

---
+++ Projects/UserProject/MySimulants.fs
@@ -0,0 +1,28 @@
+﻿namespace MyGame
+open Nu
+
+[<RequireQualifiedAccess>]
+module Simulants =
+
+    // same as above, but for the splash screen
+    let Splash = Screen "Splash"
+
+    // same as above, but for the title screen and its children
+    let Title = Screen "Title"
+    let TitleGui = Title / "Gui"
+    let TitlePlay = TitleGui / "Play"
+    let TitleCredits = TitleGui / "Credits"
+    let TitleExit = TitleGui / "Exit"
+
+    // credits screen handles
+    let Credits = Screen "Credits"
+    let CreditsGui = Credits / "Gui"
+    let CreditsBack = CreditsGui / "Back"
+
+    // gameplay screen handles
+    let Gameplay = Screen "Gameplay"
+    let Hud = Gameplay / "Hud"
+    let Back = Hud / "Back"
+    let Scene = Gameplay / "Scene"
+    let Player = Scene / "Player"
+    let Level = Gameplay / "Level"
