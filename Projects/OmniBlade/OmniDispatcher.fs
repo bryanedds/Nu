592f1621540a4e8910d52875fbdf092374443ace
bryanedds <bryanedds@gmail.com>
2020-10-24 14:44:18 -0400

    Normalized Omni event structure.

--- Projects/OmniBlade/OmniDispatcher.fs
+++ Projects/OmniBlade/OmniDispatcher.fs
@@ -21,9 +21,13 @@ module GameDispatcher =
 
     type [<NoEquality; NoComparison>] OmniMessage =
         | Update
-        | UpdateOmni of Omni
+        | UpdateGui of Gui
         | UpdateField of Field
-        | UpdateBattle of Battle
+        | UpdateBattle of Field * Battle
+        | Change of Omni
+        | ChangeGui of Gui
+        | ChangeField of Field
+        | ChangeBattle of Battle
 
     type [<NoEquality; NoComparison>] OmniCommand =
         | Show of Screen
@@ -44,52 +48,63 @@ module GameDispatcher =
 
         override this.Channel (_, _) =
             [Simulants.Game.UpdateEvent => msg Update
-             Simulants.TitleCredits.ClickEvent => msg (UpdateOmni (Gui Credits))
-             Simulants.CreditsBack.ClickEvent => msg (UpdateOmni (Gui Title))
-             Simulants.FieldBack.ClickEvent => msg (UpdateOmni (Gui Title))
-             Simulants.TitlePlay.ClickEvent => msg (UpdateOmni (Field Field.initial))
-             Simulants.Field.Field.ChangeEvent =|> fun evt -> msg (UpdateField (evt.Data.Value :?> Field))
-             Simulants.Battle.Battle.ChangeEvent =|> fun evt -> msg (UpdateBattle (evt.Data.Value :?> Battle))
+             Simulants.TitleCredits.ClickEvent => msg (Change (Gui Credits))
+             Simulants.CreditsBack.ClickEvent => msg (Change (Gui Title))
+             Simulants.FieldBack.ClickEvent => msg (Change (Gui Title))
+             Simulants.TitlePlay.ClickEvent => msg (Change (Field Field.initial))
+             Simulants.Field.Field.ChangeEvent =|> fun evt -> msg (ChangeField (evt.Data.Value :?> Field))
+             Simulants.Battle.Battle.ChangeEvent =|> fun evt -> msg (ChangeBattle (evt.Data.Value :?> Battle))
              Simulants.TitleExit.ClickEvent => cmd Exit]
 
         override this.Message (omni, message, _, world) =
 
             match message with
-            | UpdateOmni omni ->
-                just omni
+            | Update ->
+                match omni with
+                | Gui gui -> withMsg (UpdateGui gui) omni
+                | Field field -> withMsg (UpdateField field) omni
+
+            | UpdateGui gui ->
+                match gui with
+                | Splashing -> just omni
+                | Title -> withCmd (Show Simulants.Title) omni
+                | Credits -> withCmd (Show Simulants.Credits) omni
 
             | UpdateField field ->
+                match field.BattleOpt with
+                | Some battle -> withMsg (UpdateBattle (field, battle)) omni
+                | None -> withCmd (Show Simulants.Field) omni
+
+            | UpdateBattle (field, battle) ->
+                match battle.BattleState with
+                | BattleCease (result, time) ->
+                    if World.getTickTime world - time = 120L then
+                        if result
+                        then withCmd (Show Simulants.Field) (Field (Field.synchronizeFromBattle battle field))
+                        else withCmd (Show Simulants.Title) (Gui Title)
+                    else withCmd (Show Simulants.Battle) omni
+                | _ -> withCmd (Show Simulants.Battle) omni
+
+            | Change omni ->
+                just omni
+
+            | ChangeGui gui ->
+                match omni with
+                | Gui _ -> just (Gui gui)
+                | Field _ -> just omni
+
+            | ChangeField field ->
                 match omni with
                 | Gui _ -> just omni
                 | Field _ -> just (Field field)
 
-            | UpdateBattle battle ->
+            | ChangeBattle battle ->
                 match omni with
                 | Gui _ -> just omni
                 | Field field ->
                     match field.BattleOpt with
                     | None -> just omni
                     | Some _ -> just (Field (Field.updateBattleOpt (constant (Some battle)) field))
-
-            | Update ->
-                match omni with
-                | Gui gui ->
-                    match gui with
-                    | Splashing -> just omni
-                    | Title -> withCmd (Show Simulants.Title) omni
-                    | Credits -> withCmd (Show Simulants.Credits) omni
-                | Field field ->
-                    match field.BattleOpt with
-                    | Some battle ->
-                        match battle.BattleState with
-                        | BattleCease (result, time) ->
-                            if World.getTickTime world - time = 120L then
-                                if result
-                                then withCmd (Show Simulants.Field) (Field (Field.synchronizeFromBattle battle field))
-                                else withCmd (Show Simulants.Title) (Gui Title)
-                            else withCmd (Show Simulants.Battle) omni
-                        | _ -> withCmd (Show Simulants.Battle) omni
-                    | None -> withCmd (Show Simulants.Field) omni
 
         override this.Command (_, command, _, world) =
             match command with
