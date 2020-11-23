8e10eb90a3551f98fc395d945c52586e95724bfb
bryanedds <bryanedds@gmail.com>
2020-11-23 08:14:05 -0500

    Filled out implementation of KeyedArray.

--- Nu/Nu/KeyedArray.fs
+++ Nu/Nu/KeyedArray.fs
@@ -5,33 +5,47 @@ namespace Nu
 open Prime
 open System.Collections.Generic
 
-/// A garbage-collected keyed array.
-/// TODO: once this is well-tested, let's consider moving into Prime.
-type [<NoEquality; NoComparison>] KeyedArray<'k, 'v when 'k : comparison> =
-    private
-        { Keys_ : SortedDictionary<int, 'k>
-          Indices_ : SortedDictionary<'k, int>
-          mutable Values_ : struct (bool * 'k * 'v) array
-          mutable Current_ : int
-          mutable Removed_ : single // single to elide unecessary conversions
-          mutable Threshold_ : single }
-
-    member this.Values =
-        this.Values_
-
-    member this.Threshold =
-        this.Threshold_
-
 [<RequireQualifiedAccess>]
 module KeyedArray =
 
-    let overflowing karr =
+    /// A garbage-collected keyed array.
+    /// TODO: once this is well-tested, let's consider moving into Prime.
+    type [<NoEquality; NoComparison>] KeyedArray<'k, 'v when 'k : comparison> =
+        private
+            { Keys_ : SortedDictionary<int, 'k> // sorted so that compacting does not change order
+              Indices_ : Dictionary<'k, int>
+              mutable Values_ : struct (bool * 'k * 'v) array
+              mutable Current_ : int
+              mutable Removed_ : single // single to elide unecessary conversions
+              mutable Threshold_ : single }
+
+        interface struct (bool * 'k * 'v) IEnumerable with
+            member this.GetEnumerator () = this.Values_.GetEnumerator ()
+            member this.GetEnumerator () = (this.Values_ :> _ IEnumerable).GetEnumerator ()
+
+        /// The keyed array values.
+        member this.Values =
+            this.Values_
+
+        /// The threshold for compaction.
+        member this.Threshold =
+            this.Threshold_
+
+        /// The current number of values (some of which may be empty).
+        member this.Length =
+            this.Current_
+
+        /// Index a keyed value.
+        member this.Item key =
+            &this.Values_.[this.Indices_.[key]]
+
+    let private overflowing karr =
         karr.Current_ = karr.Values.Length
 
-    let underflowing karr =
+    let private underflowing karr =
         karr.Removed_ / single karr.Keys_.Count > karr.Threshold_
 
-    let expand karr =
+    let private expand karr =
 
         // check overflow
         if overflowing karr then
@@ -41,7 +55,7 @@ module KeyedArray =
             Array.blit karr.Values_ 0 values 0 karr.Current_
             karr.Values_ <- values
 
-    let compact karr =
+    let private compact karr =
         
         // check underflow
         if underflowing karr then
@@ -63,8 +77,22 @@ module KeyedArray =
             karr.Values_ <- values
 
             // clean up
+            karr.Current_ <- current
             karr.Removed_ <- 0.0f
 
+    /// Get the keyed array values.
+    let getValues karr =
+        karr.Values_
+
+    /// Get the threshold for compaction.
+    let getThreshold karr =
+        karr.Threshold_
+    
+    /// Get the current number of values (some of which may be empty).
+    let getLength karr =
+        karr.Current_
+
+    /// Add a keyed value, or update one if it already exists.
     let add key value karr =
         match karr.Indices_.TryGetValue key with
         | (false, _) ->
@@ -77,6 +105,7 @@ module KeyedArray =
         | (true, index) ->
             karr.Values_.[index] <- struct (true, key, value)
 
+    /// Remove a keyed value if it exists.
     let remove key karr =
         match karr.Indices_.TryGetValue key with
         | (true, index) ->
@@ -87,10 +116,29 @@ module KeyedArray =
             compact karr
         | (false, _) -> ()
 
-    let make<'k, 'v when 'k : comparison> threshold size =
+    /// Query that a keyed value exists.
+    let containsKey key karr =
+        karr.Indices_.ContainsKey key
+
+    /// Attempt to find a keyed value.
+    let tryFind key karr =
+        match karr.Indices_.TryGetValue key with
+        | (true, index) -> Some karr.Values_.[index]
+        | (false, _) -> None
+
+    /// Find a keyed value.
+    let find key (karr : KeyedArray<_, _>) =
+        &karr.[key]
+
+    /// Make a KeyedArray with the given compaction threshold and initial capaticy.
+    let make<'k, 'v when 'k : comparison> threshold capacity =
         { Keys_ = SortedDictionary<int, 'k> ()
-          Indices_ = SortedDictionary<'k, int> ()
-          Values_ = Array.zeroCreate size
+          Indices_ = Dictionary<'k, int> ()
+          Values_ = Array.zeroCreate capacity
           Current_ = 0
           Removed_ = 0.0f
           Threshold_ = threshold }
+
+/// A garbage-collected keyed array.
+/// TODO: once this is well-tested, let's consider moving into Prime.
+type KeyedArray<'k, 'v when 'k : comparison> = KeyedArray.KeyedArray<'k, 'v>
