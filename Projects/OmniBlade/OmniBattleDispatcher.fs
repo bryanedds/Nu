96ee325ce24c3cdff713bcb08a7c0dc5475145ec
bryanedds <bryanedds@gmail.com>
2020-12-09 19:19:14 -0500

    OmniBlade index fix.

--- Projects/OmniBlade/OmniBattleDispatcher.fs
+++ Projects/OmniBlade/OmniBattleDispatcher.fs
@@ -905,7 +905,7 @@ module BattleDispatcher =
                                     match ally.InputState with NoInput | RegularMenu -> false | _ -> true)
                                     allies
                             alliesPastRegularMenu
-                         Entity.RingMenu == { Items = Map.ofList [("Attack", (0, true)); ("Defend", (0, true)); ("Consumable", (0, true)); ("Tech", (0, true))]; ItemCancelOpt = None }
+                         Entity.RingMenu == { Items = Map.ofList [("Attack", (0, true)); ("Defend", (1, true)); ("Consumable", (2, true)); ("Tech", (3, true))]; ItemCancelOpt = None }
                          Entity.ItemSelectEvent ==|> fun evt -> msg (RegularItemSelect (index, evt.Data))
                          Entity.CancelEvent ==> msg (RegularItemCancel index)]
 
