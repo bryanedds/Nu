62f9ee2b32f9c37539ce61d3bd7f8df713892918
Dean J Lee <djl6@hotmail.com>
2020-12-19 12:11:14 +1100

    a bit of file reorganization
    Former-commit-id: a0d1bcf6964bde5e9ba34bf18c6c76023a74714e

--- Projects/InfinityRpg/InfinityCharacter.fs
+++ Projects/InfinityRpg/InfinityCharacter.fs
@@ -1,176 +1,6 @@
 ﻿namespace InfinityRpg
 open Prime
 
-type CharacterIndex =
-    | EnemyIndex of int
-    | PlayerIndex
-
-    member this.IsEnemy =
-        match this with EnemyIndex _ -> true | PlayerIndex -> false
-
-    member this.IsAlly =
-        not this.IsEnemy
-
-type AllyType =
-    | Player
-
-type EnemyType =
-    | Goopy
-    | Goblin
-    | Snake
-    | Zommie
-
-type CharacterType =
-    | Ally of AllyType
-    | Enemy of EnemyType
-
-type ControlType =
-    | PlayerControlled
-    | Chaos
-    | Uncontrolled
-
-type ElementType =
-    | Fire // beats nothing; strongest
-    | Water // beats fire, lightning; weakest
-    | Lightning // beats water
-
-type StatusType =
-    | Defending // also implies countering
-    | Poisoned
-    | Muted
-    | Sleeping
-
-type EquipmentType =
-    | Weapon
-    | Armor
-    | Accessory
-
-type ConsumableType =
-    | Herb
-
-type KeyItemType =
-    | Feather // jump
-    | Gills // swim in shallow water
-    | Hammer // smash obstacles
-    | SteelSoles // walk over spikes
-    | Wings // fly in overworld
-    | Raft // navigate over deep water
-
-type ItemType =
-    | Equipment of EquipmentType
-    | Consumable of ConsumableType
-    | KeyItem of KeyItemType
-
-type TargetType =
-    | SingleTarget of Distance : int * SelfOnly : bool
-    | RangeTarget of Distance : int * SelfOnly : bool * Range : int
-    | LineTarget of Distance : int
-    | QuarterScreenTarget // in one of four cardinal directions like <, >, ^, or v
-    | FullScreenTarget
-
-type EffectType =
-    | Physical
-    | Magical
-
-type SpecialType =
-    | Spark
-    | Bomb
-    | DragonBreath
-    | Plasma
-    | Bolt
-    | PowerUp
-    | PowerDown
-    | ShieldUp
-    | ShieldDown
-    | MindUp
-    | MindDown
-    | CounterUp
-    | CounterDown
-    | Sleep
-    | Poison
-    | Immunity // immunizes against status changes
-
-type ActionType =
-    | Attack
-    | Defend // auto counters at rate of counter stat
-    | Consume of ConsumableType
-    | Special of SpecialType
-    | Interact // general interaction such as talking to NPCs
-
-type WeaponType =
-    | Body
-    | OakSword
-
-type WeaponSubtype =
-    | Melee
-    | Sword
-
-type WeaponData =
-    { WeaponType : WeaponType // key
-      WeaponSubtype : WeaponSubtype
-      PowerBase : int
-      MagicBase : int }
-
-type ArmorType =
-    | HoboRags
-    | LeatherHide
-    | ChainMail
-
-type ArmorSubtype =
-    | Robe
-    | Vest
-    | Mail
-
-type ArmorData =
-    { ArmorType : ArmorType // key
-      ArmorSubtype : ArmorSubtype
-      HitPointsBase : int
-      SpecialPointsBase : int }
-
-type AccessoryType =
-    | LeatherBoots
-    | BlingRing
-    | MagicPorkRinds
-
-type AccessoryData =
-    { AccessoryType : AccessoryType // key
-      ShieldBase : int
-      CounterBase : int }
-
-type DrainData =
-    { EffectType : EffectType
-      Percentage : single }
-
-type ActionData =
-    { ActionType : ActionType
-      EffectType : EffectType
-      SpecialPointCost : int
-      SuccessRate : single
-      Curative : bool
-      ElementType : ElementType
-      AddStatusType : StatusType Set
-      RemoveStatusType : StatusType Set
-      TargetType : TargetType }
-
-type ConsumableData =
-    { ConsumableData : unit }
-
-type KeyItemData =
-    { KeyItemData : unit }
-
-type ItemData =
-    { ItemType : ItemType // key
-      Description : string }
-
-type RewardData =
-    { Gold : int }
-
-type CharacterData =
-    { CharacterType : CharacterType // key
-      CharacterName : string
-      BaseActions : ActionData list // base actions for all instances of character
-      Reward : RewardData }
-
 type [<ReferenceEquality; NoComparison>] Character =
     { CharacterIndex : CharacterIndex
       CharacterType : CharacterType
@@ -226,3 +56,25 @@ type [<ReferenceEquality; NoComparison>] Character =
     static member makePlayer = { Character.empty with HitPoints = 30; ControlType = PlayerControlled }
 
     static member makeEnemy index = { Character.empty with CharacterIndex = index; CharacterType = Enemy Goopy; HitPoints = 10; ControlType = Chaos }
+
+type CharacterAnimationType =
+    | CharacterAnimationFacing
+    | CharacterAnimationActing
+    | CharacterAnimationDefending
+    | CharacterAnimationSpecial // works for jump, cast magic, being healed, and perhaps others!
+    | CharacterAnimationSlain
+
+type [<ReferenceEquality; NoComparison>] CharacterAnimationState =
+    { StartTime : int64
+      AnimationType : CharacterAnimationType
+      Direction : Direction }
+    
+    static member initial =
+        { StartTime = 0L
+          AnimationType = CharacterAnimationFacing
+          Direction = Upward }
+
+    static member make time animationType direction =
+        { StartTime = time
+          AnimationType = animationType
+          Direction = direction }
