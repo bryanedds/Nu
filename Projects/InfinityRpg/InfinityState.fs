841bab9ce6cafde6d73c526e8b4546895000f41c
Dean J Lee <djl6@hotmail.com>
2020-12-27 22:17:02 +1100

    bit of work on inventory

--- Projects/InfinityRpg/InfinityState.fs
+++ Projects/InfinityRpg/InfinityState.fs
@@ -2,11 +2,31 @@
 open Prime
 
 type [<ReferenceEquality; NoComparison>] Inventory =
-    { Items : Map<ItemType, int>
-      Gold : int }
+    { Items : Map<ItemType, int> }
 
+    static member containsItem item inventory =
+        match Map.tryFind item inventory.Items with
+        | Some itemCount when itemCount > 0 -> true
+        | _ -> false
+    
+    static member tryAddItem item inventory =
+        match Map.tryFind item inventory.Items with
+        | Some itemCount ->
+            if itemCount < Constants.InfinityRpg.ItemLimit then
+                { inventory with Items = Map.add item (inc itemCount) inventory.Items }
+            else inventory
+        | None -> { inventory with Items = Map.add item 1 inventory.Items }
+    
+    static member removeItem item inventory =
+        match Map.tryFind item inventory.Items with
+        | Some itemCount when itemCount > 1 ->
+            { inventory with Items = Map.add item (dec itemCount) inventory.Items }
+        | Some itemCount when itemCount = 1 ->
+            { inventory with Items = Map.remove item inventory.Items }
+        | _ -> inventory
+    
     static member initial =
-        { Items = Map.empty; Gold = 0 }
+        { Items = Map.singleton (Special MagicMissile) 3 }
 
 type [<ReferenceEquality; NoComparison>] Character =
     { CharacterIndex : CharacterIndex
