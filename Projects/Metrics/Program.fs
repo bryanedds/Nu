617f88af0360c941fe1e5be9b678226d60d666d6
bryanedds <bryanedds@gmail.com>
2021-02-22 15:23:41 -0500

    Increased Elmish performance goal.

--- Projects/Metrics/Program.fs
+++ Projects/Metrics/Program.fs
@@ -219,7 +219,7 @@ type [<ReferenceEquality>] Intss =
         { Intss = intss.Intss |> Seq.map (fun kvp -> (kvp.Key, Ints.inc kvp.Value)) |> Map.ofSeq }
 
 type ElmishGameDispatcher () =
-    inherit GameDispatcher<Intss, int, unit> (Intss.init 50) // 2500 ints (goal: 60FPS)
+    inherit GameDispatcher<Intss, int, unit> (Intss.init 70) // 4900 ints (goal: 60FPS)
 
     override this.Channel (_, game) =
         [game.UpdateEvent => msg 0]
