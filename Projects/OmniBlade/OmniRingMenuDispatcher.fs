9e3805bd3e0aa7fc2887a20b872aca3e7925a21c
bryanedds <bryanedds@gmail.com>
2021-04-13 13:57:19 -0400

    Minor clean-up of ring menu.
    Former-commit-id: 4938da15ce8722a246cb1b9d7b08a4db70a6707f

--- Projects/OmniBlade/OmniRingMenuDispatcher.fs
+++ Projects/OmniBlade/OmniRingMenuDispatcher.fs
@@ -18,7 +18,6 @@ module RingMenuDispatcher =
     type [<NoEquality; NoComparison>] RingMenuCommand =
         | ItemCancel
         | ItemSelect of string
-        | ArrangeItemButton of Entity * Lens<int, World>
 
     type Entity with
         member this.GetRadius = this.Get Property? Radius
@@ -43,34 +42,27 @@ module RingMenuDispatcher =
             match command with
             | ItemCancel -> just (World.publishPlus () menu.CancelEvent [] menu true world)
             | ItemSelect item -> just (World.publishPlus item menu.ItemSelectEvent [] menu true world)
-            | ArrangeItemButton (button, orderLens) ->
-                let radius = menu.GetRadius world
-                let itemCount = Map.count ringMenu.Items
-                if Lens.validate orderLens world then
-                    let order = Lens.getWithoutValidation orderLens world
-                    let progress = single order / single itemCount
-                    let rotation = progress * single Math.PI * 2.0f
-                    let position = v2 (radius * sin rotation) (radius * cos rotation)
-                    let world = button.SetPositionLocal (position - button.GetSize world * 0.5f) world
-                    just world
-                else just world
 
         override this.Content (ringMenu, menu) =
             [Content.entities ringMenu
                 (fun ringMenu _ -> ringMenu.Items)
-                (fun items _ -> items)
-                (fun index itemOrderAndEnabled world ->
+                (fun items _ -> Map.map (constant (Triple.insert (Map.count items))) items)
+                (fun index itemOrderAndCountAndEnabled world ->
                     let buttonName = menu.Name + "+" + scstring index
-                    let itemOrder = Lens.map fst itemOrderAndEnabled
                     let button = menu.Parent / buttonName
                     Content.button button.Name
-                        [Entity.EnabledLocal <== itemOrderAndEnabled --> snd
+                        [Entity.EnabledLocal <== itemOrderAndCountAndEnabled --> Triple.thd
+                         Entity.PositionLocal <== itemOrderAndCountAndEnabled --> fun (itemOrder, itemCount, _) ->
+                            let radius = menu.GetRadius world
+                            let progress = single itemOrder / single itemCount
+                            let rotation = progress * single Math.PI * 2.0f
+                            let position = v2 (radius * sin rotation) (radius * cos rotation)
+                            position - button.GetSize world * 0.5f
                          Entity.Size == v2 48.0f 48.0f
                          Entity.Elevation <== menu.Elevation
                          Entity.UpImage == asset Assets.Battle.PackageName (index + "Up")
                          Entity.DownImage == asset Assets.Battle.PackageName (index + "Down")
-                         Entity.ClickEvent ==> cmd (ItemSelect index)
-                         Entity.UpdateEvent ==> cmd (ArrangeItemButton (button, itemOrder))])
+                         Entity.ClickEvent ==> cmd (ItemSelect index)])
              Content.entityOpt ringMenu (fun ringMenu _ -> ringMenu.ItemCancelOpt) $ fun itemCancel world ->
                 let itemCancelValue = itemCancel.Get world
                 let buttonName = menu.Name + "+" + itemCancelValue
