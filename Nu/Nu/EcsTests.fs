36832219020a9505a8ca95b6380236ee90ae389e
bryanedds <bryanedds@gmail.com>
2021-05-16 17:23:21 -0400

    Fixed ECS test code.
    Former-commit-id: b9b8445d5b64fee0497e09b86697bd2e5bec964b

--- Nu/Nu/EcsTests.fs
+++ Nu/Nu/EcsTests.fs
@@ -79,7 +79,7 @@ module EcsTests =
 
         // create and register our airship
         let airshipId = Gen.id
-        let airship = ecs.RegisterCorrelated Unchecked.defaultof<Airship> Gen.id
+        let airship = ecs.RegisterCorrelated Unchecked.defaultof<Airship> airshipId
 
         // change some airship properties
         airship.Index.Transform.Index.Position.X <- 0.5f
