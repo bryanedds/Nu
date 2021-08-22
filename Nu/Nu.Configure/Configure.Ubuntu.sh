0839eb9e72c521e9bf6af2686ee927551628fa9c
bryanedds <bryanedds@gmail.com>
2021-08-22 18:03:13 -0400

    Updated scripts.

--- Nu/Nu.Configure/Configure.Ubuntu.sh
+++ Nu/Nu.Configure/Configure.Ubuntu.sh
@@ -2,11 +2,12 @@
 echo Configuring Ubuntu for Nu applications...
 
 # configure sdl2 and components
-sudo apt install -y \
- libsdl2-dev libsdl2-2.0-0 \
- libjpeg-dev libtiff5-dev libsdl2-image-dev libsdl2-image-2.0-0 \
- libmikmod-dev liboggz2-dev libflac-dev libsdl2-mixer-dev libsdl2-mixer-2.0-0 \
- libfreetype6-dev libsdl2-ttf-dev libsdl2-ttf-2.0-0
+sudo apt update
+sudo apt install \
+  libsdl2-dev libsdl2-2.0-0 \
+  libjpeg-dev libwebp-dev libtiff5-dev libsdl2-image-dev libsdl2-image-2.0-0 \
+  libmikmod-dev libfishsound1-dev libsmpeg-dev liboggz2-dev libflac-dev libfluidsynth-dev libsdl2-mixer-dev libsdl2-mixer-2.0-0 \
+  libfreetype6-dev libsdl2-ttf-dev libsdl2-ttf-2.0-0
 
 # configure mono
 sudo apt install gnupg ca-certificates
