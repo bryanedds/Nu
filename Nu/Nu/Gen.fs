376fd2ebfbfd867828fd8838c7ece30234cc10dc
bryanedds <bryanedds@gmail.com>
2021-09-04 13:25:36 -0400

    Removed Unique type since it's inherently unrewindable.

--- Nu/Nu/Gen.fs
+++ Nu/Nu/Gen.fs
@@ -15,7 +15,6 @@ module Gen =
     let private Cids = dictPlus<string, Guid> StringComparer.Ordinal []
     let private CidBytes = Array.zeroCreate 16 // TODO: P1: use stack-based allocation via NativePtr.stackalloc and Span - https://bartoszsypytkowski.com/writing-high-performance-f-code/
     let private CnameBytes = Array.zeroCreate 16 // TODO: P1: use stack-based allocation via NativePtr.stackalloc and Span - https://bartoszsypytkowski.com/writing-high-performance-f-code/
-    let private UniqueAllocator = Unique.make ()
     let mutable private Counter = -1L
 
     /// Generates engine-specific values on-demand.
@@ -175,14 +174,6 @@ module Gen =
             let id = Gen.id
             let name = Gen.nameIf nameOpt
             (id, name)
-
-        /// Allocate a unique int.
-        static member alloc () =
-            Unique.alloc UniqueAllocator
-
-        /// Free a unique int.
-        static member free number =
-            Unique.free number UniqueAllocator
 
         /// Generate a unique counter.
         static member counter =
