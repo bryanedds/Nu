2cbf51d1cfdfa805067e93825871728953df0e2e
bryanedds <bryanedds@gmail.com>
2021-09-11 10:20:30 -0400

    #if in SelectablePanel.
    Former-commit-id: a3f7d4b33503b36ce80f6a984dee3194aae3d040

--- Nu/Nu.Gaia.Design/SelectablePanel.cs
+++ Nu/Nu.Gaia.Design/SelectablePanel.cs
@@ -4,6 +4,54 @@ using System.Windows.Forms;
 
 namespace Nu.Gaia.Design
 {
+#if WINDOWS
+    public class SelectablePanel : Panel
+    {
+        public SelectablePanel()
+        {
+            SetStyle(ControlStyles.Selectable, true);
+            TabStop = true;
+        }
+        
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            Focus();
+            base.OnMouseDown(e);
+        }
+        
+        protected override bool IsInputKey(Keys keyData)
+        {
+            if (keyData == Keys.Up || keyData == Keys.Down) return true;
+            if (keyData == Keys.Left || keyData == Keys.Right) return true;
+            if (keyData == Keys.W || keyData == Keys.S) return true;
+            if (keyData == Keys.A || keyData == Keys.D) return true;
+            return base.IsInputKey(keyData);
+        }
+        
+        protected override void OnEnter(EventArgs e)
+        {
+            Invalidate();
+            base.OnEnter(e);
+        }
+        
+        protected override void OnLeave(EventArgs e)
+        {
+            Invalidate();
+            base.OnLeave(e);
+        }
+        
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            if (Focused)
+            {
+                var rc = this.ClientRectangle;
+                rc.Inflate(-2, -2);
+                ControlPaint.DrawFocusRectangle(pe.Graphics, rc);
+            }
+        }
+    }
+#else
     public class SelectablePanel : UserControl
     {
         public SelectablePanel()
@@ -17,6 +65,7 @@ namespace Nu.Gaia.Design
             base.OnMouseDown(e);
         }
         
+        /// NOTE: I don't think this override does anything...
         protected override bool IsInputKey(Keys keyData)
         {
             if (keyData == Keys.Up || keyData == Keys.Down) return true;
@@ -26,4 +75,5 @@ namespace Nu.Gaia.Design
             return base.IsInputKey(keyData);
         }
     }
+#endif
 }
