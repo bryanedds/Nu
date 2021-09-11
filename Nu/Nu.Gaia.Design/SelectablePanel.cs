dd37097880dd02845c75ce9f4987c9c731d4b269
bryanedds <bryanedds@gmail.com>
2021-09-11 10:18:05 -0400

    Using custom user control to maintain focus.
    Former-commit-id: 7b1ebda67bbd1f43e9857ed6b4a57e7210d37d5f

--- Nu/Nu.Gaia.Design/SelectablePanel.cs
+++ Nu/Nu.Gaia.Design/SelectablePanel.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Nu.Gaia.Design
 {
-    public class SelectablePanel : Button
+    public class SelectablePanel : UserControl
     {
+        public SelectablePanel()
+        {
+            SetStyle(ControlStyles.Selectable, true);
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            if (!Focused) Focus();
+            base.OnMouseDown(e);
+        }
+        
         protected override bool IsInputKey(Keys keyData)
         {
             if (keyData == Keys.Up || keyData == Keys.Down) return true;
