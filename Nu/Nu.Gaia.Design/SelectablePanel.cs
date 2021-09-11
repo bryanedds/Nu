b6d141973e973b04a31a2e722ef1834673c096f8
bryanedds <bryanedds@gmail.com>
2021-09-11 09:51:01 -0400

    Added WASD direction keys to Gaia.

--- Nu/Nu.Gaia.Design/SelectablePanel.cs
+++ Nu/Nu.Gaia.Design/SelectablePanel.cs
@@ -21,6 +21,8 @@ namespace Nu.Gaia.Design
         {
             if (keyData == Keys.Up || keyData == Keys.Down) return true;
             if (keyData == Keys.Left || keyData == Keys.Right) return true;
+            if (keyData == Keys.W || keyData == Keys.S) return true;
+            if (keyData == Keys.A || keyData == Keys.D) return true;
             return base.IsInputKey(keyData);
         }
         
