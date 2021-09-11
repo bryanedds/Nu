e9819373cb32f084ec235136f339a4cea883c874
bryanedds <bryanedds@gmail.com>
2021-09-11 09:49:42 -0400

    Added WASD for direction keys in Gaia.
    Former-commit-id: 19196690072b34caa85bf1c9072340205fa7dbcd

--- Nu/Nu.Gaia.Design/SelectablePanel.cs
+++ Nu/Nu.Gaia.Design/SelectablePanel.cs
@@ -3,53 +3,15 @@ using System.Windows.Forms;
 
 namespace Nu.Gaia.Design
 {
-    public class SelectablePanel : Panel
+    public class SelectablePanel : Button{
     {
-        public SelectablePanel()
-        {
-            SetStyle(
-                ControlStyles.Selectable |
-                ControlStyles.UserPaint |
-                ControlStyles.AllPaintingInWmPaint |
-                ControlStyles.OptimizedDoubleBuffer,
-                true);
-            TabStop = true;
-        }
-        
-        protected override void OnMouseDown(MouseEventArgs e)
-        {
-            Focus();
-            base.OnMouseDown(e);
-        }
-        
         protected override bool IsInputKey(Keys keyData)
         {
             if (keyData == Keys.Up || keyData == Keys.Down) return true;
             if (keyData == Keys.Left || keyData == Keys.Right) return true;
+            if (keyData == Keys.W || keyData == Keys.S) return true;
+            if (keyData == Keys.A || keyData == Keys.D) return true;
             return base.IsInputKey(keyData);
         }
-        
-        protected override void OnEnter(EventArgs e)
-        {
-            Invalidate();
-            base.OnEnter(e);
-        }
-        
-        protected override void OnLeave(EventArgs e)
-        {
-            Invalidate();
-            base.OnLeave(e);
-        }
-        
-        protected override void OnPaint(PaintEventArgs pe)
-        {
-            base.OnPaint(pe);
-            if (Focused)
-            {
-                var rc = this.ClientRectangle;
-                rc.Inflate(-2, -2);
-                ControlPaint.DrawFocusRectangle(pe.Graphics, rc);
-            }
-        }
-    }
+    }}
 }
