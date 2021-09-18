bf692eee442a37d2269cc7fb9b275817a2a7935e
bryanedds <bryanedds@gmail.com>
2021-09-18 12:55:23 -0400

    Designed castle narrative room.
    Former-commit-id: afd6c3521d1fdc8903b21bbd51d3356e6d2fd4b1

---
+++ Projects/OmniBlade/Assets/Castle/CastleInterior.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="CastleInterior" tilewidth="48" tileheight="48" tilecount="1680" columns="48">
+ <properties>
+  <property name="Image" value="[Castle CastleInterior]"/>
+ </properties>
+ <image source="CastleInterior.png" width="2304" height="1680"/>
+</tileset>
