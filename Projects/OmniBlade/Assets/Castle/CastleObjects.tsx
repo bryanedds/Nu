afd6c3521d1fdc8903b21bbd51d3356e6d2fd4b1
bryanedds <bryanedds@gmail.com>
2021-09-18 12:55:23 -0400

    Designed castle narrative room.

---
+++ Projects/OmniBlade/Assets/Castle/CastleObjects.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="CastleObjects" tilewidth="48" tileheight="48" tilecount="256" columns="48">
+ <properties>
+  <property name="Image" value="[Castle CastleObjects]"/>
+ </properties>
+ <image source="CastleObjects.png" width="768" height="768"/>
+</tileset>
