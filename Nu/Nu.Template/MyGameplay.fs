d198ed3c1f36799c5cdee34fb169eb22208294b8
bryanedds <bryanedds@gmail.com>
2021-09-30 18:56:07 -0400

    Placing template types inside modules.

--- Nu/Nu.Template/MyGameplay.fs
+++ Nu/Nu.Template/MyGameplay.fs
@@ -3,97 +3,98 @@ open Prime
 open Nu
 open Nu.Declarative
 
-// this is our Elm-style model type. Either we're playing or we're quitting back to the title screen.
-type Gameplay =
-    | Playing
-    | Quitting
-
-// this is our Elm-style message type.
-type GameplayMessage =
-    | Quit
-
-// this is our Elm-style command type. Commands are used instead of messages when things like physics are involved.
-type GameplayCommand =
-    | MoveLeft
-    | MoveRight
-    | Jump
-    | EyeTrack
-    | Nop
-
-// this extends the Screen API to expose the above model. This is an unfortunate bit of boilerplate
-// needed when defining a new dispatcher.
 [<AutoOpen>]
-module ScreenExtension =
+module MyGameplay =
+
+    // this is our Elm-style model type. Either we're playing or we're quitting back to the title screen.
+    type Gameplay =
+        | Playing
+        | Quitting
+
+    // this is our Elm-style message type.
+    type GameplayMessage =
+        | Quit
+
+    // this is our Elm-style command type. Commands are used instead of messages when things like physics are involved.
+    type GameplayCommand =
+        | MoveLeft
+        | MoveRight
+        | Jump
+        | EyeTrack
+        | Nop
+
+    // this extends the Screen API to expose the above model. This is an unfortunate bit of boilerplate
+    // needed when defining a new dispatcher.
     type Screen with
         member this.GetGameplay = this.GetModelGeneric<Gameplay>
         member this.SetGameplay = this.SetModelGeneric<Gameplay>
         member this.Gameplay = this.ModelGeneric<Gameplay> ()
 
-// this is the screen dispatcher that defines the screen where gameplay takes place
-type MyGameplayDispatcher () =
-    inherit ScreenDispatcher<Gameplay, GameplayMessage, GameplayCommand> (Quitting)
+    // this is the screen dispatcher that defines the screen where gameplay takes place
+    type MyGameplayDispatcher () =
+        inherit ScreenDispatcher<Gameplay, GameplayMessage, GameplayCommand> (Quitting)
 
-    // here we channel from events to signals
-    override this.Channel (_, _) =
-        [Simulants.Game.KeyboardKeyDownEvent =|> fun evt ->
-            if evt.Data.KeyboardKey = KeyboardKey.Up && not evt.Data.Repeated then cmd Jump
-            else cmd Nop
-         Simulants.Gameplay.Screen.UpdateEvent =|> fun _ ->
-            if KeyboardState.isKeyDown KeyboardKey.Left then cmd MoveLeft
-            elif KeyboardState.isKeyDown KeyboardKey.Right then cmd MoveRight
-            else cmd Nop
-         Simulants.Gameplay.Screen.PostUpdateEvent => cmd EyeTrack]
+        // here we channel from events to signals
+        override this.Channel (_, _) =
+            [Simulants.Game.KeyboardKeyDownEvent =|> fun evt ->
+                if evt.Data.KeyboardKey = KeyboardKey.Up && not evt.Data.Repeated then cmd Jump
+                else cmd Nop
+             Simulants.Gameplay.Screen.UpdateEvent =|> fun _ ->
+                if KeyboardState.isKeyDown KeyboardKey.Left then cmd MoveLeft
+                elif KeyboardState.isKeyDown KeyboardKey.Right then cmd MoveRight
+                else cmd Nop
+             Simulants.Gameplay.Screen.PostUpdateEvent => cmd EyeTrack]
 
-    // here we handle the above messages
-    override this.Message (_, message, _, _) =
-        match message with
-        | Quit -> just Quitting
+        // here we handle the above messages
+        override this.Message (_, message, _, _) =
+            match message with
+            | Quit -> just Quitting
 
-    // here we handle the above commands
-    override this.Command (_, command, _, world) =
-        let world =
-            match command with
-            | MoveLeft ->
-                let physicsId = Simulants.Gameplay.Scene.Player.GetPhysicsId world
-                if World.isBodyOnGround physicsId world
-                then World.applyBodyForce (v2 -2000.0f 0.0f) physicsId world
-                else World.applyBodyForce (v2 -500.0f 0.0f) physicsId world
-            | MoveRight ->
-                let physicsId = Simulants.Gameplay.Scene.Player.GetPhysicsId world
-                if World.isBodyOnGround physicsId world
-                then World.applyBodyForce (v2 2000.0f 0.0f) physicsId world
-                else World.applyBodyForce (v2 500.0f 0.0f) physicsId world
-            | Jump ->
-                let physicsId = Simulants.Gameplay.Scene.Player.GetPhysicsId world
-                if World.isBodyOnGround physicsId world then
-                    let world = World.applyBodyForce (v2 0.0f 90000.0f) physicsId world
-                    World.playSound Constants.Audio.SoundVolumeDefault (asset "Gameplay" "Jump") world
-                else world
-            | EyeTrack ->
-                if World.getUpdateRate world <> 0L
-                then Simulants.Game.SetEyeCenter (Simulants.Gameplay.Scene.Player.GetCenter world) world
-                else world
-            | Nop -> world
-        just world
+        // here we handle the above commands
+        override this.Command (_, command, _, world) =
+            let world =
+                match command with
+                | MoveLeft ->
+                    let physicsId = Simulants.Gameplay.Scene.Player.GetPhysicsId world
+                    if World.isBodyOnGround physicsId world
+                    then World.applyBodyForce (v2 -2000.0f 0.0f) physicsId world
+                    else World.applyBodyForce (v2 -500.0f 0.0f) physicsId world
+                | MoveRight ->
+                    let physicsId = Simulants.Gameplay.Scene.Player.GetPhysicsId world
+                    if World.isBodyOnGround physicsId world
+                    then World.applyBodyForce (v2 2000.0f 0.0f) physicsId world
+                    else World.applyBodyForce (v2 500.0f 0.0f) physicsId world
+                | Jump ->
+                    let physicsId = Simulants.Gameplay.Scene.Player.GetPhysicsId world
+                    if World.isBodyOnGround physicsId world then
+                        let world = World.applyBodyForce (v2 0.0f 90000.0f) physicsId world
+                        World.playSound Constants.Audio.SoundVolumeDefault (asset "Gameplay" "Jump") world
+                    else world
+                | EyeTrack ->
+                    if World.getUpdateRate world <> 0L
+                    then Simulants.Game.SetEyeCenter (Simulants.Gameplay.Scene.Player.GetCenter world) world
+                    else world
+                | Nop -> world
+            just world
 
-    // here we describe the content of the game including the level, the hud, and the player
-    override this.Content (_, screen) =
+        // here we describe the content of the game including the level, the hud, and the player
+        override this.Content (_, screen) =
 
-        [// the gui group
-         Content.group Simulants.Gameplay.Gui.Group.Name []
-             [Content.button Simulants.Gameplay.Gui.Quit.Name
-                 [Entity.Text == "Quit"
-                  Entity.Position == v2 260.0f -260.0f
-                  Entity.Elevation == 10.0f
-                  Entity.ClickEvent ==> msg Quit]]
+            [// the gui group
+             Content.group Simulants.Gameplay.Gui.Group.Name []
+                 [Content.button Simulants.Gameplay.Gui.Quit.Name
+                     [Entity.Text == "Quit"
+                      Entity.Position == v2 260.0f -260.0f
+                      Entity.Elevation == 10.0f
+                      Entity.ClickEvent ==> msg Quit]]
 
-         // the player group
-         Content.groupIfScreenSelected screen $ fun _ _ ->
-            Content.group Simulants.Gameplay.Scene.Group.Name []
-                [Content.character Simulants.Gameplay.Scene.Player.Name
-                    [Entity.Position == v2 0.0f 0.0f
-                     Entity.Size == v2 108.0f 108.0f]]
-        
-         // the level group
-         Content.groupIfScreenSelected screen $ fun _ _ ->
-            Content.groupFromFile Simulants.Gameplay.Level.Group.Name "Assets/Gameplay/Level.nugroup"]
+             // the player group
+             Content.groupIfScreenSelected screen $ fun _ _ ->
+                Content.group Simulants.Gameplay.Scene.Group.Name []
+                    [Content.character Simulants.Gameplay.Scene.Player.Name
+                        [Entity.Position == v2 0.0f 0.0f
+                         Entity.Size == v2 108.0f 108.0f]]
+
+             // the level group
+             Content.groupIfScreenSelected screen $ fun _ _ ->
+                Content.groupFromFile Simulants.Gameplay.Level.Group.Name "Assets/Gameplay/Level.nugroup"]
--- Projects/UserProject/MyGame.fs
+++ Projects/UserProject/MyGame.fs
@@ -3,68 +3,78 @@ open Prime
 open Nu
 open Nu.Declarative
 
-// this is our Elm-style model type. It determines what state the game is in. To learn about the
-// Elm-style in Nu, read this article here -
-// https://vsyncronicity.com/2020/03/01/a-game-engine-in-the-elm-style/
-type Model =
-    | Splash
-    | Title
-    | Credits
-    | Gameplay of Gameplay
+[<AutoOpen>]
+module MyGame =
 
-// this is our Elm-style message type. It provides a signal to show the various screens.
-type Message =
-    | ShowTitle
-    | ShowCredits
-    | ShowGameplay
+    // this is our Elm-style model type. It determines what state the game is in. To learn about the
+    // Elm-style in Nu, read this article here -
+    // https://vsyncronicity.com/2020/03/01/a-game-engine-in-the-elm-style/
+    type Model =
+        | Splash
+        | Title
+        | Credits
+        | Gameplay of Gameplay
 
-// this is our Elm-style command type. Commands are used instead of messages when explicitly
-// updating the world is involved.
-type Command =
-    | Exit
+    // this is our Elm-style message type. It provides a signal to show the various screens.
+    type Message =
+        | ShowTitle
+        | ShowCredits
+        | ShowGameplay
 
-// this is the game dispatcher that is customized for our game. In here, we create screens as
-// content and bind them up with events and properties.
-type MyGameDispatcher () =
-    inherit GameDispatcher<Model, Message, Command> (Splash)
+    // this is our Elm-style command type. Commands are used instead of messages when explicitly
+    // updating the world is involved.
+    type Command =
+        | Exit
 
-    // here we channel from screen events to signals
-    override this.Channel (_, _) =
-        [Simulants.Title.Gui.Credits.ClickEvent => msg ShowCredits
-         Simulants.Title.Gui.Play.ClickEvent => msg ShowGameplay
-         Simulants.Title.Gui.Exit.ClickEvent => cmd Exit
-         Simulants.Credits.Gui.Back.ClickEvent => msg ShowTitle]
+    // this extends the Game API to expose the above model. This is an unfortunate bit of boilerplate
+    // needed when defining a new dispatcher.
+    type Game with
+        member this.GetModel = this.GetModelGeneric<Model>
+        member this.SetModel = this.SetModelGeneric<Model>
+        member this.Model = this.ModelGeneric<Model> ()
 
-    // here we bind the desired screen based on the state of the game (or None if splashing), then we
-    // bind the game model and gameplay model in both directions (two-way binding).
-    override this.Initializers (model, game) =
-        [game.DesiredScreenOpt <== model --> fun model ->
-            match model with
-            | Splash -> None
-            | Title -> Some Simulants.Title.Screen
-            | Credits -> Some Simulants.Credits.Screen
-            | Gameplay gameplay ->
-                match gameplay with
-                | Playing -> Some Simulants.Gameplay.Screen
-                | Quitting -> Some Simulants.Title.Screen
-         game.ModelGeneric () <== Simulants.Gameplay.Screen.ModelGeneric () --> function gameplay -> Gameplay gameplay
-         Simulants.Gameplay.Screen.ModelGeneric () <== game.ModelGeneric () --> function Gameplay gameplay -> gameplay | _ -> Quitting]
+    // this is the game dispatcher that is customized for our game. In here, we create screens as
+    // content and bind them up with events and properties.
+    type MyGameDispatcher () =
+        inherit GameDispatcher<Model, Message, Command> (Splash)
 
-    // here we handle the above messages
-    override this.Message (_, message, _, _) =
-        match message with
-        | ShowTitle -> just Title
-        | ShowCredits -> just Credits
-        | ShowGameplay -> just (Gameplay Playing)
+        // here we channel from screen events to signals
+        override this.Channel (_, _) =
+            [Simulants.Title.Gui.Credits.ClickEvent => msg ShowCredits
+             Simulants.Title.Gui.Play.ClickEvent => msg ShowGameplay
+             Simulants.Title.Gui.Exit.ClickEvent => cmd Exit
+             Simulants.Credits.Gui.Back.ClickEvent => msg ShowTitle]
 
-    // here we handle the above commands
-    override this.Command (_, command, _, world) =
-        match command with
-        | Exit -> just (World.exit world)
+        // here we bind the game model and gameplay model in both directions (two-way binding), then we
+        // bind the desired screen based on the state of the game (or None if splashing), then .
+        override this.Initializers (model, game) =
+            [game.DesiredScreenOpt <== model --> fun model ->
+                match model with
+                | Splash -> None
+                | Title -> Some Simulants.Title.Screen
+                | Credits -> Some Simulants.Credits.Screen
+                | Gameplay gameplay ->
+                    match gameplay with
+                    | Playing -> Some Simulants.Gameplay.Screen
+                    | Quitting -> Some Simulants.Title.Screen
+             game.Model <== Simulants.Gameplay.Screen.Gameplay --> Gameplay
+             Simulants.Gameplay.Screen.Gameplay <== game.Model --> function Gameplay gameplay -> gameplay | _ -> Quitting]
 
-    // here we describe the content of the game, including all of its screens.
-    override this.Content (_, _) =
-        [Content.screen Simulants.Splash.Screen.Name (Nu.Splash (Constants.Dissolve.Default, Constants.Splash.Default, None, Simulants.Title.Screen)) [] []
-         Content.screenFromGroupFile Simulants.Title.Screen.Name (Dissolve (Constants.Dissolve.Default, None)) "Assets/Gui/Title.nugroup"
-         Content.screenFromGroupFile Simulants.Credits.Screen.Name (Dissolve (Constants.Dissolve.Default, None)) "Assets/Gui/Credits.nugroup"
-         Content.screen<MyGameplayDispatcher> Simulants.Gameplay.Screen.Name (Dissolve (Constants.Dissolve.Default, None)) [] []]
+        // here we handle the above messages
+        override this.Message (_, message, _, _) =
+            match message with
+            | ShowTitle -> just Title
+            | ShowCredits -> just Credits
+            | ShowGameplay -> just (Gameplay Playing)
+
+        // here we handle the above commands
+        override this.Command (_, command, _, world) =
+            match command with
+            | Exit -> just (World.exit world)
+
+        // here we describe the content of the game, including all of its screens.
+        override this.Content (_, _) =
+            [Content.screen Simulants.Splash.Screen.Name (Nu.Splash (Constants.Dissolve.Default, Constants.Splash.Default, None, Simulants.Title.Screen)) [] []
+             Content.screenFromGroupFile Simulants.Title.Screen.Name (Dissolve (Constants.Dissolve.Default, None)) "Assets/Gui/Title.nugroup"
+             Content.screenFromGroupFile Simulants.Credits.Screen.Name (Dissolve (Constants.Dissolve.Default, None)) "Assets/Gui/Credits.nugroup"
+             Content.screen<MyGameplayDispatcher> Simulants.Gameplay.Screen.Name (Dissolve (Constants.Dissolve.Default, None)) [] []]
