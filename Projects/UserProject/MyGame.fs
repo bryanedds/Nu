61316c45a48f373bbcd1e649063039f6cbd3842a
bryanedds <bryanedds@gmail.com>
2021-10-05 11:24:35 -0400

    Comment fixes.
    Former-commit-id: 56924104389e382b3b5f9093092878d53edb1707

--- Projects/UserProject/MyGame.fs
+++ Projects/UserProject/MyGame.fs
@@ -40,7 +40,7 @@ module MyGame =
     type MyGameDispatcher () =
         inherit GameDispatcher<Model, Message, Command> (Splash)
 
-        // here we channel from screen events to signals
+        // here we channel from gui events to signals
         override this.Channel (_, _) =
             [Simulants.Title.Gui.Credits.ClickEvent => msg ShowCredits
              Simulants.Title.Gui.Play.ClickEvent => msg ShowGameplay
