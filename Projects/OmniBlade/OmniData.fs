0c51f658f2b4f5376804587c11c6699c60b36e53
bryanedds <bryanedds@gmail.com>
2021-10-05 22:56:10 -0400

    Fixed warnings.

--- Projects/OmniBlade/OmniData.fs
+++ Projects/OmniBlade/OmniData.fs
@@ -602,7 +602,8 @@ type [<NoEquality; NoComparison>] Cue =
     static member notNil cue = match cue with Nil -> false | _ -> true
     static member isInterrupting (inventory : Inventory) (advents : Advent Set) cue =
         match cue with
-        | Nil | PlaySound _ | PlaySong _ | FadeOutSong _ | Face _ | Recruit _ | Unseal _ | AddItem _ | RemoveItem _ | AddAdvent _ | RemoveAdvent _ | ReplaceAdvent _ -> false
+        | Nil | PlaySound _ | PlaySong _ | FadeOutSong _ | Face _ | Recruit _ | Unseal _ -> false
+        | ShowCharacter _ | HideCharacter _ | AddItem _ | RemoveItem _ | AddAdvent _ | RemoveAdvent _ | ReplaceAdvent _ -> false
         | Wait _ | WaitState _ | Move _ | MoveState _ | Warp _ | WarpState _ | Battle _ | BattleState _ | Dialog _ | DialogState _ | Prompt _ | PromptState _ -> true
         | Animate (_, _, wait) | AnimateState (_, wait) -> match wait with Timed 0L | NoWait -> false | _ -> true
         | If (p, c, a) ->
