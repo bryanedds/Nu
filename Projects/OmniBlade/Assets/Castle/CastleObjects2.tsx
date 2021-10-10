506c13a18f74076a3d1564b95d0e904b8befd242
bryanedds <bryanedds@gmail.com>
2021-10-10 15:12:42 -0400

    Made BS sections into throne rooms.

---
+++ Projects/OmniBlade/Assets/Castle/CastleObjects2.tsx
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="CastleObjects2" tilewidth="48" tileheight="48" tilecount="99" columns="9">
+ <image source="CastleObjects2.png" width="432" height="528"/>
+ <properties>
+  <property name="Image" value="[Castle CastleObjects2]"/>
+ </properties>
+ <tile id="10">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+ <tile id="93">
+  <properties>
+   <property name="C" value=""/>
+  </properties>
+ </tile>
+</tileset>
