57847ac27bbca821b05afad065279aa96024e7bd
bryanedds <bryanedds@gmail.com>
2022-04-06 17:34:15 -0400

    Added Integration and Drawing concepts.

---
+++ Nu/Nu/Drawing.fs
@@ -0,0 +1,7 @@
+﻿// Nu Game Engine.
+// Copyright (C) Bryan Edds, 2013-2020.
+
+namespace Nu
+open Nu
+
+// 3D rendering implemented in here with the 'Draw' nomenclature.
---
+++ Nu/Nu/Integration.fs
@@ -0,0 +1,7 @@
+﻿// Nu Game Engine.
+// Copyright (C) Bryan Edds, 2013-2020.
+
+namespace Nu
+open Nu
+
+// 3D physics integrated in here with the 'Integration' nomenclature.
