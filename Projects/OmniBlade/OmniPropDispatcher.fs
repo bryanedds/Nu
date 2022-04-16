ea7879b99dad68ff093d469ecee66c7091b99a58
bryanedds <bryanedds@gmail.com>
2022-04-16 13:36:58 -0400

    More Omniblade conversion.
    Former-commit-id: 4647b113f2e6080c1695979330f7e419b689f748

--- Projects/OmniBlade/OmniPropDispatcher.fs
+++ Projects/OmniBlade/OmniPropDispatcher.fs
@@ -18,10 +18,10 @@ module PropDispatcher =
         member this.Prop = this.ModelGeneric<Prop> ()
 
     type PropDispatcher () =
-        inherit EntityDispatcher<Prop, unit, unit> (Prop.empty)
+        inherit EntityDispatcher2d<Prop, unit, unit> (Prop.empty)
 
         static member Facets =
-            [typeof<RigidBody2dFastFacet>]
+            [typeof<RigidBody2dFacet>]
 
         override this.Initializers (prop, entity) =
             [entity.BodyType == Static
@@ -29,7 +29,7 @@ module PropDispatcher =
              entity.FixedRotation == true
              entity.GravityScale == 0.0f
              entity.BodyType == Static
-             entity.Bounds <== prop --> fun prop -> prop.Bounds
+             entity.DimensionsRaw <== prop --> fun prop -> prop.Bounds
              entity.IsSensor <== prop --> fun prop ->
                 match prop.PropData with
                 | Portal _ | Sensor _ | SavePoint -> true
@@ -39,44 +39,44 @@ module PropDispatcher =
                 | Sprite _ ->
                     BodyEmpty
                 | Portal _ | Switch _ ->
-                    BodyBox { Extent = v2 0.5f 0.5f; Center = v2Zero; PropertiesOpt = None }
+                    BodyBox { Extent = v3 0.5f 0.5f 0.0f; Center = v3Zero; PropertiesOpt = None }
                 | SavePoint _ ->
-                    BodyCircle { Radius = 0.1f; Center = v2Zero; PropertiesOpt = None }
+                    BodySphere { Radius = 0.1f; Center = v3Zero; PropertiesOpt = None }
                 | Door _ ->
                     match prop.PropState with
                     | DoorState true -> BodyEmpty
-                    | _ -> BodyBox { Extent = v2 0.5f 0.5f; Center = v2Zero; PropertiesOpt = None }
+                    | _ -> BodyBox { Extent = v3 0.5f 0.5f 0.0f; Center = v3Zero; PropertiesOpt = None }
                 | Chest _ ->
-                    BodyBox { Extent = v2 0.5f 0.5f; Center = v2Zero; PropertiesOpt = None }
+                    BodyBox { Extent = v3 0.5f 0.5f 0.0f; Center = v3Zero; PropertiesOpt = None }
                 | Sensor (_, shapeOpt, _, _, _) ->
                     match shapeOpt with
                     | Some shape -> shape
-                    | None -> BodyBox { Extent = v2 0.5f 0.5f; Center = v2Zero; PropertiesOpt = None }
+                    | None -> BodyBox { Extent = v3 0.5f 0.5f 0.0f; Center = v3Zero; PropertiesOpt = None }
                 | Seal (_, _, requirements) ->
                     if prop.Advents.IsSupersetOf requirements
-                    then BodyBox { Extent = v2 0.5f 0.5f; Center = v2Zero; PropertiesOpt = None }
+                    then BodyBox { Extent = v3 0.5f 0.5f 0.0f; Center = v3Zero; PropertiesOpt = None }
                     else BodyEmpty
                 | Character (_, _, _, _, _, requirements) ->
                     if prop.Advents.IsSupersetOf requirements
-                    then BodyBox { Extent = v2 0.16f 0.16f; Center = v2 -0.01f -0.36f; PropertiesOpt = None }
+                    then BodyBox { Extent = v3 0.16f 0.16f 0.0f; Center = v3 -0.01f -0.36f 0.0f; PropertiesOpt = None }
                     else BodyEmpty
                 | Npc (npcType, _, _, requirements) | NpcBranching (npcType, _, _, requirements) ->
                     if prop.Advents.IsSupersetOf requirements && NpcType.exists prop.Advents npcType then
                         match npcType with
                         | ShadeNpc | MaelNpc | RiainNpc | PericNpc
                         | RavelNpc | AdvenNpc | EildaenNpc | NostrusNpc
-                        | MadTrixterNpc | HeavyArmorosNpc -> BodyBox { Extent = v2 0.16f 0.16f; Center = v2 -0.01f -0.36f; PropertiesOpt = None }
-                        | AraneaImplicitumNpc -> BodyBox { Extent = v2 0.16f 0.16f; Center = v2 -0.01f -0.36f; PropertiesOpt = None }
+                        | MadTrixterNpc | HeavyArmorosNpc -> BodyBox { Extent = v3 0.16f 0.16f 0.0f; Center = v3 -0.01f -0.36f 0.0f; PropertiesOpt = None }
+                        | AraneaImplicitumNpc -> BodyBox { Extent = v3 0.16f 0.16f 0.0f; Center = v3 -0.01f -0.36f 0.0f; PropertiesOpt = None }
                     else BodyEmpty
                 | Shopkeep (_, _, _, requirements) ->
                     if prop.Advents.IsSupersetOf requirements
-                    then BodyBox { Extent = v2 0.16f 0.16f; Center = v2 -0.01f -0.36f; PropertiesOpt = None }
+                    then BodyBox { Extent = v3 0.16f 0.16f 0.0f; Center = v3 -0.01f -0.36f 0.0f; PropertiesOpt = None }
                     else BodyEmpty
                 | Flame _ | ChestSpawn | EmptyProp ->
                     BodyEmpty]
 
         override this.View (prop, entity, world) =
-            if entity.GetVisible world && entity.GetInView world then
+            if entity.GetVisible world && entity.GetInView2d world then
                 let transform = entity.GetTransform world
                 let (background, image, color, blend, glow, insetOpt, flip) =
                     match prop.PropData with
@@ -97,7 +97,7 @@ module PropDispatcher =
                                 let localTime = time / 10L
                                 let celSize = Constants.Gameplay.TileCelSize
                                 let celColumn = single (localTime % 4L)
-                                let inset = v4Bounds (v2 (celSize.X * celColumn) 0.0f) celSize // TODO: P1: turn this into a general animation function if one doesn't already exist...
+                                let inset = Box2 (v2 (celSize.X * celColumn) 0.0f, celSize) // TODO: P1: turn this into a general animation function if one doesn't already exist...
                                 let image = Assets.Field.WarpAnimationSheet
                                 (true, image, colWhite, Transparent, colZero, Some inset, FlipNone)
                             | StairsPortal descending ->
@@ -109,7 +109,7 @@ module PropDispatcher =
                                     | Downward -> Constants.Gameplay.TileCelSize.X * 2.0f
                                     | Leftward -> Constants.Gameplay.TileCelSize.X * 3.0f
                                 let offset = v2 offsetX offsetY
-                                (true, Assets.Field.StairsImage, colWhite, Transparent, colZero, Some (v4Bounds offset Constants.Gameplay.TileCelSize), FlipNone)
+                                (true, Assets.Field.StairsImage, colWhite, Transparent, colZero, Some (Box2 (offset, Constants.Gameplay.TileCelSize)), FlipNone)
                         else (false, Assets.Default.ImageEmpty, colWhite, Transparent, colZero, None, FlipNone)
                     | Door (doorType, _, _, _, _) ->
                         let image =
@@ -151,7 +151,7 @@ module PropDispatcher =
                             let localTime = time / 20L
                             let celSize = v2 32.0f 32.0f // TODO: P1: put this in Constants.
                             let celColumn = single (localTime % 4L)
-                            let inset = v4Bounds (v2 (celSize.X * celColumn) 0.0f) celSize // TODO: P1: turn this into a general animation function if one doesn't already exist...
+                            let inset = Box2 (v2 (celSize.X * celColumn) 0.0f, celSize) // TODO: P1: turn this into a general animation function if one doesn't already exist...
                             let image = Assets.Field.SealAnimationSheet
                             (false, image, color, Transparent, colZero, Some inset, FlipNone)
                         else (false, Assets.Default.ImageEmpty, colWhite, Transparent, colZero, None, FlipNone)
@@ -197,12 +197,12 @@ module PropDispatcher =
                                 match directionOpt with
                                 | Some direction -> direction
                                 | None ->
-                                    let direction = Direction.ofVector2 (prop.PointOfInterest - prop.Bottom)
+                                    let direction = Direction.ofVector3 (prop.PointOfInterest - prop.Bottom)
                                     if direction = Upward then Downward else direction
                             let column = column + CharacterAnimationState.directionToInt direction
-                            let celSize = size / 3.0f
+                            let celSize = (size / 3.0f).XY
                             let insetPosition = v2 (single column) (single row) * celSize
-                            let inset = v4Bounds insetPosition celSize
+                            let inset = Box2 (insetPosition, celSize)
                             (false, image, colWhite, Transparent, colZero, Some inset, FlipNone)
                         else (false, Assets.Default.ImageEmpty, colWhite, Transparent, colZero, None, FlipNone)
                     | Shopkeep (shopkeepType, directionOpt, _, requirements) ->
@@ -213,11 +213,11 @@ module PropDispatcher =
                                 match directionOpt with
                                 | Some direction -> direction
                                 | None ->
-                                    let direction = Direction.ofVector2 (prop.PointOfInterest - prop.Bottom)
+                                    let direction = Direction.ofVector3 (prop.PointOfInterest - prop.Bottom)
                                     if direction = Upward then Downward else direction
                             let column = CharacterAnimationState.directionToInt direction
                             let insetPosition = v2 (single column) (single row) * Constants.Gameplay.CharacterCelSize
-                            let inset = v4Bounds insetPosition Constants.Gameplay.CharacterCelSize
+                            let inset = Box2 (insetPosition, Constants.Gameplay.CharacterCelSize)
                             (false, image, colWhite, Transparent, colZero, Some inset, FlipNone)
                         else (false, Assets.Default.ImageEmpty, colWhite, Transparent, colZero, None, FlipNone)
                     | Flame (flameType, mirror) ->
@@ -226,27 +226,25 @@ module PropDispatcher =
                         let row = if mirror then 4 else 0
                         let cel = int (World.getUpdateTime world / 10L % 4L) // TODO: P1: put this in Constants.
                         let inset =
-                            v4 // TODO: P1: put the following hard-coded dimensions in Constants.
-                                (single column * 16.0f) (single (row + cel) * 16.0f)
-                                16.0f 16.0f
+                            Box2 // TODO: P1: put the following hard-coded dimensions in Constants.
+                                (v2 (single column * 16.0f) (single (row + cel) * 16.0f),
+                                 v2 16.0f 16.0f)
                         (false, image, colWhite, Transparent, colZero, Some inset, FlipNone)
                     | SavePoint ->
                         let time = World.getUpdateTime world
                         let image = Assets.Field.SavePointImage
                         let column = (int time / 15) % 4
                         let insetPosition = v2 (single column) 0.0f * Constants.Gameplay.TileCelSize
-                        let inset = v4Bounds insetPosition Constants.Gameplay.TileCelSize
+                        let inset = Box2 (insetPosition, Constants.Gameplay.TileCelSize)
                         (false, image, colWhite, Additive, colZero, Some inset, FlipNone)
                     | ChestSpawn | EmptyProp ->
                         (false, Assets.Default.ImageEmpty, colWhite, Transparent, colZero, None, FlipNone)
                 let elevation = if background then Constants.Field.FlooringElevation else Constants.Field.ForegroundElevation
-                let positionY = transform.Position.Y
+                let positionY = transform.DimensionsRaw.Position.Y
                 let assetTag = AssetTag.generalize image
-                Render (elevation, positionY, assetTag,
+                Render2d (elevation, positionY, assetTag,
                     SpriteDescriptor
                         { Transform = transform
-                          Absolute = entity.GetAbsolute world
-                          Offset = Vector2.Zero
                           InsetOpt = insetOpt
                           Image = image
                           Color = color
