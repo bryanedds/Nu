8737e03a40e7786e7a379213528c77955db72287
bryanedds <bryanedds@gmail.com>
2022-05-23 08:07:37 -0400

    Metrics change.
    Former-commit-id: 9f5dbceaf5d93de2473d90bae6eb67262f687285

--- Projects/Metrics/Program.fs
+++ Projects/Metrics/Program.fs
@@ -194,7 +194,7 @@ type MyGameDispatcher () =
         let world = World.createEntity<FpsDispatcher> (Some Fps.Surnames) DefaultOverlay Simulants.DefaultGroup world |> snd
         let world = Fps.SetPosition (v3 200.0f -250.0f 0.0f) world
 #if !ECS
-        let positions = // 20,000 entity positions (goal: 60FPS, current: 60FPS)
+        let positions = // ~20,000 entity positions (goal: 60FPS, current: 60FPS)
             seq {
                 for i in 0 .. 52 do
                     for j in 0 .. 52 do
@@ -297,7 +297,7 @@ type [<ReferenceEquality>] Intss =
         { Intss = intss.Intss |> Map.map (fun k v -> if k % 1 = 0 then Ints.inc v else v) }
 
 type ElmishGameDispatcher () =
-    inherit GameDispatcher<Intss, int, unit> (Intss.init 70) // 4900 elmish entities (goal: 60FPS w/o Stalls, current: 60FPS w/o Stalls)
+    inherit GameDispatcher<Intss, int, unit> (Intss.init 70) // ~5000 elmish entities (goal: 60FPS w/o Stalls, current: 60FPS w/o Stalls)
 
     override this.Channel (_, game) =
         [game.UpdateEvent => msg 0]
