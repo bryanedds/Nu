5cbf19a4a291d564a48030d9d5d5870d14bc3a44
bryanedds <bryanedds@gmail.com>
2022-05-24 22:50:13 -0400

    Now debugging OpenGL messages.

--- Nu/Nu/OpenGL.Hl.fs
+++ Nu/Nu/OpenGL.Hl.fs
@@ -34,6 +34,7 @@ module OpenGL = let _ = ()
 
 namespace OpenGL
 open System.Runtime.InteropServices
+open System.Text
 open SDL2
 open Prime
 open Nu
@@ -43,12 +44,37 @@ module Hl =
 
     /// Assert a lack of Gl error. Has an generic parameter to enable value pass-through.
     let Assert (a : 'a) =
-#if DEBUG_RENDERING
+#if DEBUG_RENDERING_ASSERT
         let error = OpenGL.Gl.GetError ()
         if error <> OpenGL.ErrorCode.NoError then
             Log.debug ("OpenGL assertion failed due to: " + string error)
 #endif
         a
+
+#if DEBUG_RENDERING_MESSAGE
+    let private DebugMessageListener (_ : OpenGL.DebugSource) (_ : OpenGL.DebugType) (_ : uint) (severity : OpenGL.DebugSeverity) (length : int) (message : nativeint) (_ : nativeint) =
+        let messageBytes = Array.zeroCreate<byte> length
+        Marshal.Copy (message, messageBytes, 0, length)
+        let messageStr = Encoding.ASCII.GetString (messageBytes, 0, length)
+        match severity with
+        | OpenGL.DebugSeverity.DebugSeverityNotification
+        | OpenGL.DebugSeverity.DebugSeverityLow -> Log.info messageStr
+        | OpenGL.DebugSeverity.DebugSeverityMedium
+        | OpenGL.DebugSeverity.DebugSeverityHigh -> Log.debug messageStr
+        | OpenGL.DebugSeverity.DontCare
+        | _ -> ()
+
+    let DebugMessageProc =
+        OpenGL.Gl.DebugProc DebugMessageListener
+
+    /// Listen to the OpenGL error stream.
+    let AttachDebugMessageCallback () =
+        OpenGL.Gl.DebugMessageCallback (DebugMessageProc, nativeint 0)
+#else
+    /// Listen to the OpenGL error stream.
+    let AttachDebugMessageCallback () =
+        ()
+#endif
 
     /// Create an SDL OpenGL context.
     let CreateSgl410Context window =
@@ -56,6 +82,9 @@ module Hl =
         SDL.SDL_GL_SetAttribute (SDL.SDL_GLattr.SDL_GL_CONTEXT_MAJOR_VERSION, 4) |> ignore<int>
         SDL.SDL_GL_SetAttribute (SDL.SDL_GLattr.SDL_GL_CONTEXT_MINOR_VERSION, 1) |> ignore<int>
         SDL.SDL_GL_SetAttribute (SDL.SDL_GLattr.SDL_GL_CONTEXT_PROFILE_MASK, SDL.SDL_GLprofile.SDL_GL_CONTEXT_PROFILE_CORE) |> ignore<int>
+#if DEBUG_RENDERING_PLUS
+        SDL.SDL_GL_SetAttribute (SDL.SDL_GLattr.SDL_GL_CONTEXT_FLAGS,  int SDL.SDL_GLcontext.SDL_GL_CONTEXT_DEBUG_FLAG) |> ignore<int>
+#endif
         let glContext = SDL.SDL_GL_CreateContext window
         SDL.SDL_GL_SetSwapInterval 1 |> ignore<int>
         OpenGL.Gl.BindAPI ()
