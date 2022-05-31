37501907cbaea8786ad55a8dcaa911e5544d47a1
bryanedds <bryanedds@gmail.com>
2022-05-30 21:09:47 -0400

    More work.

--- Nu/Nu/Ecs.fs
+++ Nu/Nu/Ecs.fs
@@ -96,40 +96,6 @@ type [<StructuralEquality; NoComparison>] 'w SystemSlot =
     { SystemIndex : int
       System : 'w System }
 
-type Query<'c, 'c2, 'c3, 'w when
-    'c : struct and 'c :> 'c Component and
-    'c2 : struct and 'c2 :> 'c2 Component and
-    'c3 : struct and 'c3 :> 'c3 Component> (compName : string, comp2Name : string, comp3Name : string) =
-    
-    let systems = dictPlus<SystemId, 'w System> HashIdentity.Structural []
-
-    member this.CheckCompatibility (system : 'w System) =
-        let stores = system.Stores
-        stores.ContainsKey compName &&
-        stores.ContainsKey comp2Name &&
-        stores.ContainsKey comp3Name
-
-    member this.RegisterSystem (system : 'w System) =
-        systems.Add (system.Id, system)
-
-    member this.Iterate (statement : Statement<'c, 'c2, 'c3, 'w>) world =
-        let mutable world = world
-        for systemEntry in systems do
-            let system = systemEntry.Value
-            let stores = system.Stores
-            let store = stores.[compName] :?> 'c Store
-            let store2 = stores.[comp2Name] :?> 'c2 Store
-            let store3 = stores.[comp3Name] :?> 'c3 Store
-            let mutable i = 0
-            while i < store.Length do
-                world <- statement.Invoke (&store.[i], &store2.[i], &store3.[i], world)
-                i <- inc i
-        world
-
-    interface 'w Query with
-        member this.CheckCompatibility system = this.CheckCompatibility system
-        member this.RegisterSystem system = this.RegisterSystem system
-
 type 'w Ecs () =
 
     let mutable entityIdCurrent = 0UL
