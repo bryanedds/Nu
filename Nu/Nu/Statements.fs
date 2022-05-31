a30e6f63a8ba900a2a645e301a173f5e10325f92
bryanedds <bryanedds@gmail.com>
2022-05-31 02:40:27 -0400

    Renamed ECS System to Archetype.
    Former-commit-id: e83fa2b35cb4490354df7d0fcfe730374f70b3ab

--- Nu/Nu/Statements.fs
+++ Nu/Nu/Statements.fs
@@ -60,3 +60,28 @@ type Statement<'c, 'c2, 'c3, 'c4, 'c5, 'c6, 'c7, 'w when
     'c6 : struct and 'c6 :> 'c6 Component and
     'c7 : struct and 'c7 :> 'c7 Component> =
     delegate of 'c byref * 'c2 byref * 'c3 byref * 'c4 byref * 'c5 byref * 'c6 byref * 'c7 byref * 'w -> 'w
+
+/// A delegate for interfacing with correlated components.
+type Statement<'c, 'c2, 'c3, 'c4, 'c5, 'c6, 'c7, 'c8, 'w when
+    'c : struct and 'c :> 'c Component and
+    'c2 : struct and 'c2 :> 'c2 Component and
+    'c3 : struct and 'c3 :> 'c3 Component and
+    'c4 : struct and 'c4 :> 'c4 Component and
+    'c5 : struct and 'c5 :> 'c5 Component and
+    'c6 : struct and 'c6 :> 'c6 Component and
+    'c7 : struct and 'c7 :> 'c7 Component and
+    'c8 : struct and 'c8 :> 'c8 Component> =
+    delegate of 'c byref * 'c2 byref * 'c3 byref * 'c4 byref * 'c5 byref * 'c6 byref * 'c7 byref * 'c8 byref * 'w -> 'w
+
+/// A delegate for interfacing with correlated components.
+type Statement<'c, 'c2, 'c3, 'c4, 'c5, 'c6, 'c7, 'c8, 'c9, 'w when
+    'c : struct and 'c :> 'c Component and
+    'c2 : struct and 'c2 :> 'c2 Component and
+    'c3 : struct and 'c3 :> 'c3 Component and
+    'c4 : struct and 'c4 :> 'c4 Component and
+    'c5 : struct and 'c5 :> 'c5 Component and
+    'c6 : struct and 'c6 :> 'c6 Component and
+    'c7 : struct and 'c7 :> 'c7 Component and
+    'c8 : struct and 'c8 :> 'c8 Component and
+    'c9 : struct and 'c9 :> 'c9 Component> =
+    delegate of 'c byref * 'c2 byref * 'c3 byref * 'c4 byref * 'c5 byref * 'c6 byref * 'c7 byref * 'c8 byref * 'c9 byref * 'w -> 'w
