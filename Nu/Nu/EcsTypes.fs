b76022d08fe51c8634a36fdc7c794d25973873ec
bryanedds <bryanedds@gmail.com>
2022-06-06 19:00:06 -0400

    Fixed over-constrained schedule return type.
    Former-commit-id: 2572972657274ce6a92cc55b0ce7d0f5ddec6ac5

--- Nu/Nu/EcsTypes.fs
+++ Nu/Nu/EcsTypes.fs
@@ -161,6 +161,87 @@ type Statement<'c, 'c2, 'c3, 'c4, 'c5, 'c6, 'c7, 'c8, 'c9, 's when
     'c9 : struct and 'c9 :> 'c9 Component> =
     delegate of 'c byref * 'c2 byref * 'c3 byref * 'c4 byref * 'c5 byref * 'c6 byref * 'c7 byref * 'c8 byref * 'c9 byref * 's -> 's
 
+/// A delegate for interfacing with Ecs components.
+type StatementPlus<'c, 's when
+    'c : struct and 'c :> 'c Component> =
+    delegate of 'c byref * 's -> unit
+
+/// A delegate for interfacing with Ecs components.
+type StatementPlus<'c, 'c2, 's when
+    'c : struct and 'c :> 'c Component and
+    'c2 : struct and 'c2 :> 'c2 Component> =
+    delegate of 'c byref * 'c2 byref * 's -> unit
+
+/// A delegate for interfacing with Ecs components.
+type StatementPlus<'c, 'c2, 'c3, 's when
+    'c : struct and 'c :> 'c Component and
+    'c2 : struct and 'c2 :> 'c2 Component and
+    'c3 : struct and 'c3 :> 'c3 Component> =
+    delegate of 'c byref * 'c2 byref * 'c3 byref * 's -> unit
+
+/// A delegate for interfacing with Ecs components.
+type StatementPlus<'c, 'c2, 'c3, 'c4, 's when
+    'c : struct and 'c :> 'c Component and
+    'c2 : struct and 'c2 :> 'c2 Component and
+    'c3 : struct and 'c3 :> 'c3 Component and
+    'c4 : struct and 'c4 :> 'c4 Component> =
+    delegate of 'c byref * 'c2 byref * 'c3 byref * 'c4 byref * 's -> unit
+
+/// A delegate for interfacing with Ecs components.
+type StatementPlus<'c, 'c2, 'c3, 'c4, 'c5, 's when
+    'c : struct and 'c :> 'c Component and
+    'c2 : struct and 'c2 :> 'c2 Component and
+    'c3 : struct and 'c3 :> 'c3 Component and
+    'c4 : struct and 'c4 :> 'c4 Component and
+    'c5 : struct and 'c5 :> 'c5 Component> =
+    delegate of 'c byref * 'c2 byref * 'c3 byref * 'c4 byref * 'c5 byref * 's -> unit
+
+/// A delegate for interfacing with Ecs components.
+type StatementPlus<'c, 'c2, 'c3, 'c4, 'c5, 'c6, 's when
+    'c : struct and 'c :> 'c Component and
+    'c2 : struct and 'c2 :> 'c2 Component and
+    'c3 : struct and 'c3 :> 'c3 Component and
+    'c4 : struct and 'c4 :> 'c4 Component and
+    'c5 : struct and 'c5 :> 'c5 Component and
+    'c6 : struct and 'c6 :> 'c6 Component> =
+    delegate of 'c byref * 'c2 byref * 'c3 byref * 'c4 byref * 'c5 byref * 'c6 byref * 's -> unit
+
+/// A delegate for interfacing with Ecs components.
+type StatementPlus<'c, 'c2, 'c3, 'c4, 'c5, 'c6, 'c7, 's when
+    'c : struct and 'c :> 'c Component and
+    'c2 : struct and 'c2 :> 'c2 Component and
+    'c3 : struct and 'c3 :> 'c3 Component and
+    'c4 : struct and 'c4 :> 'c4 Component and
+    'c5 : struct and 'c5 :> 'c5 Component and
+    'c6 : struct and 'c6 :> 'c6 Component and
+    'c7 : struct and 'c7 :> 'c7 Component> =
+    delegate of 'c byref * 'c2 byref * 'c3 byref * 'c4 byref * 'c5 byref * 'c6 byref * 'c7 byref * 's -> unit
+
+/// A delegate for interfacing with Ecs components.
+type StatementPlus<'c, 'c2, 'c3, 'c4, 'c5, 'c6, 'c7, 'c8, 's when
+    'c : struct and 'c :> 'c Component and
+    'c2 : struct and 'c2 :> 'c2 Component and
+    'c3 : struct and 'c3 :> 'c3 Component and
+    'c4 : struct and 'c4 :> 'c4 Component and
+    'c5 : struct and 'c5 :> 'c5 Component and
+    'c6 : struct and 'c6 :> 'c6 Component and
+    'c7 : struct and 'c7 :> 'c7 Component and
+    'c8 : struct and 'c8 :> 'c8 Component> =
+    delegate of 'c byref * 'c2 byref * 'c3 byref * 'c4 byref * 'c5 byref * 'c6 byref * 'c7 byref * 'c8 byref * 's -> unit
+
+/// A delegate for interfacing with Ecs components.
+type StatementPlus<'c, 'c2, 'c3, 'c4, 'c5, 'c6, 'c7, 'c8, 'c9, 's when
+    'c : struct and 'c :> 'c Component and
+    'c2 : struct and 'c2 :> 'c2 Component and
+    'c3 : struct and 'c3 :> 'c3 Component and
+    'c4 : struct and 'c4 :> 'c4 Component and
+    'c5 : struct and 'c5 :> 'c5 Component and
+    'c6 : struct and 'c6 :> 'c6 Component and
+    'c7 : struct and 'c7 :> 'c7 Component and
+    'c8 : struct and 'c8 :> 'c8 Component and
+    'c9 : struct and 'c9 :> 'c9 Component> =
+    delegate of 'c byref * 'c2 byref * 'c3 byref * 'c4 byref * 'c5 byref * 'c6 byref * 'c7 byref * 'c8 byref * 'c9 byref * 's -> unit
+
 and [<StructuralEquality; NoComparison>] Term =
     | Error of string
     | Tag
