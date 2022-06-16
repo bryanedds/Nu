0622ca01c736b363e10055ee991f5d0331a4f09e
bryanedds <bryanedds@gmail.com>
2022-06-16 14:23:18 -0400

    Added functions to load shaders from disk.
    Former-commit-id: 0ae3600a700c5e8870b9c4ed0fb5f68b0d64ad10

--- Nu/Nu/Render3d.fs
+++ Nu/Nu/Render3d.fs
@@ -11,24 +11,24 @@ open Nu
 // I may borrow some sky dome code from here or related - https://github.com/shff/opengl_sky/blob/master/main.mm
 // There appears to be a bias constant that can be used with VSMs to fix up light leaks, so consider that.
 
-/// Describes the material of a 3d surface.
-type Material =
+/// Renders a material of 3d surfaces.
+type RenderMaterial =
     interface // same derived type indicates two materials can potentially be batched
         inherit IComparable // CompareTo of 0 indicates two materials can be drawn in the same batch with the same parameters
         abstract Bounds : Box3 // allows for z-sorting of translucent surfaces
         abstract Transparent : bool // can affect order in which materials are drawn such as in deferred rendering and may disallow batching
-        abstract RenderMany : Material array * Matrix4x4 byref * Matrix4x4 byref * Vector3 * Vector3 * Vector3 * Renderer -> unit // does actual batched opengl calls
+        abstract RenderMany : RenderMaterial array * Matrix4x4 byref * Matrix4x4 byref * Vector3 * Vector3 * Vector3 * Renderer -> unit // does actual batched opengl calls
         end
 
-/// A collection of materials to render in a pass.
-type Materials =
-    { MaterialsOpaque : Map<Material, Material array>
-      MaterialsTransparent : Material array }
+/// A collection of render materials in a pass.
+type RenderMaterials =
+    { RenderMaterialsOpaque : Map<RenderMaterial, RenderMaterial array>
+      RenderMaterialsTransparent : RenderMaterial array }
 
 /// Describes a 3d render pass.
 type [<CustomEquality; CustomComparison>] RenderPassDescriptor3d =
     { RenderPassOrder : int64
-      RenderPass3d : Materials * Matrix4x4 * Renderer3d -> unit }
+      RenderPass3d : RenderMaterials * Matrix4x4 * Renderer3d -> unit }
     interface IComparable with
         member this.CompareTo that =
             match that with
@@ -42,8 +42,8 @@ type [<CustomEquality; CustomComparison>] RenderPassDescriptor3d =
 
 /// A message to the 3d renderer.
 and [<NoEquality; NoComparison>] RenderMessage3d =
-    | MaterialDescriptor of Material
-    | MaterialsDescriptor of Material array
+    | RenderMaterialDescriptor of RenderMaterial
+    | RenderMaterialsDescriptor of RenderMaterial array
     | RenderCallbackDescriptor3d of (Matrix4x4 * Matrix4x4 * Vector3 * Vector3 * Vector3 * Renderer3d -> unit)
     | RenderPrePassDescriptor3d of RenderPassDescriptor3d
     | RenderPostPassDescriptor3d of RenderPassDescriptor3d
