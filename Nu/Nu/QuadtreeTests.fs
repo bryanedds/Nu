b253fc88c86c9646ed4bacfbf870f95882b07af9
bryanedds <bryanedds@gmail.com>
2022-06-30 13:04:48 -0400

    Prevented LOH thrashing for 2D as well.
    Former-commit-id: 33594990e84a9734fcccd3e39232cab9b3d32447

--- Nu/Nu/QuadtreeTests.fs
+++ Nu/Nu/QuadtreeTests.fs
@@ -3,6 +3,7 @@
 
 namespace Nu.Tests
 open System
+open System.Collections.Generic
 open System.Numerics
 open Xunit
 open Prime
@@ -13,10 +14,10 @@ module QuadtreeTests =
         let tree = Quadtree.make<string> 2 2 (Box2 (0.0f, 0.0f, 1.0f, 1.0f))
         Quadtree.addElement true (Box2 (0.0f, 0.0f, 0.0f, 0.0f)) "Omni" tree
         Quadtree.addElement false (Box2 (0.0f, 0.0f, 0.0f, 0.0f)) "Zero" tree
-        Assert.Equal (2, Seq.length (Quadtree.getElementsAtPoint Vector2.Zero tree))
-        Assert.Equal (2, Seq.length (Quadtree.getElementsAtPoint Vector2.One tree))
-        Assert.Equal (2, Seq.length (Quadtree.getElementsAtPoint (Vector2 0.5f) tree))
-        Assert.Equal (2, Seq.length (Quadtree.getElementsAtPoint (Vector2 2.0f) tree))
+        Assert.Equal (2, Seq.length (Quadtree.getElementsAtPoint Vector2.Zero (HashSet ()) tree))
+        Assert.Equal (2, Seq.length (Quadtree.getElementsAtPoint Vector2.One (HashSet ()) tree))
+        Assert.Equal (2, Seq.length (Quadtree.getElementsAtPoint (Vector2 0.5f) (HashSet ()) tree))
+        Assert.Equal (2, Seq.length (Quadtree.getElementsAtPoint (Vector2 2.0f) (HashSet ()) tree))
 
     let [<Fact>] quadtreeThreeWorks () =
         let tree = Quadtree.make<string> 2 3 (Box2 (0.0f, 0.0f, 1.0f, 1.0f))
@@ -25,7 +26,7 @@ module QuadtreeTests =
         Quadtree.addElement false (Box2 (0.5f, 0.5f, 0.5f, 0.5f)) "Half" tree
         Quadtree.addElement false (Box2 (0.0f, 0.0f, 1.0f, 1.0f)) "ZeroOne" tree
         Quadtree.addElement false (Box2 (1.0f, 1.0f, 0.0f, 0.0f)) "OneZero" tree
-        Assert.Equal (4, Seq.length (Quadtree.getElementsAtPoint Vector2.Zero tree))
-        Assert.Equal (5, Seq.length (Quadtree.getElementsAtPoint Vector2.One tree))
-        Assert.Equal (5, Seq.length (Quadtree.getElementsAtPoint (Vector2 0.5f) tree))
-        Assert.Equal (3, Seq.length (Quadtree.getElementsAtPoint (Vector2 2.0f) tree))
+        Assert.Equal (4, Seq.length (Quadtree.getElementsAtPoint Vector2.Zero (HashSet ()) tree))
+        Assert.Equal (5, Seq.length (Quadtree.getElementsAtPoint Vector2.One (HashSet ()) tree))
+        Assert.Equal (5, Seq.length (Quadtree.getElementsAtPoint (Vector2 0.5f) (HashSet ()) tree))
+        Assert.Equal (3, Seq.length (Quadtree.getElementsAtPoint (Vector2 2.0f) (HashSet ()) tree))
