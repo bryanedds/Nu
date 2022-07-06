9770f1ad456bf5c9fdb02470840256661bf67417
bryanedds <bryanedds@gmail.com>
2022-07-06 17:36:22 -0400

    More work on materials.
    Former-commit-id: da9e52b9efe009fd0fafa97257c3304e90377d6e

--- Projects/Elmario/Assets/Default/PhysicallyBasedDeferred.glsl
+++ Projects/Elmario/Assets/Default/PhysicallyBasedDeferred.glsl
@@ -8,15 +8,21 @@ layout (location = 0) in vec3 position;
 layout (location = 1) in vec2 texCoords;
 layout (location = 2) in vec3 normal;
 layout (location = 3) in mat4 model;
+layout (location = 7) in vec4 albedo;
+layout (location = 8) in vec3 material;
 
 out vec3 positionOut;
 out vec2 texCoordsOut;
+out vec4 albedoOut;
+out vec3 materialOut;
 out vec3 normalOut;
 
 void main()
 {
     positionOut = vec3(model * vec4(position, 1.0));
     texCoordsOut = texCoords;
+    albedoOut = albedo;
+    materialOut = material;
     normalOut = transpose(inverse(mat3(model))) * normal;
     gl_Position = projection * view * vec4(positionOut, 1.0);
 }
@@ -24,8 +30,7 @@ void main()
 #shader fragment
 #version 410 core
 
-const float GAMMA_SQRT = 1.5;
-const float TONE = GAMMA_SQRT * GAMMA_SQRT;
+const float GAMMA = 2.2;
 
 uniform sampler2D albedoTexture;
 uniform sampler2D metalnessTexture;
@@ -35,6 +40,8 @@ uniform sampler2D normalTexture;
 
 in vec3 positionOut;
 in vec2 texCoordsOut;
+in vec4 albedoOut;
+in vec3 materialOut;
 in vec3 normalOut;
 
 layout (location = 0) out vec3 position;
@@ -50,7 +57,7 @@ vec3 getNormal()
     vec2 st1 = dFdx(texCoordsOut);
     vec2 st2 = dFdy(texCoordsOut);
     vec3 normal = normalize(normalOut);
-    vec3 tangent = normalize(q1*st2.t - q2*st1.t);
+    vec3 tangent = normalize(q1 * st2.t - q2 * st1.t);
     vec3 binormal = -normalize(cross(normal, tangent));
     mat3 tbn = mat3(tangent, binormal, normal);
     return normalize(tbn * tangentNormal);
@@ -61,13 +68,14 @@ void main()
     // forward position
     position = positionOut;
 
-    // compute albedo
-    albedo = pow(texture(albedoTexture, texCoordsOut).rgb, vec3(TONE));
+    // compute albedo without alpha
+    vec4 albedoSample = texture(albedoTexture, texCoordsOut);
+    albedo = pow(albedoSample.rgb * albedoOut.rgb, vec3(GAMMA));
 
     // compute material properties
-    float metalness = texture(metalnessTexture, texCoordsOut).r;
-    float roughness = texture(roughnessTexture, texCoordsOut).r;
-    float ambientOcclusion = texture(ambientOcclusionTexture, texCoordsOut).r;
+    float metalness = texture(metalnessTexture, texCoordsOut).r * materialOut.r;
+    float roughness = texture(roughnessTexture, texCoordsOut).r * materialOut.g;
+    float ambientOcclusion = texture(ambientOcclusionTexture, texCoordsOut).r * materialOut.b;
     material = vec3(metalness, roughness, ambientOcclusion);
 
     // forward normal
