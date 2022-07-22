697bb791768b3a142f7c6c0ebe009f8ca7927fb6
bryanedds <bryanedds@gmail.com>
2022-07-22 16:36:05 -0400

    Intermediate commit.
    Former-commit-id: 581a0de0be4503a5c40a22e646895f46ae0eacda

---
+++ Projects/Scenery/Assets/Field/Forest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Forest" tilewidth="48" tileheight="48" tilecount="2310" columns="66">
+ <image source="Forest.png" width="3168" height="1680"/>
+</tileset>
