4ff4a28a952e4116cbd0ba333b264cef4aff86b0
bryanedds <bryanedds@gmail.com>
2022-07-24 04:06:13 -0400

    Reduced resolution of field tiles.

--- Projects/Scenery/Assets/Field/Height.tsx
+++ Projects/Scenery/Assets/Field/Height.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Height" tilewidth="48" tileheight="48" tilecount="16" columns="4">
+<tileset version="1.5" tiledversion="1.7.2" name="Height" tilewidth="16" tileheight="16" tilecount="16" columns="4">
  <properties>
   <property name="Image" value="[Field Height]"/>
  </properties>
- <image source="Height.png" width="192" height="192"/>
+ <image source="Height.png" width="64" height="64"/>
 </tileset>
