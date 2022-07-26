68ba3f0131dc1fa5237b22407908579b8bd5656b
bryanedds <bryanedds@gmail.com>
2022-07-25 23:17:53 -0400

    Second pass of billboard.
    Former-commit-id: d432f5a6c277840eae3e2a235e6ce521a39a0a11

--- Nu/Nu/World/WorldDispatchers.fs
+++ Nu/Nu/World/WorldDispatchers.fs
@@ -1406,6 +1406,25 @@ module BillboardFacetModule =
                     world
             world
 
+        override this.GetQuickSize (entity, world) =
+            v3 1.0f 2.0f 1.0f
+
+        override this.RayCast (ray, entity, world) =
+            // TODO: 3D: intersect against oriented quad rather than box.
+            match this.TryGetHighlightBounds (entity, world) with
+            | Some bounds ->
+                let intersectionOpt = ray.Intersects bounds
+                if intersectionOpt.HasValue then [|intersectionOpt.Value|]
+                else [||]
+            | None -> [||]
+
+        override this.TryGetHighlightBounds (entity, world) =
+            let bounds = entity.GetBounds world
+            Some
+                (box3
+                    (bounds.Position + bounds.Size * v3 0.0f 0.5f 0.0f)
+                    (bounds.Size * v3 1.0f 0.5f 1.0f))
+
 [<AutoOpen>]
 module StaticModelFacetModule =
 
