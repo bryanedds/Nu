9ec252ef88126b18f43878b366631c33415fb9cd
bryanedds <bryanedds@gmail.com>
2022-08-01 14:00:17 -0400

    Attempt to add jpg image More AssetGraph normalization.

--- Nu/Nu/Render/Render2d.fs
+++ Nu/Nu/Render/Render2d.fs
@@ -107,7 +107,7 @@ type [<ReferenceEquality; NoComparison>] GlRenderer2d =
     static member private tryLoadRenderAsset (asset : obj Asset) renderer =
         GlRenderer2d.invalidateCaches renderer
         match Path.GetExtension asset.FilePath with
-        | ".bmp" | ".png" | ".tif" ->
+        | ".bmp" | ".png" | ".jpg" | ".jpeg" | ".tif" | ".tiff" ->
             match OpenGL.Texture.TryCreateTextureUnfiltered asset.FilePath with
             | Right (textureMetadata, texture) ->
                 Some (TextureAsset (asset.FilePath, textureMetadata, texture))
