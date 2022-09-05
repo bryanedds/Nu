2833874c97d42db2f9f15f404a5b55c33f77fb2d
bryanedds <bryanedds@gmail.com>
2022-09-05 17:05:02 -0400

    Doc comment fixes.

--- Nu/Nu/OpenGL/OpenGL.PhysicallyBased.fs
+++ Nu/Nu/OpenGL/OpenGL.PhysicallyBased.fs
@@ -345,7 +345,7 @@ module PhysicallyBased =
                 +0.5f; +0.5f; -0.5f;        1.0f; 1.0f;          0.0f; +1.0f;  0.0f; // top-right     
                 +0.5f; +0.5f; +0.5f;        1.0f; 0.0f;          0.0f; +1.0f;  0.0f; // bottom-right
                 -0.5f; +0.5f; -0.5f;        0.0f; 1.0f;          0.0f; +1.0f;  0.0f; // top-left
-                -0.5f; +0.5f; +0.5f;        0.0f; 0.0f;           0.0f; +1.0f;  0.0f  // bottom-left     
+                -0.5f; +0.5f; +0.5f;        0.0f; 0.0f;          0.0f; +1.0f;  0.0f  // bottom-left     
             |]
 
         // make index data trivially
