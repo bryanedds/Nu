b42fe9a23e4ef6ab4ea274b58b43305933def0e8
bryanedds <bryanedds@gmail.com>
2022-10-10 16:20:09 -0400

    Added create/destroyUserDefinedStaticModel functions as side-effects for the cases where you need to call them from in a binding.
    Former-commit-id: e527cd921a723e817b2a56c18862248c7656b8e0

--- Nu/Nu/World/WorldRender.fs
+++ Nu/Nu/World/WorldRender.fs
@@ -17,42 +17,42 @@ module WorldRender =
         static member internal withRendererProcess updater world =
             updater (World.getRendererProcess world)
 
-        /// Enqueue a rendering message.
+        /// Enqueue a 2d rendering message.
         static member enqueueRenderMessage2d (message : RenderMessage2d) world =
             (World.getRendererProcess world).EnqueueMessage2d message
             world
 
-        /// Enqueue multiple rendering messages.
+        /// Enqueue multiple 2d rendering messages.
         static member enqueueRenderMessages2d (messages : RenderMessage2d seq) world =
             let rendererProcess = World.getRendererProcess world
             for message in messages do rendererProcess.EnqueueMessage2d message
             world
             
-        /// Enqueue a layered message for rendering.
+        /// Enqueue a layered message for 2d rendering.
         static member enqueueRenderLayeredMessage2d message world =
             (World.getRendererProcess world).EnqueueMessage2d (RenderLayeredMessage2d message)
             world
 
-        /// Enqueue multiple layered rendering messages to the world, bypassing enqueueRenderMessage for speed.
+        /// Enqueue multiple layered messages for 2d rendering, bypassing enqueueRenderMessage for speed.
         static member enqueueRenderLayeredMessages2d (messages : RenderLayeredMessage2d seq) world =
             let rendererProcess = World.getRendererProcess world
             for message in messages do rendererProcess.EnqueueMessage2d (RenderLayeredMessage2d message)
             world
             
-        /// Load an audio asset package. Should be used to avoid loading assets at inconvenient times (such as in the
+        /// Load an 2d render asset package. Should be used to avoid loading assets at inconvenient times (such as in the
         /// middle of game play!)
         [<FunctionBinding>]
         static member hintRenderPackageUse2d packageName world =
             let hintRenderPackageUseMessage = LoadRenderPackageMessage2d packageName
             World.enqueueRenderMessage2d hintRenderPackageUseMessage world
             
-        /// Unload an audio package should be unloaded since its assets will not be used again soon.
+        /// Unload a 2d render package should be unloaded since its assets will not be used again soon.
         [<FunctionBinding>]
         static member unloadRenderPackage2d packageName world =
             let unloadRenderPackageMessage = UnloadRenderPackageMessage2d packageName
             World.enqueueRenderMessage2d unloadRenderPackageMessage world
 
-        /// Send a message to the renderer to reload its rendering assets.
+        /// Send a message to the 2d renderer to reload its rendering assets.
         [<FunctionBinding>]
         static member reloadRenderAssets2d world =
             let reloadRenderAssetsMessage = ReloadRenderAssetsMessage2d
@@ -62,14 +62,26 @@ module WorldRender =
         static member enqueueRenderMessage3d (message : RenderMessage3d) world =
             (World.getRendererProcess world).EnqueueMessage3d message
 
-        /// Enqueue multiple rendering messages to the world.
+        /// Enqueue multiple 3d rendering messages to the world.
         static member enqueueRenderMessages3d (messages : RenderMessage3d seq) world =
             let rendererProcess = World.getRendererProcess world
             for message in messages do rendererProcess.EnqueueMessage3d message
 
-        /// Send a message to the renderer to reload its rendering assets.
+        /// Send a message to the 3d renderer to reload its rendering assets.
         [<FunctionBinding>]
         static member reloadRenderAssets3d world =
             let reloadRenderAssetsMessage = ReloadRenderAssetsMessage3d
             World.enqueueRenderMessage3d reloadRenderAssetsMessage world
             world
+
+        /// Send a message to the render to create the given user-defined static model.
+        /// NOTE: this is available as a side-effect to allow use from inside a binding.
+        static member createUserDefinedStaticModel staticModelSurfaces bounds assetTag world =
+            let message = CreateUserDefinedStaticModelMessage (staticModelSurfaces, bounds, assetTag)
+            World.enqueueRenderMessage3d message world
+
+        /// Send a message to the render to destroy the given user-defined static model.
+        /// NOTE: this is available as a side-effect to allow use from inside a binding.
+        static member destroyUserDefinedStaticModel assetTag world =
+            let message = DestroyUserDefinedStaticModelMessage assetTag
+            World.enqueueRenderMessage3d message world
