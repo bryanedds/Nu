2efca52f9d03e8546853dedd78e3a262748e0469
bryanedds <bryanedds@gmail.com>
2022-11-12 19:47:33 -0500

    Converted UserProject.
    Former-commit-id: fddace53cfc4c4e5f9f8867791add149f4f9f279

--- Projects/UserProject/MyGame.fs
+++ Projects/UserProject/MyGame.fs
@@ -2,6 +2,7 @@
 open Prime
 open Nu
 open Nu.Declarative
+open Nu.ForgeOperators
 
 [<AutoOpen>]
 module MyGame =
@@ -19,10 +20,12 @@ module MyGame =
         | ShowTitle
         | ShowCredits
         | ShowGameplay
+        | GameplayChanged of Gameplay
 
     // this is our Elm-style command type. Commands are used instead of messages when explicitly
     // updating the world is involved.
     type Command =
+        | ModelChanged
         | Exit
 
     // this extends the Game API to expose the above model as well as the model bimapped to Gameplay,
@@ -30,50 +33,49 @@ module MyGame =
         member this.GetModel world = this.GetModelGeneric<Model> world
         member this.SetModel value world = this.SetModelGeneric<Model> value world
         member this.Model = this.ModelGeneric<Model> ()
-        member this.Gameplay = this.Model.Bimap (function Gameplay gameplay -> Some gameplay | _ -> None) (constant Gameplay) 
 
     // this is the game dispatcher that is customized for our game. In here, we create screens as
     // content and bind them up with events and properties.
     type MyGameDispatcher () =
-        inherit GameDispatcher<Model, Message, Command> (Splash)
-
-        // here we channel from gui events to signals
-        override this.Channel (_, _) =
-            [Simulants.Splash.Screen.DeselectingEvent => msg ShowTitle
-             Simulants.Title.Gui.Credits.ClickEvent => msg ShowCredits
-             Simulants.Title.Gui.Play.ClickEvent => msg ShowGameplay
-             Simulants.Title.Gui.Exit.ClickEvent => cmd Exit
-             Simulants.Credits.Gui.Back.ClickEvent => msg ShowTitle]
-
-        // here we link the game and gameplay models (two-way bind), then we bind the desired
-        // screen based on the state of the game (or None if splashing),
-        override this.Initializers (model, game) =
-            [game.Gameplay <=> Simulants.Gameplay.Screen.Gameplay
-             game.DesiredScreen <== model --> fun model ->
-                match model with
-                | Splash -> Desire Simulants.Splash.Screen
-                | Title -> Desire Simulants.Title.Screen
-                | Credits -> Desire Simulants.Credits.Screen
-                | Gameplay gameplay ->
-                    match gameplay with
-                    | Playing -> Desire Simulants.Gameplay.Screen
-                    | Quitting -> Desire Simulants.Title.Screen]
+        inherit GameForger<Model, Message, Command> (Splash)
 
         // here we handle the above messages
-        override this.Message (_, message, _, _) =
+        override this.Message (model, message, _, _) =
             match message with
             | ShowTitle -> just Title
             | ShowCredits -> just Credits
             | ShowGameplay -> just (Gameplay Playing)
+            | GameplayChanged gameplay -> match gameplay with Playing | Quitting -> just (Gameplay gameplay) | Quit -> just model
 
         // here we handle the above commands
-        override this.Command (_, command, _, world) =
+        override this.Command (model, command, _, world) =
             match command with
+            | ModelChanged ->
+                match model with
+                | Gameplay gameplay -> just (Simulants.Gameplay.Screen.SetGameplay gameplay world)
+                | _ -> just world
             | Exit -> just (World.exit world)
 
         // here we describe the content of the game, including all of its screens.
-        override this.Content (_, _) =
-            [Content.screen Simulants.Splash.Screen.Name (WorldTypes.Splash (Constants.Dissolve.Default, Constants.Splash.Default, None, Simulants.Title.Screen)) [] []
-             Content.screenFromGroupFile Simulants.Title.Screen.Name (Dissolve (Constants.Dissolve.Default, None)) "Assets/Gui/Title.nugroup"
-             Content.screenFromGroupFile Simulants.Credits.Screen.Name (Dissolve (Constants.Dissolve.Default, None)) "Assets/Gui/Credits.nugroup"
-             Content.screen<MyGameplayDispatcher> Simulants.Gameplay.Screen.Name (Dissolve (Constants.Dissolve.Default, None)) [] []]
+        override this.Forge (model, game) =
+            Forge.game
+                [game.DesiredScreen ==
+                    match model with
+                    | Splash -> Desire Simulants.Splash.Screen
+                    | Title -> Desire Simulants.Title.Screen
+                    | Credits -> Desire Simulants.Credits.Screen
+                    | Gameplay gameplay ->
+                        match gameplay with
+                        | Playing -> Desire Simulants.Gameplay.Screen
+                        | Quitting | Quit -> Desire Simulants.Title.Screen
+                 game.Model.ChangeEvent ==> cmd ModelChanged
+                 Simulants.Splash.Screen.DeselectingEvent ==> msg ShowTitle
+                 Simulants.Title.Gui.Credits.ClickEvent ==> msg ShowCredits
+                 Simulants.Title.Gui.Play.ClickEvent ==> msg ShowGameplay
+                 Simulants.Title.Gui.Exit.ClickEvent ==> cmd Exit
+                 Simulants.Credits.Gui.Back.ClickEvent ==> msg ShowTitle
+                 Simulants.Gameplay.Screen.Gameplay.ChangeEvent ==|> fun event -> msg (GameplayChanged (event.Data.Value :?> Gameplay))]
+                [Forge.screen Simulants.Splash.Screen.Name (WorldTypes.Splash (Constants.Dissolve.Default, Constants.Splash.Default, None, Simulants.Title.Screen)) [] []
+                 Forge.screenWithGroupFromFile Simulants.Title.Screen.Name (Dissolve (Constants.Dissolve.Default, None)) "Assets/Gui/Title.nugroup" [] []
+                 Forge.screenWithGroupFromFile Simulants.Credits.Screen.Name (Dissolve (Constants.Dissolve.Default, None)) "Assets/Gui/Credits.nugroup" [] []
+                 Forge.screen<MyGameplayDispatcher> Simulants.Gameplay.Screen.Name (Dissolve (Constants.Dissolve.Default, None)) [] []]
