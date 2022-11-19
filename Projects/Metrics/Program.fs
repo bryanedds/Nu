c4ba64d974e282edc9dca65a8aca5be9b33e5626
bryanedds <bryanedds@gmail.com>
2022-11-19 09:31:23 -0500

    Did TODO.

--- Projects/Metrics/Program.fs
+++ Projects/Metrics/Program.fs
@@ -194,9 +194,9 @@ type ElmishGameDispatcher () =
 
     override this.Content (intss, _) =
         [Content.screen Simulants.Default.Screen.Name Vanilla []
-            [|for (i, ints) in intss.Intss.Pairs do
+            [|for (i, ints) in intss.Intss.Pairs' do
                 Content.group (string i) []
-                    [|for (j, int) in ints.Ints.Pairs do
+                    [|for (j, int) in ints.Ints.Pairs' do
                         Content.entity<ElmishEntityDispatcher> (string j)
                             [Entity.Position == v3 (single i * 5.0f - 250.0f) (single j * 2.5f - 125.0f) -250.0f
                              Entity.Scale := v3Dup (single (int % 10)) * 0.5f
