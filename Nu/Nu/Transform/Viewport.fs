13951e75b65db690d7ffae60eee669ff0cd12cc2
bryanedds <bryanedds@gmail.com>
2022-11-19 23:16:22 -0500

    Broke Presence out into own file.

--- Nu/Nu/Transform/Viewport.fs
+++ Nu/Nu/Transform/Viewport.fs
@@ -3,45 +3,6 @@ open System.Numerics
 open Prime
 open Nu
 
-/// Describes the form of an element's presence.
-[<Syntax
-    ("Enclosed Exposed Imposter Prominent Omnipresent", "", "", "", "",
-     Constants.PrettyPrinter.DefaultThresholdMin,
-     Constants.PrettyPrinter.DefaultThresholdMax)>]
-type [<StructuralEquality; NoComparison; Struct>] Presence =
-    /// Inside an enclosed structure so you have to be close to see them.
-    | Enclosed
-    /// Outside an enclosed structure so visible from a distance.
-    | Exposed
-    /// Visible for miles but invisible once as close as Exposed or Enclosed.
-    | Imposter
-    /// Both Exposed and Imposter.
-    | Prominent
-    /// Always visible.
-    | Omnipresent 
-    member this.EnclosedType with get () = match this with Enclosed -> true | _ -> false
-    member this.ExposedType with get () = match this with Exposed -> true | _ -> false
-    member this.ImposterType with get () = match this with Imposter -> true | _ -> false
-    member this.ProminentType with get () = match this with Prominent -> true | _ -> false
-    member this.OmnipresentType with get () = match this with Omnipresent -> true | _ -> false
-
-[<AutoOpen>]
-module PresenceOperators =
-    
-    /// Test two presence values for equality.
-    let presenceEq left right =
-        match (left, right) with
-        | (Enclosed, Enclosed)
-        | (Exposed, Exposed)
-        | (Imposter, Imposter)
-        | (Prominent, Prominent)
-        | (Omnipresent, Omnipresent) -> true
-        | (_, _) -> false
-
-    /// Test two presence values for inequality.
-    let presenceNeq left right =
-        not (presenceEq left right)
-
 [<AutoOpen>]
 module Viewport =
 
