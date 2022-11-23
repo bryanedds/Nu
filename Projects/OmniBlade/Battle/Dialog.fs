253a389628d252edc48f9f55f47327c303b473ed
bryanedds <bryanedds@gmail.com>
2022-11-23 00:03:57 -0500

    More re-org.

--- Projects/OmniBlade/Battle/Dialog.fs
+++ Projects/OmniBlade/Battle/Dialog.fs
@@ -5,7 +5,6 @@ namespace OmniBlade
 open System
 open Prime
 open Nu
-open Nu.Declarative
 open OmniBlade
 
 type [<NoComparison>] DialogForm =
@@ -73,42 +72,3 @@ type [<NoComparison>] Dialog =
                 wordWrap acc right
             else text :: acc
         wordWrap [] text |> List.rev |> String.join "\n"
-
-[<AutoOpen>]
-module DialogContent =
-
-    [<RequireQualifiedAccess>]
-    module Content =
-    
-        let dialog name elevation promptLeft promptRight (detokenize : string -> string) (dialogOpt : Dialog option) =
-            [match dialogOpt with
-             | Some dialog ->
-                Content.composite<TextDispatcher> name
-                    [Entity.Perimeter :=
-                        match dialog.DialogForm with
-                        | DialogThin -> box3 (v3 -432.0f 150.0f 0.0f) (v3 864.0f 90.0f 0.0f)
-                        | DialogThick -> box3 (v3 -432.0f 78.0f 0.0f) (v3 864.0f 174.0f 0.0f)
-                        | DialogNarration -> box3 (v3 -432.0f 78.0f 0.0f) (v3 864.0f 174.0f 0.0f)
-                     Entity.Elevation := elevation
-                     Entity.BackgroundImageOpt :=
-                        match dialog.DialogForm with
-                        | DialogThin -> Some Assets.Gui.DialogThinImage
-                        | DialogThick -> Some Assets.Gui.DialogThickImage
-                        | DialogNarration -> Some Assets.Default.ImageEmpty
-                     Entity.Text := Dialog.getText detokenize dialog
-                     Entity.Justification :=
-                        match dialog.DialogForm with
-                        | DialogThin | DialogThick -> Unjustified true
-                        | DialogNarration -> Justified (JustifyCenter, JustifyMiddle)
-                     Entity.Margins == v3 30.0f 30.0f 0.0f]
-                    [Content.button "Left"
-                        [Entity.PositionLocal == v3 186.0f 18.0f 0.0f; Entity.ElevationLocal == 2.0f; Entity.Size == v3 192.0f 48.0f 0.0f
-                         Entity.VisibleLocal := Option.isSome dialog.DialogPromptOpt && Dialog.isExhausted detokenize dialog
-                         Entity.Text := match dialog.DialogPromptOpt with Some ((promptText, _), _) -> promptText | None -> ""
-                         Entity.ClickEvent => msg promptLeft]
-                     Content.button "Right"
-                        [Entity.PositionLocal == v3 486.0f 18.0f 0.0f; Entity.ElevationLocal == 2.0f; Entity.Size == v3 192.0f 48.0f 0.0f
-                         Entity.VisibleLocal := Option.isSome dialog.DialogPromptOpt && Dialog.isExhausted detokenize dialog
-                         Entity.Text := match dialog.DialogPromptOpt with Some (_, (promptText, _)) -> promptText | None -> ""
-                         Entity.ClickEvent => msg promptRight]]
-             | None -> ()]
