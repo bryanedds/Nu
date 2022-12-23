15cc3b1a8d15a199834560ce5f99697fe6e718a2
bryanedds <bryanedds@gmail.com>
2022-12-22 23:41:14 -0500

    Avoiding creating projects that already exist.
    Former-commit-id: 5b4873ca33bca3bf3cdb46cd3c9bb4d52c3fc292

--- Nu/Nu/Program.fs
+++ Nu/Nu/Program.fs
@@ -28,7 +28,7 @@ module Program =
                 let name = Console.ReadLine ()
                 let name = name.Replace(" ", "").Replace("\t", "").Replace(".", "")
                 if Array.notExists (fun char -> name.Contains (string char)) (Path.GetInvalidPathChars ()) then
-                    
+
                     // compute directories
                     let templateIdentifier = templateDir.Replace("/", "\\") // this is what dotnet knows the template as for uninstall...
                     let templateFileName = "Nu.Template.fsproj"
@@ -36,60 +36,70 @@ module Program =
                     let newProjDir = projectsDir + "/" + name |> Path.Simplify
                     let newFileName = name + ".fsproj"
                     let newProj = newProjDir + "/" + newFileName |> Path.Simplify
-                    Console.WriteLine ("Creating project '" + name + "' in '" + projectsDir + "'...")
 
-                    // install nu template
-                    Directory.SetCurrentDirectory templateDir
-                    Process.Start("dotnet", "new -u \"" + templateIdentifier + "\"").WaitForExit()
-                    Process.Start("dotnet", "new -i ./").WaitForExit()
+                    // ensure project doesn't already exist
+                    if not (File.Exists newProj) then
 
-                    // instantiate nu template
-                    Directory.SetCurrentDirectory projectsDir
-                    Directory.CreateDirectory name |> ignore<DirectoryInfo>
-                    Directory.SetCurrentDirectory newProjDir
-                    Process.Start("dotnet", "new nu-game --force").WaitForExit()
+                        // echo creation begun
+                        Console.Write ("Creating project '" + name + "' in '" + projectsDir + "'...")
 
-                    // rename project file
-                    File.Copy (templateFileName, newFileName, true)
-                    File.Delete templateFileName
+                        // install nu template
+                        Directory.SetCurrentDirectory templateDir
+                        Process.Start("dotnet", "new -u \"" + templateIdentifier + "\"").WaitForExit()
+                        Process.Start("dotnet", "new -i ./").WaitForExit()
 
-                    // substitute $safeprojectname$ in project file
-                    let newProjStr = File.ReadAllText newProj
-                    let newProjStr = newProjStr.Replace("$safeprojectname$", name)
-                    File.WriteAllText (newProj, newProjStr)
+                        // instantiate nu template
+                        Directory.SetCurrentDirectory projectsDir
+                        Directory.CreateDirectory name |> ignore<DirectoryInfo>
+                        Directory.SetCurrentDirectory newProjDir
+                        Process.Start("dotnet", "new nu-game --force").WaitForExit()
 
-                    // add project to sln file
-                    Directory.SetCurrentDirectory slnDir
-                    let projectGuid = Gen.id
-                    let projectGuidStr = projectGuid.ToString().ToUpper()
-                    let slnLines = "Nu.sln" |> File.ReadAllLines |> Array.toList
-                    let insertionIndex = List.findIndex ((=) "Global") slnLines
-                    let slnLines =
-                        List.take insertionIndex slnLines @
-                        ["Project(\"{F2A71F9B-5D33-465A-A702-920D77279786}\") = \"" + name + "\", \"Projects\\" + name + "\\" + name + ".fsproj\", \"{" + string projectGuid + "}\""
-                         "EndProject"] @
-                        List.skip insertionIndex slnLines
-                    let insertionIndex = List.findIndex ((=) "\tGlobalSection(SolutionProperties) = preSolution") slnLines - 1
-                    let slnLines =
-                        List.take insertionIndex slnLines @
-                        ["\t\t{" + projectGuidStr + "}.Debug|x64.ActiveCfg = Debug|x64"
-                         "\t\t{" + projectGuidStr + "}.Debug|x64.Build.0 = Debug|x64"
-                         "\t\t{" + projectGuidStr + "}.Release|x64.ActiveCfg = Release|x64"
-                         "\t\t{" + projectGuidStr + "}.Release|x64.Build.0 = Release|x64"] @
-                        List.skip insertionIndex slnLines
-                    let insertionIndex = List.findIndex ((=) "\tGlobalSection(ExtensibilityGlobals) = postSolution") slnLines - 1
-                    let slnLines =
-                        List.take insertionIndex slnLines @
-                        ["\t\t{" + projectGuidStr + "} = {E3C4D6E1-0572-4D80-84A9-8001C21372D3}"] @
-                        List.skip insertionIndex slnLines
-                    File.WriteAllLines ("Nu.sln", List.toArray slnLines)
-                    
-                    // success
-                    Constants.Engine.ExitCodeSuccess
+                        // rename project file
+                        File.Copy (templateFileName, newFileName, true)
+                        File.Delete templateFileName
+
+                        // substitute $safeprojectname$ in project file
+                        let newProjStr = File.ReadAllText newProj
+                        let newProjStr = newProjStr.Replace("$safeprojectname$", name)
+                        File.WriteAllText (newProj, newProjStr)
+
+                        // add project to sln file
+                        Directory.SetCurrentDirectory slnDir
+                        let projectGuid = Gen.id
+                        let projectGuidStr = projectGuid.ToString().ToUpper()
+                        let slnLines = "Nu.sln" |> File.ReadAllLines |> Array.toList
+                        let insertionIndex = List.findIndex ((=) "Global") slnLines
+                        let slnLines =
+                            List.take insertionIndex slnLines @
+                            ["Project(\"{F2A71F9B-5D33-465A-A702-920D77279786}\") = \"" + name + "\", \"Projects\\" + name + "\\" + name + ".fsproj\", \"{" + string projectGuid + "}\""
+                             "EndProject"] @
+                            List.skip insertionIndex slnLines
+                        let insertionIndex = List.findIndex ((=) "\tGlobalSection(SolutionProperties) = preSolution") slnLines - 1
+                        let slnLines =
+                            List.take insertionIndex slnLines @
+                            ["\t\t{" + projectGuidStr + "}.Debug|x64.ActiveCfg = Debug|x64"
+                             "\t\t{" + projectGuidStr + "}.Debug|x64.Build.0 = Debug|x64"
+                             "\t\t{" + projectGuidStr + "}.Release|x64.ActiveCfg = Release|x64"
+                             "\t\t{" + projectGuidStr + "}.Release|x64.Build.0 = Release|x64"] @
+                            List.skip insertionIndex slnLines
+                        let insertionIndex = List.findIndex ((=) "\tGlobalSection(ExtensibilityGlobals) = postSolution") slnLines - 1
+                        let slnLines =
+                            List.take insertionIndex slnLines @
+                            ["\t\t{" + projectGuidStr + "} = {E3C4D6E1-0572-4D80-84A9-8001C21372D3}"] @
+                            List.skip insertionIndex slnLines
+                        File.WriteAllLines ("Nu.sln", List.toArray slnLines)
+
+                        // success
+                        Constants.Engine.ExitCodeSuccess
+
+                    // project already exists
+                    else
+                        Console.Write ("Project name '" + name + "' already exists.")
+                        Constants.Engine.ExitCodeFailure
 
                 // invalid name; failure
                 else
-                    Console.WriteLine ("Project name '" + name + "' contains invalid path characters.")
+                    Console.Write ("Project name '" + name + "' contains invalid characters.")
                     Constants.Engine.ExitCodeFailure
 
             // rejected
