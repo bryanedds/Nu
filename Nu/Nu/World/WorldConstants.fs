447ed47c432c8d3b3d6c39a97fbe34238d95519e
bryanedds <bryanedds@gmail.com>
2023-01-09 08:04:31 -0500

    Implemented app.config overriding.
    Former-commit-id: 5f820b3da8a96e1c196b9100bf9977b61159545a

--- Nu/Nu/World/WorldConstants.fs
+++ Nu/Nu/World/WorldConstants.fs
@@ -2,6 +2,7 @@
 // Copyright (C) Bryan Edds, 2013-2020.
 
 namespace Nu.Constants
+open System.Configuration
 open Prime
 open Nu
 
@@ -22,3 +23,23 @@ module Slide =
         { DissolveDescriptor = Dissolve.Default
           IdlingTime = 60L
           SlideImageOpt = Some Assets.Default.Image5 }
+
+[<RequireQualifiedAccess>]
+module Override =
+
+    /// Override certain constants with values from an App.config file of the given executable assembly.
+    let fromAppConfig (exeFilePath : string) =
+        try let configuration = ConfigurationManager.OpenExeConfiguration exeFilePath
+            let settings = configuration.AppSettings.Settings
+            for key in settings.AllKeys do
+                let value = settings.[key].Value
+                match key with
+                | nameof Engine.EntityCentered2dDefault -> Engine.EntityCentered2dDefault <- scvalue<bool> value
+                | nameof Engine.EntityCenteredGuiDefault -> Engine.EntityCenteredGuiDefault <- scvalue<bool> value
+                | nameof Engine.EntityCentered3dDefault -> Engine.EntityCentered3dDefault <- scvalue<bool> value
+                | _ -> ()
+        with
+        | :? ConfigurationErrorsException ->
+            Log.info ("Configuration value override failed due to: Could not find App.config file for " + exeFilePath + ".")
+        | exn ->
+            Log.info ("Configuration value override failed due to: " + scstring exn)
