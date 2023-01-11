51f092c60dd1d9bfd42fdb62cd1165d816db83aa
bryanedds <bryanedds@gmail.com>
2023-01-11 10:17:35 -0500

    Less enemy action time variance at start of game.
    Former-commit-id: 245bf7e381ebc975016fa3316742d704b6b2b005

--- Projects/OmniBlade/Character/Character.fs
+++ Projects/OmniBlade/Character/Character.fs
@@ -481,9 +481,9 @@ module Character =
                 let characterType = characterData.CharacterType
                 let characterState = CharacterState.make characterData hitPoints techPoints expPoints characterData.WeaponOpt characterData.ArmorOpt characterData.Accessories
                 let actionTime =
-                    if waitSpeed
-                    then 1000.0f - 125.0f - Gen.randomf1 8.0f * 75.0f
-                    else 1000.0f - (if allyCount = 1 then 525.0f else 450.0f) - Gen.randomf1 8.0f * 75.0f
+                    if waitSpeed then       1000.0f - 125.0f - Gen.randomf1 8.0f * 75.0f
+                    elif allyCount = 1 then 1000.0f - 450.0f - Gen.randomf1 5.0f * 75.0f
+                    else                    1000.0f - 450.0f - Gen.randomf1 8.0f * 75.0f
                 let enemy = make bounds (EnemyIndex index) characterType characterState characterData.AnimationSheet celSize Rightward chargeTechOpt actionTime
                 Some enemy
             | None -> None
