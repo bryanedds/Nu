54e6028456b5ead0b99f01662798caab31c3719a
bryanedds <bryanedds@gmail.com>
2023-02-02 03:45:46 -0500

    Switch to VS 2022.
    Former-commit-id: c084bcbd3caaf0b9eeb62d05f168577914198d3d

--- Build.Windows.Release.bat
+++ Build.Windows.Release.bat
@@ -1,7 +1,7 @@
 @echo Attempting to restore dependencies...
-"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/MSBuild.exe" Nu.sln /t:restore
+"C:/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe" Nu.sln /t:restore
 
 @echo Attempting to build...
-"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/MSBuild.exe" Nu.sln /t:build /property:Configuration=Release
+"C:/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe" Nu.sln /t:build /property:Configuration=Release
 
 @pause
