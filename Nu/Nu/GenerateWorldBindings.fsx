1a395c2f18bbfafdb1ef9808a226e389d7859ddd
bryanedds <bryanedds@gmail.com>
2023-02-10 09:45:37 -0500

    Script clean-up.
    Former-commit-id: 253153004c260d3422da56a2bf70c325d575e1ff

--- Nu/Nu/GenerateWorldBindings.fsx
+++ Nu/Nu/GenerateWorldBindings.fsx
@@ -4,7 +4,7 @@
 #I __SOURCE_DIRECTORY__
 #r "nuget: Magick.NET-Q8-x64, 7.5.0.1"
 #r "nuget: Csv, 1.0.58"
-#r "nuget: FParsec, 1.0.3" // MUST be referenced BEFORE FParsec.dll!
+#r "nuget: FParsec, 1.0.3"
 #r "nuget: Prime, 9.0.0"
 #r "nuget: Prime.Scripting, 9.0.0"
 #r "nuget: Aether.Physics2D, 1.5.0"
