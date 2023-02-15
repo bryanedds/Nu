8b7b2eb394fa4476daa661d0db95adda91d6bda6
bryanedds <bryanedds@gmail.com>
2023-02-15 06:59:09 -0500

    Got rid of platform-specific time-stamping due to NET7 upgrade.
    Former-commit-id: 6d029bc382bdf080bd5fd3660851c0ee7ee6edd6

--- Nu/Nu/Core/Core.fs
+++ Nu/Nu/Core/Core.fs
@@ -4,44 +4,17 @@
 namespace Nu
 open System
 open System.Diagnostics
-#if !PLATFORM_AGNOSTIC_TIMESTAMPING
-open System.Runtime.InteropServices
-#endif
 open Prime
-
-[<RequireQualifiedAccess>]
-module internal CoreInternal =
-
-// NOTE: the drawbacks of using PLATFORM_AGNOSTIC_TIMESTAMPING are two-fold -
-//  1) Stopwatch.GetTimestamp is too low resolution on Windows .NET.
-//  2) Even on Mono, Stopwatch.GetTimestamp, which is ultimately implemented in terms of
-//     mono_100ns_ticks as defined here - https://github.com/mono/mono/blob/master/mono/utils/mono-time.c
-//     does not necessarily lead to a high-resolution, linear-time path on all platforms.
-#if PLATFORM_AGNOSTIC_TIMESTAMPING
-    /// Get a time stamp at the highest-available resolution on linux.
-    let internal getTimeStampInternal () =
-        Stopwatch.GetTimestamp ()
-#else
-    /// Query the windows performance counter.
-    [<DllImport "Kernel32.dll">]
-    extern bool private QueryPerformanceCounter (int64&)
-
-    /// Get a time stamp at the highest-available resolution on windows.
-    let internal getTimeStampInternal () =
-        let mutable t = 0L
-        QueryPerformanceCounter &t |> ignore
-        t
-#endif
 
 [<RequireQualifiedAccess>]
 module Core =
 
     let mutable private lastStamp =
-        CoreInternal.getTimeStampInternal ()
+        Stopwatch.GetTimestamp ()
 
     /// Get a time stamp at the highest-available resolution.
     let getTimeStamp () =
-        CoreInternal.getTimeStampInternal ()
+        Stopwatch.GetTimestamp ()
 
     /// Get a unique time stamp, spinning until the time stamp advances if need be.
     let getUniqueTimeStamp () =
