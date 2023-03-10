3979c8f59bce9a87bcd9d407289e2824ef35b2da
bryanedds <bryanedds@gmail.com>
2023-03-10 09:43:56 -0500

    More work on bhvr.

--- Nu/Nu/Behavior/Behavior.fs
+++ Nu/Nu/Behavior/Behavior.fs
@@ -22,69 +22,85 @@ module Behavior =
     let lift2<'a, 'b, 'c>
         (op : 'a -> 'b -> 'c)
         (bhvr : 'a Behavior)
-        (behv2 : 'b Behavior) :
+        (bhvr2 : 'b Behavior) :
         'c Behavior =
-        fun time ->
+        let bhvr = fun time ->
             let a = bhvr time
-            let b = behv2 time
+            let b = bhvr2 time
             op a b
+        bhvr
 
     let lift3<'a, 'b, 'c, 'd>
         (op : 'a -> 'b -> 'c -> 'd)
         (bhvr : 'a Behavior)
-        (behv2 : 'b Behavior)
-        (behv3 : 'c Behavior) :
+        (bhvr2 : 'b Behavior)
+        (bhvr3 : 'c Behavior) :
         'd Behavior =
-        fun time ->
+        let bhvr = fun time ->
             let a = bhvr time
-            let b = behv2 time
-            let c = behv3 time
+            let b = bhvr2 time
+            let c = bhvr3 time
             op a b c
+        bhvr
 
     let lift4<'a, 'b, 'c, 'd, 'e>
         (op : 'a -> 'b -> 'c -> 'd -> 'e)
         (bhvr : 'a Behavior)
-        (behv2 : 'b Behavior)
-        (behv3 : 'c Behavior)
-        (behv4 : 'd Behavior) :
+        (bhvr2 : 'b Behavior)
+        (bhvr3 : 'c Behavior)
+        (bhvr4 : 'd Behavior) :
         'e Behavior =
-        fun time ->
+        let bhvr = fun time ->
             let a = bhvr time
-            let b = behv2 time
-            let c = behv3 time
-            let d = behv4 time
+            let b = bhvr2 time
+            let c = bhvr3 time
+            let d = bhvr4 time
             op a b c d
+        bhvr
 
     let lift5<'a, 'b, 'c, 'd, 'e, 'f>
         (op : 'a -> 'b -> 'c -> 'd -> 'e -> 'f)
         (bhvr : 'a Behavior)
-        (behv2 : 'b Behavior)
-        (behv3 : 'c Behavior)
-        (behv4 : 'd Behavior)
-        (behv5 : 'e Behavior) :
+        (bhvr2 : 'b Behavior)
+        (bhvr3 : 'c Behavior)
+        (bhvr4 : 'd Behavior)
+        (bhvr5 : 'e Behavior) :
         'f Behavior =
-        fun time ->
+        let bhvr = fun time ->
             let a = bhvr time
-            let b = behv2 time
-            let c = behv3 time
-            let d = behv4 time
-            let e = behv5 time
+            let b = bhvr2 time
+            let c = bhvr3 time
+            let d = bhvr4 time
+            let e = bhvr5 time
             op a b c d e
+        bhvr
 
-    let product<'a, 'b> (bhvr : 'a Behavior) (behv2 : 'b Behavior) =
-        fun time ->
-            (bhvr time, behv2 time)
+    let inline loop stride bounce bhvr =
+        map (fun a ->
+            let local = a % stride
+            if bounce then
+                if int (a / stride) % 2 = 0
+                then local
+                else stride - local
+            else local)
+            bhvr
 
-    let local<'a> start (bhvr : 'a Behavior) : 'a Behavior =
-        fun time ->
-            bhvr (time - start)
+    let inline slice start length bhvr =
+        map (fun a ->
+            let local = a - start
+            if local > length then length
+            elif local < Generics.zero () then Generics.zero ()
+            else local)
+            bhvr
 
-    let slice<'a> start length (bhvr : 'a Behavior) : 'a option Behavior =
-        fun time ->
-            if time >= start && time < start + length
-            then Some (bhvr (time - start))
-            else None
+    let inline normalize (length : GameTime) bhvr =
+        map (fun (time : GameTime) ->
+            let length = length.Seconds
+            let time = time.Seconds
+            time / length)
+            bhvr
 
+    // basic behavior combinators
     let id bhvr = map id bhvr
     let constant k bhvr = map (constant k) bhvr
     let not bhvr = map not bhvr
@@ -98,24 +114,26 @@ module Behavior =
     let mapFst mapper bhvr = map (fun (a, b) -> (mapper a, b)) bhvr
     let mapSnd mapper bhvr = map (fun (a, b) -> (a, mapper b)) bhvr
     let swap bhvr = map (fun (a, b) -> (b, a)) bhvr
+    let product bhvr bhvr2 = let bhvr3 = fun time -> (bhvr time, bhvr2 time) in bhvr3
 
-    let unit : unit Behavior = fun _ -> ()
-    let time (time : GameTime) = constant time unit
-    let sin = fun (time : GameTime) -> sin time.Seconds
-    let cos = fun (time : GameTime) -> cos time.Seconds
-    let step (delay : GameTime) = fun (time : GameTime) -> time.Seconds / delay.Seconds |> int
-    let pulse (delay : GameTime) = map (fun steps -> steps % 2 <> 0) (step delay)
+    // boot-strapping combinators
+    let unit = let (bhvr : unit Behavior) = fun _ -> () in bhvr
+    let time : GameTime Behavior = let bhvr = fun (time : GameTime) -> time in bhvr
+    let timeLoopRaw stride bounce = loop stride bounce time
+    let timeSliceRaw start length = slice start length time
+    let timeLoop stride bounce = let bhvr = timeLoopRaw stride bounce in normalize stride bhvr
+    let timeSlice start length = let bhvr = timeSliceRaw start length in  normalize length bhvr
 
-    let inline lerp (start : GameTime) (length : GameTime) : single Behavior =
-        fun (time : GameTime) ->
-            let local = time - start
-            local.Seconds / length.Seconds
-
-    let inline normalize length (bhvr : _ Behavior) =
-        fun time ->
-            let b = bhvr time
-            let c = b / length
-            c
+    // advanced behavior combinators
+    let inline lerp a b bhvr = map (fun p -> a + p * (b - a)) bhvr
+    let inline step stride bhvr = map (fun p -> int (p / stride)) bhvr
+    let inline pulse stride bhvr = map (fun steps -> steps % 2 <> 0) (step stride bhvr)
+    let inline sin bhvr = map sin bhvr
+    let inline cos bhvr = map cos bhvr
+    let inline sum addend bhvr = map (fun a -> a + addend) bhvr
+    let inline delta difference bhvr = map (fun a -> a - difference) bhvr
+    let inline scale scalar bhvr = map (fun a -> a * scalar) bhvr
+    let inline ratio divisor bhvr = map (fun a -> a / divisor) bhvr
 
 /// Modifies behaviors.
 type Modifier<'a, 'b> = 'a Behavior -> 'b Behavior
@@ -254,13 +272,14 @@ module Modifier =
     let ( *** ) (left, right) = split left right
     let (&&&) (left, right) = fanOut left right
 
-    let hop<'a> (p : Vector3) (p2 : Vector3) h (start : GameTime) (length : GameTime) =
+    let private hop (p : Vector3) (p2 : Vector3) (h : single) (start : GameTime) (length : GameTime) =
         let lerp =
-            Behavior.lerp start length |>
-            Behavior.map (fun s -> p + s * p2 - p)
+            Behavior.timeSlice start length |>
+            Behavior.lerp p p2
         let jump =
+            Behavior.timeSlice start length |>
             Behavior.sin |>
-            Behavior.map (fun s -> s * h)
+            Behavior.scale h
         Behavior.lift2 (fun (p : Vector3) h -> v3 p.X (p.Y + h) p.Z) lerp jump
 
 [<RequireQualifiedAccess>]
@@ -270,6 +289,3 @@ module GameTimeExtension =
 
         member this.Run (behavior : 'a Behavior) =
             behavior this
-
-        member this.RunHop start length =
-            Modifier.hop v3Zero v3One 10.0f start length |> this.Run
