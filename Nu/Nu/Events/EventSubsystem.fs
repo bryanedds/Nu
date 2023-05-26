6da967f3945288989577696880e5d95bf8546467
bryanedds <bryanedds@gmail.com>
2023-05-26 00:42:41 -0400

    Event code file re-org.

--- Nu/Nu/Events/EventSubsystem.fs
+++ Nu/Nu/Events/EventSubsystem.fs
@@ -22,6 +22,34 @@ type [<ReferenceEquality>] SubscriptionEntry =
       Subscriber : Simulant
       CallbackBoxed : obj }
 
+/// Conveys an event's trace information.
+type EventTrace = EventInfo list
+
+[<RequireQualifiedAccess>]
+module EventTrace =
+
+    /// Record an event trace.
+    let record moduleName functionName moreInfo eventTrace : EventTrace =
+        EventInfo.make moduleName functionName moreInfo :: eventTrace
+
+    /// Record event only in debug mode.
+    let debug (moduleName : string) (functionName : string) (moreInfo : string) (eventTrace : EventTrace) =
+#if DEBUG
+        record moduleName functionName moreInfo eventTrace
+#else
+        ignore moduleName
+        ignore functionName
+        ignore moreInfo
+        eventTrace
+#endif
+
+    /// Record event only in all modes.
+    let trace moduleName functionName moreInfo eventTrace =
+        record moduleName functionName moreInfo eventTrace
+
+    /// The empty event trace.
+    let empty : EventTrace = []
+
 /// The generalized event type (can be used to handle any event).
 type Event = Event<obj, Simulant>
 
