64b6bcd9840605ca6211e369467259b01d9d457c
bryanedds <bryanedds@gmail.com>
2023-06-07 14:38:01 -0400

    Updated Metrics.
    Former-commit-id: 158a87fe5ec21f5c22c3b3d2a7a2d20d60cb77bf

--- Projects/Metrics/Program.fs
+++ Projects/Metrics/Program.fs
@@ -182,7 +182,7 @@ type Message =
     interface Nu.Message
 
 type ElmishGameDispatcher () =
-    inherit GameDispatcher<Intss, Message, Command> (Intss.init 100) // 10,000 elmish entities (goal: steady 60FPS, current: unsteady 52FPS)
+    inherit GameDispatcher<Intss, Message, Command> (Intss.init 100) // 10,000 elmish entities (goal: steady 60FPS, current: unsteady 60FPS)
 
     override this.Initialize (_, _) =
         [Game.UpdateEvent => Inc]
