10893e01b51e06b1084e6a4d8cb3348fef769b1a
bryanedds <bryanedds@gmail.com>
2023-06-13 18:28:25 -0400

    Removed likely unnecessary memoization from tmx extensions.
    Former-commit-id: 6c3b8b9c917145c8e10143a7733c1466d5d6a9a2

--- Nu/Nu/AssetGraph/AssetTypes.fs
+++ Nu/Nu/AssetGraph/AssetTypes.fs
@@ -36,29 +36,22 @@ exception TileSetPropertyNotFoundException of string
 [<AutoOpen>]
 module TmxExtensions =
 
-    // OPTIMIZATION: cache tileset image assets.
-    let ImageAssetsMemo = dictPlus<TmxTileset, Image AssetTag> HashIdentity.Structural []
-
     type TmxTileset with
         member this.GetImageAsset tileMapPackage =
-            match ImageAssetsMemo.TryGetValue this with
-            | (false, _) ->
-                let imageAsset =
-                    match this.Properties.TryGetValue "Image" with
-                    | (true, imageAssetTagString) ->
-                        try scvalue<Image AssetTag> imageAssetTagString
-                        with :? KeyNotFoundException ->
-                            let errorMessage =
-                                "Tileset '" + this.Name + "' missing Image property.\n" +
-                                "You must add a Custom Property to the tile set called 'Image' and give it an asset value like '[PackageName AssetName]'.\n" +
-                                "This will specify where the engine can find the tile set's associated image asset."
-                            raise (TileSetPropertyNotFoundException errorMessage)
-                    | (false, _) ->
-                        let name = Path.GetFileNameWithoutExtension this.Image.Source
-                        asset tileMapPackage name // infer asset tag
-                ImageAssetsMemo.Add (this, imageAsset)
-                imageAsset
-            | (true, imageAssets) -> imageAssets
+            let imageAsset =
+                match this.Properties.TryGetValue "Image" with
+                | (true, imageAssetTagString) ->
+                    try scvalue<Image AssetTag> imageAssetTagString
+                    with :? KeyNotFoundException ->
+                        let errorMessage =
+                            "Tileset '" + this.Name + "' missing Image property.\n" +
+                            "You must add a Custom Property to the tile set called 'Image' and give it an asset value like '[PackageName AssetName]'.\n" +
+                            "This will specify where the engine can find the tile set's associated image asset."
+                        raise (TileSetPropertyNotFoundException errorMessage)
+                | (false, _) ->
+                    let name = Path.GetFileNameWithoutExtension this.Image.Source
+                    asset tileMapPackage name // infer asset tag
+            imageAsset
 
     type TmxMap with
         member this.GetImageAssets tileMapPackage =
