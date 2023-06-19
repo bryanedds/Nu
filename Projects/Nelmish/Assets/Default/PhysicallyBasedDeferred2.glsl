0407827612d2f0702aa2eaead4bce040bf5c0e10
bryanedds <bryanedds@gmail.com>
2023-06-19 16:41:30 -0400

    Applied forgotten ssao changes to rest of asset folders.
    Former-commit-id: b5bb7b74f78b3f1d210cf375cb43e816e57b2069

--- Projects/Nelmish/Assets/Default/PhysicallyBasedDeferred2.glsl
+++ Projects/Nelmish/Assets/Default/PhysicallyBasedDeferred2.glsl
@@ -20,23 +20,8 @@ const float FLOAT_MAX = 3.402823466e+38;
 const float REFLECTION_LOD_MAX = 5.0;
 const float GAMMA = 2.2;
 const float ATTENUATION_CONSTANT = 1.0;
-const float PARALLAX_CORRECTION_SCALE = 0.02;
 const int LIGHT_MAPS_MAX = 12;
 const int LIGHTS_MAX = 64;
-const float SSAO = 1.4;
-const float SSAO_BIAS = 0.01;
-const float SSAO_RADIUS = 0.25;
-const int SSAO_SAMPLES = 32;
-const float SSAO_SAMPLES_INVERSE = 1.0 / float(SSAO_SAMPLES);
-const vec3[SSAO_SAMPLES] SSAO_SAMPLING_DIRECTIONS = vec3[](
-    vec3(0.9359231, -0.02727632, -0.35178102), vec3(0.8210779, -0.56898764, 0.04045432), vec3(-0.55349622, -0.24700827, -0.79513649), vec3(-0.96813618, -0.11483972, -0.22411245),
-    vec3(0.27629185, 0.9501148, -0.1451443 ), vec3(-0.06949984, 0.99414051, -0.08414021), vec3(0.41758068, -0.88436643, -0.20611643), vec3(0.1080913, -0.98697543, 0.11580234),
-    vec3(0.38256372, 0.50147269, 0.776086), vec3(-0.52093203, 0.65556912, -0.54715596), vec3(-0.82166309, -0.53712316, -0.19158611), vec3(-0.05843224, 0.97212569, 0.2273377),
-    vec3(0.05989327, -0.80092543, -0.59577847), vec3(-0.88521547, -0.43258009, -0.17180302), vec3(0.61371228, 0.75813784, -0.21964663), vec3(0.38108761, 0.6994238, 0.60440729),
-    vec3(-0.6606408, -0.52617907, 0.53501332), vec3(0.46617216, -0.8148892, -0.34363632), vec3(-0.52490061, 0.07865526, -0.84734162), vec3(0.35224499, 0.93218048, 0.0823843 ),
-    vec3(-0.43173574, -0.87088326, -0.23551558), vec3(0.93189228, -0.04152137, -0.36085101), vec3(0.90774428, 0.30885884, 0.28341675), vec3(0.26065878, -0.7230991, -0.63901384),
-    vec3(-0.49612224, -0.82538313, -0.27002421), vec3(-0.9541399, -0.28490529, -0.09172737), vec3(0.13630145, 0.7227553, 0.67767699), vec3(0.39768804, -0.20326345, -0.89493027),
-    vec3(-0.7792292, 0.34389984, 0.52529078), vec3(-0.2842151, -0.91795119, -0.27790138), vec3(0.44169986, 0.22560122, 0.86954608), vec3(0.52438351, -0.57048116, 0.63150796));
 
 uniform mat4 view;
 uniform mat4 projection;
@@ -47,6 +32,7 @@ uniform sampler2D positionTexture;
 uniform sampler2D albedoTexture;
 uniform sampler2D materialTexture;
 uniform sampler2D normalAndHeightTexture;
+uniform sampler2D ssaoTexture;
 uniform samplerCube irradianceMap;
 uniform samplerCube environmentFilterMap;
 uniform sampler2D brdfTexture;
@@ -143,6 +129,9 @@ void main()
     vec3 albedo = texture(albedoTexture, texCoordsOut).rgb;
     vec4 material = texture(materialTexture, texCoordsOut);
 
+    // retrieve data from ssao buffer, smoothing in the process
+    float ambientOcclusionScreen = textureLod(ssaoTexture, texCoordsOut, 0.5).r;
+
     // compute materials
     float metallic = material.r;
     float roughness = material.g;
@@ -203,36 +192,6 @@ void main()
         // add to outgoing lightAccum
         lightAccum += (kD * albedo / PI + specular) * radiance * nDotL;
     }
-
-    // compute screen space ambient occlusion
-    float ambientOcclusionScreen = 0.0;
-    vec3 positionView = (view * vec4(position, 1.0)).xyz;
-    for (int i = 0; i < SSAO_SAMPLES; ++i)
-    {
-        // compute sampling direction in world space
-        vec3 samplingDirection = SSAO_SAMPLING_DIRECTIONS[i];
-        samplingDirection *= SSAO_RADIUS; // scale by radius
-        samplingDirection *= mix(SSAO_SAMPLES_INVERSE, 1.0f, i * SSAO_SAMPLES_INVERSE); // linearly increase sampling distance from origin
-        samplingDirection = dot(samplingDirection, normal) > 0.0f ? samplingDirection : -samplingDirection; // only sampling upper hemisphere
-
-        // compute sampling position in screen space
-        vec3 samplingPosition = position + samplingDirection;
-        vec3 samplingPositionView = (view * vec4(samplingPosition, 1.0)).xyz;
-        vec4 samplingPositionClip = projection * vec4(samplingPositionView, 1.0);
-        vec2 samplingPositionScreen = samplingPositionClip.xy / samplingPositionClip.w * 0.5 + 0.5;
-
-        // sample position in view space
-        vec3 samplePosition = texture(positionTexture, samplingPositionScreen).rgb;
-        vec3 samplePositionView = (view * vec4(samplePosition, 1.0)).xyz;
-
-        // perform range check and accumulate if occluded
-        float rangeCheck = smoothstep(0.0, 1.0, SSAO_RADIUS / abs(positionView.z - samplePositionView.z));
-        ambientOcclusionScreen += samplePositionView.z >= samplingPositionView.z + SSAO_BIAS ? rangeCheck : 0.0;
-    }
-    ambientOcclusionScreen *= SSAO_SAMPLES_INVERSE;
-    ambientOcclusionScreen *= SSAO;
-    ambientOcclusionScreen = 1.0 - ambientOcclusionScreen;
-    ambientOcclusionScreen = max(0.0, ambientOcclusionScreen);
 
     // compute nearest light map indices
     int lm1 = -1;
