62df21609d0c0a5537b335ba625c514c1dce49de
bryanedds <bryanedds@gmail.com>
2023-06-19 22:48:21 -0400

    Changed order of ssao rendering.

--- Nu/Nu/OpenGL/OpenGL.Framebuffer.fs
+++ Nu/Nu/OpenGL/OpenGL.Framebuffer.fs
@@ -154,46 +154,6 @@ module Framebuffer =
         Gl.DeleteFramebuffers [|framebuffer|]
         Gl.DeleteTextures [|position; albedo; material; normalAndHeight|]
 
-    /// Create a ssao buffers.
-    let TryCreateSsaoBuffers () =
-
-        // create frame buffer object
-        let framebuffer = Gl.GenFramebuffer ()
-        Gl.BindFramebuffer (FramebufferTarget.Framebuffer, framebuffer)
-        Hl.Assert ()
-
-        // create ssao buffer
-        let ssao = Gl.GenTexture ()
-        Gl.BindTexture (TextureTarget.Texture2d, ssao)
-        Gl.TexImage2D (TextureTarget.Texture2d, 0, InternalFormat.R32f, Constants.Render.SsaoResolutionX, Constants.Render.SsaoResolutionY, 0, PixelFormat.Red, PixelType.Float, nativeint 0)
-        Gl.TexParameter (TextureTarget.Texture2d, TextureParameterName.TextureMinFilter, int TextureMinFilter.LinearMipmapLinear)
-        Gl.TexParameter (TextureTarget.Texture2d, TextureParameterName.TextureMagFilter, int TextureMagFilter.Linear)
-        Gl.TexParameter (TextureTarget.Texture2d, TextureParameterName.TextureWrapS, int TextureWrapMode.ClampToEdge)
-        Gl.TexParameter (TextureTarget.Texture2d, TextureParameterName.TextureWrapT, int TextureWrapMode.ClampToEdge)
-        Gl.FramebufferTexture2D (FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0, TextureTarget.Texture2d, ssao, 0)
-        Hl.Assert ()
-
-        // associate draw buffers
-        Gl.DrawBuffers [|int FramebufferAttachment.ColorAttachment0|]
-
-        // create render buffer with depth and stencil
-        let renderbuffer = Gl.GenRenderbuffer ()
-        Gl.BindRenderbuffer (RenderbufferTarget.Renderbuffer, renderbuffer)
-        Gl.RenderbufferStorage (RenderbufferTarget.Renderbuffer, InternalFormat.Depth24Stencil8, Constants.Render.SsaoResolutionX, Constants.Render.SsaoResolutionY)
-        Gl.FramebufferRenderbuffer (FramebufferTarget.Framebuffer, FramebufferAttachment.DepthStencilAttachment, RenderbufferTarget.Renderbuffer, renderbuffer)
-        Hl.Assert ()
-
-        // ensure framebuffer is complete
-        if Gl.CheckFramebufferStatus FramebufferTarget.Framebuffer = FramebufferStatus.FramebufferComplete
-        then Right (ssao, renderbuffer, framebuffer)
-        else Left "Could not create complete geometry framebuffer."
-
-    /// Destroy ssao buffers.
-    let DestroySsaoBuffers (ssao, renderbuffer, framebuffer) =
-        Gl.DeleteRenderbuffers [|renderbuffer|]
-        Gl.DeleteFramebuffers [|framebuffer|]
-        Gl.DeleteTextures [|ssao|]
-
     /// Create light mapping buffers.
     let TryCreateLightMappingBuffers () =
 
@@ -307,6 +267,46 @@ module Framebuffer =
         if Gl.CheckFramebufferStatus FramebufferTarget.Framebuffer = FramebufferStatus.FramebufferComplete
         then Right (environmentFilter, renderbuffer, framebuffer)
         else Left "Could not create complete geometry framebuffer."
+
+    /// Create a ssao buffers.
+    let TryCreateSsaoBuffers () =
+
+        // create frame buffer object
+        let framebuffer = Gl.GenFramebuffer ()
+        Gl.BindFramebuffer (FramebufferTarget.Framebuffer, framebuffer)
+        Hl.Assert ()
+
+        // create ssao buffer
+        let ssao = Gl.GenTexture ()
+        Gl.BindTexture (TextureTarget.Texture2d, ssao)
+        Gl.TexImage2D (TextureTarget.Texture2d, 0, InternalFormat.R32f, Constants.Render.SsaoResolutionX, Constants.Render.SsaoResolutionY, 0, PixelFormat.Red, PixelType.Float, nativeint 0)
+        Gl.TexParameter (TextureTarget.Texture2d, TextureParameterName.TextureMinFilter, int TextureMinFilter.LinearMipmapLinear)
+        Gl.TexParameter (TextureTarget.Texture2d, TextureParameterName.TextureMagFilter, int TextureMagFilter.Linear)
+        Gl.TexParameter (TextureTarget.Texture2d, TextureParameterName.TextureWrapS, int TextureWrapMode.ClampToEdge)
+        Gl.TexParameter (TextureTarget.Texture2d, TextureParameterName.TextureWrapT, int TextureWrapMode.ClampToEdge)
+        Gl.FramebufferTexture2D (FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0, TextureTarget.Texture2d, ssao, 0)
+        Hl.Assert ()
+
+        // associate draw buffers
+        Gl.DrawBuffers [|int FramebufferAttachment.ColorAttachment0|]
+
+        // create render buffer with depth and stencil
+        let renderbuffer = Gl.GenRenderbuffer ()
+        Gl.BindRenderbuffer (RenderbufferTarget.Renderbuffer, renderbuffer)
+        Gl.RenderbufferStorage (RenderbufferTarget.Renderbuffer, InternalFormat.Depth24Stencil8, Constants.Render.SsaoResolutionX, Constants.Render.SsaoResolutionY)
+        Gl.FramebufferRenderbuffer (FramebufferTarget.Framebuffer, FramebufferAttachment.DepthStencilAttachment, RenderbufferTarget.Renderbuffer, renderbuffer)
+        Hl.Assert ()
+
+        // ensure framebuffer is complete
+        if Gl.CheckFramebufferStatus FramebufferTarget.Framebuffer = FramebufferStatus.FramebufferComplete
+        then Right (ssao, renderbuffer, framebuffer)
+        else Left "Could not create complete geometry framebuffer."
+
+    /// Destroy ssao buffers.
+    let DestroySsaoBuffers (ssao, renderbuffer, framebuffer) =
+        Gl.DeleteRenderbuffers [|renderbuffer|]
+        Gl.DeleteFramebuffers [|framebuffer|]
+        Gl.DeleteTextures [|ssao|]
 
     /// Destroy environment filter buffers.
     let DestroyEnvironmentFilterBuffers (environmentFilter, renderbuffer, framebuffer) =
