c172b74287677f228444a326f230e97c4fb73f31
bryanedds <bryanedds@gmail.com>
2023-06-25 14:04:13 -0400

    More work on imgui renderer.

---
+++ Nu/Nu/ImGui/ImGuiRenderer.fs
@@ -0,0 +1,212 @@
+﻿namespace Nu
+open System
+open System.Numerics
+open System.Runtime.CompilerServices
+open Prime
+open ImGuiNET
+open Nu
+
+    /// Renders ImGui view via OpenGL.
+    /// NOTE: code in has a lot of mutability because it's ported from a port of a port...
+    type ImGuiRenderer (windowWidth : int, windowHeight : int) =
+
+        do ignore windowWidth
+        let mutable vertexArrayObject = 0u
+        let mutable vertexBuffer = 0u
+        let mutable vertexBufferSize = 10000u
+        let mutable indexBuffer = 0u
+        let mutable indexBufferSize = 2000u
+        let mutable shader = 0u
+        let mutable shaderProjectionMatrixUniform = 0
+        let mutable shaderFontTextureUniform = 0
+        let mutable fontTextureWidth = 0
+        let mutable fontTextureHeight = 0
+        let mutable fontTexture = 0u
+
+        member this.Initialize (fonts : ImFontAtlasPtr) =
+        
+            // initialize vao
+            vertexArrayObject <- OpenGL.Gl.GenVertexArray ()
+            OpenGL.Gl.BindVertexArray vertexArrayObject
+            OpenGL.Hl.Assert ()
+
+            // create vertex buffer
+            vertexBuffer <- OpenGL.Gl.GenBuffer ()
+            OpenGL.Gl.BindBuffer (OpenGL.BufferTarget.ArrayBuffer, vertexBuffer)
+            OpenGL.Gl.BufferData (OpenGL.BufferTarget.ArrayBuffer, vertexBufferSize, nativeint 0, OpenGL.BufferUsage.DynamicDraw)
+
+            // create index buffer
+            indexBuffer <- OpenGL.Gl.GenBuffer ()
+            OpenGL.Gl.BindBuffer (OpenGL.BufferTarget.ElementArrayBuffer, indexBuffer)
+            OpenGL.Gl.BufferData (OpenGL.BufferTarget.ElementArrayBuffer, indexBufferSize, nativeint 0, OpenGL.BufferUsage.DynamicDraw)
+
+            // configure vao
+            let stride = Unsafe.SizeOf<ImDrawVert> ()
+            OpenGL.Gl.EnableVertexAttribArray 0u
+            OpenGL.Gl.VertexAttribPointer (0u, 2, OpenGL.VertexAttribPointerType.Float, false, stride, 0)
+            OpenGL.Gl.EnableVertexAttribArray 1u
+            OpenGL.Gl.VertexAttribPointer (1u, 2, OpenGL.VertexAttribPointerType.Float, false, stride, 8)
+            OpenGL.Gl.EnableVertexAttribArray 2u
+            OpenGL.Gl.VertexAttribPointer (2u, 4, OpenGL.VertexAttribPointerType.UnsignedShort, true, stride, 16)
+            OpenGL.Hl.Assert ()
+
+            // finalize vao
+            OpenGL.Gl.BindVertexArray 0u
+            OpenGL.Hl.Assert ()
+
+            // vertex shader code
+            let vertexStr =
+                [Constants.OpenGl.GlslVersionPragma
+                 ""
+                 "uniform mat4 projection;"
+                 ""
+                 "layout (location = 0) in vec2 position;"
+                 "layout (location = 1) in vec2 texCoords;"
+                 "layout (location = 2) in vec4 color;"
+                 ""
+                 "out vec4 colorOut;"
+                 "out vec2 texCoordsOut;"
+                 ""
+                 "void main()"
+                 "{"
+                 "    colorOut = color;"
+                 "    texCoordsOut = texCoords;"
+                 "    gl_Position = projection * vec4(position, 0, 1);"
+                 "}"] |> String.join "\n"
+
+            // fragment shader code
+            let fragmentStr =
+                [Constants.OpenGl.GlslVersionPragma
+                 ""
+                 "uniform sampler2D fontTexture;"
+                 ""
+                 "in vec4 colorOut;"
+                 "in vec2 texCoordsOut;"
+                 ""
+                 "out vec4 frag;"
+                 ""
+                 "void main()"
+                 "{"
+                 "    frag = colorOut * texture(fontTexture, texCoordsOut);"
+                 "}"] |> String.join "\n"
+
+            // create shader
+            shader <- OpenGL.Shader.CreateShaderFromStrs (vertexStr, fragmentStr)
+            shaderProjectionMatrixUniform <- OpenGL.Gl.GetUniformLocation (shader, "projection")
+            shaderFontTextureUniform <- OpenGL.Gl.GetUniformLocation (shader, "fontTexture")
+            OpenGL.Hl.Assert ()
+
+            // create font texture
+            let mutable pixels = Unchecked.defaultof<nativeint>
+            let mutable bytesPerPixel = Unchecked.defaultof<_>
+            fonts.GetTexDataAsRGBA32 (&pixels, &fontTextureWidth, &fontTextureHeight, &bytesPerPixel)
+            fontTexture <- OpenGL.Texture.CreateTexture (OpenGL.InternalFormat.Rgba32f, fontTextureWidth, fontTextureHeight, OpenGL.PixelFormat.Rgba, OpenGL.PixelType.Float, OpenGL.TextureMinFilter.Linear, OpenGL.TextureMagFilter.Linear, false, pixels)
+            fonts.SetTexID (nativeint fontTexture)
+            fonts.ClearTexData ()
+
+        member this.Destroy () =
+
+            // destroy vao
+            OpenGL.Gl.BindVertexArray vertexArrayObject
+            OpenGL.Gl.DeleteBuffers [|vertexBuffer|]
+            OpenGL.Gl.DeleteBuffers [|indexBuffer|]
+            OpenGL.Gl.BindVertexArray 0u
+            OpenGL.Gl.DeleteVertexArrays [|vertexArrayObject|]
+
+            // destroy shader
+            OpenGL.Gl.DeleteProgram shader
+
+            // destroy font texture
+            OpenGL.Gl.DeleteTextures [|fontTexture|]
+
+        member this.Render (drawData : ImDrawDataPtr) =
+
+            // attempt to draw im gui draw data
+            let mutable vertexOffsetInVertices = 0
+            let mutable indexOffsetInElements = 0
+            if drawData.CmdListsCount <> 0 then
+
+                // resize vertex buffer if necessary
+                let vertexBufferSizeNeeded = uint (drawData.TotalVtxCount * Unsafe.SizeOf<ImDrawVert>())
+                if vertexBufferSizeNeeded > vertexBufferSize then
+                    vertexBufferSize <- max (vertexBufferSize * 2u) vertexBufferSizeNeeded
+                    OpenGL.Gl.BindBuffer (OpenGL.BufferTarget.ArrayBuffer, vertexBuffer)
+                    OpenGL.Gl.BufferData (OpenGL.BufferTarget.ArrayBuffer, vertexBufferSize, nativeint 0, OpenGL.BufferUsage.DynamicDraw)
+                    OpenGL.Gl.BindBuffer (OpenGL.BufferTarget.ArrayBuffer, 0u)
+                    OpenGL.Hl.Assert ()
+
+                // resize index buffer if necessary
+                let indexBufferSizeNeeded = uint (drawData.TotalIdxCount * sizeof<uint16>)
+                if indexBufferSizeNeeded > indexBufferSize then
+                    indexBufferSize <- max (indexBufferSize * 2u) indexBufferSizeNeeded
+                    OpenGL.Gl.BindBuffer (OpenGL.BufferTarget.ArrayBuffer, indexBuffer)
+                    OpenGL.Gl.BufferData (OpenGL.BufferTarget.ArrayBuffer, indexBufferSize, nativeint 0, OpenGL.BufferUsage.DynamicDraw)
+                    OpenGL.Gl.BindBuffer (OpenGL.BufferTarget.ArrayBuffer, 0u)
+                    OpenGL.Hl.Assert ()
+
+                // compute offsets
+                let cmdsRange = drawData.CmdListsRange
+                for i in 0 .. dec drawData.CmdListsCount do
+                    let cmds = cmdsRange.[i]
+                    OpenGL.Gl.BindBuffer (OpenGL.BufferTarget.ArrayBuffer, vertexBuffer)
+                    OpenGL.Gl.BufferSubData (OpenGL.BufferTarget.ArrayBuffer, nativeint (vertexOffsetInVertices * Unsafe.SizeOf<ImDrawVert> ()), uint (cmds.VtxBuffer.Size * Unsafe.SizeOf<ImDrawVert> ()), cmds.VtxBuffer.Data)
+                    OpenGL.Gl.BindBuffer (OpenGL.BufferTarget.ArrayBuffer, indexBuffer)
+                    OpenGL.Gl.BufferSubData (OpenGL.BufferTarget.ArrayBuffer, nativeint (indexOffsetInElements * sizeof<uint16>), uint (cmds.IdxBuffer.Size * sizeof<uint16>), cmds.IdxBuffer.Data)
+                    vertexOffsetInVertices <- vertexOffsetInVertices + cmds.VtxBuffer.Size
+                    indexOffsetInElements <- indexOffsetInElements + cmds.IdxBuffer.Size
+                    OpenGL.Hl.Assert ()
+
+                // compute orthographic projection
+                let projection = Matrix4x4.CreateOrthographicOffCenter (0.0f, single windowWidth, single windowHeight, 0.0f, -1.0f, 1.0f)
+                let projectionArray = projection.ToArray ()
+
+                // setup state
+                OpenGL.Gl.Enable OpenGL.EnableCap.Blend
+                OpenGL.Gl.Enable OpenGL.EnableCap.ScissorTest
+                OpenGL.Gl.BlendEquation OpenGL.BlendEquationMode.FuncAdd
+                OpenGL.Gl.BlendFunc (OpenGL.BlendingFactor.SrcAlpha, OpenGL.BlendingFactor.OneMinusSrcAlpha)
+                OpenGL.Gl.Disable OpenGL.EnableCap.CullFace
+                OpenGL.Gl.Disable OpenGL.EnableCap.DepthTest
+                OpenGL.Hl.Assert ()
+
+                // setup vao
+                OpenGL.Gl.BindVertexArray vertexArrayObject
+                OpenGL.Hl.Assert ()
+
+                // setup shader
+                OpenGL.Gl.UseProgram shader
+                OpenGL.Gl.UniformMatrix4 (shaderProjectionMatrixUniform, false, projectionArray)
+                OpenGL.Gl.Uniform1 (shaderFontTextureUniform, 0)
+                OpenGL.Hl.Assert ()
+
+                // draw command lists
+                let mutable vertexOffset = 0
+                let mutable indexOffset = 0
+                for i in 0 .. dec drawData.CmdListsCount do
+                    let cmdsRange = drawData.CmdListsRange
+                    let cmds = cmdsRange.[i]
+                    for cmd in 0 .. dec cmds.CmdBuffer.Size do
+                        let pcmds = cmds.CmdBuffer
+                        let pcmd = pcmds.[cmd]
+                        if pcmd.UserCallback = nativeint 0 then
+                            OpenGL.Gl.ActiveTexture OpenGL.TextureUnit.Texture0
+                            OpenGL.Gl.BindTexture (OpenGL.TextureTarget.Texture2d, uint pcmd.TextureId)
+                            OpenGL.Hl.Assert ()
+                            let clip = pcmd.ClipRect
+                            OpenGL.Gl.Scissor(int clip.X, windowHeight - int clip.W, int (clip.Z - clip.X), int (clip.W - clip.Y))
+                            OpenGL.Hl.Assert ()
+                            OpenGL.Gl.DrawElementsBaseVertex (OpenGL.PrimitiveType.Triangles, int pcmd.ElemCount, OpenGL.DrawElementsType.UnsignedShort, nativeint (indexOffset * sizeof<uint16>), vertexOffset)
+                            OpenGL.Hl.Assert ()
+                        else raise (NotImplementedException ())
+                        indexOffset <- indexOffset + int pcmd.ElemCount
+                    vertexOffset <- vertexOffset + cmds.VtxBuffer.Size
+
+                // teardown shader
+                OpenGL.Gl.UseProgram 0u
+
+                // teardown vao
+                OpenGL.Gl.BindVertexArray 0u
+
+                // teardown state
+                OpenGL.Gl.Disable OpenGL.EnableCap.Blend
+                OpenGL.Gl.Disable OpenGL.EnableCap.ScissorTest
