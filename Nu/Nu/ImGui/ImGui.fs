ce7f61cc11d06e179d280d88b3c9f58084c8ef7e
bryanedds <bryanedds@gmail.com>
2023-07-18 11:48:41 -0400

    Added ImGui.FileDialog extension.

--- Nu/Nu/ImGui/ImGui.fs
+++ Nu/Nu/ImGui/ImGui.fs
@@ -1,3 +1,6 @@
+// Nu Game Engine.
+// Copyright (C) Bryan Edds, 2013-2023.
+
 namespace Nu
 open System
 open System.Collections.Generic
@@ -6,27 +9,6 @@ open ImGuiNET
 open ImGuizmoNET
 open Prime
 open Nu
-
-// TODO: document this!
-
-[<AutoOpen>]
-module ImGuiIOPtr =
-
-    // HACK: allows manual tracking of mouse and keyboard event swallowing since Dead ImGui doesn't seem to yet have
-    // it worked out - https://github.com/ocornut/imgui/issues/3370
-    let mutable internal wantCaptureMousePlus = false
-    let mutable internal wantCaptureKeyboardPlus = false
-
-    let internal BeginFrame () =
-        wantCaptureMousePlus <- false
-        wantCaptureKeyboardPlus <- false
-
-    type ImGuiIOPtr with
-
-        member this.WantCaptureMousePlus = wantCaptureMousePlus || this.WantCaptureMouse
-        member this.WantCaptureKeyboardPlus = wantCaptureKeyboardPlus || this.WantCaptureKeyboard
-        member this.SwallowMouse () = wantCaptureMousePlus <- true
-        member this.SwallowKeyboard () = wantCaptureKeyboardPlus <- true
 
 /// Wraps ImGui context, state, and calls. Also extends the ImGui interface with static methods.
 /// NOTE: API is primarily object-oriented / mutation-based because it's ported from a port of a port.
---
+++ Nu/Nu/ImGui/ImGuiIOPtr.fs
@@ -0,0 +1,26 @@
+﻿// Nu Game Engine.
+// Copyright (C) Bryan Edds, 2013-2023.
+
+namespace ImGuiNET
+open System
+open Prime
+open Nu
+
+[<AutoOpen>]
+module ImGuiIOPtr =
+
+    // HACK: allows manual tracking of mouse and keyboard event swallowing since Dead ImGui doesn't seem to yet have
+    // it worked out - https://github.com/ocornut/imgui/issues/3370
+    let mutable internal wantCaptureMousePlus = false
+    let mutable internal wantCaptureKeyboardPlus = false
+
+    let internal BeginFrame () =
+        wantCaptureMousePlus <- false
+        wantCaptureKeyboardPlus <- false
+
+    type ImGuiIOPtr with
+
+        member this.WantCaptureMousePlus = wantCaptureMousePlus || this.WantCaptureMouse
+        member this.WantCaptureKeyboardPlus = wantCaptureKeyboardPlus || this.WantCaptureKeyboard
+        member this.SwallowMouse () = wantCaptureMousePlus <- true
+        member this.SwallowKeyboard () = wantCaptureKeyboardPlus <- true
