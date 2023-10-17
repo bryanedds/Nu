9ab8f3435fdaced6bcd87e22f89f3fdee753a00f
bryanedds <bryanedds@gmail.com>
2023-10-17 09:30:15 -0400

    Compile fix.

--- Nu/Nu/Render/Render3d.fs
+++ Nu/Nu/Render/Render3d.fs
@@ -1594,8 +1594,6 @@ type [<ReferenceEquality>] GlRenderer3d =
 
                 // TODO: check if geometry needed to render terrain is already memoized so no unecessary computation occurs
 
-                let bounds = rt.ModelMatrix.
-
                 match rt.TerrainDescriptor.HeightMap with
                 | RawHeightMap map ->
 
