fca2466b8ac8163dc9dc9817e85ce45773f0cac4
bryanedds <bryanedds@gmail.com>
2023-10-19 10:14:10 -0400

    Code clean-up.
    Former-commit-id: a398906a1abaf9bc1a2808e1dece66b681fbcbae

--- Nu/Nu/AssetGraph/AssetTypes.fs
+++ Nu/Nu/AssetGraph/AssetTypes.fs
@@ -14,11 +14,11 @@ type Image = private { __ : unit }
 /// A font. Currently just used as a phantom type.
 type Font = private { __ : unit }
 
-/// A tile map. Currently just used as a phantom type.
-type TileMap = private { __ : unit }
-
 /// A Live2D Cubism model. Currently just used as a phantom type.
 type CubismModel = private { __ : unit }
+
+/// A tile map. Currently just used as a phantom type.
+type TileMap = private { __ : unit }
 
 /// A cube map. Currently just used as a phantom type.
 type CubeMap = private { __ : unit }
