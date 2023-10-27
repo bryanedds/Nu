d1e1d3783796e66ee1d9cc3064e7b147aba2c6c6
bryanedds <bryanedds@gmail.com>
2023-10-27 14:32:37 -0400

    Using proper texture bordering for non-inset sprites.
    Former-commit-id: 18180ffb67185e4461e6ee34efb3ff657c604e7e

--- Nu/Nu/Render/Render2d.fs
+++ Nu/Nu/Render/Render2d.fs
@@ -322,18 +322,23 @@ type [<ReferenceEquality>] GlRenderer2d =
 
         // compute unflipped tex coords
         let texCoordsUnflipped =
+            let texelWidth = textureMetadata.TextureTexelWidth
+            let texelHeight = textureMetadata.TextureTexelHeight
+            let borderWidth = texelWidth * Constants.Render.SpriteBorderTexelScalar
+            let borderHeight = texelHeight * Constants.Render.SpriteBorderTexelScalar
             match insetOpt with
             | ValueSome inset ->
-                let texelWidth = textureMetadata.TextureTexelWidth
-                let texelHeight = textureMetadata.TextureTexelHeight
-                let borderWidth = texelWidth * Constants.Render.SpriteBorderTexelScalar
-                let borderHeight = texelHeight * Constants.Render.SpriteBorderTexelScalar
-                let px = inset.Min.X * texelWidth + borderWidth
-                let py = (inset.Min.Y + inset.Size.Y) * texelHeight - borderHeight
+                let mx = inset.Min.X * texelWidth + borderWidth
+                let my = (inset.Min.Y + inset.Size.Y) * texelHeight - borderHeight
                 let sx = inset.Size.X * texelWidth - borderWidth * 2.0f
                 let sy = -inset.Size.Y * texelHeight + borderHeight * 2.0f
-                Box2 (px, py, sx, sy)
-            | ValueNone -> Box2 (0.0f, 1.0f, 1.0f, -1.0f) // TODO: shouldn't we still be using borders?
+                Box2 (mx, my, sx, sy)
+            | ValueNone ->
+                let mx = borderWidth
+                let my = 1.0f - borderHeight
+                let sx = 1.0f - borderWidth * 2.0f
+                let sy = -1.0f + borderWidth * -2.0f
+                Box2 (mx, my, sx, sy)
 
         // compute a flipping flags
         let struct (flipH, flipV) =
