d131b01b5e45d10dac5cf967c90fdfc7546b1cbf
bryanedds <bryanedds@gmail.com>
2023-10-30 14:50:45 -0400

    Removed Tactics project.
    Former-commit-id: 1f9788320a9254b625078525168b60c179d77d62

--- Projects/Tactics/Assets/Field/TileMaps/Forest.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Forest" tilewidth="16" tileheight="16" tilecount="2310" columns="66">
- <properties>
-  <property name="Image" value="[Field Forest]"/>
- </properties>
- <image source="Forest.png" width="1056" height="560"/>
-</tileset>
