9b774e3f7ffb39f5e88be02d0a2acecd9a311f05
bryanedds <bryanedds@gmail.com>
2023-11-06 11:39:20 -0500

    Ripped out mario from breakout.

---
+++ Projects/Breakout/Assets/Default/PhysicallyBasedDeferredLightMapping.glsl
@@ -0,0 +1,77 @@
+#shader vertex
+#version 410 core
+
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec2 texCoords;
+
+out vec2 texCoordsOut;
+
+void main()
+{
+    texCoordsOut = texCoords;
+    gl_Position = vec4(position, 1.0);
+}
+
+#shader fragment
+#version 410 core
+
+const float PI = 3.141592654;
+const float FLOAT_MAX = 3.402823466e+38;
+const int LIGHT_MAPS_MAX = 27;
+
+uniform sampler2D positionTexture;
+uniform sampler2D normalAndHeightTexture;
+uniform vec3 lightMapOrigins[LIGHT_MAPS_MAX];
+uniform vec3 lightMapMins[LIGHT_MAPS_MAX];
+uniform vec3 lightMapSizes[LIGHT_MAPS_MAX];
+uniform int lightMapsCount;
+
+in vec2 texCoordsOut;
+
+out vec4 frag;
+
+bool inBounds(vec3 point, vec3 min, vec3 size)
+{
+    return
+        all(greaterThanEqual(point, min)) &&
+        all(lessThanEqual(point, min + size));
+}
+
+void main()
+{
+    // retrieve normal value first, allowing for early-out
+    vec3 normal = texture(normalAndHeightTexture, texCoordsOut).rgb;
+    if (normal == vec3(1.0)) discard; // discard if geometry pixel was not written (equal to the buffer clearing color of white)
+
+    // retrieve remaining data from geometry buffers
+    vec3 position = texture(positionTexture, texCoordsOut).rgb;
+
+    // compute nearest light map indices
+    int lm1 = -1;
+    int lm2 = -1;
+    float lm1DistanceSquared = FLOAT_MAX;
+    float lm2DistanceSquared = FLOAT_MAX;
+    for (int i = 0; i < lightMapsCount; ++i)
+    {
+        if (inBounds(position, lightMapMins[i], lightMapSizes[i]))
+        {
+            vec3 delta = lightMapOrigins[i] - position;
+            float distanceSquared = dot(delta, delta);
+            if (distanceSquared < lm1DistanceSquared)
+            {
+                lm2 = lm1;
+                lm1 = i;
+                lm2DistanceSquared = lm1DistanceSquared;
+                lm1DistanceSquared = distanceSquared;
+            }
+            else if (distanceSquared < lm2DistanceSquared)
+            {
+                lm2 = i;
+                lm2DistanceSquared = distanceSquared;
+            }
+        }
+    }
+
+    // write with indices starting at 0.0 rather than -1.0 so that a black texture can be passed in for no light mapping
+    frag = vec4(float(lm1 + 1), float(lm2 + 1), sqrt(lm1DistanceSquared), sqrt(lm2DistanceSquared));
+}
---
+++ Projects/Breakout/Gameplay.fs
@@ -0,0 +1,64 @@
+﻿namespace MyGame
+open System
+open Prime
+open Nu
+
+[<AutoOpen>]
+module Gameplay =
+
+    // this represents that state of the simulation during gameplay.
+    type GameplayState =
+        | Playing
+        | Quitting
+        | Quit
+
+    // this is our MMCC model type representing gameplay.
+    type Gameplay =
+        { Score : int
+          State : GameplayState }
+
+    // this is our MMCC message type.
+    type GameplayMessage =
+        | Update
+        | StartQutting
+        | FinishQuitting
+        interface Message
+
+    // this extends the Screen API to expose the above Gameplay model.
+    type Screen with
+        member this.GetGameplay world = this.GetModelGeneric<Gameplay> world
+        member this.SetGameplay value world = this.SetModelGeneric<Gameplay> value world
+        member this.Gameplay = this.ModelGeneric<Gameplay> ()
+
+    // this is the screen dispatcher that defines the screen where gameplay takes place
+    type GameplayDispatcher () =
+        inherit ScreenDispatcher<Gameplay, GameplayMessage, Command> ({ Score = 0; State = Quit })
+
+        // here we define the screen's properties and event handling
+        override this.Initialize (_, _) =
+            [Screen.UpdateEvent => Update
+             Screen.DeselectingEvent => FinishQuitting]
+
+        // here we handle the above messages
+        override this.Message (gameplay, message, _, _) =
+            match message with
+            | Update -> just gameplay
+            | StartQutting -> just { gameplay with State = Quitting }
+            | FinishQuitting -> just { gameplay with State = Quit }
+
+        // here we describe the content of the game including the level, the hud, and the player
+        override this.Content (gameplay, _) =
+
+            [// the gui group
+             Content.group Simulants.GameplayGui.Name []
+                [Content.button Simulants.GameplayQuit.Name
+                    [Entity.Position == v3 336.0f -216.0f 0.0f
+                     Entity.Elevation == 10.0f
+                     Entity.Text == "Quit"
+                     Entity.ClickEvent => StartQutting]]
+
+             // the scene group while playing or quitting
+             match gameplay.State with
+             | Playing | Quitting ->
+                Content.groupFromFile Simulants.GameplayScene.Name "Assets/Gameplay/Scene.nugroup" [] []
+             | Quit -> ()]
