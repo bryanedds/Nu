6ec4da42ae586745671409cd4ee2f360850968c1
bryanedds <bryanedds@gmail.com>
2023-11-15 11:31:21 -0500

    Some code org and clean-up.
    Former-commit-id: 3e55cc3a32bc134c7459f61d025f7b9c251dd137

--- Nu/Nu/Core/Assimp.fs
+++ Nu/Nu/Core/Assimp.fs
@@ -10,67 +10,16 @@ open Prime
 [<AutoOpen>]
 module AssimpExtensions =
 
-    /// Node extensions.
-    type Assimp.Node with
-
-        /// Get the world transform of the node.
-        member this.TransformWorld =
-            let mutable parentOpt = this.Parent
-            let mutable transform = this.Transform
-            while notNull parentOpt do
-                transform <- transform * parentOpt.Transform
-                parentOpt <- parentOpt.Parent
-            transform
-
-        /// Convert a matrix from an Assimp representation to Nu's.
-        member this.ImportMatrix (m : Assimp.Matrix4x4) =
-            Matrix4x4
-                (m.A1, m.B1, m.C1, m.D1,
-                 m.A2, m.B2, m.C2, m.D2,
-                 m.A3, m.B3, m.C3, m.D3,
-                 m.A4, m.B4, m.C4, m.D4)
-
-        /// Collect all the child nodes of a node, including the node itself.
-        member this.CollectNodes () =
-            seq {
-                yield this
-                for child in this.Children do
-                    yield! child.CollectNodes () }
-
-        /// Collect all the child nodes and transforms of a node, including the node itself.
-        member this.CollectNodesAndTransforms (unitType, parentTransform : Matrix4x4) =
-            seq {
-                let localTransform = this.ImportMatrix this.Transform
-                let worldTransform = localTransform * parentTransform
-                yield (this, worldTransform)
-                for child in this.Children do
-                    yield! child.CollectNodesAndTransforms (unitType, worldTransform) }
-
-        /// Map to a TreeNode.
-        member this.Map<'a> (parentNames : string array, parentTransform : Matrix4x4, mapper : Assimp.Node -> string array -> Matrix4x4 -> 'a array TreeNode) : 'a array TreeNode =
-            let localName = this.Name
-            let localTransform = this.ImportMatrix this.Transform
-            let worldNames = Array.append parentNames [|localName|]
-            let worldTransform = localTransform * parentTransform
-            let node = mapper this worldNames worldTransform
-            for child in this.Children do
-                let child = child.Map<'a> (worldNames, worldTransform, mapper)
-                node.Add child
-            node
-
-[<RequireQualifiedAccess>]
-module AssimpAnimation =
-
     type [<NoEquality; NoComparison; Struct>] BoneInfo =
         { BoneOffset : Assimp.Matrix4x4
-          mutable FinalTransform : Assimp.Matrix4x4 }
+          mutable TransformWorld : Assimp.Matrix4x4 }
 
         static member make offset =
             { BoneOffset = offset
-              FinalTransform = Assimp.Matrix4x4.Identity }
+              TransformWorld = Assimp.Matrix4x4.Identity }
 
     /// Convert a matrix from an Assimp representation to Nu's.
-    let ImportMatrix (m : Assimp.Matrix4x4) =
+    let AssimpToNu (m : Assimp.Matrix4x4) =
         Matrix4x4
             (m.A1, m.B1, m.C1, m.D1,
                 m.A2, m.B2, m.C2, m.D2,
@@ -139,7 +88,7 @@ module AssimpAnimation =
             let StartRotationQ = channel.RotationKeys.[RotationIndex].Value
             let EndRotationQ = channel.RotationKeys.[NextRotationIndex].Value
             let Result = Assimp.Quaternion.Slerp (StartRotationQ, EndRotationQ, Factor)
-            Result.Normalize ()
+            Result.Normalize () // TODO: consider not normalizing here since we should already have two unit quaternions.
             Result
 
     let CalcInterpolatedScaling (animationTime : single, channel : Assimp.NodeAnimationChannel) =
@@ -158,7 +107,7 @@ module AssimpAnimation =
             let Result = Start + Factor * Delta
             Result
 
-    let rec ReadNodeHierarchy (boneMapping : Dictionary<string, int>, boneInfos : BoneInfo array, animationTime : single, animationIndex : int, node : Assimp.Node, parentTransform : Assimp.Matrix4x4, rootTransformInverse : Assimp.Matrix4x4, scene : Assimp.Scene) =
+    let rec UpdateBoneTransforms (boneMapping : Dictionary<string, int>, boneInfos : BoneInfo array, animationTime : single, animationIndex : int, node : Assimp.Node, parentTransform : Assimp.Matrix4x4, rootTransformInverse : Assimp.Matrix4x4, scene : Assimp.Scene) =
 
         // compute bone's local transform
         let name = node.Name
@@ -168,13 +117,7 @@ module AssimpAnimation =
                 let scale = Assimp.Matrix4x4.FromScaling (CalcInterpolatedScaling (animationTime, channel))
                 let rotation = Assimp.Matrix4x4 ((CalcInterpolatedRotation (animationTime, channel)).GetMatrix ())
                 let translation = Assimp.Matrix4x4.FromTranslation (CalcInterpolatedPosition (animationTime, channel))
-                //let scale = Assimp.Matrix4x4.FromScaling (channel.ScalingKeys.[FindScaling (animationTime, channel)].Value)
-                //let rotation = Assimp.Matrix4x4 (channel.RotationKeys.[FindRotation (animationTime, channel)].Value.GetMatrix ())
-                //let translation = Assimp.Matrix4x4.FromTranslation (channel.PositionKeys.[FindPosition (animationTime, channel)].Value)
-                //let scale = Assimp.Matrix4x4.FromScaling (channel.ScalingKeys.[0].Value)
-                //let rotation = Assimp.Matrix4x4 (channel.RotationKeys.[0].Value.GetMatrix ())
-                //let translation = Assimp.Matrix4x4.FromTranslation (channel.PositionKeys.[0].Value)
-                translation * rotation * scale
+                translation * rotation * scale // NOTE: should be a faster way.
             | None -> node.Transform
 
         // compute bone's world transform
@@ -182,28 +125,72 @@ module AssimpAnimation =
         match boneMapping.TryGetValue name with
         | (true, boneId) ->
             let boneOffset = boneInfos.[boneId].BoneOffset
-            boneInfos.[boneId].FinalTransform <- rootTransformInverse * transformWorld * boneOffset
+            boneInfos.[boneId].TransformWorld <- rootTransformInverse * transformWorld * boneOffset
         | (false, _) -> ()
 
         // recur
         for i in 0 .. dec node.Children.Count do
             let child = node.Children.[i]
-            ReadNodeHierarchy (boneMapping, boneInfos, animationTime, animationIndex, child, transformWorld, rootTransformInverse, scene)
+            UpdateBoneTransforms (boneMapping, boneInfos, animationTime, animationIndex, child, transformWorld, rootTransformInverse, scene)
 
-    let AnimateBones (animationTime, animationIndex, mesh : Assimp.Mesh, scene : Assimp.Scene) =
+    /// Mesh extensions.
+    type Assimp.Mesh with
 
-        let boneIds = dictPlus StringComparer.Ordinal []
-        let boneInfos = Array.zeroCreate<BoneInfo> mesh.Bones.Count
-        for i in 0 .. dec mesh.Bones.Count do
-            let bone = mesh.Bones.[i]
-            let boneName = bone.Name
-            boneInfos.[i] <- BoneInfo.make bone.OffsetMatrix
-            boneIds.[boneName] <- i
+        member this.AnimateBones (animationTime, animationIndex, scene : Assimp.Scene) =
 
-        let rootTransformInverse = scene.RootNode.Transform
-        rootTransformInverse.Inverse ()
-        ReadNodeHierarchy (boneIds, boneInfos, animationTime, animationIndex, scene.RootNode, Assimp.Matrix4x4.Identity, rootTransformInverse, scene)
+            // pre-compute bone id dict and bone info storage (these should probably persist outside of this function and be reused)
+            let boneIds = dictPlus StringComparer.Ordinal []
+            let boneInfos = Array.zeroCreate<BoneInfo> this.Bones.Count
+            for i in 0 .. dec this.Bones.Count do
+                let bone = this.Bones.[i]
+                let boneName = bone.Name
+                boneIds.[boneName] <- i
+                boneInfos.[i] <- BoneInfo.make bone.OffsetMatrix
 
-        Array.map (fun (boneInfo : BoneInfo) ->
-            ImportMatrix boneInfo.FinalTransform)
-            boneInfos
+            // write bone transforms to bone infos array
+            let rootTransformInverse = scene.RootNode.Transform
+            rootTransformInverse.Inverse ()
+            UpdateBoneTransforms (boneIds, boneInfos, animationTime, animationIndex, scene.RootNode, Assimp.Matrix4x4.Identity, rootTransformInverse, scene)
+
+            // convert bone info transforms to Nu's m4 representation
+            Array.map (fun (boneInfo : BoneInfo) -> AssimpToNu boneInfo.TransformWorld) boneInfos
+
+    /// Node extensions.
+    type Assimp.Node with
+
+        /// Get the world transform of the node.
+        member this.TransformWorld =
+            let mutable parentOpt = this.Parent
+            let mutable transform = this.Transform
+            while notNull parentOpt do
+                transform <- transform * parentOpt.Transform
+                parentOpt <- parentOpt.Parent
+            transform
+
+        /// Collect all the child nodes of a node, including the node itself.
+        member this.CollectNodes () =
+            seq {
+                yield this
+                for child in this.Children do
+                    yield! child.CollectNodes () }
+
+        /// Collect all the child nodes and transforms of a node, including the node itself.
+        member this.CollectNodesAndTransforms (unitType, parentTransform : Matrix4x4) =
+            seq {
+                let localTransform = AssimpToNu this.Transform
+                let worldTransform = localTransform * parentTransform
+                yield (this, worldTransform)
+                for child in this.Children do
+                    yield! child.CollectNodesAndTransforms (unitType, worldTransform) }
+
+        /// Map to a TreeNode.
+        member this.Map<'a> (parentNames : string array, parentTransform : Matrix4x4, mapper : Assimp.Node -> string array -> Matrix4x4 -> 'a array TreeNode) : 'a array TreeNode =
+            let localName = this.Name
+            let localTransform = AssimpToNu this.Transform
+            let worldNames = Array.append parentNames [|localName|]
+            let worldTransform = localTransform * parentTransform
+            let node = mapper this worldNames worldTransform
+            for child in this.Children do
+                let child = child.Map<'a> (worldNames, worldTransform, mapper)
+                node.Add child
+            node
