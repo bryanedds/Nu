6f121dd2b1706bb4d82056f713a6b2db8aafb2a7
bryanedds <bryanedds@gmail.com>
2023-11-22 19:56:24 -0500

    Enabled character to walk up some slopes.
    Former-commit-id: 38ad97a80e7d6e517383f9cc360c350870e8cb12

--- Projects/TerrainDemo/Character.fs
+++ Projects/TerrainDemo/Character.fs
@@ -28,24 +28,54 @@ module CharacterDispatcher =
              define Entity.BodyShape (BodyCapsule { Height = 1.0f; Radius = 0.3f; TransformOpt = Some (Affine.makeTranslation (v3 0.0f 0.8f 0.0f)); PropertiesOpt = None })]
 
         override this.Update (entity, world) =
+
+            // disable gravity when grounded
             let bodyId = entity.GetBodyId world
+            let grounded = World.getBodyGrounded bodyId world
+            let world =
+                if grounded
+                then entity.SetGravityOverride (Some v3Zero) world
+                else entity.SetGravityOverride None world
+
+            // apply movement forces
             let position = entity.GetPosition world
             let rotation = entity.GetRotation world
             let forward = rotation.Forward
             let right = rotation.Right
-            let grounded = World.getBodyGrounded bodyId world
-            let walkForce = if grounded then 1.0f else 0.5f
+            let contactNormalOpt = World.getBodyToGroundContactNormalOpt bodyId world
+            let walkForceScalar = if grounded then 1.25f else 0.625f
+            let walkForce = 
+                (if World.isKeyboardKeyDown KeyboardKey.Up world then forward * walkForceScalar else v3Zero) +
+                (if World.isKeyboardKeyDown KeyboardKey.Down world then -forward * walkForceScalar else v3Zero) +
+                (if World.isKeyboardKeyDown KeyboardKey.A world then -right * walkForceScalar else v3Zero) +
+                (if World.isKeyboardKeyDown KeyboardKey.D world then right * walkForceScalar else v3Zero)
+            let world =
+                match contactNormalOpt with
+                | Some contactUp when walkForce <> v3Zero ->
+                    let walkForward = walkForce.Normalized
+                    let groundPlane = Plane3 (contactUp, -1.0f)
+                    let slope = Vector3.Project (walkForward, groundPlane) - v3Up
+                    let walkForceOriented =
+                        if Vector3.Dot (slope, v3Up) >= 0.2f then
+                            let angleBetween = walkForward.AngleBetween slope
+                            let rotationMatrix = Matrix4x4.CreateFromAxisAngle (Vector3.Cross (walkForward, v3Up), angleBetween)
+                            let walkForceOriented = Vector3.Transform (walkForce, rotationMatrix)
+                            Log.info (scstring walkForce + " " + scstring walkForceOriented + " " + scstring angleBetween)
+                            walkForceOriented
+                        else walkForce
+                    World.applyBodyForce walkForceOriented v3Zero bodyId world
+                | Some _ | None -> World.applyBodyForce walkForce v3Zero bodyId world
+
+            // apply turn force
             let turnForce = if grounded then 1.0f else 0.5f
-            let jumpForce = 0.4f
-            let world = if World.isKeyboardKeyDown KeyboardKey.Up world then World.applyBodyForce (forward * walkForce) v3Zero bodyId world else world
-            let world = if World.isKeyboardKeyDown KeyboardKey.Down world then World.applyBodyForce (-forward * walkForce) v3Zero bodyId world else world
-            let world = if World.isKeyboardKeyDown KeyboardKey.A world then World.applyBodyForce (-right * walkForce) v3Zero bodyId world else world
-            let world = if World.isKeyboardKeyDown KeyboardKey.D world then World.applyBodyForce (right * walkForce) v3Zero bodyId world else world
             let world = if World.isKeyboardKeyDown KeyboardKey.Right world then World.applyBodyTorque (-v3Up * turnForce) bodyId world else world
             let world = if World.isKeyboardKeyDown KeyboardKey.Left world then World.applyBodyTorque (v3Up * turnForce) bodyId world else world
+
+            // apply jump force
+            let jumpForce = 0.4f
             let world = if World.isKeyboardKeyDown KeyboardKey.Space world && grounded then World.applyBodyLinearImpulse (v3Up * jumpForce) v3Zero bodyId world else world
-            let world = World.setEyeRotation3d rotation world
-            let world = World.setEyeCenter3d (position + v3Up * 1.5f - rotation.Forward * 3.0f) world
+            
+            // apply physics-based animations
             let linearVelocity = World.getBodyLinearVelocity bodyId world
             let angularVelocity = World.getBodyAngularVelocity bodyId world
             let forwardness = (Vector3.Dot (linearVelocity, rotation.Forward))
@@ -72,4 +102,8 @@ module CharacterDispatcher =
                     animations
                 //else animations
             let world = entity.SetAnimations (List.toArray animations) world
+
+            // orient camera            
+            let world = World.setEyeRotation3d rotation world
+            let world = World.setEyeCenter3d (position + v3Up * 1.5f - rotation.Forward * 3.0f) world
             world
