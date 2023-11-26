444a6e78985f3ef69cbd908faba2e10a8a286c24
bryanedds <bryanedds@gmail.com>
2023-11-26 15:32:21 -0500

    Removed likely incorrect presumption in pbr lighting code.
    Former-commit-id: 7b6ee8f554fa2c0ee51294d6f8254b2a8cffb97b

--- Projects/Metrics/Assets/Default/PhysicallyBasedDeferredLighting.glsl
+++ Projects/Metrics/Assets/Default/PhysicallyBasedDeferredLighting.glsl
@@ -80,12 +80,12 @@ float geometrySchlick(vec3 n, vec3 v, vec3 l, float roughness)
 
 vec3 fresnelSchlick(float cosTheta, vec3 f0)
 {
-    return f0 + (1.0 - f0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), REFLECTION_LOD_MAX);
+    return f0 + (1.0 - f0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
 }
 
 vec3 fresnelSchlickRoughness(float cosTheta, vec3 f0, float roughness)
 {
-    return f0 + (max(vec3(1.0 - roughness), f0) - f0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), REFLECTION_LOD_MAX);
+    return f0 + (max(vec3(1.0 - roughness), f0) - f0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
 }
 
 void main()
