93f6671062d91a43358ff7268e888437dcb6acbf
bryanedds <bryanedds@gmail.com>
2023-12-14 11:03:56 -0500

    Padding fix.

--- Nu/Nu.Gaia/Assets/Default/PhysicallyBasedForwardStatic.glsl
+++ Nu/Nu.Gaia/Assets/Default/PhysicallyBasedForwardStatic.glsl
@@ -103,6 +103,7 @@ uniform float lightConeInners[LIGHTS_MAX];
 uniform float lightConeOuters[LIGHTS_MAX];
 uniform int lightShadowIndices[LIGHTS_MAX];
 uniform int lightsCount;
+uniform mat4 shadowMatrices[SHADOWS_MAX];
 
 in vec4 positionOut;
 in vec2 texCoordsOut;
@@ -220,6 +221,21 @@ void main()
     vec3 lightAccum = vec3(0.0);
     for (int i = 0; i < lightsCount; ++i)
     {
+        // compute shadow factor
+        int ShadowIndex = lightShadowIndices[i];
+        float ShadowFactor = 1.0;
+        if (ShadowIndex > -1)
+        {
+            vec4 LightSpacePos = shadowMatrices[ShadowIndex] * vec4(position, 1.0);
+            vec3 ProjCoords = LightSpacePos.xyz / LightSpacePos.w;
+            vec2 UVCoords;
+            UVCoords.x = 0.5 * ProjCoords.x + 0.5;
+            UVCoords.y = 0.5 * ProjCoords.y + 0.5;
+            float Z = 0.5 * ProjCoords.z + 0.5;
+            float Depth = texture(shadowTextures[ShadowIndex], UVCoords).r;
+            ShadowFactor = Depth < Z + 0.00001 ? 0.25 : 1.0;
+        }
+
         // per-light radiance
         vec3 d = lightOrigins[i] - position;
         float distanceSquared = dot(d, d);
@@ -268,7 +284,7 @@ void main()
         float nDotL = max(dot(n, l), 0.0);
 
         // add to outgoing lightAccum
-        lightAccum += (kD * albedo.rgb / PI + specular) * radiance * nDotL;
+        lightAccum += (kD * albedo.rgb / PI + specular) * radiance * nDotL * ShadowFactor;
     }
 
     // determine light map indices, including their validity
