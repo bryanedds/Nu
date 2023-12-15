f81836e646e7a3a6d3bf490ea36d27f4c2c968e6
bryanedds <bryanedds@gmail.com>
2023-12-15 16:00:08 -0500

    More simplification.
    Former-commit-id: 2810b63984d8c469e95b6b3539886bf936bcaf45

--- Nu/Nu.Gaia/Assets/Default/PhysicallyBasedShadowAnimated.glsl
+++ Nu/Nu.Gaia/Assets/Default/PhysicallyBasedShadowAnimated.glsl
@@ -1,27 +1,9 @@
 #shader vertex
 #version 410 core
 
-const int TEX_COORDS_OFFSET_VERTS = 6;
 const int BONES_MAX = 128;
 const int BONES_INFLUENCE_MAX = 4;
 
-const vec2 TEX_COORDS_OFFSET_FILTERS[TEX_COORDS_OFFSET_VERTS] =
-    vec2[TEX_COORDS_OFFSET_VERTS](
-        vec2(1,1),
-        vec2(0,1),
-        vec2(0,0),
-        vec2(1,1),
-        vec2(0,0),
-        vec2(1,0));
-
-const vec2 TEX_COORDS_OFFSET_FILTERS_2[TEX_COORDS_OFFSET_VERTS] =
-    vec2[TEX_COORDS_OFFSET_VERTS](
-        vec2(0,0),
-        vec2(1,0),
-        vec2(1,1),
-        vec2(0,0),
-        vec2(1,1),
-        vec2(0,1));
 
 uniform mat4 view;
 uniform mat4 projection;
@@ -29,13 +11,10 @@ uniform mat4 bones[BONES_MAX];
 
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec2 texCoords;
-layout (location = 2) in vec3 normal;
 layout (location = 3) in vec4 boneIds;
 layout (location = 4) in vec4 weights;
 layout (location = 5) in mat4 model;
-layout (location = 9) in vec4 texCoordsOffset;
 layout (location = 10) in vec4 albedo;
-layout (location = 12) in float height;
 
 out vec2 texCoordsOut;
 flat out vec4 albedoOut;
