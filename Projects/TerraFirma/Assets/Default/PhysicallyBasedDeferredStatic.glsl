bd437b4a5284815aaab7279fa766e0e19f691838
bryanedds <bryanedds@gmail.com>
2023-12-18 22:12:09 -0500

    Propadated assets.
    Former-commit-id: 3f270cfe2c9d8777f2ff353674e1ba1e3595feb1

--- Projects/TerraFirma/Assets/Default/PhysicallyBasedDeferredStatic.glsl
+++ Projects/TerraFirma/Assets/Default/PhysicallyBasedDeferredStatic.glsl
@@ -23,22 +23,16 @@ const vec2 TEX_COORDS_OFFSET_FILTERS_2[TEX_COORDS_OFFSET_VERTS] =
 
 uniform mat4 view;
 uniform mat4 projection;
+uniform vec4 texCoordsOffset;
 
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec2 texCoords;
 layout (location = 2) in vec3 normal;
 layout (location = 3) in mat4 model;
-layout (location = 7) in vec4 texCoordsOffset;
-layout (location = 8) in vec4 albedo;
-layout (location = 9) in vec4 material;
-layout (location = 10) in float height;
 
 out vec4 positionOut;
 out vec2 texCoordsOut;
 out vec3 normalOut;
-flat out vec4 albedoOut;
-flat out vec4 materialOut;
-flat out float heightOut;
 
 void main()
 {
@@ -47,10 +41,7 @@ void main()
     vec2 texCoordsOffsetFilter = TEX_COORDS_OFFSET_FILTERS[texCoordsOffsetIndex];
     vec2 texCoordsOffsetFilter2 = TEX_COORDS_OFFSET_FILTERS_2[texCoordsOffsetIndex];
     texCoordsOut = texCoords + texCoordsOffset.xy * texCoordsOffsetFilter + texCoordsOffset.zw * texCoordsOffsetFilter2;
-    albedoOut = albedo;
-    materialOut = material;
     normalOut = transpose(inverse(mat3(model))) * normal;
-    heightOut = height;
     gl_Position = projection * view * positionOut;
 }
 
@@ -60,6 +51,9 @@ void main()
 const float GAMMA = 2.2;
 
 uniform vec3 eyeCenter;
+uniform vec4 albedo;
+uniform vec4 material;
+uniform float height;
 uniform sampler2D albedoTexture;
 uniform sampler2D roughnessTexture;
 uniform sampler2D metallicTexture;
@@ -71,13 +65,10 @@ uniform sampler2D heightTexture;
 in vec4 positionOut;
 in vec2 texCoordsOut;
 in vec3 normalOut;
-flat in vec4 albedoOut;
-flat in vec4 materialOut;
-flat in float heightOut;
 
 layout (location = 0) out vec4 position;
-layout (location = 1) out vec3 albedo;
-layout (location = 2) out vec4 material;
+layout (location = 1) out vec3 albedo_;
+layout (location = 2) out vec4 material_;
 layout (location = 3) out vec4 normalAndHeight;
 
 void main()
@@ -100,21 +91,21 @@ void main()
     vec3 eyeCenterTangent = toTangent * eyeCenter;
     vec3 positionTangent = toTangent * positionOut.xyz;
     vec3 toEyeTangent = normalize(eyeCenterTangent - positionTangent);
-    float height = texture(heightTexture, texCoordsOut).r * heightOut;
+    float height = texture(heightTexture, texCoordsOut).r * height;
     vec2 parallax = toEyeTangent.xy * height;
     vec2 texCoords = texCoordsOut - parallax;
 
     // compute albedo, discarding on zero alpha
     vec4 albedoSample = texture(albedoTexture, texCoords);
     if (albedoSample.a == 0.0f) discard;
-    albedo = pow(albedoSample.rgb, vec3(GAMMA)) * albedoOut.rgb;
+    albedo_ = pow(albedoSample.rgb, vec3(GAMMA)) * albedo.rgb;
 
     // compute material properties
-    float roughness = texture(roughnessTexture, texCoords).r * materialOut.r;
-    float metallic = texture(metallicTexture, texCoords).g * materialOut.g;
-    float ambientOcclusion = texture(ambientOcclusionTexture, texCoords).b * materialOut.b;
-    float emission = texture(emissionTexture, texCoords).r * materialOut.a;
-    material = vec4(roughness, metallic, ambientOcclusion, emission);
+    float roughness = texture(roughnessTexture, texCoords).r * material.r;
+    float metallic = texture(metallicTexture, texCoords).g * material.g;
+    float ambientOcclusion = texture(ambientOcclusionTexture, texCoords).b * material.b;
+    float emission = texture(emissionTexture, texCoords).r * material.a;
+    material_ = vec4(roughness, metallic, ambientOcclusion, emission);
 
     // compute normal and height
     normalAndHeight.xyz = normalize(toWorld * (texture(normalTexture, texCoords).xyz * 2.0 - 1.0));
