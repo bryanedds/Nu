6e4afae6c5b9dbac023113482d14b1c48ef1d8b8
bryanedds <bryanedds@gmail.com>
2023-12-22 11:18:44 -0500

    Reduced shadow biasing.

--- Nu/Nu.Gaia/Assets/Default/PhysicallyBasedDeferredLighting.glsl
+++ Nu/Nu.Gaia/Assets/Default/PhysicallyBasedDeferredLighting.glsl
@@ -161,7 +161,7 @@ void main()
             if (shadowZ < 1.0f && shadowTexCoords.x >= 0.0 && shadowTexCoords.x <= 1.0 && shadowTexCoords.y >= 0.0 && shadowTexCoords.y <= 1.0)
             {
                 float depth = texture(shadowTextures[shadowIndex], shadowTexCoords).r;
-                float biasFloor = lightDirectionals[i] == 0 ? 0.0005 : 0.002;
+                float biasFloor = lightDirectionals[i] == 0 ? 0.0001 : 0.001;
                 float bias = max(biasFloor * (1.0 - dot(normal, l)), biasFloor);
                 shadowScalar = depth + bias < shadowZ ? 0.0 : 1.0;
             }
