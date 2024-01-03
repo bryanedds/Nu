f7739d24c7c922c5fdb3bdbd1b803ad545bac6a2
bryanedds <bryanedds@gmail.com>
2024-01-03 18:55:18 -0500

    Lighting adjustments.
    Former-commit-id: b851b666e7beb8674fe00ac09000ef84ade1d467

--- Projects/Nelmish/Assets/Default/PhysicallyBasedForwardStatic.glsl
+++ Projects/Nelmish/Assets/Default/PhysicallyBasedForwardStatic.glsl
@@ -260,7 +260,7 @@ void main()
             if (shadowZ < 1.0f && shadowTexCoords.x >= 0.0 && shadowTexCoords.x <= 1.0 && shadowTexCoords.y >= 0.0 && shadowTexCoords.y <= 1.0)
             {
                 float depth = texture(shadowTextures[shadowIndex], shadowTexCoords).r;
-                float biasFloor = lightDirectionals[i] == 0 ? 0.00025 : 0.001;
+                float biasFloor = 0.00025;
                 float bias = max(biasFloor * (1.0 - dot(normal, l)), biasFloor);
                 shadowScalar = depth + bias < shadowZ ? 0.0 : 1.0;
             }
