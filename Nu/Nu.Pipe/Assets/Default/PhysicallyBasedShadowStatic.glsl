709b856aab82c07656b0da3c4601163b4aa7d344
bryanedds <bryanedds@gmail.com>
2024-01-03 00:42:46 -0500

    Cleaned up shader code further.

--- Nu/Nu.Pipe/Assets/Default/PhysicallyBasedShadowStatic.glsl
+++ Nu/Nu.Pipe/Assets/Default/PhysicallyBasedShadowStatic.glsl
@@ -5,7 +5,6 @@ uniform mat4 view;
 uniform mat4 projection;
 
 layout (location = 0) in vec3 position;
-layout (location = 1) in vec2 texCoords;
 layout (location = 3) in mat4 model;
 
 out vec4 positionOut;
@@ -14,14 +13,11 @@ out vec2 texCoordsOut;
 void main()
 {
     vec4 positionWorld = model * vec4(position, 1.0);
-    texCoordsOut = texCoords;
     gl_Position = projection * view * positionWorld;
 }
 
 #shader fragment
 #version 410 core
-
-in vec2 texCoordsOut;
 
 void main()
 {
