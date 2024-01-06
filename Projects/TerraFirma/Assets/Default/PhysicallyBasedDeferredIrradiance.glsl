e33713fa8de2e5e755c6d3d3e21bdc2af8f51427
bryanedds <bryanedds@gmail.com>
2024-01-06 00:16:48 -0500

    Propagated assets.

--- Projects/TerraFirma/Assets/Default/PhysicallyBasedDeferredIrradiance.glsl
+++ Projects/TerraFirma/Assets/Default/PhysicallyBasedDeferredIrradiance.glsl
@@ -19,20 +19,51 @@ void main()
 const float PI = 3.141592654;
 const int LIGHT_MAPS_MAX = 27;
 
+layout (bindless_sampler) uniform sampler2D positionTexture;
 layout (bindless_sampler) uniform sampler2D normalPlusTexture;
 layout (bindless_sampler) uniform sampler2D lightMappingTexture;
 layout (bindless_sampler) uniform samplerCube irradianceMap;
 layout (bindless_sampler) uniform samplerCube irradianceMaps[LIGHT_MAPS_MAX];
+uniform vec3 lightMapOrigins[LIGHT_MAPS_MAX];
+uniform vec3 lightMapMins[LIGHT_MAPS_MAX];
+uniform vec3 lightMapSizes[LIGHT_MAPS_MAX];
 
 in vec2 texCoordsOut;
 
 out vec4 frag;
+
+vec2 rayBoxIntersectionRatios(vec3 rayOrigin, vec3 rayDirection, vec3 boxMin, vec3 boxSize)
+{
+    vec3 rayDirectionInv = vec3(1.0) / rayDirection;
+    vec3 boxMax = boxMin + boxSize;
+    vec3 t1 = (boxMin - rayOrigin) * rayDirectionInv;
+    vec3 t2 = (boxMax - rayOrigin) * rayDirectionInv;
+    vec3 tMin = min(t1, t2);
+    vec3 tMax = max(t1, t2);
+    float tEnter = max(max(tMin.x / boxSize.x, tMin.y / boxSize.y), tMin.z / boxSize.z);
+    float tExit = min(min(tMax.x / boxSize.x, tMax.y / boxSize.y), tMax.z / boxSize.z);
+    return tEnter < tExit ? vec2(tEnter, tExit) : vec2(0.0);
+}
+
+float computeDepthRatio(vec3 position, vec3 normal, vec3 minA, vec3 sizeA, vec3 minB, vec3 sizeB)
+{
+    vec3 centerA = minA + sizeA * 0.5;
+    vec3 centerB = minB + sizeB * 0.5;
+    vec3 direction = normalize(cross(cross(centerB - centerA, normal), normal));
+    vec3 intersectionMin = max(minA, minB);
+    vec3 intersectionSize = min(minA + sizeA, minB + sizeB) - intersectionMin;
+    vec2 intersectionRatios = rayBoxIntersectionRatios(position, direction, intersectionMin, intersectionSize);
+    return intersectionRatios != vec2(0.0) ? intersectionRatios.y / (intersectionRatios.y - intersectionRatios.x) : 0.5;
+}
 
 void main()
 {
     // retrieve normal and height values first, allowing for early-out
     vec3 normal = texture(normalPlusTexture, texCoordsOut).xyz;
     if (normal == vec3(1.0)) discard; // discard if geometry pixel was not written (equal to the buffer clearing color of white)
+
+    // retrieve remaining data from geometry buffers
+    vec3 position = texture(positionTexture, texCoordsOut).xyz;
 
     // retrieve light mapping data
     vec4 lmData = texture(lightMappingTexture, texCoordsOut);
@@ -54,13 +85,10 @@ void main()
     else
     {
         // compute blended irradiance
-        float distanceTotal = lm1Distance + lm2Distance;
-        float distanceTotalInverse = 1.0 / distanceTotal;
-        float scalar1 = (distanceTotal - lm1Distance) * distanceTotalInverse;
-        float scalar2 = (distanceTotal - lm2Distance) * distanceTotalInverse;
+        float ratio = computeDepthRatio(position, normal, lightMapMins[lm1], lightMapSizes[lm1], lightMapMins[lm2], lightMapSizes[lm2]);
         vec3 irradiance1 = texture(irradianceMaps[lm1], normal).rgb;
         vec3 irradiance2 = texture(irradianceMaps[lm2], normal).rgb;
-        irradiance = irradiance1 * scalar1 + irradiance2 * scalar2;
+        irradiance = mix(irradiance1, irradiance2, ratio);
     }
 
     // write
