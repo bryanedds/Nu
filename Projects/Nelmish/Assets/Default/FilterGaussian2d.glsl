07aaee1e35bc8a37438140a66803e3c51daad352
bryanedds <bryanedds@gmail.com>
2024-01-07 16:53:34 -0500

    VSM blurring in.
    Former-commit-id: 51943265bb13df4842334392620386f220323a56

---
+++ Projects/Nelmish/Assets/Default/FilterGaussian2d.glsl
@@ -0,0 +1,37 @@
+#shader vertex
+#version 410 core
+
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec2 texCoords;
+
+out vec2 texCoordsOut;
+
+void main()
+{
+    texCoordsOut = texCoords;
+    gl_Position = vec4(position, 1.0);
+}
+
+#shader fragment
+#version 410 core
+#extension GL_ARB_bindless_texture : require
+
+in vec2 texCoordsOut;
+
+out vec2 frag;
+
+uniform vec2 scale;
+layout (bindless_sampler) uniform sampler2D inputTexture;
+
+void main()
+{
+    vec2 moments =
+        texture(inputTexture, texCoordsOut + vec2(-3.0) * scale).xy * (1.0 / 64.0) +
+        texture(inputTexture, texCoordsOut + vec2(-2.0) * scale).xy * (6.0 / 64.0) +
+        texture(inputTexture, texCoordsOut + vec2(-1.0) * scale).xy * (15.0 / 64.0) +
+        texture(inputTexture, texCoordsOut + vec2(0.0) * scale).xy * (20.0 / 64.0) +
+        texture(inputTexture, texCoordsOut + vec2(1.0) * scale).xy * (15.0 / 64.0) +
+        texture(inputTexture, texCoordsOut + vec2(2.0) * scale).xy * (6.0 / 64.0) +
+        texture(inputTexture, texCoordsOut + vec2(3.0) * scale).xy * (1.0 / 64.0);
+    frag = moments;
+}
