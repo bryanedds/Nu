d80b8b146964ce71e1422f9291f02f926946c874
bryanedds <bryanedds@gmail.com>
2024-01-11 17:58:30 -0500

    Started outlining structure of job algebra.

---
+++ Nu/Nu/Collections/JobAgency.fs
@@ -0,0 +1,33 @@
+﻿namespace Nu
+open System
+open System.Collections.Concurrent
+open Prime
+open Nu
+
+type JobId =
+    obj
+
+type JobPriority =
+    single
+
+type JobResult =
+    | JobSuccess of DateTimeOffset * obj
+    | JobFailure of DateTimeOffset * Exception
+
+type Job =
+    { JobId : JobId
+      Issuance : DateTimeOffset
+      Work : unit -> JobResult }
+
+type JobQueue =
+    ConcurrentPriorityQueue<JobPriority, Job>
+
+type JobResults =
+    ConcurrentDictionary<JobId, JobResult>
+
+type JobQueryResult =
+    | JobResult of JobResult
+    | JobTimeout
+
+type JobQuery =
+    TimeSpan -> JobId -> JobQueryResult
