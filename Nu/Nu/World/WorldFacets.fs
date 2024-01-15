d86e76adf4d8fc43ef0e64314e5af2bcad9e7165
bryanedds <bryanedds@gmail.com>
2024-01-15 18:29:18 -0500

    Lowered animation await time.

--- Nu/Nu/World/WorldFacets.fs
+++ Nu/Nu/World/WorldFacets.fs
@@ -2656,7 +2656,7 @@ module AnimatedModelFacetModule =
             let animatedModel = entity.GetAnimatedModel world
             let sceneOpt = match Metadata.tryGetAnimatedModelMetadata animatedModel with Some model -> model.SceneOpt | None -> None
             let boneTransformsOpt =
-                match World.tryAwaitJob (world.DateTime + TimeSpan.FromSeconds 0.1) (entity, nameof AnimatedModelFacet) world with
+                match World.tryAwaitJob (world.DateTime + TimeSpan.FromSeconds 0.001) (entity, nameof AnimatedModelFacet) world with
                 | Some (JobCompletion (_, _, (:? (Matrix4x4 array option) as boneTransformsOpt))) -> boneTransformsOpt
                 | _ -> None
             let world =
