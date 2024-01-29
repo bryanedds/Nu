05f570eedbfab1b685ef3196689745b9ac90ff1b
bryanedds <bryanedds@gmail.com>
2024-01-28 22:32:13 -0500

    Updating sdl mixer files.
    Former-commit-id: 9b3535ad1918674ba10a8a6c2f6a2c78a791be99

--- Configure.Linux.Development.sh
+++ Configure.Linux.Development.sh
@@ -6,7 +6,7 @@ sudo apt update
 sudo apt install \
   libsdl2-dev libsdl2-2.0-0 \
   libjpeg-dev libwebp-dev libtiff5-dev libsdl2-image-dev libsdl2-image-2.0-0 \
-  libmikmod-dev libfishsound1-dev libsmpeg-dev liboggz2-dev libflac-dev libfluidsynth-dev libsdl2-mixer-dev libsdl2-mixer-2.0-0 \
+  libmikmod-dev libfishsound1-dev libsmpeg-dev liboggz2-dev libflac-dev libfluidsynth-dev libsdl2-mixer-dev libsdl2-mixer-2.8-0 \
   libfreetype6-dev libsdl2-ttf-dev libsdl2-ttf-2.22-0
 
 # configure .net
