5f8590b83796b04ea0e86ea5120982b1273f9830
bryanedds <bryanedds@gmail.com>
2024-04-04 09:51:35 -0400

    Adjusting metrics to new GC config.
    Former-commit-id: 9cbf1bf44babd31870826f9743ea7347922c1a12

--- Projects/Metrics/Program.fs
+++ Projects/Metrics/Program.fs
@@ -39,10 +39,10 @@ type MyGameDispatcher () =
         let (fps, world) = World.createEntity<FpsDispatcher> DefaultOverlay (Some [|"Fps"|]) group world
         let world = World.createEntity<SkyBoxDispatcher> DefaultOverlay None group world |> snd
         let world = fps.SetPosition (v3 134.0f -168.0f 0.0f) world
-        let positions = // 50,000 entities (goal: 60FPS, current 47FPS)
+        let positions = // 25,000 entities (goal: 60FPS, current 60FPS)
             [|for i in 0 .. dec 50 do
                 for j in 0 .. dec 50 do
-                    for k in 0 .. dec 20 do
+                    for k in 0 .. dec 10 do
                         yield v3 (single i * 0.5f) (single j * 0.5f) (single k * 0.5f)|]
         let world =
             Array.fold (fun world position ->
@@ -75,7 +75,7 @@ type Message =
     interface Nu.Message
 
 type MmccGameDispatcher () =
-    inherit GameDispatcher<Intss, Message, Command> (Intss.init 118) // 13,924 MMCC entities (goal: 60FPS, current: 59FPS)
+    inherit GameDispatcher<Intss, Message, Command> (Intss.init 105) // 11,025 MMCC entities (goal: 60FPS, current: 59FPS)
 
     override this.Definitions (_, _) =
         [Game.UpdateEvent => Inc]
