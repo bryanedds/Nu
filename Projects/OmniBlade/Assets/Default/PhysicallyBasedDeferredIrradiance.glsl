e5dec2cdb5ad6893c9bb7ad127d3c2c171606111
bryanedds <bryanedds@gmail.com>
2024-04-13 20:15:30 -0400

    Propagated assets.
    Former-commit-id: ecc78f545fa4e610af47e62df72d1e655d5a2296

--- Projects/OmniBlade/Assets/Default/PhysicallyBasedDeferredIrradiance.glsl
+++ Projects/OmniBlade/Assets/Default/PhysicallyBasedDeferredIrradiance.glsl
@@ -32,30 +32,6 @@ in vec2 texCoordsOut;
 
 layout (location = 0) out vec4 frag;
 
-vec2 rayBoxIntersectionRatios(vec3 rayOrigin, vec3 rayDirection, vec3 boxMin, vec3 boxSize)
-{
-    vec3 rayDirectionInv = vec3(1.0) / rayDirection;
-    vec3 boxMax = boxMin + boxSize;
-    vec3 t1 = (boxMin - rayOrigin) * rayDirectionInv;
-    vec3 t2 = (boxMax - rayOrigin) * rayDirectionInv;
-    vec3 tMin = min(t1, t2);
-    vec3 tMax = max(t1, t2);
-    float tEnter = max(max(tMin.x / boxSize.x, tMin.y / boxSize.y), tMin.z / boxSize.z);
-    float tExit = min(min(tMax.x / boxSize.x, tMax.y / boxSize.y), tMax.z / boxSize.z);
-    return tEnter < tExit ? vec2(tEnter, tExit) : vec2(0.0);
-}
-
-float computeDepthRatio(vec3 minA, vec3 sizeA, vec3 minB, vec3 sizeB, vec3 position, vec3 normal)
-{
-    vec3 centerA = minA + sizeA * 0.5;
-    vec3 centerB = minB + sizeB * 0.5;
-    vec3 direction = normalize(cross(cross(centerB - centerA, normal), normal));
-    vec3 intersectionMin = max(minA, minB);
-    vec3 intersectionSize = min(minA + sizeA, minB + sizeB) - intersectionMin;
-    vec2 intersectionRatios = rayBoxIntersectionRatios(position, direction, intersectionMin, intersectionSize);
-    return intersectionRatios != vec2(0.0) ? intersectionRatios.y / (intersectionRatios.y - intersectionRatios.x) : 0.5;
-}
-
 void main()
 {
     // retrieve normal and height values first, allowing for early-out
@@ -69,8 +45,7 @@ void main()
     vec4 lmData = texture(lightMappingTexture, texCoordsOut);
     int lm1 = int(lmData.r) - 1;
     int lm2 = int(lmData.g) - 1;
-    float lm1Distance = lmData.b;
-    float lm2Distance = lmData.a;
+    float lmRatio = lmData.b;
 
     // compute irradiance terms
     vec3 irradiance = vec3(0.0);
@@ -85,10 +60,9 @@ void main()
     else
     {
         // compute blended irradiance
-        float ratio = computeDepthRatio(lightMapMins[lm1], lightMapSizes[lm1], lightMapMins[lm2], lightMapSizes[lm2], position, normal);
         vec3 irradiance1 = texture(irradianceMaps[lm1], normal).rgb;
         vec3 irradiance2 = texture(irradianceMaps[lm2], normal).rgb;
-        irradiance = mix(irradiance1, irradiance2, ratio);
+        irradiance = mix(irradiance1, irradiance2, lmRatio);
     }
 
     // write
