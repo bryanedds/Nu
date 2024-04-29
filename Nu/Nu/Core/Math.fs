3ce400f38a1f90fb4fe15b7c36bd1a2a77465d88
bryanedds <bryanedds@gmail.com>
2024-04-29 17:54:01 -0400

    Moved OrdDict ext into Math.fs in anticpation of moving other collection types into Prime.
    Former-commit-id: c073b4e3ea3d1b26c9307e9142e0694d2eea7f8c

--- Nu/Nu/Core/Math.fs
+++ Nu/Nu/Core/Math.fs
@@ -3,6 +3,7 @@
 
 namespace Nu
 open System
+open System.Collections.Generic
 open System.ComponentModel
 open System.Globalization
 open System.Numerics
@@ -1133,6 +1134,31 @@ type [<Struct>] Affine =
 
     static member Identity =
         Affine.make v3Zero quatIdentity v3One
+
+[<RequireQualifiedAccess>]
+module OrderedDictionary =
+
+    /// Make a dictionary with a single entry.
+    let inline singleton (comparer : KeyValuePair<'k, 'v> IEqualityComparer) key value =
+        let dictionary = OrderedDictionary comparer
+        dictionary.Add (key, value)
+        dictionary
+
+    /// Map over an ordered dictionary. A new ordered dictionary is produced.
+    let map (mapper : KeyValuePair<'k, 'v> -> 'v) (dictionary : OrderedDictionary<'k, 'v>) =
+        let result = Dictionary<'k, 'v> dictionary.Comparer
+        for kvp in dictionary do result.Add (kvp.Key, mapper kvp)
+        result
+
+    /// Fold over an ordered dictionary.
+    let fold<'s, 'k, 'v> folder (state : 's) (dictionary : OrderedDictionary<'k, 'v>) =
+        let folder = OptimizedClosures.FSharpFunc<_, _, _, _>.Adapt folder
+        let mutable state = state
+        let mutable enr = dictionary.GetEnumerator ()
+        while enr.MoveNext () do
+            let kvp = enr.Current
+            state <- folder.Invoke (state, kvp.Key, kvp.Value)
+        state
 
 /// The flipness of an image.
 type [<Struct>] Flip =
