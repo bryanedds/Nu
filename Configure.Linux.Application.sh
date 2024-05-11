c6b4a5de284899fa2ef9d3bfdda6f5bed06a293a
bryanedds <bryanedds@gmail.com>
2024-05-11 14:37:57 -0400

    Downgraded sdl files needed for linux install.
    Former-commit-id: ff15b59d03a8cde6111f55d2673560bf62f95bcd

--- Configure.Linux.Application.sh
+++ Configure.Linux.Application.sh
@@ -4,10 +4,10 @@ echo Configuring Linux for Nu application...
 # configure sdl2 and components
 sudo apt update
 sudo apt install \
-  libsdl2-dev libsdl2-2.28-0 \
-  libjpeg-dev libwebp-dev libtiff5-dev libsdl2-image-dev libsdl2-image-2.8-0 \
-  libmikmod-dev libfishsound1-dev libsmpeg-dev liboggz2-dev libflac-dev libfluidsynth-dev libsdl2-mixer-dev libsdl2-mixer-2.8-0 \
-  libfreetype6-dev libsdl2-ttf-dev libsdl2-ttf-2.22-0
+  libsdl2-dev libsdl2-2.0-0 \
+  libjpeg-dev libwebp-dev libtiff5-dev libsdl2-image-dev libsdl2-image-2.0-0 \
+  libmikmod-dev libfishsound1-dev libsmpeg-dev liboggz2-dev libflac-dev libfluidsynth-dev libsdl2-mixer-dev libsdl2-mixer-2.0-0 \
+  libfreetype6-dev libsdl2-ttf-dev libsdl2-ttf-2.0-0
 
 # configure .net
 # if this reports that the package can't be found, try the advice here - https://dev.to/solrevdev/unable-to-locate-package-dotnet-sdk-3-1-4b6
