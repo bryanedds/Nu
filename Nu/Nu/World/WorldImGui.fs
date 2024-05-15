e24dd288a7f2adc8077e2a0767654d77e90a9fd0
bryanedds <bryanedds@gmail.com>
2024-05-15 12:00:15 -0400

    Added 2d imgui drawing API.
    Former-commit-id: f34ea3e858326eef3a45d9fedc4a51adbe3c0ce0

--- Nu/Nu/World/WorldImGui.fs
+++ Nu/Nu/World/WorldImGui.fs
@@ -15,6 +15,43 @@ module WorldImGui =
         static member internal getImGui world =
             world.Subsystems.ImGui
 
+        /// Render circles via ImGui in the current eye 2d space, computing color as specified.
+        static member imGuiCircles2dPlus absolute (positions : Vector2 seq) radius filled (computeColor : Vector2 -> Color) world =
+            let drawList = ImGui.GetBackgroundDrawList ()
+            let eyeCenter = World.getEye2dCenter world
+            for position in positions do
+                let positionWindow = if absolute then position else position - eyeCenter
+                let color = computeColor position
+                if filled
+                then drawList.AddCircleFilled (positionWindow, radius, color.Abgr)
+                else drawList.AddCircle (positionWindow, radius, color.Abgr)
+
+        /// Render circles via ImGui in the current eye 2d space.
+        static member imGuiCircles2d absolute position radius filled color world =
+            World.imGuiCircles2dPlus absolute position radius filled (constant color) world
+
+        /// Render a circle via ImGui in the current eye 3d space.
+        static member imGuiCircle2d absolute position radius filled color world =
+            World.imGuiCircles2d absolute (SArray.singleton position) radius filled color world
+
+        /// Render segments via ImGui in the current eye 2d space, computing color as specified.
+        static member imGuiSegments2dPlus absolute (segments : struct (Vector2 * Vector2) seq) thickness (computeColor : struct (Vector2 * Vector2) -> Color) world =
+            let drawList = ImGui.GetBackgroundDrawList ()
+            let eyeCenter = World.getEye2dCenter world
+            for struct (start, stop) in segments do
+                let color = computeColor struct (start, stop)
+                let startWindow = if absolute then start else start - eyeCenter
+                let stopWindow = if absolute then stop else stop - eyeCenter
+                drawList.AddLine (startWindow, stopWindow, color.Abgr, thickness)
+
+        /// Render segments via ImGui in the current eye 2d space.
+        static member imGuiSegments2d absolute segments thickness color world =
+            World.imGuiSegments2dPlus absolute segments thickness (constant color) world
+
+        /// Render a segment via ImGui in the current eye 2d space.
+        static member imGuiSegment3d absolute segment thickness color world =
+            World.imGuiSegments2d absolute (SArray.singleton segment) thickness color world
+
         /// Render circles via ImGui in the current eye 3d space, computing color as specified.
         static member imGuiCircles3dPlus absolute (positions : Vector3 seq) radius filled (computeColor : Vector3 -> Color) world =
             let drawList = ImGui.GetBackgroundDrawList ()
@@ -35,7 +72,7 @@ module WorldImGui =
                     then drawList.AddCircleFilled (positionWindow, radius, color.Abgr)
                     else drawList.AddCircle (positionWindow, radius, color.Abgr)
 
-        /// Render segments via ImGui in the current eye 3d space.
+        /// Render circles via ImGui in the current eye 3d space.
         static member imGuiCircles3d absolute position radius filled color world =
             World.imGuiCircles3dPlus absolute position radius filled (constant color) world
 
