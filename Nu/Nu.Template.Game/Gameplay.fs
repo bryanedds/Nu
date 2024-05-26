3b8d89aa5e828329340a26b50b524bc481578b19
bryanedds <bryanedds@gmail.com>
2024-05-26 12:39:29 -0400

    Shinier spinning.
    Former-commit-id: 3acd1e2fa2774ba5ce02b2ad36afce6d4064c560

--- Nu/Nu.Template.Game/Gameplay.fs
+++ Nu/Nu.Template.Game/Gameplay.fs
@@ -104,7 +104,7 @@ type GameplayDispatcher () =
             Content.groupFromFile Simulants.GameplayScene.Name "Assets/Gameplay/Scene.nugroup" []
                 [Content.staticModel "StaticModel"
                     [Entity.Position == v3 0.0f 1.0f 0.0f
-                     Entity.Rotation := Quaternion.CreateFromAxisAngle ((v3 0.25f 1.0f 0.5f).Normalized, gameplay.GameplayTime % 360L |> single |> Math.DegreesToRadians)]]
+                     Entity.Rotation := Quaternion.CreateFromAxisAngle ((v3 0.5f 1.0f 0.25f).Normalized, gameplay.GameplayTime % 360L |> single |> Math.DegreesToRadians)]]
 
          // no scene group otherwise
          | Quit -> ()]
