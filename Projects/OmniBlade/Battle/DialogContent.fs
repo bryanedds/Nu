0cb97a26e0c2e842aff9a21bc3687825ea6d8eb7
bryanedds <bryanedds@gmail.com>
2024-06-01 11:00:26 -0400

    OB: Added drop shadows to narrative dialog text.
    Former-commit-id: e955d531ac2e48e26b031b726b57dc4124b44613

--- Projects/OmniBlade/Battle/DialogContent.fs
+++ Projects/OmniBlade/Battle/DialogContent.fs
@@ -15,27 +15,58 @@ module DialogContent =
         let dialog name elevation promptLeft promptRight (detokenize : string -> string) (dialogOpt : Dialog option) =
             [match dialogOpt with
              | Some dialog ->
+                let perimeter =
+                    match dialog.DialogForm with
+                    | DialogShort -> box3 (v3 -270.0f 159.0f 0.0f) (v3 540.0f 81.0f 0.0f)
+                    | DialogThin -> box3 (v3 -432.0f 159.0f 0.0f) (v3 864.0f 81.0f 0.0f)
+                    | DialogThick -> box3 (v3 -432.0f 78.0f 0.0f) (v3 864.0f 174.0f 0.0f)
+                    | DialogNarration -> box3 (v3 -432.0f 78.0f 0.0f) (v3 864.0f 174.0f 0.0f)
+                let backdropImageOpt =
+                    match dialog.DialogForm with
+                    | DialogShort -> Some Assets.Gui.DialogShortImage
+                    | DialogThin -> Some Assets.Gui.DialogThinImage
+                    | DialogThick -> Some Assets.Gui.DialogThickImage
+                    | DialogNarration -> Some Assets.Default.EmptyImage
+                let text =
+                    Dialog.getText detokenize dialog
+                let justification =
+                    match dialog.DialogForm with
+                    | DialogShort | DialogThin | DialogThick -> Unjustified true
+                    | DialogNarration -> Justified (JustifyCenter, JustifyMiddle)
+                let textMargin =
+                    v2 30.0f 30.0f
                 Content.composite<TextDispatcher> name
-                    [Entity.Perimeter :=
-                        match dialog.DialogForm with
-                        | DialogShort -> box3 (v3 -270.0f 159.0f 0.0f) (v3 540.0f 81.0f 0.0f)
-                        | DialogThin -> box3 (v3 -432.0f 159.0f 0.0f) (v3 864.0f 81.0f 0.0f)
-                        | DialogThick -> box3 (v3 -432.0f 78.0f 0.0f) (v3 864.0f 174.0f 0.0f)
-                        | DialogNarration -> box3 (v3 -432.0f 78.0f 0.0f) (v3 864.0f 174.0f 0.0f)
+                    [Entity.Perimeter := perimeter
                      Entity.Elevation := elevation
-                     Entity.BackdropImageOpt :=
-                        match dialog.DialogForm with
-                        | DialogShort -> Some Assets.Gui.DialogShortImage
-                        | DialogThin -> Some Assets.Gui.DialogThinImage
-                        | DialogThick -> Some Assets.Gui.DialogThickImage
-                        | DialogNarration -> Some Assets.Default.EmptyImage
-                     Entity.Text := Dialog.getText detokenize dialog
-                     Entity.Justification :=
-                        match dialog.DialogForm with
-                        | DialogShort | DialogThin | DialogThick -> Unjustified true
-                        | DialogNarration -> Justified (JustifyCenter, JustifyMiddle)
-                     Entity.TextMargin == v2 30.0f 30.0f]
-                    [Content.button "Left"
+                     Entity.BackdropImageOpt := backdropImageOpt
+                     Entity.Text := text
+                     Entity.Justification := justification
+                     Entity.TextMargin == textMargin]
+                    [Content.text "DropShadow"
+                        [Entity.PositionLocal == v3 -2.0f 0.0f 0.0f; Entity.Size := perimeter.Size; Entity.ElevationLocal == -0.1f
+                         Entity.VisibleLocal := dialog.DialogForm = DialogNarration
+                         Entity.BackdropImageOpt == None
+                         Entity.Text := text
+                         Entity.Justification := justification
+                         Entity.TextMargin == textMargin
+                         Entity.TextColor == Color.Black]
+                     Content.text "DropShadow2"
+                        [Entity.PositionLocal == v3 -2.0f -2.0f 0.0f; Entity.Size := perimeter.Size; Entity.ElevationLocal == -0.1f
+                         Entity.VisibleLocal := dialog.DialogForm = DialogNarration
+                         Entity.BackdropImageOpt == None
+                         Entity.Text := text
+                         Entity.Justification := justification
+                         Entity.TextMargin == textMargin
+                         Entity.TextColor == Color.Black]
+                     Content.text "DropShadow3"
+                        [Entity.PositionLocal == v3 0.0f -2.0f 0.0f; Entity.Size := perimeter.Size; Entity.ElevationLocal == -0.1f
+                         Entity.VisibleLocal := dialog.DialogForm = DialogNarration
+                         Entity.BackdropImageOpt == None
+                         Entity.Text := text
+                         Entity.Justification := justification
+                         Entity.TextMargin == textMargin
+                         Entity.TextColor == Color.Black]
+                     Content.button "Left"
                         [Entity.PositionLocal == v3 186.0f 18.0f 0.0f; Entity.Size == v3 192.0f 48.0f 0.0f; Entity.ElevationLocal == 2.0f
                          Entity.VisibleLocal := Option.isSome dialog.DialogPromptOpt && Dialog.isExhausted detokenize dialog
                          Entity.Text := match dialog.DialogPromptOpt with Some ((promptText, _), _) -> promptText | None -> ""
