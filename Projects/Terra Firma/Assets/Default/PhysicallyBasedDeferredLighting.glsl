13b9cbfbf0c244c39c6ac500dec1c5c121d14e78
bryanedds <bryanedds@gmail.com>
2024-07-24 01:05:04 -0400

    Maximized reflectionFineness.

--- Projects/Terra Firma/Assets/Default/PhysicallyBasedDeferredLighting.glsl
+++ Projects/Terra Firma/Assets/Default/PhysicallyBasedDeferredLighting.glsl
@@ -240,7 +240,7 @@ void main()
         float specularWeight = 0.0;
 
         // uniform values
-        float reflectionFineness = 0.32;
+        float reflectionFineness = 0.34;
         float reflectionDepthMax = 24.0;
         float reflectionDistanceMax = 24.0;
         int reflectionStepsMax = 320;
