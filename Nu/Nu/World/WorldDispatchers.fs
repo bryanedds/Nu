4aa4378583ab119d9464a093544d7cf469990203
bryanedds <bryanedds@gmail.com>
2024-08-09 19:42:12 -0400

    Fixed default Effect3D.EffectDescriptor define.

--- Nu/Nu/World/WorldDispatchers.fs
+++ Nu/Nu/World/WorldDispatchers.fs
@@ -491,7 +491,9 @@ type Effect3dDispatcher () =
         [typeof<EffectFacet>]
 
     static member Properties =
-        [define Entity.EffectDescriptor (scvalue<Effects.EffectDescriptor> "[[EffectName Effect] [LifeTimeOpt None] [Definitions []] [Content [Contents [Shift 0] [[StaticSprite [Resource Default Image] [] Nil]]]]]")]
+        [define Entity.EffectDescriptor
+            (scvalue<Effects.EffectDescriptor>
+                "[[EffectName Effect] [LifeTimeOpt None] [Definitions []] [Content [Contents [Shift 0] [[Billboard [Resource Default MaterialAlbedo] [Resource Default MaterialRoughness] [Resource Default MaterialMetallic] [Resource Default MaterialAmbientOcclusion] [Resource Default MaterialEmission] [Resource Default MaterialNormal] [Resource Default MaterialHeightMap] False [] Nil]]]]]")]
 
 /// Gives an entity the base behavior of a rigid 3d block using static physics.
 type Block3dDispatcher () =
