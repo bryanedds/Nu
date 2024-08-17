3e9c436938af5a442600c21ad689bf0c64112f25
bryanedds <bryanedds@gmail.com>
2024-08-16 23:02:24 -0400

    New way to do shaders?

--- Nu/Nu.Gaia/Assets/Default/SkyBox.Vulkan.glsl
+++ Nu/Nu.Gaia/Assets/Default/SkyBox.Vulkan.glsl
@@ -1,37 +1,37 @@
 #shader vertex
 #version 460
 
-layout (binding = 0) uniform UniformBufferVertex
+layout(binding = 0) uniform Uniforms
 {
     mat4 View;
     mat4 Projection;
-} ubv;
+} unis;
 
-layout (location = 0) in vec3 position;
+layout(location = 0) in vec3 position;
 
 out vec3 texCoordsOut;
 
 void main()
 {
     texCoordsOut = position;
-    gl_Position = (ubv.Projection * ubv.View * vec4(position, 1.0)).xyww;
+    gl_Position = (unis.Projection * unis.View * vec4(position, 1.0)).xyww;
 }
 
 #shader fragment
 #version 460
 
-layout (binding = 1) uniform UniformBufferFragment
+layout(binding = 1) uniform Uniforms
 {
     vec3 Color;
     float Brightness;
     samplerCube CubeMap;
-} ubf;
+} unis;
 
 in vec3 texCoordsOut;
 
-layout (location = 0) out vec4 frag;
+layout(location = 0) out vec4 frag;
 
 void main()
 {
-    frag = texture(ubf.CubeMap, texCoordsOut) * vec4(ubf.Color, 1.0f) * ubf.Brightness;
+    frag = texture(unis.CubeMap, texCoordsOut) * vec4(unis.Color, 1.0f) * unis.Brightness;
 }
