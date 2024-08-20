33deb1ab34d18a07952789b88f742dbea51eb5e9
bryanedds <bryanedds@gmail.com>
2024-08-20 12:06:20 -0400

    Added glFinish call before swapping window for better compatibility with Intel iGPUs.

--- Nu/Nu/Render/RendererProcess.fs
+++ Nu/Nu/Render/RendererProcess.fs
@@ -396,7 +396,9 @@ type RendererThread () =
 
                     // attempt to swap
                     match windowOpt with
-                    | Some window -> match window with SglWindow window -> SDL.SDL_GL_SwapWindow window.SglWindow
+                    | Some (SglWindow window) ->
+                        OpenGL.Gl.Finish () // NOTE: some architectures seem to require that we call this before swapping.
+                        SDL.SDL_GL_SwapWindow window.SglWindow
                     | None -> ()
 
                     // complete swap request
