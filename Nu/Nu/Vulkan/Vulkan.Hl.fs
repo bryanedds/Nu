35949e1da1d01cc3c2c18920160dced95577bcbc
Dean J Lee <deanjlee@protonmail.com>
2024-08-31 10:35:37 +1000

    Added ArrayPin but discovered degeneracy.

--- Nu/Nu/Vulkan/Vulkan.Hl.fs
+++ Nu/Nu/Vulkan/Vulkan.Hl.fs
@@ -10,3 +10,28 @@ namespace Nu
 module Vulkan = let _ = ()
 
 namespace Vulkan
+open System
+open FSharp.NativeInterop
+
+[<RequireQualifiedAccess>]
+module Hl =
+
+    // TODO: have Bryan verify safety, try to apply implicit conversion and move to appropriate location.
+
+    /// An experimental container for a pinned array exposing its pointer.
+    type ArrayPin<'a when 'a: unmanaged> private (handle : Buffers.MemoryHandle, pointer : nativeptr<'a>) =
+
+        let handle = handle
+        let pointer = pointer
+
+        member private this.Handle = handle
+        member this.Pointer = pointer
+
+        new (array : 'a array) =
+            let handle = array.AsMemory().Pin()
+            let pointer = NativePtr.ofVoidPtr<'a> handle.Pointer
+            new ArrayPin<'a> (handle, pointer)
+
+        interface IDisposable with
+            // TODO: according to the warning this.Handle is being copied, it needs to NOT BE COPIED!
+            member this.Dispose() = this.Handle.Dispose()
