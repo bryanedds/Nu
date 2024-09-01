b1f8931d80964f7e17e8cdc9bb68d82beffb9f30
Dean J Lee <deanjlee@protonmail.com>
2024-09-01 13:02:40 +1000

    Moved ArrayPin to Core/Native.fs.

--- Nu/Nu/Vulkan/Vulkan.Hl.fs
+++ Nu/Nu/Vulkan/Vulkan.Hl.fs
@@ -13,29 +13,11 @@ namespace Vulkan
 open System
 open FSharp.NativeInterop
 open Vortice.Vulkan
-open type Vortice.Vulkan.Vulkan
+open type Vulkan
 open Nu
 
 [<RequireQualifiedAccess>]
 module Hl =
-
-    // TODO: have Bryan verify safety, try to apply implicit conversion and move to appropriate location.
-
-    /// An experimental container for a pinned array exposing its pointer.
-    type ArrayPin<'a when 'a: unmanaged> private (handle : Buffers.MemoryHandle, pointer : nativeptr<'a>) =
-
-        let handle = handle
-        let pointer = pointer
-
-        member this.Pointer = pointer
-
-        new (array : 'a array) =
-            let handle = array.AsMemory().Pin()
-            let pointer = NativePtr.ofVoidPtr<'a> handle.Pointer
-            new ArrayPin<'a> (handle, pointer)
-
-        interface IDisposable with
-            member this.Dispose() = handle.Dispose()
 
     /// Check the given Vulkan operation result, logging on non-Success.
     let check (result : VkResult) =
