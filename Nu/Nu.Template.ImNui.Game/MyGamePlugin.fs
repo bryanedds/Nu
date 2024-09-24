eadc0e09fe7e763f499cc1a2427ec6a9d750e924
bryanedds <bryanedds@gmail.com>
2024-09-24 11:34:26 -0400

    Added Gameplay package as initial package in templates.

--- Nu/Nu.Template.ImNui.Game/MyGamePlugin.fs
+++ Nu/Nu.Template.ImNui.Game/MyGamePlugin.fs
@@ -19,4 +19,5 @@ type MyGamePlugin () =
 
     // this specifies which packages are automatically loaded at game start-up.
     override this.InitialPackages =
-        [Assets.Gui.PackageName]
+        [Assets.Gui.PackageName
+         Assets.Gameplay.PackageName]
