6a8e6ea7c9c6dd1cab3009e49754524aea5e4ceb
bryanedds <bryanedds@gmail.com>
2024-09-25 23:45:01 -0400

    Restored old behavior for broken demo physics.

--- Projects/Blaze Vector/Enemy.fs
+++ Projects/Blaze Vector/Enemy.fs
@@ -66,7 +66,7 @@ type EnemyDispatcher () =
         | Update ->
             let world =
                 if entity.GetInView2dRelative world
-                then World.applyBodyForce WalkForce None (entity.GetBodyId world) world
+                then World.applyBodyForce WalkForce (Some v3Zero) (entity.GetBodyId world) world
                 else world
             let world =
                 if enemy.Health <= 0 then
