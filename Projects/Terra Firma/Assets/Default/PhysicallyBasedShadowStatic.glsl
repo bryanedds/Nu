7e1384119013e6223a666b2cfc5e30ae32bfd029
bryanedds <bryanedds@gmail.com>
2024-11-09 11:34:35 -0500

    Propagated assets.

--- Projects/Terra Firma/Assets/Default/PhysicallyBasedShadowStatic.glsl
+++ Projects/Terra Firma/Assets/Default/PhysicallyBasedShadowStatic.glsl
@@ -7,27 +7,43 @@ uniform mat4 projection;
 layout(location = 0) in vec3 position;
 layout(location = 3) in mat4 model;
 
+out vec4 positionOut;
 out float depthDirectionalOut;
 
 void main()
 {
-	vec4 positionWorld = model * vec4(position, 1.0);
-	gl_Position = projection * view * positionWorld;
+	positionOut = model * vec4(position, 1.0);
+	gl_Position = projection * view * positionOut;
 	depthDirectionalOut = gl_Position.z / gl_Position.w;
 }
 
 #shader fragment
 #version 410
 
-uniform int lightShadowDirectional;
+uniform vec3 eyeCenter;
+uniform int lightType;
 uniform float lightShadowExponent;
 
 layout(location = 0) out vec2 depths;
 
+in vec4 positionOut;
 in float depthDirectionalOut;
 
 void main()
 {
-	depths.x = gl_FragCoord.z;
-	depths.y = exp(lightShadowExponent * (lightShadowDirectional == 0 ? gl_FragCoord.z : depthDirectionalOut));
+	switch (lightType)
+	{
+	case 0: // point light
+		depths.x = length(positionOut.xyz - eyeCenter);
+		depths.y = exp(lightShadowExponent * 0.1 * depths.x);
+		break;
+	case 1: // spot light
+		depths.x = gl_FragCoord.z;
+		depths.y = exp(lightShadowExponent * depths.x);
+		break;
+	case 2: // directional light
+		depths.x = gl_FragCoord.z;
+		depths.y = exp(lightShadowExponent * depthDirectionalOut);
+		break;
+	}
 }
