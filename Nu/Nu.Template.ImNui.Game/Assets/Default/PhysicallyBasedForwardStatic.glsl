88e468fb6bfdf9bd426152ffb9187b8d01e8a090
bryanedds <bryanedds@gmail.com>
2024-11-16 11:21:00 -0500

    Propadated assets.

--- Nu/Nu.Template.ImNui.Game/Assets/Default/PhysicallyBasedForwardStatic.glsl
+++ Nu/Nu.Template.ImNui.Game/Assets/Default/PhysicallyBasedForwardStatic.glsl
@@ -67,7 +67,6 @@ const int SHADOW_TEXTURES_MAX = 16;
 const int SHADOW_MAPS_MAX = 4;
 const float SHADOW_FOV_MAX = 2.1;
 const float SHADOW_SEAM_INSET = 0.001;
-const float SHADOW_SAMPLE_SCALAR = 0.0025;
 const int SHADOW_SAMPLE_OFFSETS_COUNT = 20;
 const vec3 SHADOW_SAMPLE_OFFSETS[SHADOW_SAMPLE_OFFSETS_COUNT] =
     vec3[](
@@ -90,8 +89,7 @@ uniform vec3 lightAmbientColor;
 uniform float lightAmbientBrightness;
 uniform float lightShadowExponent;
 uniform float lightShadowDensity;
-uniform float lightShadowBleedFilter;
-uniform float lightShadowVarianceMin;
+uniform float lightShadowSampleScalar;
 uniform int ssvfEnabled;
 uniform int ssvfSteps;
 uniform float ssvfAsymmetry;
@@ -266,7 +264,7 @@ float computeShadowMapScalar(vec4 position, vec3 lightOrigin, samplerCube shadow
     for (int i = 0; i < SHADOW_SAMPLE_OFFSETS_COUNT; ++i)
     {
         // NOTE: we divide at each step to avoid overflow with an already large number.
-        shadowDepthExp += texture(shadowMap, positionShadow / shadowZ + SHADOW_SAMPLE_OFFSETS[i] * SHADOW_SAMPLE_SCALAR).y / SHADOW_SAMPLE_OFFSETS_COUNT;
+        shadowDepthExp += texture(shadowMap, positionShadow / shadowZ + SHADOW_SAMPLE_OFFSETS[i] * lightShadowSampleScalar).y / SHADOW_SAMPLE_OFFSETS_COUNT;
     }
     float shadowScalar = clamp(shadowZExp * shadowDepthExp, 0.0, 1.0);
     return pow(shadowScalar, lightShadowDensity);
--- Projects/Breakout ImNui/Assets/Default/PhysicallyBasedDeferredLighting.glsl
+++ Projects/Breakout ImNui/Assets/Default/PhysicallyBasedDeferredLighting.glsl
@@ -23,7 +23,6 @@ const int SHADOW_TEXTURES_MAX = 16;
 const int SHADOW_MAPS_MAX = 8;
 const float SHADOW_FOV_MAX = 2.1;
 const float SHADOW_SEAM_INSET = 0.001;
-const float SHADOW_SAMPLE_SCALAR = 0.0025;
 const int SHADOW_SAMPLE_OFFSETS_COUNT = 20;
 const vec3 SHADOW_SAMPLE_OFFSETS[SHADOW_SAMPLE_OFFSETS_COUNT] =
     vec3[](
@@ -44,10 +43,9 @@ uniform vec3 eyeCenter;
 uniform mat4 view;
 uniform mat4 projection;
 uniform float lightCutoffMargin;
+uniform float lightShadowSampleScalar;
 uniform float lightShadowExponent;
 uniform float lightShadowDensity;
-uniform float lightShadowBleedFilter;
-uniform float lightShadowVarianceMin;
 uniform int ssvfEnabled;
 uniform int ssvfSteps;
 uniform float ssvfAsymmetry;
@@ -201,7 +199,7 @@ float computeShadowMapScalar(vec4 position, vec3 lightOrigin, samplerCube shadow
     for (int i = 0; i < SHADOW_SAMPLE_OFFSETS_COUNT; ++i)
     {
         // NOTE: we divide at each step to avoid overflow with an already large number.
-        shadowDepthExp += texture(shadowMap, positionShadow / shadowZ + SHADOW_SAMPLE_OFFSETS[i] * SHADOW_SAMPLE_SCALAR).y / SHADOW_SAMPLE_OFFSETS_COUNT;
+        shadowDepthExp += texture(shadowMap, positionShadow / shadowZ + SHADOW_SAMPLE_OFFSETS[i] * lightShadowSampleScalar).y / SHADOW_SAMPLE_OFFSETS_COUNT;
     }
     float shadowScalar = clamp(shadowZExp * shadowDepthExp, 0.0, 1.0);
     return pow(shadowScalar, lightShadowDensity);
