63d48558e35f47ea2b70bff1150f07ea45b2fe0a
bryanedds <bryanedds@gmail.com>
2024-12-25 23:00:35 -0500

    Changed ImNui template to be more amenable to its form factor's particulars.

--- Nu/Nu.Template.ImNui.Game/Gameplay.fs
+++ Nu/Nu.Template.ImNui.Game/Gameplay.fs
@@ -28,22 +28,24 @@ type GameplayDispatcher () =
     // here we define the behavior of our gameplay
     override this.Process (gameplay, world) =
 
-        // declare scene group while screen is selected
-        let world =
-            if gameplay.GetSelected world then
-                let world = World.beginGroupFromFile "Scene" "Assets/Gameplay/Scene.nugroup" [] world
-                let rotation = Quaternion.CreateFromAxisAngle ((v3 1.0f 0.75f 0.5f).Normalized, world.UpdateTime % 360L |> single |> Math.DegreesToRadians)
-                let world = World.doStaticModel "StaticModel" [Entity.Scale .= v3Dup 0.5f; Entity.Rotation @= rotation] world
-                let world = World.endGroup world
-                world
-            else world
+        // only declare entities while gameplay screen is selected
+        if gameplay.GetSelected world then
 
-        // declare gui group
-        let world = World.beginGroup "Gui" [] world
-        let (clicked, world) = World.doButton "Quit" [Entity.Position .= v3 232.0f -144.0f 0.0f; Entity.Text .= "Quit"] world
-        let world = if clicked then gameplay.SetGameplayState Quit world else world
-        let world = World.endGroup world
-        world
+            // declare scene group
+            let world = World.beginGroupFromFile "Scene" "Assets/Gameplay/Scene.nugroup" [] world
+            let rotation = Quaternion.CreateFromAxisAngle ((v3 1.0f 0.75f 0.5f).Normalized, world.UpdateTime % 360L |> single |> Math.DegreesToRadians)
+            let world = World.doStaticModel "StaticModel" [Entity.Scale .= v3Dup 0.5f; Entity.Rotation @= rotation] world
+            let world = World.endGroup world
+
+            // declare gui group
+            let world = World.beginGroup "Gui" [] world
+            let (clicked, world) = World.doButton "Quit" [Entity.Position .= v3 232.0f -144.0f 0.0f; Entity.Text .= "Quit"] world
+            let world = if clicked then gameplay.SetGameplayState Quit world else world
+            let world = World.endGroup world
+            world
+
+        // otherwise declare nothing
+        else world
 
     // this is a semantic fix-up that allows the editor to avoid creating an unused group. This is specific to the
     // ImNui API that is needed to patch a little semantic hole inherent in the immediate-mode programming idiom.
