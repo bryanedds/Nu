dc5bddb412f70e6c55cce3767919565d170797a8
Dean J Lee <deanjlee@protonmail.com>
2024-12-29 16:47:07 +1100

    Began work on Pipeline.

---
+++ Nu/Nu/Vulkan/Vulkan.Pipeline.fs
@@ -0,0 +1,23 @@
+﻿// Nu Game Engine.
+// Copyright (C) Bryan Edds, 2013-2023.
+
+namespace Vortice.Vulkan
+
+[<RequireQualifiedAccess>]
+module Pipeline =
+
+    /// An abstraction of a rendering pipeline.
+    type Pipeline =
+        { Pipeline : VkPipeline
+          DescriptorPool : VkDescriptorPool
+          DescriptorSet : VkDescriptorSet
+          PipelineLayout : VkPipelineLayout
+          DescriptorSetLayout : VkDescriptorSetLayout
+          mutable VertexBuffer : Hl.AllocatedBuffer
+          mutable IndexBuffer : Hl.AllocatedBuffer }
+
+        /// Create a Pipeline.
+        static member create shaderPath pushConstants layoutBindings vulkanGlobal =
+            
+
+            ()
