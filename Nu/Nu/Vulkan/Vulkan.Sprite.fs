760f420d7826904ad6cc8dee8c0929ddb59831dd
Dean J Lee <deanjlee@protonmail.com>
2025-01-10 16:17:42 +1100

    Began work on Vulkan DrawSprite function.

--- Nu/Nu/Vulkan/Vulkan.Sprite.fs
+++ Nu/Nu/Vulkan/Vulkan.Sprite.fs
@@ -48,7 +48,7 @@ module Sprite =
         (vertexBuffer, indexBuffer)
 
     /// Draw a sprite whose indices and vertices were created by Vulkan.CreateSpriteQuad using the sprite shader.
-    let DrawSprite (vertices, indices, vao, modelViewProjection : single array, insetOpt : Box2 ValueOption, color : Color, flip, textureWidth, textureHeight, texture : Texture.Texture, modelViewProjectionUniform, texCoords4Uniform, colorUniform, textureUniform, shader) =
+    let DrawSprite (vertices : Hl.AllocatedBuffer, indices : Hl.AllocatedBuffer, modelViewProjection : single array, insetOpt : Box2 ValueOption, color : Color, flip, textureWidth, textureHeight, texture : Texture.Texture, pipeline : Pipeline.SpritePipeline, vulkanGlobal : Hl.VulkanGlobal) =
 
         // compute unflipped tex coords
         let texCoordsUnflipped =
@@ -88,5 +88,44 @@ module Sprite =
                     (if flipH then -texCoordsUnflipped.Size.X else texCoordsUnflipped.Size.X)
                     (if flipV then -texCoordsUnflipped.Size.Y else texCoordsUnflipped.Size.Y))
 
+        // commonly used handles
+        let allocator = vulkanGlobal.VmaAllocator
+        let commandBuffer = vulkanGlobal.RenderCommandBuffer
+        
+        // bind pipeline
+        Vulkan.vkCmdBindPipeline (commandBuffer, Vulkan.VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline.Pipeline)
 
-        ()
+        // set viewport
+        let mutable viewport = VkViewport ()
+        viewport.x <- 0.0f
+        viewport.y <- 0.0f
+        viewport.width <- single vulkanGlobal.SwapExtent.width
+        viewport.height <- single vulkanGlobal.SwapExtent.height
+        viewport.minDepth <- 0.0f
+        viewport.maxDepth <- 1.0f
+        Vulkan.vkCmdSetViewport (commandBuffer, 0u, 1u, asPointer &viewport)
+        
+        // set scissor
+        let mutable scissor = VkRect2D (VkOffset2D.Zero, vulkanGlobal.SwapExtent)
+        Vulkan.vkCmdSetScissor (commandBuffer, 0u, 1u, asPointer &scissor)
+        
+        // bind vertex and index buffer
+        let mutable vertexBuffer = vertices.Buffer
+        let mutable vertexOffset = 0UL
+        Vulkan.vkCmdBindVertexBuffers (commandBuffer, 0u, 1u, asPointer &vertexBuffer, asPointer &vertexOffset)
+        Vulkan.vkCmdBindIndexBuffer (commandBuffer, indices.Buffer, 0UL, Vulkan.VK_INDEX_TYPE_UINT32)
+
+        // bind descriptor set
+        let mutable descriptorSet = pipeline.DescriptorSet
+        Vulkan.vkCmdBindDescriptorSets
+            (commandBuffer,
+             Vulkan.VK_PIPELINE_BIND_POINT_GRAPHICS,
+             pipeline.PipelineLayout, 0u,
+             1u, asPointer &descriptorSet,
+             0u, nullPtr)
+        
+
+        
+        // reset scissor
+        let mutable scissor = VkRect2D (VkOffset2D.Zero, vulkanGlobal.SwapExtent)
+        Vulkan.vkCmdSetScissor (commandBuffer, 0u, 1u, asPointer &scissor)
