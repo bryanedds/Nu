0e191b13076d870d6f8993308ac20dcc6aa27225
bryanedds <bryanedds@gmail.com>
2025-01-27 09:53:26 -0500

    Turned Nu.Globals into a module so it can't be opened explicitly.

--- Nu/Nu/Core/Globals.fs
+++ Nu/Nu/Core/Globals.fs
@@ -1,16 +1,19 @@
 ﻿// Nu Game Engine.
 // Copyright (C) Bryan Edds.
 
-namespace Nu.Globals
+namespace Nu
 open System
 open System.Configuration
 open Prime
 
 [<RequireQualifiedAccess>]
-module Render =
+module Globals =
 
-    /// The global mutable display scalar. This may be changed by the engine at run-time.
-    let mutable DisplayScalar = match ConfigurationManager.AppSettings.["DisplayScalar"] with null -> 3 | scalar -> scvalue scalar
+    [<RequireQualifiedAccess>]
+    module Render =
 
-    /// The global mutable shadow scalar. This may be changed by the engine at run-time.
-    let mutable ShadowScalar = match ConfigurationManager.AppSettings.["ShadowScalar"] with null -> 4 | scalar -> scvalue scalar
+        /// The global mutable display scalar. This may be changed by the engine at run-time.
+        let mutable DisplayScalar = match ConfigurationManager.AppSettings.["DisplayScalar"] with null -> 3 | scalar -> scvalue scalar
+
+        /// The global mutable shadow scalar. This may be changed by the engine at run-time.
+        let mutable ShadowScalar = match ConfigurationManager.AppSettings.["ShadowScalar"] with null -> 4 | scalar -> scvalue scalar
