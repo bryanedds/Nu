e8ec137605df6fa26ca0bc8eab619f595a2e50d5
bryanedds <bryanedds@gmail.com>
2025-02-07 23:48:47 -0500

    Updated OB.

--- Projects/Omni Blade/Field/FieldDispatcher.fs
+++ Projects/Omni Blade/Field/FieldDispatcher.fs
@@ -758,7 +758,7 @@ type FieldDispatcher () =
                 let tileMapPerimeter2d = (Simulants.FieldTileMap.GetPerimeter world).Box2
                 let eyeBounds = tileMapPerimeter2d.WithMin (tileMapPerimeter2d.Min + v2 48.0f 48.0f)
                 let eyeBounds = eyeBounds.WithSize (tileMapPerimeter2d.Size - v2 96.0f 96.0f)
-                let world = World.constrainEyeBounds2d eyeBounds world
+                let world = World.constrainEye2dBounds eyeBounds world
                 just world
             else just world
 
