8077d0791567bc760b7d2947d0dde409e1e4b677
bryanedds <bryanedds@gmail.com>
2025-02-10 09:56:21 -0500

    Changed FollowMoveSpeed default to 1.0f.

--- Nu/Nu/World/WorldFacets.fs
+++ Nu/Nu/World/WorldFacets.fs
@@ -3356,7 +3356,7 @@ type FollowerFacet () =
 
     static member Properties =
         [define Entity.Following true
-         define Entity.FollowMoveSpeed 2.0f
+         define Entity.FollowMoveSpeed 1.0f
          define Entity.FollowTurnSpeed 3.0f
          define Entity.FollowDistanceMinOpt None
          define Entity.FollowDistanceMaxOpt None
