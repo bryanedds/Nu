29b0bfabac3e0f7e96526e863b6a8eb0d72d0023
bryanedds <bryanedds@gmail.com>
2025-02-15 17:55:31 -0500

    Comments on TF events.

--- Projects/Terra Firma/Events.fs
+++ Projects/Terra Firma/Events.fs
@@ -2,8 +2,12 @@
 open System
 open Nu
 
+// this module specifies user-defined event types for Terra  Firma.
 [<RequireQualifiedAccess>]
 module Events =
 
+    // this event is called when an attack is detected during gameplay.
     let AttackEvent = stoa<Entity> "Attack/Event"
+
+    // this event is called when the player or an enemy dies during gameplay.
     let DieEvent = stoa<Entity> "Die/Event"
