3785e300ad4402887d141d983d9bf32516d382b9
bryanedds <bryanedds@gmail.com>
2025-04-19 17:18:02 -0400

    Starting OpenGL with Core profile in an attempt to improve predictability / compatibility with AMD.

--- Nu/Nu/Core/Constants.fs
+++ Nu/Nu/Core/Constants.fs
@@ -6,6 +6,7 @@ open System
 open System.Configuration
 open System.Diagnostics
 open System.Numerics
+open SDL2
 open Prime
 open Nu
 
@@ -13,7 +14,8 @@ module OpenGL =
 
     let [<Literal>] VersionMajor = 4
     let [<Literal>] VersionMinor = 1
-    let [<Uniform>] GlslVersionPragma = "#version " + string VersionMajor + string VersionMinor + "0"
+    let [<Literal>] Profile = SDL.SDL_GLprofile.SDL_GL_CONTEXT_PROFILE_CORE
+    let [<Uniform>] GlslVersionPragma = "#version " + string VersionMajor + string VersionMinor + "0" // TODO: consider added "core" profile specifier here and in the glsl asset files.
     let [<Literal>] UncompressedTextureFormat = OpenGL.InternalFormat.Rgba8
     let [<Literal>] BlockCompressedTextureFormat = OpenGL.InternalFormat.CompressedRgbaS3tcDxt5Ext
     let [<Uniform>] mutable HlDebug = match ConfigurationManager.AppSettings.["HlDebug"] with null -> false | value -> scvalue value
