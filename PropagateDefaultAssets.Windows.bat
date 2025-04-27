9e42d18669d86f504db2d5265f0f5fd76cd8b3a6
bryanedds <bryanedds@gmail.com>
2025-04-27 15:20:58 -0400

    More ImNui to ImSim renaming.

--- PropagateDefaultAssets.Windows.bat
+++ PropagateDefaultAssets.Windows.bat
@@ -3,11 +3,11 @@
 del Nu\Nu.Pipe\Assets\Default\* /Q
 copy Nu\Nu.Gaia\Assets\Default\*, Nu\Nu.Pipe\Assets\Default /Y
 
-del Nu\Nu.Template.ImNui.Empty\Assets\Default\* /Q
-copy Nu\Nu.Gaia\Assets\Default\*, Nu\Nu.Template.ImNui.Empty\Assets\Default /Y
+del Nu\Nu.Template.ImSim.Empty\Assets\Default\* /Q
+copy Nu\Nu.Gaia\Assets\Default\*, Nu\Nu.Template.ImSim.Empty\Assets\Default /Y
 
-del Nu\Nu.Template.ImNui.Game\Assets\Default\* /Q
-copy Nu\Nu.Gaia\Assets\Default\*, Nu\Nu.Template.ImNui.Game\Assets\Default /Y
+del Nu\Nu.Template.ImSim.Game\Assets\Default\* /Q
+copy Nu\Nu.Gaia\Assets\Default\*, Nu\Nu.Template.ImSim.Game\Assets\Default /Y
 
 del Nu\Nu.Template.Mmcc.Empty\Assets\Default\* /Q
 copy Nu\Nu.Gaia\Assets\Default\*, Nu\Nu.Template.Mmcc.Empty\Assets\Default /Y
@@ -18,14 +18,14 @@ copy Nu\Nu.Gaia\Assets\Default\*, Nu\Nu.Template.Mmcc.Game\Assets\Default /Y
 del Nu\Nu.Tests\Assets\Default\* /Q
 copy Nu\Nu.Gaia\Assets\Default\*, Nu\Nu.Tests\Assets\Default /Y
 
-del "Projects\Blaze Vector ImNui\Assets\Default\*" /Q
-copy Nu\Nu.Gaia\Assets\Default\*, "Projects\Blaze Vector ImNui\Assets\Default" /Y
+del "Projects\Blaze Vector ImSim\Assets\Default\*" /Q
+copy Nu\Nu.Gaia\Assets\Default\*, "Projects\Blaze Vector ImSim\Assets\Default" /Y
 
 del "Projects\Blaze Vector Mmcc\Assets\Default\*" /Q
 copy Nu\Nu.Gaia\Assets\Default\*, "Projects\Blaze Vector Mmcc\Assets\Default" /Y
 
-del "Projects\Breakout ImNui\Assets\Default\*" /Q
-copy Nu\Nu.Gaia\Assets\Default\*, "Projects\Breakout ImNui\Assets\Default" /Y
+del "Projects\Breakout ImSim\Assets\Default\*" /Q
+copy Nu\Nu.Gaia\Assets\Default\*, "Projects\Breakout ImSim\Assets\Default" /Y
 
 del "Projects\Breakout Mmcc\Assets\Default\*" /Q
 copy Nu\Nu.Gaia\Assets\Default\*, "Projects\Breakout Mmcc\Assets\Default" /Y
