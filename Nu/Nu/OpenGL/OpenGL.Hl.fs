1ca569cfaacf1e22ab42f1482651e56c617717a0
bryanedds <bryanedds@gmail.com>
2025-05-07 12:38:15 -0400

    Dummied out GetGraphicsResetStatus diagnostic call.

--- Nu/Nu/OpenGL/OpenGL.Hl.fs
+++ Nu/Nu/OpenGL/OpenGL.Hl.fs
@@ -187,9 +187,13 @@ module Hl =
 
     /// End an OpenGL frame.
     let EndFrame () =
-        match OpenGL.Gl.GetGraphicsResetStatus () with
-        | OpenGL.GraphicsResetStatus.NoError -> ()
-        | status -> Log.fail ("Unexpected OpenGL graphics reset (GraphicResetStatus = " + string status + ").")
+        // NOTE: on some deployments, this API call appears to not be available, so this is dummied out. Apparently,
+        // this call requires the KHR_robustness extension -
+        // https://github.com/KhronosGroup/OpenGL-Registry/blob/main/extensions/KHR/KHR_robustness.txt
+        //match OpenGL.Gl.GetGraphicsResetStatus () with
+        //| OpenGL.GraphicsResetStatus.NoError -> ()
+        //| status -> Log.fail ("Unexpected OpenGL graphics reset (GraphicResetStatus = " + string status + ").")
+        ()
 
     /// Save the current bound RGBA framebuffer to an image file.
     /// Only works on Windows platforms for now.
