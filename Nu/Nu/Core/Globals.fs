2ffe7c959d84176ddf183e8f68c82b0e9403e019
bryanedds <bryanedds@gmail.com>
2025-05-07 12:43:54 -0400

    DisplayScalar now 2 by default for better deployment diagnostics.

--- Nu/Nu/Core/Globals.fs
+++ Nu/Nu/Core/Globals.fs
@@ -13,7 +13,7 @@ module Globals =
     module Render =
 
         /// The global mutable display scalar. This may be changed by the engine at run-time.
-        let mutable DisplayScalar = match ConfigurationManager.AppSettings.["DisplayScalar"] with null -> 3 | value -> scvalue value
+        let mutable DisplayScalar = match ConfigurationManager.AppSettings.["DisplayScalar"] with null -> 2 | value -> scvalue value
 
         /// The global mutable shadow scalar. This may be changed by the engine at run-time.
         let mutable ShadowScalar = match ConfigurationManager.AppSettings.["ShadowScalar"] with null -> 4 | value -> scvalue value
