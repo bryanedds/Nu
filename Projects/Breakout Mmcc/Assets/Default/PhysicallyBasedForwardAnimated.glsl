4144b7c53c9ecbd4a394ebebcb664a3a51e93a93
bryanedds <bryanedds@gmail.com>
2025-05-27 00:50:48 -0400

    Comment fixes.

--- Projects/Breakout Mmcc/Assets/Default/PhysicallyBasedForwardAnimated.glsl
+++ Projects/Breakout Mmcc/Assets/Default/PhysicallyBasedForwardAnimated.glsl
@@ -304,7 +304,7 @@ float computeShadowScalarDirectional(vec4 position, int shadowIndex)
     vec3 shadowTexCoords = shadowTexCoordsProj * 0.5 + 0.5;
     if (shadowTexCoords.x > SHADOW_SEAM_INSET && shadowTexCoords.x < 1.0 - SHADOW_SEAM_INSET &&
         shadowTexCoords.y > SHADOW_SEAM_INSET && shadowTexCoords.y < 1.0 - SHADOW_SEAM_INSET &&
-        shadowTexCoords.z > 0.5 + SHADOW_SEAM_INSET && shadowTexCoords.z < 1.0 - SHADOW_SEAM_INSET) // TODO: figure out why shadowTexCoords.z is 0.5 to 1.0.
+        shadowTexCoords.z > 0.5 + SHADOW_SEAM_INSET && shadowTexCoords.z < 1.0 - SHADOW_SEAM_INSET) // TODO: figure out why shadowTexCoords.z range is 0.5 to 1.0.
     {
         float shadowZ = shadowTexCoords.z;
         float shadowZExp = exp(-lightShadowExponent * shadowZ);
