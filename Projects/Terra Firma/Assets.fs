a01dc0d0f89678ecfed8334e53d88ad6de31fad2
bryanedds <bryanedds@gmail.com>
2025-07-02 00:47:57 -0400

    No advantage to using unit constants in user code.

--- Projects/Terra Firma/Assets.fs
+++ Projects/Terra Firma/Assets.fs
@@ -9,13 +9,13 @@ module Assets =
     module Gui =
 
         let PackageName = "Gui"
-        let GuiSong = { FadeInTime = 0L; FadeOutTime = Constants.Audio.FadeOutTimeDefault; StartTime = 0L; RepeatLimitOpt = None; Volume = Constants.Audio.SongVolumeDefault; Song = asset<Song> PackageName "Gui" }
+        let GuiSong = { FadeInTime = GameTime.zero; FadeOutTime = Constants.Audio.FadeOutTimeDefault; StartTime = 0L; RepeatLimitOpt = None; Volume = 1.0f; Song = asset PackageName "Gui" }
 
     [<RequireQualifiedAccess>]
     module Gameplay =
 
         let PackageName = "Gameplay"
-        let DesertSong = { FadeInTime = 0L; FadeOutTime = Constants.Audio.FadeOutTimeDefault; StartTime = 0L; RepeatLimitOpt = None; Volume = Constants.Audio.SongVolumeDefault; Song = asset<Song> PackageName "Desert" }
+        let DesertSong = { FadeInTime = GameTime.zero; FadeOutTime = Constants.Audio.FadeOutTimeDefault; StartTime = 0L; RepeatLimitOpt = None; Volume = 1.0f; Song = asset PackageName "Desert" }
         let SlashSound = asset<Sound> PackageName "Slash"
         let Slash2Sound = asset<Sound> PackageName "Slash2"
         let InjureSound = asset<Sound> PackageName "Injure"
