a3d9ee94a62b8ec1123b1ea7333f12669f88b846
Hadrian Tang <hadrianwttang@outlook.com>
2025-08-24 02:31:09 +0800

    Disable DPI zoom

--- Nu/Nu.Gaia/Program.fs
+++ Nu/Nu.Gaia/Program.fs
@@ -1,15 +1,17 @@
-﻿// Gaia - The Nu Game Engine editor.
+// Gaia - The Nu Game Engine editor.
 // Copyright (C) Bryan Edds.
 
 namespace Nu.Gaia
 open System
 open System.IO
-open Prime
+open System.Runtime.InteropServices
 open Nu
 open Nu.Gaia
 module Program =
-
+    [<DllImport("user32.dll")>]
+    extern bool SetProcessDPIAware()
     let [<EntryPoint; STAThread>] main _ =
         Directory.SetCurrentDirectory AppContext.BaseDirectory
+        if OperatingSystem.IsWindows() then SetProcessDPIAware() |> ignore // Disable DPI zoom on Windows, otherwise the scaled UI will be cut off
         let (gaiaState, targetDir, plugin) = Nu.initPlus (fun () -> Gaia.selectNuPlugin (GaiaPlugin ()))
         Gaia.run gaiaState targetDir plugin
