047c710ab70ebd3fc7b56e1d5ddd149b2318d59e
bryanedds <bryanedds@gmail.com>
2025-08-29 10:34:43 -0400

    Demoted #1083 to TODO: P1's.

--- Nu/Nu/Transform/Quadtree.fs
+++ Nu/Nu/Transform/Quadtree.fs
@@ -358,6 +358,7 @@ module Quadtree =
         // HACK: because the above logic maintains that a fallback'd element can't also be in a tree node doesn't
         // hold (likely due to a subtle bug), we unconditionally remove the element from the tree here.
         // NOTE: I can no longer reproduce the bug that caused this, so I've wrapped it in a #if.
+        // TODO: P1: remove this hack if the log never gets triggered after a while.
 #if DEBUG
         if Quadnode.removeElement bounds &element tree.Node <> 0 then
             Log.errorOnce "Element was in tree node when it shouldn't have been."
