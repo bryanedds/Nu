ff7d7f2ecce15ff120d6eef4cfb311ab1ce898cc
Hadrian Tang <hadrianwttang@outlook.com>
2025-08-30 03:14:39 +0800

    Fix the mouse control to work properly

--- Projects/Physics2D/D01_SingleFixture.fs
+++ Projects/Physics2D/D01_SingleFixture.fs
@@ -23,6 +23,7 @@ type D01_SingleFixtureDispatcher () =
 
     // here we define the screen's behavior
     override this.Process (_, screen, world) =
+        World.setGravity true v3Zero world
 
         World.beginGroup Simulants.SceneGroup [] world
         
@@ -40,45 +41,51 @@ type D01_SingleFixtureDispatcher () =
                       Closed = true
                       TransformOpt = None
                       PropertiesOpt = None }
+                 Entity.CollisionDetection .= Continuous // the box will tunnel through the broder otherwise
+                 Entity.Elevation .= -1f // draw order of the same elevation prioritizes entities lower than higher otherwise
                  Entity.StaticImage .= Assets.Gameplay.SkyBoxFront] world
         let (agentBody, _) =
             World.doBox2d "Agent" // A box uses dynamic physics by default.
-                [Entity.GravityOverride .= Some v3Zero
-                 Entity.Elevation .= 10f] world
-        let agent = world.DeclaredEntity
+                [Entity.Restitution .= 0.333f // bounciness
+                 ] world
         
         // Mouse control
         let mousePosition = World.getMousePostion2dWorld false world
         if World.isMouseButtonPressed MouseLeft world then
             for entity in World.getEntities2dAtPoint mousePosition (new _()) world do
-                if entity = agent then
+                if entity.Has<RigidBodyFacet> world && entity.GetBodyType world = Dynamic then
                     screen.SetDraggedEntity (Some entity) world
         elif World.isMouseButtonUp MouseLeft world then
             screen.SetDraggedEntity None world
 
         match screen.GetDraggedEntity world with
-        | Some entity ->
-            let _ =
-                World.doSphere2d "MouseSensor"
-                    [Entity.BodyType .= Kinematic
-                     Entity.BodyShape .= SphereShape
-                        { Radius = 0.1f
-                          PropertiesOpt = Some
-                            { BodyShapeProperties.empty with SensorOpt = Some true } // No collision
-                          TransformOpt = None }
-                     Entity.Visible .= false
-                     Entity.Position @= v3 mousePosition.X mousePosition.Y 0f] world
+        | Some draggedEntity ->
+
+            // declare kinematic sensor for mouse body
+            World.doSphere2d "MouseSensor"
+                [Entity.BodyType .= Kinematic
+                 Entity.BodyShape .= SphereShape
+                    { Radius = 0.1f
+                      PropertiesOpt = Some { BodyShapeProperties.empty with SensorOpt = Some true }
+                      TransformOpt = None }
+                 Entity.Visible .= false
+                 Entity.Position @= v3 mousePosition.X mousePosition.Y 0f] world |> ignore
             let mouseSensor = world.DeclaredEntity
-            let _ =
-                let mouseJoint = world.ContextGroup / "MouseJoint"
-                World.doBodyJoint2d mouseJoint.Name
-                    [Entity.BodyJointTarget .= Relation.relate mouseJoint.EntityAddress entity.EntityAddress
-                     Entity.BodyJointTarget2Opt .= Some (Relation.relate mouseJoint.EntityAddress mouseSensor.EntityAddress)
-                     Entity.BodyJoint .= TwoBodyJoint2d
-                        { CreateTwoBodyJoint = fun a b ->
-                            let mousePosition = PhysicsEngine2d.toPhysicsV2 mousePosition.V3
-                            WeldJoint(a, b, mousePosition, mousePosition, true, FrequencyHz = 1f, DampingRatio = 0.5f) }] world
-            ()
+
+            // declare distance joint for mouse body
+            let mouseJoint = world.ContextGroup / "MouseJoint"
+            World.doBodyJoint2d mouseJoint.Name
+                [Entity.BodyJointTarget .= Relation.relate mouseJoint.EntityAddress draggedEntity.EntityAddress
+                 Entity.BodyJointTarget2Opt .= Some (Relation.relate mouseJoint.EntityAddress mouseSensor.EntityAddress)
+                 Entity.BodyJoint .= TwoBodyJoint2d
+                    { CreateTwoBodyJoint = fun _ toPhysicsV2 a b ->
+                        let mousePosition = toPhysicsV2 mousePosition.V3
+                        DistanceJoint (a, b, mousePosition, mousePosition, true, Frequency = 1.5f, DampingRatio = 0.5f) }] world |> ignore
+
+            // apply damping to body in order to stabilize it while dragged
+            draggedEntity.LinearVelocity.Map ((*) 0.9f) world
+            draggedEntity.AngularVelocity.Map ((*) 0.9f) world
+
         | None -> ()
         
         // Agent control
@@ -93,10 +100,14 @@ type D01_SingleFixtureDispatcher () =
         if World.isKeyboardKeyDown KeyboardKey.S world then
             World.applyBodyForce (v3 0f -1f 0f * agentForce) None agentBody world
         if World.isKeyboardKeyDown KeyboardKey.Q world then
-            World.applyBodyTorque (v3 -1f 0f 0f * agentTorque) agentBody world
-        if World.isKeyboardKeyDown KeyboardKey.E world then
             World.applyBodyTorque (v3 1f 0f 0f * agentTorque) agentBody world
+        if World.isKeyboardKeyDown KeyboardKey.E world then
+            World.applyBodyTorque (v3 -1f 0f 0f * agentTorque) agentBody world
 
         // Exit button (click behavior specified at Physics2D.fs)
-        let _ = World.doButton Simulants.BackEntity [Entity.Position .= v3 232.0f -144.0f 0.0f; Entity.Text .= "Exit"] world
+        let _ =
+            World.doButton Simulants.BackEntity
+                [Entity.Position .= v3 232.0f -144.0f 0.0f
+                 Entity.Elevation .= 1f
+                 Entity.Text .= "Exit"] world
         World.endGroup world
