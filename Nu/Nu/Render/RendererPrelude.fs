cc5e433b3b3b1359d28a2c4768de41dfd1054b78
bryanedds <bryanedds@gmail.com>
2025-09-14 10:24:20 -0400

    Added doc comment on line breaks.

--- Nu/Nu/Render/RendererPrelude.fs
+++ Nu/Nu/Render/RendererPrelude.fs
@@ -32,6 +32,7 @@ type [<Struct>] JustificationV =
     | JustifyBottom
 
 /// Justification (such as for text alignment).
+/// Use `Unjustified true` in order to render line breaks.
 type Justification =
     | Justified of JustificationH * JustificationV
     | Unjustified of Wrapped : bool
