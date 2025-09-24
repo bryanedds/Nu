3a8d77b1f97b23610b3ca60cac343974a0eec152
bryanedds <bryanedds@gmail.com>
2025-09-24 01:20:41 -0400

    Added some custom cimgui interop to access dock window size.

---
+++ Nu/Nu/ImGui/ImGuiInterop.fs
@@ -0,0 +1,41 @@
+﻿// Nu Game Engine.
+// Copyright (C) Bryan Edds.
+
+namespace ImGuiNET
+open System
+open System.Numerics
+open System.Runtime.InteropServices
+open Microsoft.FSharp.NativeInterop
+
+[<Struct; StructLayout (LayoutKind.Sequential)>]
+type ImVec2 =
+    val mutable X : single
+    val mutable Y : single
+
+[<Struct; StructLayout (LayoutKind.Sequential)>]
+type ImRect =
+    val mutable Min : ImVec2
+    val mutable Max : ImVec2
+
+[<RequireQualifiedAccess>]
+module ImGuiInternal =
+
+    [<DllImport("cimgui", CallingConvention = CallingConvention.Cdecl, EntryPoint = "igDockBuilderGetCentralNode")>]
+    extern nativeint DockBuilder_GetCentralNode (uint32)
+
+    [<DllImport("cimgui", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ImGuiDockNode_Rect")>]
+    extern void DockNode_GetRect (nativeint, nativeint)
+
+    /// Try to get the bounds of the central dock node in the given dock space.
+    let tryGetCentralDockNodeBounds dockSpaceId =
+        let centralNode = DockBuilder_GetCentralNode dockSpaceId
+        if centralNode <> nativeint 0 then
+            let io = ImGui.GetIO ()
+            let displaySize = io.DisplaySize
+            let mutable rect = Unchecked.defaultof<ImRect>
+            DockNode_GetRect (NativePtr.toNativeInt &&rect, centralNode)
+            let min = v2i (int rect.Min.X) (int rect.Min.Y)
+            let max = v2i (int rect.Max.X) (int rect.Max.Y)
+            let bounds = box2i min (max - min)
+            Some bounds
+        else None
