7594de96c3bc82cb5c34c9d4e7bad7486ca55c0b
bryanedds <bryanedds@gmail.com>
2025-10-18 19:29:33 -0400

    Fixed inputs being shown after end of battle.

--- Projects/Omni Blade/Battle/BattleDispatcher.fs
+++ Projects/Omni Blade/Battle/BattleDispatcher.fs
@@ -519,80 +519,81 @@ type BattleDispatcher () =
                                  Entity.BorderImage == Assets.Gui.TechBorderImage
                                  Entity.BorderColor == color8 (byte 60) (byte 60) (byte 60) (byte 191)]] // TODO: use a constant.
 
-             // inputs
-             for (index, ally) in (Battle.getCharactersHealthy battle).Pairs do
+             // inputs while battle running
+             if battle.BattleState.IsBattleRunning then
+                for (index, ally) in (Battle.getCharactersHealthy battle).Pairs do
 
-                // input
-                Content.composite (CharacterIndex.toEntityName index + "+Input") []
-                    [match ally.CharacterInputState with
-                     | RegularMenu ->
-                        Content.entity<RingMenuDispatcher> "RegularMenu"
-                            [Entity.Position := ally.Perimeter.CenterOffset
-                             Entity.Elevation == Constants.Battle.GuiInputElevation
-                             Entity.Enabled :=
-                                Battle.getAllies battle |>
-                                Map.toValueList |>
-                                Seq.notExists (fun (ally : Character) -> match ally.CharacterInputState with NoInput | RegularMenu -> false | _ -> true)
-                             Entity.RingMenu == { Items = Map.ofList [("Attack", (0, true)); ("Tech", (1, true)); ("Consumable", (2, true)); ("Defend", (3, true))]; Cancellable = false }
-                             Entity.ItemSelectEvent =|> fun evt -> RegularItemSelect (index, evt.Data) |> signal
-                             Entity.CancelEvent => RegularItemCancel index]
+                   // input
+                   Content.composite (CharacterIndex.toEntityName index + "+Input") []
+                       [match ally.CharacterInputState with
+                        | RegularMenu ->
+                           Content.entity<RingMenuDispatcher> "RegularMenu"
+                               [Entity.Position := ally.Perimeter.CenterOffset
+                                Entity.Elevation == Constants.Battle.GuiInputElevation
+                                Entity.Enabled :=
+                                   Battle.getAllies battle |>
+                                   Map.toValueList |>
+                                   Seq.notExists (fun (ally : Character) -> match ally.CharacterInputState with NoInput | RegularMenu -> false | _ -> true)
+                                Entity.RingMenu == { Items = Map.ofList [("Attack", (0, true)); ("Tech", (1, true)); ("Consumable", (2, true)); ("Defend", (3, true))]; Cancellable = false }
+                                Entity.ItemSelectEvent =|> fun evt -> RegularItemSelect (index, evt.Data) |> signal
+                                Entity.CancelEvent => RegularItemCancel index]
 
-                     | ItemMenu ->
-                        let consumables =
-                            battle.Inventory |>
-                            Inventory.getConsumables |>
-                            Map.ofSeqBy (fun kvp -> (getCaseName kvp.Key, (getCaseTag kvp.Key, true)))
-                        Content.entity<RingMenuDispatcher> "ConsumableMenu"
-                            [Entity.Position := ally.Perimeter.CenterOffset
-                             Entity.Elevation == Constants.Battle.GuiInputElevation
-                             Entity.RingMenu := { Items = consumables; Cancellable = true }
-                             Entity.ItemSelectEvent =|> fun evt -> ConsumableItemSelect (index, evt.Data) |> signal
-                             Entity.CancelEvent => ConsumableItemCancel index]
+                        | ItemMenu ->
+                           let consumables =
+                               battle.Inventory |>
+                               Inventory.getConsumables |>
+                               Map.ofSeqBy (fun kvp -> (getCaseName kvp.Key, (getCaseTag kvp.Key, true)))
+                           Content.entity<RingMenuDispatcher> "ConsumableMenu"
+                               [Entity.Position := ally.Perimeter.CenterOffset
+                                Entity.Elevation == Constants.Battle.GuiInputElevation
+                                Entity.RingMenu := { Items = consumables; Cancellable = true }
+                                Entity.ItemSelectEvent =|> fun evt -> ConsumableItemSelect (index, evt.Data) |> signal
+                                Entity.CancelEvent => ConsumableItemCancel index]
 
-                     | TechMenu ->
-                        let techs =
-                            Map.ofSeqBy (fun tech ->
-                                let affordable =
-                                    match Map.tryFind tech Data.Value.Techs with
-                                    | Some techData -> techData.TechCost <= ally.TechPoints && not (Map.containsKey Silence ally.Statuses)
-                                    | None -> false
-                                let castable =
-                                    if tech.ConjureTech then
-                                        match ally.ConjureChargeOpt with
-                                        | Some conjureCharge -> conjureCharge >= Constants.Battle.ChargeMax
-                                        | None -> true
-                                    else true
-                                let usable = affordable && castable
-                                (getCaseName tech, (getCaseTag tech, usable)))
-                                ally.Techs
-                        Content.entity<RingMenuDispatcher> "TechMenu"
-                            [Entity.Position := ally.Perimeter.CenterOffset
-                             Entity.Elevation == Constants.Battle.GuiInputElevation
-                             Entity.RingMenu := { Items = techs; Cancellable = true }
-                             Entity.ItemSelectEvent =|> fun evt -> TechItemSelect (index, evt.Data) |> signal
-                             Entity.CancelEvent => TechItemCancel index]
+                        | TechMenu ->
+                           let techs =
+                               Map.ofSeqBy (fun tech ->
+                                   let affordable =
+                                       match Map.tryFind tech Data.Value.Techs with
+                                       | Some techData -> techData.TechCost <= ally.TechPoints && not (Map.containsKey Silence ally.Statuses)
+                                       | None -> false
+                                   let castable =
+                                       if tech.ConjureTech then
+                                           match ally.ConjureChargeOpt with
+                                           | Some conjureCharge -> conjureCharge >= Constants.Battle.ChargeMax
+                                           | None -> true
+                                       else true
+                                   let usable = affordable && castable
+                                   (getCaseName tech, (getCaseTag tech, usable)))
+                                   ally.Techs
+                           Content.entity<RingMenuDispatcher> "TechMenu"
+                               [Entity.Position := ally.Perimeter.CenterOffset
+                                Entity.Elevation == Constants.Battle.GuiInputElevation
+                                Entity.RingMenu := { Items = techs; Cancellable = true }
+                                Entity.ItemSelectEvent =|> fun evt -> TechItemSelect (index, evt.Data) |> signal
+                                Entity.CancelEvent => TechItemCancel index]
 
-                     | AimReticles (actionStr, aimType) ->
-                        let infoText = actionStr.Spaced
-                        Content.text "Info"
-                            [Entity.PositionLocal := ally.Perimeter.Center + v3 -270.0f 15.0f 0.0f
-                             Entity.Size == v3 540.0f 81.0f 0.0f
-                             Entity.Elevation == Constants.Battle.GuiBackgroundElevation
-                             Entity.VisibleLocal := actionStr <> nameof Attack
-                             Entity.BackdropImageOpt := Some (if infoText.Length <= 14 then Assets.Battle.InfoShortImage else Assets.Battle.InfoLongImage) // never more than 16 characters
-                             Entity.Color == Color.White.WithA 0.8f
-                             Entity.Text := infoText
-                             Entity.TextColor == Color.White.WithA 0.7f
-                             Entity.TextOffset == v2 2.0f -1.5f]
-                        Content.entity<ReticlesDispatcher> "Reticles"
-                            [Entity.Elevation == Constants.Battle.GuiInputElevation
-                             Entity.Reticles :=
-                                Battle.getTargets aimType battle |>
-                                Map.map (fun _ (target : Character) ->
-                                    match target.Stature with
-                                    | BossStature -> target.Perimeter.CenterOffset2
-                                    | _ -> target.Perimeter.CenterOffset)
-                             Entity.TargetSelectEvent =|> fun evt -> ReticlesSelect (index, evt.Data) |> signal
-                             Entity.CancelEvent => ReticlesCancel index]
+                        | AimReticles (actionStr, aimType) ->
+                           let infoText = actionStr.Spaced
+                           Content.text "Info"
+                               [Entity.PositionLocal := ally.Perimeter.Center + v3 -270.0f 15.0f 0.0f
+                                Entity.Size == v3 540.0f 81.0f 0.0f
+                                Entity.Elevation == Constants.Battle.GuiBackgroundElevation
+                                Entity.VisibleLocal := actionStr <> nameof Attack
+                                Entity.BackdropImageOpt := Some (if infoText.Length <= 14 then Assets.Battle.InfoShortImage else Assets.Battle.InfoLongImage) // never more than 16 characters
+                                Entity.Color == Color.White.WithA 0.8f
+                                Entity.Text := infoText
+                                Entity.TextColor == Color.White.WithA 0.7f
+                                Entity.TextOffset == v2 2.0f -1.5f]
+                           Content.entity<ReticlesDispatcher> "Reticles"
+                               [Entity.Elevation == Constants.Battle.GuiInputElevation
+                                Entity.Reticles :=
+                                   Battle.getTargets aimType battle |>
+                                   Map.map (fun _ (target : Character) ->
+                                       match target.Stature with
+                                       | BossStature -> target.Perimeter.CenterOffset2
+                                       | _ -> target.Perimeter.CenterOffset)
+                                Entity.TargetSelectEvent =|> fun evt -> ReticlesSelect (index, evt.Data) |> signal
+                                Entity.CancelEvent => ReticlesCancel index]
 
-                     | NoInput -> ()]]]
+                        | NoInput -> ()]]]
