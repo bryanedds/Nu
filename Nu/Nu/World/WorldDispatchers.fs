afed001133254747d4eb54d6c19b8b2f612e2b07
bryanedds <bryanedds@gmail.com>
2025-10-31 15:24:04 -0400

    Implemented frame stepping in Gaia.

--- Nu/Nu/World/WorldDispatchers.fs
+++ Nu/Nu/World/WorldDispatchers.fs
@@ -326,7 +326,7 @@ type Character2dDispatcher () =
          define Entity.BodyType Dynamic
          define Entity.AngularFactor v3Zero
          define Entity.SleepingAllowed true
-         define Entity.GravityOverride (Some (Constants.Physics.GravityDefault * Constants.Engine.Meter2d * 3.0f))
+         define Entity.Gravity (GravityScale 3.0f)
          define Entity.BodyShape (CapsuleShape { Height = 0.5f; Radius = 0.25f; TransformOpt = None; PropertiesOpt = None })
          define Entity.Character2dIdleImage Assets.Default.Character2dIdle
          define Entity.Character2dJumpImage Assets.Default.Character2dJump
