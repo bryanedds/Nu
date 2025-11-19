eda38fb1929cc696cdafb82266dc865c95bb7aa4
Hadrian Tang <hadrianwttang@outlook.com>
2025-11-20 05:47:02 +0800

    Formatting fix

--- Nu/Nu.Gaia/Gaia.fs
+++ Nu/Nu.Gaia/Gaia.fs
@@ -3714,7 +3714,7 @@ DockSpace           ID=0x7C6B3D9B Window=0xA87D555D Pos=0,0 Size=1280,720 Split=
                 use stream = file.OpenRead ()
                 use peReader = new PortableExecutable.PEReader (stream)
                 peReader.HasMetadata &&
-                let metadataReader = System.Reflection.Metadata.PEReaderExtensions.GetMetadataReader peReader
+                let metadataReader = Metadata.PEReaderExtensions.GetMetadataReader peReader
                 metadataReader.IsAssembly &&
                 metadataReader.TypeDefinitions |> Seq.exists (fun t ->
                     let typeDef = metadataReader.GetTypeDefinition t
