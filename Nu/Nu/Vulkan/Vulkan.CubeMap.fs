e1ddb0a90635cb0edb1efe79bc19d31e741ef6b0
Dean J Lee <deanjlee@protonmail.com>
2025-11-24 17:20:38 +1100

    Implemented vulkan cube map creation.

--- Nu/Nu/Vulkan/Vulkan.CubeMap.fs
+++ Nu/Nu/Vulkan/Vulkan.CubeMap.fs
@@ -17,8 +17,53 @@ module CubeMap =
     /// Attempt to create a cube map from 6 files.
     /// Uses file name-based inferences to look for texture files in case the ones that were hard-coded in the included
     /// files can't be located.
-    let TryCreateCubeMap (faceRightFilePath, faceLeftFilePath, faceTopFilePath, faceBottomFilePath, faceBackFilePath, faceFrontFilePath, vkc) =
+    let TryCreateCubeMap (faceRightFilePath, faceLeftFilePath, faceTopFilePath, faceBottomFilePath, faceBackFilePath, faceFrontFilePath, thread, vkc) =
 
-        //Texture.VulkanTexture.create Texture.Bgra Vulkan.VK_FILTER_LINEAR Vulkan.VK_FILTER_LINEAR false Texture.MipmapNone true Texture.Uncompressed metadata vkc
+        // load faces into cube map
+        // NOTE: DJL: opengl seems to allow individual faces to differ in compression or maybe even size, but vulkan does not, so these are now determined by the first face.
+        // TODO: DJL: maybe check that size and compression match?
+        let mutable vulkanTextureOpt = None
+        let mutable errorOpt = None
+        let faceFilePaths = [|faceLeftFilePath; faceRightFilePath; faceBottomFilePath; faceTopFilePath; faceBackFilePath; faceFrontFilePath|]
+        for i in 0 .. dec faceFilePaths.Length do
+            if Option.isNone errorOpt then
+                let faceFilePath = faceFilePaths.[i]
+                let faceFilePath = if not (File.Exists faceFilePath) then PathF.ChangeExtension (faceFilePath, ".png") else faceFilePath // in case of PsdToPng
+                let faceFilePath = if not (File.Exists faceFilePath) then PathF.ChangeExtension (faceFilePath, ".dds") else faceFilePath // in case of ConvertToDds
+                match Texture.TryCreateTextureData (false, faceFilePath) with
+                | Some textureData ->
+                    match textureData with
+                    | Texture.TextureData.TextureDataDotNet (metadata, bytes) ->
+                        let vulkanTexture =
+                            match vulkanTextureOpt with
+                            | Some vulkanTexture -> vulkanTexture
+                            | None -> Texture.VulkanTexture.create Texture.Bgra Vulkan.VK_FILTER_LINEAR Vulkan.VK_FILTER_LINEAR false Texture.MipmapNone true Texture.Uncompressed metadata vkc
+                        Texture.VulkanTexture.uploadArray metadata 0 i bytes thread vulkanTexture vkc
+                    | Texture.TextureData.TextureDataMipmap (metadata, compressed, bytes, _) ->
+                        let vulkanTexture =
+                            match vulkanTextureOpt with
+                            | Some vulkanTexture -> vulkanTexture
+                            | None ->
+                                let compression = if compressed then Texture.ColorCompression else Texture.Uncompressed
+                                Texture.VulkanTexture.create Texture.Bgra Vulkan.VK_FILTER_LINEAR Vulkan.VK_FILTER_LINEAR false Texture.MipmapNone true compression metadata vkc
+                        Texture.VulkanTexture.uploadArray metadata 0 i bytes thread vulkanTexture vkc
+                    | Texture.TextureData.TextureDataNative (metadata, bytesPtr, disposer) ->
+                        use _ = disposer
+                        let vulkanTexture =
+                            match vulkanTextureOpt with
+                            | Some vulkanTexture -> vulkanTexture
+                            | None -> Texture.VulkanTexture.create Texture.Bgra Vulkan.VK_FILTER_LINEAR Vulkan.VK_FILTER_LINEAR false Texture.MipmapNone true Texture.Uncompressed metadata vkc
+                        Texture.VulkanTexture.upload metadata 0 i bytesPtr thread vulkanTexture vkc
+                | None -> errorOpt <- Some ("Could not create surface for image from '" + faceFilePath + "'")
 
-        ()
+        // attempt to finalize cube map
+        match errorOpt with
+        | None ->
+            // TODO: DJL: review error handling.
+            let cubeMap = Texture.EagerTexture { TextureMetadata = Texture.TextureMetadata.empty; VulkanTexture = vulkanTextureOpt.Value }
+            Right cubeMap
+        | Some error ->
+            match vulkanTextureOpt with
+            | Some vulkanTexture -> Texture.VulkanTexture.destroy vulkanTexture vkc
+            | None -> ()
+            Left error
