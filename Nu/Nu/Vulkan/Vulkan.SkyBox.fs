90e4b8b3bbd409d981bb7c2a96fe03f991f9556c
Dean J Lee <deanjlee@protonmail.com>
2025-12-01 11:44:10 +1100

    Began work on sky box pipeline.

---
+++ Nu/Nu/Vulkan/Vulkan.SkyBox.fs
@@ -0,0 +1,29 @@
+﻿// Nu Game Engine.
+// Copyright (C) Bryan Edds.
+
+namespace Vortice.Vulkan
+open System
+open System.Numerics
+open Prime
+open Nu
+
+[<RequireQualifiedAccess>]
+module SkyBox =
+
+    /// Describes a sky box pipeline that's loaded into GPU.
+    type SkyBoxPipeline =
+        { ViewUniform : Buffer.Buffer
+          ProjectionUniform : Buffer.Buffer
+          ViewProjectionUniform : Buffer.Buffer
+          ColorUniform : Buffer.Buffer
+          BrightnessUniform : Buffer.Buffer
+          SkyBoxPipeline : Pipeline.Pipeline }
+
+    /// Destroy SkyBoxPipeline.
+    let DestroySkyBoxPipeline skyBoxPipeline vkc =
+        Buffer.Buffer.destroy skyBoxPipeline.ViewUniform vkc
+        Buffer.Buffer.destroy skyBoxPipeline.ProjectionUniform vkc
+        Buffer.Buffer.destroy skyBoxPipeline.ViewProjectionUniform vkc
+        Buffer.Buffer.destroy skyBoxPipeline.ColorUniform vkc
+        Buffer.Buffer.destroy skyBoxPipeline.BrightnessUniform vkc
+        Pipeline.Pipeline.destroy skyBoxPipeline.SkyBoxPipeline vkc
