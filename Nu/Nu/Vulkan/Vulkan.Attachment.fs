50cfea4501f698595b35f87412d9cb81c023c759
Dean J Lee <deanjlee@protonmail.com>
2025-12-09 07:55:18 +1100

    Created Vulkan.Attachment.fs.

---
+++ Nu/Nu/Vulkan/Vulkan.Attachment.fs
@@ -0,0 +1,19 @@
+﻿// Nu Game Engine.
+// Copyright (C) Bryan Edds.
+
+namespace Vortice.Vulkan
+open System
+open System.Numerics
+open Prime
+open Nu
+
+[<RequireQualifiedAccess>]
+module Attachment =
+    
+    /// Create general-purpose color attachment with optional linear filters.
+    let CreateColorAttachment (resolutionX, resolutionY, filtered) =
+        
+        // TODO: DJL: create attachment texture with format rgba16f (as well as fallback format mechanism)
+        // once VulkanTexture has been appropriately modified for frames in flight aware writing.
+
+        ()
