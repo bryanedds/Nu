b4055a264b470277a37a71292b32e52bd0d59bdf
bryanedds <bryanedds@gmail.com>
2025-12-21 23:18:52 -0500

    Improved DOF blur.

--- Projects/Nelmish/Assets/Default/FilterDepthOfField.glsl
+++ Projects/Nelmish/Assets/Default/FilterDepthOfField.glsl
@@ -19,6 +19,8 @@ uniform mat4 viewInverse;
 uniform mat4 projectionInverse;
 uniform float nearDistance;
 uniform float farDistance;
+uniform int focalType;
+uniform float focalDistance;
 uniform vec2 focalPoint;
 uniform sampler2D depthTexture;
 uniform sampler2D blurredTexture;
@@ -27,6 +29,15 @@ uniform sampler2D unblurredTexture;
 in vec2 texCoordsOut;
 
 layout(location = 0) out vec4 frag;
+
+float depthToDistance(float depth)
+{
+    float ndc = depth * 2.0 - 1.0;
+    vec4 clip = vec4(0.0, 0.0, ndc, 1.0);
+    vec4 view = projectionInverse * clip;
+    view /= view.w;
+    return -view.z;
+}
 
 vec4 depthToPosition(float depth, vec2 texCoords)
 {
@@ -40,19 +51,37 @@ vec4 depthToPosition(float depth, vec2 texCoords)
 void main()
 {
     vec4 unblurredColor = texture(unblurredTexture, texCoordsOut);
-    float depth = texture(depthTexture, texCoordsOut).r;
+    vec2 texelSize = vec2(1.0) / textureSize(depthTexture, 0);
+    float depth = // TODO: P1: attempt to increase efficiency here by downsampling the depth texture instead of multi-tapping.
+        (texture(depthTexture, texCoordsOut + texelSize).r +
+         texture(depthTexture, texCoordsOut + texelSize * -1.0).r +
+         texture(depthTexture, texCoordsOut + texelSize * vec2(1.0, -1.0)).r +
+         texture(depthTexture, texCoordsOut + texelSize * vec2(-1.0, -1.0)).r) *
+        0.25;
     if (depth != 0.0)
     {
         vec4 blurredColor = texture(blurredTexture, texCoordsOut);
-        float focalDepth = texture(depthTexture, focalPoint + vec2(0.5)).r;
-        if (focalDepth != 0.0)
+        if (focalType == 0)
         {
-            vec2 focalTexCoords = focalPoint + vec2(0.5);
-            float distance = length(depthToPosition(depth, texCoordsOut).xyz - depthToPosition(focalDepth, focalTexCoords).xyz);
-            float blur = smoothstep(nearDistance, farDistance, distance);
+            float distance = depthToDistance(depth);
+            float blur =
+                distance - focalDistance >= 0.0 ?
+                smoothstep(focalDistance, farDistance, distance) :
+                1.0 - smoothstep(nearDistance, focalDistance, distance);
             frag = mix(unblurredColor, blurredColor, blur);
         }
-        else frag = blurredColor;
+        else
+        {
+            float focalDistance = texture(depthTexture, focalPoint + vec2(0.5)).r;
+            if (focalDistance != 0.0)
+            {
+                vec2 focalTexCoords = focalPoint + vec2(0.5);
+                float distance = length(depthToPosition(depth, texCoordsOut).xyz - depthToPosition(focalDistance, focalTexCoords).xyz);
+                float blur = smoothstep(nearDistance, farDistance, distance);
+                frag = mix(unblurredColor, blurredColor, blur);
+            }
+            else frag = blurredColor;
+        }
     }
     else frag = unblurredColor;
 }
