9eacde351ae8cfe233925e06e28f121ae080bb32
bryanedds <bryanedds@gmail.com>
2026-01-04 15:48:50 -0500

    Magick.NET-Q8-AnyCPU 14.10.1 update. TF player data fix.

--- Nu/Nu.Gaia/Gaia.fs
+++ Nu/Nu.Gaia/Gaia.fs
@@ -1237,7 +1237,7 @@ DockSpace           ID=0x7C6B3D9B Window=0xA87D555D Pos=0,0 Size=1280,720 Split=
                           """<PackageReference Include="BCnEncoder.Net" Version="2.2.1" />"""
                           """<PackageReference Include="DotRecast.Recast.Toolset" Version="2025.2.1" />"""
                           """<PackageReference Include="JoltPhysicsSharp" Version="2.18.4" />"""
-                          """<PackageReference Include="Magick.NET-Q8-AnyCPU" Version="14.8.1" />"""
+                          """<PackageReference Include="Magick.NET-Q8-AnyCPU" Version="14.10.1" />"""
                           """<PackageReference Include="Pfim" Version="0.11.3" />"""
                           """<PackageReference Include="Prime" Version="11.1.5" />"""
                           """<PackageReference Include="System.Configuration.ConfigurationManager" Version="9.0.5" />"""
