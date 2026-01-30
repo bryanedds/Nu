85a08be04d411331ae32d74fcf9d733a82fc894f
bryanedds <bryanedds@gmail.com>
2026-01-29 20:20:50 -0500

    Updated Prime.

--- Nu/Nu.Gaia/Gaia.fs
+++ Nu/Nu.Gaia/Gaia.fs
@@ -1233,7 +1233,7 @@ DockSpace           ID=0x7C6B3D9B Window=0xA87D555D Pos=0,0 Size=1280,720 Split=
                           """<PackageReference Include="JoltPhysicsSharp" Version="2.19.5" />"""
                           """<PackageReference Include="Magick.NET-Q8-AnyCPU" Version="14.10.2" />"""
                           """<PackageReference Include="Pfim" Version="0.11.4" />"""
-                          """<PackageReference Include="Prime" Version="11.2.0" />"""
+                          """<PackageReference Include="Prime" Version="11.4.0" />"""
                           """<PackageReference Include="System.Configuration.ConfigurationManager" Version="10.0.1" />"""
                           """<PackageReference Include="System.Drawing.Common" Version="10.0.1" />"""
                           """<PackageReference Include="Twizzle.ImGui-Bundle.NET" Version="1.91.5.2" />"""|]
