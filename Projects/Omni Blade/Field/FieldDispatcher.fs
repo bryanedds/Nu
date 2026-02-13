e73876d1cff8804d0a672f53369afa2e6d38e8db
bryanedds <bryanedds@gmail.com>
2026-02-13 16:04:00 -0500

    Version tick.

--- Projects/Omni Blade/Field/FieldDispatcher.fs
+++ Projects/Omni Blade/Field/FieldDispatcher.fs
@@ -1472,7 +1472,7 @@ type FieldDispatcher () =
                         Content.text "About"
                             [Entity.PositionLocal == v3 262.0f 6.0f 0.0f; Entity.ElevationLocal == 1.0f; Entity.Size == v3 432.0f 48.0f 0.0f
                              Entity.Justification == Justified (JustifyCenter, JustifyMiddle)
-                             Entity.Text == "Omni Blade Demo v1.1.0"]
+                             Entity.Text == "Omni Blade Demo v1.1.1"]
                      else
                         Content.text "QuitConfirmation"
                             [Entity.PositionLocal == v3 336.0f 312.0f 0.0f; Entity.ElevationLocal == 1.0f; Entity.Size == v3 288.0f 48.0f 0.0f
