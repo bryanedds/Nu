be23f3a5163defdd88d9c3ecd15fdae0fbda5501
bryanedds <bryanedds@gmail.com>
2026-03-05 23:18:33 -0500

    Removed no longer necessary SDL install scripting.

--- Configure.Linux.Development.sh
+++ Configure.Linux.Development.sh
@@ -8,31 +8,6 @@ sudo apt-get update
 # configure x11 (this seems to be needed when running just Wayland due to issue #810)
 sudo apt-get install libx11-dev
 
-# configure SDL3 and extension libraries
-sudo apt install -y \
-  libsdl3-dev \
-  libsdl3-image-dev \
-  libsdl3-mixer-dev \
-  libsdl3-ttf-dev
-
-# configure SDL3 image format dependencies
-sudo apt install -y \
-  libjpeg-dev \
-  libwebp-dev \
-  libtiff5-dev
-
-# configure SDL3 audio format dependencies
-sudo apt install -y \
-  libogg-dev \
-  libvorbis-dev \
-  libflac-dev \
-  libopus-dev \
-  libmpg123-dev \
-  libfluidsynth-dev
-
-# configure SDL3 font dependencies
-sudo apt install -y libfreetype6-dev
-
 # configure .net
 sudo apt-get install -y dotnet-sdk-10.0
 
