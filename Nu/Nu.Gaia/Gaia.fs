ad231f40bbd3b227044ac29d9cadd0dfea41462b
bryanedds <bryanedds@gmail.com>
2026-03-10 22:23:02 -0400

    Fixed explicit property appending for other simulants.

--- Nu/Nu.Gaia/Gaia.fs
+++ Nu/Nu.Gaia/Gaia.fs
@@ -2046,6 +2046,13 @@ DockSpace           ID=0x7C6B3D9B Window=0xA87D555D Pos=0,0 Size=1920,1080 Split
                 |> Array.iter (fun child -> imGuiEntityHierarchy child world)
                 if visible then ImGui.TreePop ()
 
+    let private imGuiAppendDispatcherPropertiesExplicit simulant world =
+        let ty = getType (World.getDispatcher simulant world)
+        if ImGui.CollapsingHeader (ty.Name.Spaced, ImGuiTreeNodeFlags.DefaultOpen ||| ImGuiTreeNodeFlags.OpenOnArrow) then
+            let unfocusProperty () = focusPropertyOpt None world
+            let appendProperties : AppendProperties = { EditContext = makeEditContext None (Some unfocusProperty) }
+            World.edit (fun o -> o.GetType () = ty) (AppendProperties appendProperties) simulant world
+
     let private imGuiEditPropertyRecord
         (getProperty : PropertyDescriptor -> Simulant -> World -> obj)
         (setProperty : bool -> obj -> PropertyDescriptor -> Simulant -> World -> unit)
@@ -2131,11 +2138,7 @@ DockSpace           ID=0x7C6B3D9B Window=0xA87D555D Pos=0,0 Size=1920,1080 Split
                 | _ -> failwithumf ()
             match propertyCategory with // preempt with dispatcher category if it is not represented by any of the properties
             | Right ty when not appendedToDispatcher && not (ty.IsAssignableTo typeof<Dispatcher>) ->
-                let ty = getType (World.getDispatcher simulant world)
-                if ImGui.CollapsingHeader (ty.Name.Spaced, ImGuiTreeNodeFlags.DefaultOpen ||| ImGuiTreeNodeFlags.OpenOnArrow) then
-                    let unfocusProperty () = focusPropertyOpt None world
-                    let appendProperties : AppendProperties = { EditContext = makeEditContext None (Some unfocusProperty) }
-                    World.edit (fun o -> o.GetType () = ty) (AppendProperties appendProperties) simulant world
+                imGuiAppendDispatcherPropertiesExplicit simulant world
                 appendedToDispatcher <- true
             | Right _ | Left _ -> ()
             if  (propertyCategoryName <> "Model" || modelUsed) &&
@@ -2272,6 +2275,7 @@ DockSpace           ID=0x7C6B3D9B Window=0xA87D555D Pos=0,0 Size=1920,1080 Split
                     imGuiEditEntityAppliedTypes entity world
                 | _ ->
                     Log.infoOnce "Unexpected simulant type."
+        if not appendedToDispatcher then imGuiAppendDispatcherPropertiesExplicit simulant world
         detectEyeChangedElsewhere world
 
     let private imGuiViewportManipulation (world : World) =
