b17cafc2f216bc1fdadf117e90b0ebb779a4163d
Dean J Lee <deanjlee@protonmail.com>
2026-04-05 21:47:07 +1000

    Actually forgot to update lightmap draw limit checks with meaningful values, just got rid of them as they're redundant.

--- Nu/Nu/Vulkan/Vulkan.LightMap.fs
+++ Nu/Nu/Vulkan/Vulkan.LightMap.fs
@@ -179,74 +179,67 @@ module LightMap =
          pipeline : EnvironmentFilterPipeline,
          vkc : Hl.VulkanContext) =
 
-        // ensure pipeline draw limit is not exceeded
-        if drawIndex < pipeline.Pipeline.DrawLimit then
-        
-            // bind uniforms
-            let mutable transform = Transform ()
-            let mutable environmentFilter = EnvironmentFilter ()
-            transform.view <- view
-            transform.projection <- projection
-            transform.viewProjection <- viewProjection
-            environmentFilter.roughness <- roughness
-            environmentFilter.resolution <- resolution
-            Buffer.Buffer.uploadValue drawIndex 0 0 transform pipeline.TransformUniform vkc
-            Buffer.Buffer.uploadValue drawIndex 0 0 environmentFilter pipeline.EnvironmentFilterUniform vkc
-            Pipeline.Pipeline.writeDescriptorStorageBuffer drawIndex 0 0 0 pipeline.TransformUniform.[drawIndex] pipeline.Pipeline vkc
-            Pipeline.Pipeline.writeDescriptorStorageBuffer drawIndex 0 0 1 pipeline.EnvironmentFilterUniform.[drawIndex] pipeline.Pipeline vkc
+        // bind uniforms
+        let mutable transform = Transform ()
+        let mutable environmentFilter = EnvironmentFilter ()
+        transform.view <- view
+        transform.projection <- projection
+        transform.viewProjection <- viewProjection
+        environmentFilter.roughness <- roughness
+        environmentFilter.resolution <- resolution
+        Buffer.Buffer.uploadValue drawIndex 0 0 transform pipeline.TransformUniform vkc
+        Buffer.Buffer.uploadValue drawIndex 0 0 environmentFilter pipeline.EnvironmentFilterUniform vkc
+        Pipeline.Pipeline.writeDescriptorStorageBuffer drawIndex 0 0 0 pipeline.TransformUniform.[drawIndex] pipeline.Pipeline vkc
+        Pipeline.Pipeline.writeDescriptorStorageBuffer drawIndex 0 0 1 pipeline.EnvironmentFilterUniform.[drawIndex] pipeline.Pipeline vkc
 
-            // bind texture
-            Pipeline.Pipeline.writeDescriptorSampledImage drawIndex 0 0 2 cubeMap.ImageView pipeline.Pipeline vkc
-            Pipeline.Pipeline.writeDescriptorSampler 0 0 1 0 sampler pipeline.Pipeline vkc
+        // bind texture
+        Pipeline.Pipeline.writeDescriptorSampledImage drawIndex 0 0 2 cubeMap.ImageView pipeline.Pipeline vkc
+        Pipeline.Pipeline.writeDescriptorSampler 0 0 1 0 sampler pipeline.Pipeline vkc
 
-            // make viewport and scissor
-            let mutable renderArea = VkRect2D (0, 0, uint resolution, uint resolution)
-            let mutable vkViewport = Hl.makeViewport invertY renderArea
-            let mutable scissor = renderArea
+        // make viewport and scissor
+        let mutable renderArea = VkRect2D (0, 0, uint resolution, uint resolution)
+        let mutable vkViewport = Hl.makeViewport invertY renderArea
+        let mutable scissor = renderArea
 
-            // only draw if scissor (and therefore also viewport) is valid
-            if Hl.validateRect scissor then
+        // only draw if scissor (and therefore also viewport) is valid
+        if Hl.validateRect scissor then
 
-                // only draw if required vkPipeline exists
-                match Pipeline.Pipeline.tryGetVkPipeline Pipeline.NoBlend false pipeline.Pipeline with
-                | Some vkPipeline ->
-                
-                    // init render
-                    let mutable rendering = Hl.makeRenderingInfo [|colorAttachment|] None renderArea None
-                    Vulkan.vkCmdBeginRendering (cb, asPointer &rendering)
-
-                    // bind pipeline
-                    Vulkan.vkCmdBindPipeline (cb, VkPipelineBindPoint.Graphics, vkPipeline)
-
-                    // set viewport and scissor
-                    Vulkan.vkCmdSetViewport (cb, 0u, 1u, asPointer &vkViewport)
-                    Vulkan.vkCmdSetScissor (cb, 0u, 1u, asPointer &scissor)
-                    
-                    // bind vertex and index buffer
-                    let mutable vertexBuffer = geometry.VertexBuffer.VkBuffer
-                    let mutable vertexOffset = 0UL
-                    Vulkan.vkCmdBindVertexBuffers (cb, 0u, 1u, asPointer &vertexBuffer, asPointer &vertexOffset)
-                    Vulkan.vkCmdBindIndexBuffer (cb, geometry.IndexBuffer.VkBuffer, 0UL, VkIndexType.Uint32)
-
-                    // bind descriptor sets
-                    let mutable mainDescriptorSet = pipeline.Pipeline.VkDescriptorSet 0 drawIndex
-                    let mutable samplerDescriptorSet = pipeline.Pipeline.VkDescriptorSet 1 0
-                    Vulkan.vkCmdBindDescriptorSets (cb, VkPipelineBindPoint.Graphics, pipeline.Pipeline.PipelineLayout, 0u, 1u, asPointer &mainDescriptorSet, 0u, nullPtr)
-                    Vulkan.vkCmdBindDescriptorSets (cb, VkPipelineBindPoint.Graphics, pipeline.Pipeline.PipelineLayout, 1u, 1u, asPointer &samplerDescriptorSet, 0u, nullPtr)
-                    
-                    // draw
-                    Vulkan.vkCmdDrawIndexed (cb, uint geometry.ElementCount, 1u, 0u, 0, 0u)
-                    Hl.reportDrawCall 1
+            // only draw if required vkPipeline exists
+            match Pipeline.Pipeline.tryGetVkPipeline Pipeline.NoBlend false pipeline.Pipeline with
+            | Some vkPipeline ->
             
-                    // end render
-                    Vulkan.vkCmdEndRendering cb
+                // init render
+                let mutable rendering = Hl.makeRenderingInfo [|colorAttachment|] None renderArea None
+                Vulkan.vkCmdBeginRendering (cb, asPointer &rendering)
 
-                // abort
-                | None -> Log.warnOnce "Cannot draw because VkPipeline does not exist."
+                // bind pipeline
+                Vulkan.vkCmdBindPipeline (cb, VkPipelineBindPoint.Graphics, vkPipeline)
 
-        // draw limit exceeded
-        else Log.warnOnce "Attempted environment filter map draw exceeded maximum number of draws, which indicates an error in environment filter map management."
+                // set viewport and scissor
+                Vulkan.vkCmdSetViewport (cb, 0u, 1u, asPointer &vkViewport)
+                Vulkan.vkCmdSetScissor (cb, 0u, 1u, asPointer &scissor)
+                
+                // bind vertex and index buffer
+                let mutable vertexBuffer = geometry.VertexBuffer.VkBuffer
+                let mutable vertexOffset = 0UL
+                Vulkan.vkCmdBindVertexBuffers (cb, 0u, 1u, asPointer &vertexBuffer, asPointer &vertexOffset)
+                Vulkan.vkCmdBindIndexBuffer (cb, geometry.IndexBuffer.VkBuffer, 0UL, VkIndexType.Uint32)
 
+                // bind descriptor sets
+                let mutable mainDescriptorSet = pipeline.Pipeline.VkDescriptorSet 0 drawIndex
+                let mutable samplerDescriptorSet = pipeline.Pipeline.VkDescriptorSet 1 0
+                Vulkan.vkCmdBindDescriptorSets (cb, VkPipelineBindPoint.Graphics, pipeline.Pipeline.PipelineLayout, 0u, 1u, asPointer &mainDescriptorSet, 0u, nullPtr)
+                Vulkan.vkCmdBindDescriptorSets (cb, VkPipelineBindPoint.Graphics, pipeline.Pipeline.PipelineLayout, 1u, 1u, asPointer &samplerDescriptorSet, 0u, nullPtr)
+                
+                // draw
+                Vulkan.vkCmdDrawIndexed (cb, uint geometry.ElementCount, 1u, 0u, 0, 0u)
+                Hl.reportDrawCall 1
+        
+                // end render
+                Vulkan.vkCmdEndRendering cb
+
+            // abort
+            | None -> Log.warnOnce "Cannot draw because VkPipeline does not exist."
     
     /// Create an environment filter map.
     let CreateEnvironmentFilterMap (mapId, cb, invertY, resolution, environmentFilterSurface : CubeMap.CubeMapSurface, sampler, colorFormat, environmentFilterPipeline, vkc) =
