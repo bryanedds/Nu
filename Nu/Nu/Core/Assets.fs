0618fd84a9d66a07ab7f5c7f8573be9f019411d9
bryanedds <bryanedds@gmail.com>
2026-04-05 15:41:25 -0400

    Removed unnecessary Address module. Address generators like wildcard and parent now lower-case to be consistent with empty generator.

--- Nu/Nu/Core/Assets.fs
+++ Nu/Nu/Core/Assets.fs
@@ -29,12 +29,14 @@ open Prime
 /// The global assets that come with the Nu Game Engine.
 [<RequireQualifiedAccess>]
 module Global =
+
     let [<Literal>] AssetGraphFilePath = "AssetGraph.nuag"
     let [<Literal>] OverlayerFilePath = "Overlayer.nuol"
 
 /// The default assets that come with the Nu Game Engine.
 [<RequireQualifiedAccess>]
 module Default =
+
     let [<Literal>] PackageName = "Default"
     let [<Literal>] ImageName = "Image"
     let [<Literal>] EmptyImageName = "EmptyImage"
