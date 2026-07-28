233d6febe3fa34957da396c4433bdfa8c52cfc28
bryanedds <bryanedds@gmail.com>
2026-07-27 23:53:14 -0400

    Another experiment.

--- Nu/Nu/Render/RendererImGui.fs
+++ Nu/Nu/Render/RendererImGui.fs
@@ -181,6 +181,7 @@ type VulkanRendererImGui
             // update imgui's display properties
             let io = ImGui.GetIO ()
             io.DisplaySize <- viewport.Bounds.Size.V2
+            io.DisplayFramebufferScale <- v2One
 
             // render when allowed and drawData matches viewport, as it may lag behind due to -
             // https://github.com/bryanedds/Nu/issues/1248
