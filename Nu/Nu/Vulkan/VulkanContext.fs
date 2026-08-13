26d2ad283258ad10a526a35d4faf20f154af1208
bryanedds <bryanedds@gmail.com>
2026-08-13 12:49:20 -0400

    More comprehensive treatment of swapchain creation error.

--- Nu/Nu/Vulkan/VulkanContext.fs
+++ Nu/Nu/Vulkan/VulkanContext.fs
@@ -248,8 +248,7 @@ type SwapchainWrapper =
                 match DeviceApi.vkCreateSwapchainKHR (&info, nullPtr, &vkSwapchain) with
                 | VkResult.Success ->
                     Some (vkSwapchain, swapExtent)
-                | VkResult.ErrorSurfaceLostKHR 
-                | VkResult.ErrorNativeWindowInUseKHR ->
+                | result when int result < 0 ->
                     Hl.SurfaceState <- SurfaceLost
                     None
                 | result ->
