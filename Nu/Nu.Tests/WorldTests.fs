8281ccddd812b3e7ac9a4aead247ce4917313ec2
Happypig375 <hadrianwttang@outlook.com>
2026-09-01 04:51:43 +0800

    fix: stabilize Sand Box 2d physics

--- Nu/Nu.Tests/WorldTests.fs
+++ Nu/Nu.Tests/WorldTests.fs
@@ -27,7 +27,7 @@ module WorldTests =
             use _ = sdlDeps // bind explicitly to dispose automatically
             let windowViewport = Viewport.makeWindow1 windowSize
             let geometryViewport = Viewport.makeGeometry windowViewport.Bounds.Size
-            let world = World.make (constant None) sdlDeps worldConfig windowSize geometryViewport windowViewport (TestPlugin ())
+            let world = World.make (constant None) (SdlDependencies sdlDeps) worldConfig windowSize geometryViewport windowViewport (TestPlugin ())
             let result = World.runWithCleanUp (fun world -> world.UpdateTime < 1L) ignore ignore ignore ignore ignore (Some ignore) world
             Assert.Equal (Constants.Engine.ExitCodeSuccess, result)
         | Left _ -> Assert.Fail ()
